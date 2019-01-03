e78ea052502f2a3beb718fef05de28dfb7c68b8d
deadeyegoodwin <davidg@nvidia.com>
2019-01-02 16:30:50 -0800

    Fix copyright checker to handle multi-year (#31)

--- qa/common/check_copyright.py
+++ qa/common/check_copyright.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2018-2019, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -34,8 +34,12 @@ FLAGS = None
 SKIP_EXTS = ('jpeg', 'jpg', 'pgm', 'png',
              'log', 'serverlog',
              'preprocessed', 'jmx', 'gz',
-             'caffemodel', 'prototxt')
+             'caffemodel')
 SKIP_PATHS = ('docs/examples/model_repository',
+              'qa/custom_models/custom_float32_float32_float32/output0_labels.txt',
+              'qa/custom_models/custom_nobatch_float32_float32_float32/output0_labels.txt',
+              'qa/custom_models/custom_int32_int32_int32/output0_labels.txt',
+              'qa/custom_models/custom_nobatch_int32_int32_int32/output0_labels.txt',
               'serving',
               'src/servables/caffe2/testdata',
               'src/servables/tensorflow/testdata',
@@ -44,8 +48,10 @@ SKIP_PATHS = ('docs/examples/model_repository',
               'tools/patch',
               'VERSION')
 
+COPYRIGHT_YEAR_RE0 = 'Copyright \\(c\\) (20[0-9][0-9]), NVIDIA CORPORATION. All rights reserved.'
+COPYRIGHT_YEAR_RE1 = 'Copyright \\(c\\) (20[0-9][0-9])-(20[0-9][0-9]), NVIDIA CORPORATION. All rights reserved.'
+
 COPYRIGHT ='''
-Copyright (c) YYYY, NVIDIA CORPORATION. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -72,7 +78,8 @@ OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
-copyright_list = [l.rstrip() for i, l in enumerate(COPYRIGHT.splitlines()) if i > 0]
+single_re = re.compile(COPYRIGHT_YEAR_RE0)
+range_re = re.compile(COPYRIGHT_YEAR_RE1)
 
 def visit(path):
     if FLAGS.verbose:
@@ -123,8 +130,8 @@ def visit(path):
 
         line = line.strip()
 
-        # The next line must be the copyright line with the expected
-        # year. It must start with either '#' or '//'
+        # The next line must be the copyright line with a single year
+        # or a year range. It must start with either '#' or '//'
         prefix = None
         if line.startswith('#'):
             prefix = '#'
@@ -135,24 +142,43 @@ def visit(path):
                   path + ": " + line)
             return False
 
-        expected_copyright = (prefix + " " + copyright_list[0])
-        if line != expected_copyright:
-            print("incorrect copyright for " + path)
-            print("  expected: " + expected_copyright)
-            print("       got: " + line)
+        start_year = 0
+        end_year = 0
+
+        m = single_re.match(line[(len(prefix) + 1):])
+        if m and len(m.groups()) == 1:
+            start_year = end_year = int(m.group(1))
+        else:
+            m = range_re.match(line[(len(prefix) + 1):])
+            if m and len(m.groups()) == 2:
+                start_year = int(m.group(1))
+                end_year = int(m.group(2))
+            else:
+                print("copyright year is not recognized for " + path + ": " + line)
+                return False
+
+        if start_year > FLAGS.year:
+            print("copyright start year greater than current year for " + path + ": " + line)
+            return False
+        if end_year > FLAGS.year:
+            print("copyright end year greater than current year for " + path + ": " + line)
+            return False
+        if end_year < start_year:
+            print("copyright start year greater than end year for " + path + ": " + line)
             return False
 
         # Subsequent lines must match the copyright body.
-        copyright_idx = 1
+        copyright_body = [l.rstrip() for i, l in enumerate(COPYRIGHT.splitlines()) if i > 0]
+        copyright_idx = 0
         for line in f:
-            if copyright_idx >= len(copyright_list):
+            if copyright_idx >= len(copyright_body):
                 break
 
             line = line.strip()
-            if len(copyright_list[copyright_idx]) == 0:
+            if len(copyright_body[copyright_idx]) == 0:
                 expected = prefix
             else:
-                expected = (prefix + " " + copyright_list[copyright_idx])
+                expected = (prefix + " " + copyright_body[copyright_idx])
             if line != expected:
                 print("incorrect copyright body for " + path)
                 print("  expected: '" + expected + "'")
@@ -160,8 +186,8 @@ def visit(path):
                 return False
             copyright_idx += 1
 
-        if copyright_idx != len(copyright_list):
-            print("missing " + str(len(copyright_list) - copyright_idx) +
+        if copyright_idx != len(copyright_body):
+            print("missing " + str(len(copyright_body) - copyright_idx) +
                   " lines of the copyright body")
             return False
 
@@ -173,7 +199,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', action="store_true", required=False, default=False,
                         help='Enable verbose output')
-    parser.add_argument('-y', '--year', type=str, required=True,
+    parser.add_argument('-y', '--year', type=int, required=True,
                         help='Copyright year')
     parser.add_argument('paths', type=str, nargs='*', default=None,
                         help='Directories or files to check')
@@ -182,8 +208,6 @@ if __name__ == '__main__':
     if FLAGS.paths is None or len(FLAGS.paths) == 0:
         parser.print_help()
         exit(1)
-
-    copyright_list[0] = copyright_list[0].replace('YYYY', str(FLAGS.year))
 
     ret = True
     for path in FLAGS.paths:
