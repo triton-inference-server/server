dcc977e095b75220c6edacca29956010a97ec0e4
David Goodwin <davidg@nvidia.com>
2019-01-16 15:12:08 -0800

    CI test for variable-sized input tensors

---
+++ qa/L0_infer_variable/infer_variable_test.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+sys.path.append("../common")
+
+from builtins import range
+from future.utils import iteritems
+import unittest
+import numpy as np
+import infer_util as iu
+import test_util as tu
+from tensorrtserver.api import *
+
+np_dtype_string = np.dtype(object)
+
+class InferVariableTest(unittest.TestCase):
+    def _full_exact(self, req_raw, input_dtype, output0_dtype, output1_dtype,
+                    input_shape, output0_shape, output1_shape, swap=False):
+
+        if tu.validate_for_tf_model(input_dtype, output0_dtype, output1_dtype,
+                                    input_shape, output0_shape, output1_shape):
+            # model that supports batching
+            for bs in (1, 8):
+                iu.infer_exact(self, 'graphdef', input_shape, bs, req_raw,
+                               input_dtype, output0_dtype, output1_dtype, swap=swap)
+                iu.infer_exact(self, 'savedmodel', input_shape, bs, req_raw,
+                               input_dtype, output0_dtype, output1_dtype, swap=swap)
+            # model that does not support batching
+            iu.infer_exact(self, 'graphdef_nobatch', input_shape, 1, req_raw,
+                           input_dtype, output0_dtype, output1_dtype, swap=swap)
+            iu.infer_exact(self, 'savedmodel_nobatch', input_shape, 1, req_raw,
+                           input_dtype, output0_dtype, output1_dtype, swap=swap)
+
+        if tu.validate_for_c2_model(input_dtype, output0_dtype, output1_dtype,
+                                    input_shape, output0_shape, output1_shape):
+            # model that supports batching
+            for bs in (1, 8):
+                iu.infer_exact(self, 'netdef', input_shape, bs, req_raw,
+                               input_dtype, output0_dtype, output1_dtype, swap=swap)
+            # model that does not support batching
+            iu.infer_exact(self, 'netdef_nobatch', input_shape, 1, req_raw,
+                           input_dtype, output0_dtype, output1_dtype, swap=swap)
+
+        if tu.validate_for_trt_model(input_dtype, output0_dtype, output1_dtype,
+                                     input_shape, output0_shape, output1_shape):
+            # model that supports batching
+            for bs in (1, 8):
+                iu.infer_exact(self, 'plan', input_shape, bs, req_raw,
+                               input_dtype, output0_dtype, output1_dtype, swap=swap)
+            # model that does not support batching
+            iu.infer_exact(self, 'plan_nobatch', input_shape, 1, req_raw,
+                           input_dtype, output0_dtype, output1_dtype, swap=swap)
+
+        # the custom model is src/custom/addsub... it does not swap
+        # the inputs so always set to False
+        if tu.validate_for_custom_model(input_dtype, output0_dtype, output1_dtype,
+                                        input_shape, output0_shape, output1_shape):
+            # model that supports batching
+            for bs in (1, 8):
+                iu.infer_exact(self, 'custom', input_shape, bs, req_raw,
+                               input_dtype, output0_dtype, output1_dtype, swap=False)
+            # model that does not support batching
+            iu.infer_exact(self, 'custom_nobatch', input_shape, 1, req_raw,
+                           input_dtype, output0_dtype, output1_dtype, swap=False)
+
+    def test_raw_fff(self):
+        self._full_exact(True, np.float32, np.float32, np.float32, (16,), (16,), (16,))
+
+    def test_class_fff(self):
+        self._full_exact(False, np.float32, np.float32, np.float32, (16,), (16,), (16,))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ qa/L0_infer_variable/test.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+CLIENT_LOG_BASE="./client"
+INFER_TEST=infer_variable_test.py
+
+DATADIR=`pwd`/models
+
+SERVER=/opt/tensorrtserver/bin/trtserver
+SERVER_ARGS=--model-store=$DATADIR
+SERVER_LOG_BASE="./inference_server"
+source ../common/util.sh
+
+rm -f $SERVER_LOG_BASE* $CLIENT_LOG_BASE*
+
+RET=0
+
+for TARGET in cpu gpu; do
+    SERVER_LOG=$SERVER_LOG_BASE.${TARGET}.log
+    CLIENT_LOG=$CLIENT_LOG_BASE.${TARGET}.log
+
+    rm -fr models && \
+        cp -r /data/inferenceserver/qa_variable_model_repository models && \
+        cp -r ../custom_models/* models/.
+
+    KIND="KIND_GPU" && [[ "$TARGET" == "cpu" ]] && KIND="KIND_CPU"
+    for FW in graphdef savedmodel netdef custom; do
+        for MC in `ls models/${FW}*/config.pbtxt`; do
+            echo "instance_group [ { kind: ${KIND} }]" >> $MC
+        done
+    done
+
+    run_server
+    if [ "$SERVER_PID" == "0" ]; then
+        echo -e "\n***\n*** Failed to start $SERVER\n***"
+        cat $SERVER_LOG
+        exit 1
+    fi
+
+    set +e
+
+    # python unittest seems to swallow ImportError and still return 0
+    # exit code. So need to explicitly check CLIENT_LOG to make sure
+    # we see some running tests
+    python $INFER_TEST >$CLIENT_LOG 2>&1
+    if [ $? -ne 0 ]; then
+        cat $CLIENT_LOG
+        echo -e "\n***\n*** Test Failed\n***"
+        RET=1
+    fi
+
+    grep -c "HTTP/1.1 200 OK" $CLIENT_LOG
+    if [ $? -ne 0 ]; then
+        cat $CLIENT_LOG
+        echo -e "\n***\n*** Test Failed To Run\n***"
+        RET=1
+    fi
+
+    set -e
+
+    kill $SERVER_PID
+    wait $SERVER_PID
+done
+
+if [ $RET -eq 0 ]; then
+  echo -e "\n***\n*** Test Passed\n***"
+fi
+
+exit $RET
