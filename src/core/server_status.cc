f9eae66abf4815ef4c63944041cdf4d8065cd41a
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2019-01-23 16:50:01 -0800

    Fix #36 Ensured ServerStatus to update model config on modified model (#52)

--- src/core/server_status.cc
+++ src/core/server_status.cc
@@ -114,6 +114,28 @@ ServerStatusManager::InitForModel(const std::string& model_name)
 }
 
 tensorflow::Status
+ServerStatusManager::UpdateConfigForModel(const std::string& model_name)
+{
+  ModelConfig model_config;
+  TF_RETURN_IF_ERROR(
+      ModelRepositoryManager::GetModelConfig(model_name, &model_config));
+
+  std::lock_guard<std::mutex> lock(mu_);
+
+  auto& ms = *server_status_.mutable_model_status();
+  if (ms.find(model_name) == ms.end()) {
+    return tensorflow::errors::InvalidArgument(
+        "try to update config for non-existing model '", model_name, "'");
+  } else {
+    LOG_INFO << "Updating config for model '" << model_name << "'";
+  }
+
+  ms[model_name].mutable_config()->CopyFrom(model_config);
+
+  return tensorflow::Status::OK();
+}
+
+tensorflow::Status
 ServerStatusManager::Get(
     ServerStatus* server_status, const std::string& server_id,
     ServerReadyState server_ready_state, uint64_t server_uptime_ns,
