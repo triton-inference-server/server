90c424311ff586a3b91f216d42f464e0b3a9dbcb
David Goodwin <davidg@nvidia.com>
2019-03-05 11:26:42 -0800

    Make sure 1 GPU is visible for tests that require it

--- qa/L0_batcher/test.sh
+++ qa/L0_batcher/test.sh
@@ -35,6 +35,10 @@ source ../common/util.sh
 
 RET=0
 
+# Must run on a single device or else the TRTSERVER_DELAY_SCHEDULER
+# can fail when the requests are distributed to multiple devices.
+CUDA_VISIBLE_DEVICES=0
+
 # Setup non-variable-size model store
 rm -fr *.log *.serverlog models && mkdir models
 for m in \
