1362550cc8a3ad28eae4cfabbc73ad06f08324b1
hemantj <hemanham@gmail.com>
2019-04-23 11:38:27 -0700

    adding tests for multi-port options

---
+++ qa/L0_multiple_ports/test.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Copyright (c) 2018-9, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# MULTI_PORT_CLIENT=../clients/multi_port_client
+MULTI_PORT_CLIENT_PY=../clients/multi_port_client.py
+
+CLIENT_LOG="./client.log"
+
+DATADIR=`pwd`/models
+HSP_ARR=(8008 8009 8010 8011)
+HHP_ARR=(8008 8010 8010 8009)
+HPP_ARR=(8008 8011 8011 8010)
+HIP_ARR=(8008 8011 8008 8008)
+SERVER=/opt/tensorrtserver/bin/trtserver
+for (( n=0; n<4; n++ ))
+do
+:
+# do whatever on $i
+  SERVER_ARGS_ADD_HTTP="--http-status-port ${HSP_ARR[n]} --http-health-port ${HHP_ARR[n]} --http-profile-port ${HPP_ARR[n]}\
+    --http-infer-port ${HIP_ARR[n]} --http-port -1"
+  SERVER_ARGS_ADD_GRPC="--grpc-status-port $((${HSP_ARR[n]}+10)) --grpc-health-port $((${HHP_ARR[n]}+10)) \
+    --grpc-profile-port $((${HPP_ARR[n]}+10)) --grpc-infer-port $((${HIP_ARR[n]}+10)) --grpc-port -1"
+  SERVER_ARGS="--model-store=$DATADIR $SERVER_ARGS_ADD_GRPC $SERVER_ARGS_ADD_HTTP"
+  SERVER_LOG="./inference_server.log"
+  SERVER_TIMEOUT=20
+  source ../common/util.sh
+
+  rm -f $CLIENT_LOG $SERVER_LOG
+
+  run_server
+  if [ "$SERVER_PID" == "0" ]; then
+      echo -e "\n***\n*** Failed to start $SERVER\n***"
+      cat $SERVER_LOG
+      exit 1
+  fi
+
+  RET=0
+
+  # $MULTI_PORT_CLIENT -v >>$CLIENT_LOG 2>&1 -hsp $ ${HSP_ARR[n]} -hhp ${HHP_ARR[n]} -hpp ${HPP_ARR[n]} -hip ${HIP_ARR[n]}
+  # if [ $? -ne 0 ]; then
+  #     RET=1
+  # fi
+
+  python $MULTI_PORT_CLIENT_PY -v >>$CLIENT_LOG 2>&1 -hsp $ ${HSP_ARR[n]} -hhp ${HHP_ARR[n]} -hpp ${HPP_ARR[n]} -hip ${HIP_ARR[n]}
+  if [ $? -ne 0 ]; then
+      RET=1
+  fi
+
+  python $MULTI_PORT_CLIENT_PY -v >>$CLIENT_LOG 2>&1 -hsp $ ${HSP_ARR[n]} -hhp ${HHP_ARR[n]} -hpp ${HPP_ARR[n]} -hip ${HIP_ARR[n]} -i grpc
+  if [ $? -ne 0 ]; then
+      RET=1
+  fi
+
+  kill $SERVER_PID
+  wait $SERVER_PID
+
+  if [ $RET -eq 0 ]; then
+    echo -e "\n***\n*** Test $n PASSED\n***"
+  else
+      cat $CLIENT_LOG
+      echo -e "\n***\n*** Test $n FAILED\n***"
+  fi
+done
+
+exit $RET
---
+++ src/clients/python/multi_port_client.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+# Copyright (c) 2018-19, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+import numpy as np
+import os
+from builtins import range
+from tensorrtserver.api import *
+
+FLAGS = None
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action="store_true", required=False, default=False,
+                        help='Enable verbose output')
+    parser.add_argument('-u', '--url', type=str, required=False, default='localhost:',
+                        help='Inference server URL. Default is localhost:.')
+    parser.add_argument('-i', '--protocol', type=str, required=False, default='http',
+                        help='Protocol ("http"/"grpc") used to ' +
+                        'communicate with inference service. Default is "http".')
+    parser.add_argument(-'hsp', "http_status_port", type=int,
+     help="The port for the server to listen on for HTTP Status requests.")
+    parser.add_argument(-'hhp', "http_health_port", type=int,
+     help="The port for the server to listen on for HTTP Health requests.")
+    parser.add_argument(-'hpp', "http_profile_port", type=int,
+     help="The port for the server to listen on for HTTP Profile requests.")
+    parser.add_argument(-'hip', "http_infer_port", type=int,
+     help="The port for the server to listen on for HTTP Infer requests.")
+
+    FLAGS = parser.parse_args()
+    protocol = ProtocolType.from_str(FLAGS.protocol)
+
+    # We use a simple model that takes 2 input tensors of 16 integers
+    # each and returns 2 output tensors of 16 integers each. One
+    # output tensor is the element-wise sum of the inputs and one
+    # output is the element-wise difference.
+    model_name = "simple"
+    model_version = -1
+    batch_size = 1
+
+    # Create the inference context for the model.
+    if FLAGS.protocol == "http":
+        # infer
+        ctx = InferContext(FLAGS.url+str(FLAGS.http_infer_port), protocol, model_name, model_version, FLAGS.verbose)
+        # health
+        hctx = ServerHealthContext(FLAGS.url+str(FLAGS.http_health_port), protocol, True)
+        assert hctx.is_ready() == False
+        assert hctx.is_live() == False
+        # status
+        sctx = ServerStatusContext(FLAGS.url+str(FLAGS.http_status_port), protocol, model_name, True)
+        ss = sctx.get_server_status()
+        assert server_status.SERVER_READY == ss.ready_state
+    elif FLAGS.protocol == "grpc":
+        # infer
+        ctx = InferContext(FLAGS.url+str(FLAGS.http_infer_port+10), protocol, model_name, model_version, FLAGS.verbose)
+        # health
+        hctx = ServerHealthContext(FLAGS.url+str(FLAGS.http_health_port), protocol, True)
+        assert hctx.is_ready() == False
+        assert hctx.is_live() == False
+        # status
+        sctx = ServerStatusContext(FLAGS.url+str(FLAGS.http_status_port+10), protocol, model_name, True)
+        ss = sctx.get_server_status()
+        assert server_status.SERVER_READY == ss.ready_state
+
+    # Create the data for the two input tensors. Initialize the first
+    # to unique integers and the second to all ones.
+    input0_data = np.arange(start=0, stop=16, dtype=np.int32)
+    input1_data = np.ones(shape=16, dtype=np.int32)
+
+    # Send inference request to the inference server. Get results for
+    # both output tensors.
+    result = ctx.run({ 'INPUT0' : (input0_data,),
+                       'INPUT1' : (input1_data,) },
+                     { 'OUTPUT0' : InferContext.ResultFormat.RAW,
+                       'OUTPUT1' : InferContext.ResultFormat.RAW },
+                     batch_size)
+
+    # We expect there to be 2 results (each with batch-size 1). Walk
+    # over all 16 result elements and print the sum and difference
+    # calculated by the model.
+    output0_data = result['OUTPUT0'][0]
+    output1_data = result['OUTPUT1'][0]
+
+    for i in range(16):
+        print(str(input0_data[i]) + " + " + str(input1_data[i]) + " = " + str(output0_data[i]))
+        print(str(input0_data[i]) + " - " + str(input1_data[i]) + " = " + str(output1_data[i]))
+        if (input0_data[i] + input1_data[i]) != output0_data[i]:
+            print("error: incorrect sum");
+            sys.exit(1);
+        if (input0_data[i] - input1_data[i]) != output1_data[i]:
+            print("error: incorrect difference");
+            sys.exit(1);
