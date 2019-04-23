07d8ed5ff6740fdb742d75cfcb0e598896e54f1b
hemantj <hemanham@gmail.com>
2019-04-23 13:49:39 -0700

    fix copyright issue

--- qa/L0_multiple_ports/test.sh
+++ qa/L0_multiple_ports/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2018-9, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
