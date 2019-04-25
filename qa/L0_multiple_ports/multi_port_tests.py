dde9690d138322673cc73b375610c91d7e71a59c
CoderHam <hemanham@gmail.com>
2019-04-25 10:25:29 -0700

    cleanup

--- qa/L0_multiple_ports/multi_port_tests.py
+++ qa/L0_multiple_ports/multi_port_tests.py
@@ -31,7 +31,6 @@ import numpy as np
 import os
 from builtins import range
 from tensorrtserver.api import *
-import tensorrtserver.api.server_status_pb2 as server_status
 
 FLAGS = None
 
@@ -41,17 +40,19 @@ if __name__ == '__main__':
                         help='Enable verbose output')
     parser.add_argument('-u', '--url', type=str, required=False, default='localhost:',
                         help='Inference server URL. Default is localhost:.')
-    parser.add_argument('-i', '--protocol', type=str, required=False, default="http",
+    parser.add_argument('-i', '--protocol', type=str, required=False, default='http',
                         help='Protocol ("http"/"grpc") used to ' +
                         'communicate with inference service. Default is "http".')
-    parser.add_argument('-p', '--port', type=int, required=False, default=8000,
-                        help="The port for the server to listen on for all HTTP requests.")
-    parser.add_argument('-hp', "--health_port", type=int, required=False, default=-1,
-                        help="The port for the server to listen on for HTTP Health requests.")
+    parser.add_argument(-'sp', "status_port", type=int,
+     help="The port for the server to listen on for HTTP Status requests.")
+    parser.add_argument(-'hp', "health_port", type=int,
+     help="The port for the server to listen on for HTTP Health requests.")
+    parser.add_argument(-'pp', "profile_port", type=int,
+     help="The port for the server to listen on for HTTP Profile requests.")
+    parser.add_argument(-'ip', "infer_port", type=int,
+     help="The port for the server to listen on for HTTP Infer requests.")
 
     FLAGS = parser.parse_args()
-    # FLAGS.health_port = FLAGS.port if FLAGS.health_port==-1 else FLAGS.health_port
-
     protocol = ProtocolType.from_str(FLAGS.protocol)
 
     # We use a simple model that takes 2 input tensors of 16 integers
@@ -59,45 +60,58 @@ if __name__ == '__main__':
     # output tensor is the element-wise sum of the inputs and one
     # output is the element-wise difference.
     model_name = "simple"
-    model_version = 1
+    model_version = -1
     batch_size = 1
 
-    # infer
-    if FLAGS.port !=-1:
-        ctx = InferContext(FLAGS.url+str(FLAGS.port), protocol, model_name, model_version, FLAGS.verbose)
-        # Create the data for the two input tensors. Initialize the first
-        # to unique integers and the second to all ones.
-        input0_data = np.arange(start=0, stop=16, dtype=np.int32)
-        input1_data = np.ones(shape=16, dtype=np.int32)
-
-        # Send inference request to the inference server. Get results for
-        # both output tensors.
-        result = ctx.run({ 'INPUT0' : (input0_data,),
-                           'INPUT1' : (input1_data,) },
-                         { 'OUTPUT0' : InferContext.ResultFormat.RAW,
-                           'OUTPUT1' : InferContext.ResultFormat.RAW },
-                         batch_size)
-
-        # We expect there to be 2 results (each with batch-size 1). Walk
-        # over all 16 result elements and print the sum and difference
-        # calculated by the model.
-        output0_data = result['OUTPUT0'][0]
-        output1_data = result['OUTPUT1'][0]
-
-        for i in range(16):
-            if (input0_data[i] + input1_data[i]) != output0_data[i]:
-                print("error: incorrect sum");
-                sys.exit(1);
-            if (input0_data[i] - input1_data[i]) != output1_data[i]:
-                print("error: incorrect difference");
-                sys.exit(1);
-    # health
-    if FLAGS.health_port !=-1:
-        hctx = ServerHealthContext(FLAGS.url+str(FLAGS.health_port), protocol, True)
-        assert hctx.is_ready() == True
-        assert hctx.is_live() == True
-    # status
-    if FLAGS.port !=-1:
-        sctx = ServerStatusContext(FLAGS.url+str(FLAGS.port), protocol, model_name, True)
+    # Create the inference context for the model.
+    if FLAGS.protocol == "http":
+        # infer
+        ctx = InferContext(FLAGS.url+str(FLAGS.http_infer_port), protocol, model_name, model_version, FLAGS.verbose)
+        # health
+        hctx = ServerHealthContext(FLAGS.url+str(FLAGS.http_health_port), protocol, True)
+        assert hctx.is_ready() == False
+        assert hctx.is_live() == False
+        # status
+        sctx = ServerStatusContext(FLAGS.url+str(FLAGS.http_status_port), protocol, model_name, True)
         ss = sctx.get_server_status()
         assert server_status.SERVER_READY == ss.ready_state
+    elif FLAGS.protocol == "grpc":
+        # infer
+        ctx = InferContext(FLAGS.url+str(FLAGS.http_infer_port+10), protocol, model_name, model_version, FLAGS.verbose)
+        # health
+        hctx = ServerHealthContext(FLAGS.url+str(FLAGS.http_health_port), protocol, True)
+        assert hctx.is_ready() == False
+        assert hctx.is_live() == False
+        # status
+        sctx = ServerStatusContext(FLAGS.url+str(FLAGS.http_status_port+10), protocol, model_name, True)
+        ss = sctx.get_server_status()
+        assert server_status.SERVER_READY == ss.ready_state
+
+    # Create the data for the two input tensors. Initialize the first
+    # to unique integers and the second to all ones.
+    input0_data = np.arange(start=0, stop=16, dtype=np.int32)
+    input1_data = np.ones(shape=16, dtype=np.int32)
+
+    # Send inference request to the inference server. Get results for
+    # both output tensors.
+    result = ctx.run({ 'INPUT0' : (input0_data,),
+                       'INPUT1' : (input1_data,) },
+                     { 'OUTPUT0' : InferContext.ResultFormat.RAW,
+                       'OUTPUT1' : InferContext.ResultFormat.RAW },
+                     batch_size)
+
+    # We expect there to be 2 results (each with batch-size 1). Walk
+    # over all 16 result elements and print the sum and difference
+    # calculated by the model.
+    output0_data = result['OUTPUT0'][0]
+    output1_data = result['OUTPUT1'][0]
+
+    for i in range(16):
+        print(str(input0_data[i]) + " + " + str(input1_data[i]) + " = " + str(output0_data[i]))
+        print(str(input0_data[i]) + " - " + str(input1_data[i]) + " = " + str(output1_data[i]))
+        if (input0_data[i] + input1_data[i]) != output0_data[i]:
+            print("error: incorrect sum");
+            sys.exit(1);
+        if (input0_data[i] - input1_data[i]) != output1_data[i]:
+            print("error: incorrect difference");
+            sys.exit(1);
