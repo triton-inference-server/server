b50ea1350aa39f808743219e25190efd23727914
David Goodwin <davidg@nvidia.com>
2019-04-29 11:07:28 -0700

    Formatting

--- src/clients/c++/image_client.cc
+++ src/clients/c++/image_client.cc
@@ -40,13 +40,13 @@
 
 #include <opencv2/core/version.hpp>
 #if CV_MAJOR_VERSION == 2
-  #include <opencv2/core/core.hpp>
-  #include <opencv2/highgui/highgui.hpp>
-  #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #elif CV_MAJOR_VERSION == 3
-  #include <opencv2/core.hpp>
-  #include <opencv2/highgui.hpp>
-  #include <opencv2/imgproc.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #endif
 
 namespace ni = nvidia::inferenceserver;
