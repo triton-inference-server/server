a43db3058dfc80e26843b28046b2f30f6a34f81a
CoderHam <hemanham@gmail.com>
2019-05-23 11:20:01 -0700

    remove zero and variable from tests, fix typo in reshape

--- qa/L0_infer_zero/infer_zero_test.py
+++ qa/L0_infer_zero/infer_zero_test.py
@@ -67,13 +67,6 @@ class InferZeroTest(unittest.TestCase):
             # model that does not support batching
             iu.infer_zero(self, 'onnx_nobatch', 1, dtype, shapes, shapes)
 
-        if tu.validate_for_libtorch_model(dtype, dtype, dtype, shapes[0], shapes[0], shapes[0]):
-            # model that supports batching
-            for bs in (1, 8):
-                iu.infer_zero(self, 'libtorch', bs, dtype, shapes, shapes)
-            # model that does not support batching
-            iu.infer_zero(self, 'libtorch_nobatch', 1, dtype, shapes, shapes)
-
         for name in ["simple_zero", "sequence_zero", "fan_zero"]:
             if tu.validate_for_ensemble_model(name, dtype, dtype, dtype,
                                         shapes[0], shapes[0], shapes[0]):
