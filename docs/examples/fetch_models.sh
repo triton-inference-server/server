ec46e12254a0e98b5bbd06251ba5f967b25cac6c
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2019-06-04 09:58:30 -0700

    Update Documentation and example model repository (#330)
    * Add an example ONNX model and update docs and fetch_models script
    
    * Update ensemble client doc as ensemble models are in separate repo
    
    * Add notice on possible unsupported ONNX models

--- docs/examples/fetch_models.sh
+++ docs/examples/fetch_models.sh
@@ -40,3 +40,8 @@ wget -O /tmp/inception_v3_2016_08_28_frozen.pb.tar.gz \
      https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
 (cd /tmp && tar xzf inception_v3_2016_08_28_frozen.pb.tar.gz)
 mv /tmp/inception_v3_2016_08_28_frozen.pb model_repository/inception_graphdef/1/model.graphdef
+
+# ONNX densenet
+mkdir -p model_repository/densenet_onnx/1
+wget -O model_repository/densenet_onnx/1/model.onnx \
+     https://contentmamluswest001.blob.core.windows.net/content/14b2744cf8d6418c87ffddc3f3127242/9502630827244d60a1214f250e3bbca7/08aed7327d694b8dbaee2c97b8d0fcba/densenet121-1.2.onnx
