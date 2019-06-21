68e1193ce1890f6156e9962f4f92a888dbd09147
GuanLuo <gluo@nvidia.com>
2019-06-21 10:29:26 -0700

    Added large payload test using existing qa models

---
+++ qa/L0_large_payload/large_payload_test.py
@@ -0,0 +1,105 @@
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+sys.path.append("../common")
+
+import unittest
+import numpy as np
+from tensorrtserver.api import *
+import test_util as tu
+
+class LargePayLoadTest(unittest.TestCase):
+    def setUp(self):
+        self.data_type_ = np.float32
+        # n GB divided by element size
+        self.input_size_ = 6 * (1024 * 1024 * 1024) / np.dtype(self.data_type_).itemsize
+        self.protocols_ = ((ProtocolType.HTTP, 'localhost:8000'),
+                        (ProtocolType.GRPC, 'localhost:8001'))
+
+    def _test_helper(self, ctx, tensor_shape, small_tensor_shape):
+        try:
+            in0 = np.random.random(tensor_shape).astype(self.data_type_)
+            results = ctx.run({ 'INPUT0' : (in0,)},
+                            { 'OUTPUT0' : InferContext.ResultFormat.RAW},
+                            1)
+            # if the inference is completed, examine results to ensure that
+            # the framework and protocol do support large payload
+            self.assertTrue(np.array_equal(in0, results['OUTPUT0'][0]), "output is different from input")
+
+        except InferenceServerException as ex:
+            # if the inference failed, inference server should return error
+            # gracefully. In addition to this, send a small payload to
+            # verify if the server is still functional
+            sin0 = np.random.random(small_tensor_shape).astype(self.data_type_)
+            results = ctx.run({ 'INPUT0' : (sin0,)},
+                            { 'OUTPUT0' : InferContext.ResultFormat.RAW},
+                            1)
+            self.assertTrue(np.array_equal(sin0, results['OUTPUT0'][0]), "output is different from input")
+
+    def test_graphdef(self):
+        tensor_shape = (self.input_size_,)
+        small_tensor_shape = (1,)
+
+        # graphdef_nobatch_zero_1_float32 is identity model with input shape [-1]
+        for protocol, url in self.protocols_:
+            model_name = tu.get_zero_model_name("graphdef_nobatch", 1, self.data_type_)
+            ctx = InferContext(url, protocol, model_name, None, True)
+            self._test_helper(ctx, tensor_shape, small_tensor_shape)            
+
+    def test_savedmodel(self):
+        tensor_shape = (self.input_size_,)
+        small_tensor_shape = (1,)
+
+        # savedmodel_nobatch_zero_1_float32 is identity model with input shape [-1]
+        for protocol, url in self.protocols_:
+            model_name = tu.get_zero_model_name("savedmodel_nobatch", 1, self.data_type_)
+            ctx = InferContext(url, protocol, model_name, None, True)
+            self._test_helper(ctx, tensor_shape, small_tensor_shape)
+
+    def test_netdef(self):
+        tensor_shape = (self.input_size_,)
+        small_tensor_shape = (1,)
+
+        # netdef_nobatch_zero_1_float32 is identity model with input shape [-1]
+        for protocol, url in self.protocols_:
+            model_name = tu.get_zero_model_name("netdef_nobatch", 1, self.data_type_)
+            ctx = InferContext(url, protocol, model_name, None, True)
+            self._test_helper(ctx, tensor_shape, small_tensor_shape)
+
+    def test_onnx(self):
+        tensor_shape = (self.input_size_,)
+        small_tensor_shape = (1,)
+
+        # onnx_nobatch_zero_1_float32 is identity model with input shape [-1]
+        for protocol, url in self.protocols_:
+            model_name = tu.get_zero_model_name("onnx_nobatch", 1, self.data_type_)
+            ctx = InferContext(url, protocol, model_name, None, True)
+            self._test_helper(ctx, tensor_shape, small_tensor_shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ qa/L0_large_payload/test.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+LARGE_PAYLOAD_TEST_PY=large_payload_test.py
+
+CLIENT_LOG_BASE="./client.log"
+
+DATADIR=/data/inferenceserver/qa_zero_model_repository
+
+SERVER=/opt/tensorrtserver/bin/trtserver
+SERVER_ARGS=--model-store=$DATADIR
+SERVER_LOG_BASE="./inference_server.log"
+source ../common/util.sh
+
+rm -f $SERVER_LOG_BASE* $CLIENT_LOG_BASE*
+
+RET=0
+
+# Restart server before every test to make sure server state
+# is invariant to previous test
+#
+# [TODO] add LibTorch model and Plan model
+for TARGET in graphdef savedmodel netdef onnx; do
+    SERVER_LOG=$SERVER_LOG_BASE.$TARGET
+    CLIENT_LOG=$CLIENT_LOG_BASE.$TARGET
+
+    run_server
+    if [ "$SERVER_PID" == "0" ]; then
+        echo -e "\n***\n*** Failed to start $SERVER\n***"
+        cat $SERVER_LOG
+        exit 1
+    fi
+
+    set +e
+
+    python $LARGE_PAYLOAD_TEST_PY LargePayLoadTest.test_$TARGET >$CLIENT_LOG 2>&1
+    if [ $? -ne 0 ]; then
+        cat $CLIENT_LOG
+        echo -e "\n***\n*** Test Failed\n***"
+        RET=1
+    fi
+
+    grep -c "HTTP/1.1 200 OK" $CLIENT_LOG
+    if [ $? -ne 0 ]; then
+        cat $CLIENT_LOG
+        echo -e "\n***\n*** Test Failed To Run\n***"
+        RET=1
+    fi
+
+    set -e
+
+    kill $SERVER_PID
+    wait $SERVER_PID
+done
+
+if [ $RET -eq 0 ]; then
+  echo -e "\n***\n*** Test Passed\n***"
+fi
+
+exit $RET
