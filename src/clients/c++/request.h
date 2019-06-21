c8db09b591459192719b166b2406322d08011a30
GuanLuo <gluo@nvidia.com>
2019-06-21 15:55:21 -0700

    Revert change for UNIX build

--- src/clients/c++/request.h
+++ src/clients/c++/request.h
@@ -717,7 +717,7 @@ InferContext::Result::GetRawAtCursor(size_t batch_idx, T* out)
   }
 
   std::copy(buf, buf + sizeof(T), reinterpret_cast<uint8_t*>(out));
-  return err;
+  return Error::Success;
 }
 
 }}}  // namespace nvidia::inferenceserver::client
