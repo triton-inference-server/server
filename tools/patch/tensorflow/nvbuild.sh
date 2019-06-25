ed6eafde3d37f3e69c6da2da2cdbe2d0dd55e213
David Goodwin <davidg@nvidia.com>
2019-06-24 17:10:37 -0700

    Fixes to track upstream Tensorflow container

--- tools/patch/tensorflow/nvbuild.sh
+++ tools/patch/tensorflow/nvbuild.sh
@@ -1,18 +1,13 @@
 diff --git a/nvbuild.sh b/nvbuild.sh
-index 674c0dd..e0b8656 100755
+index 7e9055c..3f293f3 100755
 --- a/nvbuild.sh
 +++ b/nvbuild.sh
-@@ -57,7 +57,7 @@ export CC_OPT_FLAGS="-march=sandybridge -mtune=broadwell"
- 
- cd "$THIS_DIR"
+@@ -64,7 +64,7 @@ export CC_OPT_FLAGS="-march=sandybridge -mtune=broadwell"
+ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+ cd "${THIS_DIR}/tensorflow-source"
  export PYTHON_BIN_PATH=/usr/bin/python$PYVER
 -LIBCUDA_FOUND=$(ldconfig -p | awk '{print $1}' | grep libcuda.so | wc -l)
 +LIBCUDA_FOUND=$(ldconfig -p | grep -v compat | awk '{print $1}' | grep libcuda.so | wc -l)
  if [[ $NOCONFIG -eq 0 ]]; then
    if [[ "$LIBCUDA_FOUND" -eq 0 ]]; then
        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/stubs
-@@ -79,4 +79,3 @@ export NOCLEAN
- export PYVER
- export LIBCUDA_FOUND
- bash bazel_build.sh
--
