df6c71081d639e1794099d23a15bfbe1333aea8c
Hemant Jain <hemanham@gmail.com>
2019-06-26 11:20:32 -0700

    Add support for TensorRT provided plugins (#408)
    * register default plugins in TensorRT
    
    * generate demo plugin TRT model
    
    * Add L0_trt_plugin test
    
    * edits for review comments
    
    * fix call_once
    
    * naming for get_trt_plugin is no longer generic
    
    * Move plugin registration plan to backend factory

---
+++ qa/L0_trt_plugin/trt_plugin_test.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2018-2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+sys.path.append("../common")
+
+from builtins import range
+from future.utils import iteritems
+import unittest
+import numpy as np
+from tensorrtserver.api import *
+import os
+
+class PluginModelTest(unittest.TestCase):
+    def _full_exact(self, batch_size, input_dtype, output_dtype, model_name):
+        input_list = list()
+        for b in range(batch_size):
+            in0 = np.random.randn(16,1,1).astype(input_dtype)
+            input_list.append(in0)
+
+        ctx = InferContext("localhost:8000", ProtocolType.HTTP, model_name,
+                           correlation_id=0, streaming=False, verbose=True)
+        results = ctx.run(
+            { "INPUT0" : input_list }, { "OUTPUT0" : InferContext.ResultFormat.RAW},
+            batch_size=batch_size)
+
+        self.assertEqual(len(results), 1)
+        self.assertTrue("OUTPUT0" in results)
+        result = results["OUTPUT0"]
+        # verify values of Leaky RELU (it uses 0.1 instead of the default 0.01)
+        for b in range(batch_size):
+            test_input = np.where(input_list[b] > 0, input_list[b], input_list[b] * 0.1)
+            self.assertTrue(all(np.isclose(result[b], test_input)))
+
+    def test_raw_fff(self):
+        # model that supports batching
+        for bs in (1, 8):
+            self._full_exact(bs, np.float32, np.float32, 'plan_float32_float32_float32')
+
+if __name__ == '__main__':
+    unittest.main()
