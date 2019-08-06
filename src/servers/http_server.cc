0de387874d24fd8382e0a34cad06498c927a0284
CoderHam <hemanham@gmail.com>
2019-08-06 14:10:06 -0700

    fix http regex for shared memory

--- src/servers/http_server.cc
+++ src/servers/http_server.cc
@@ -198,8 +198,7 @@ class HTTPAPIServer : public HTTPServerImpl {
         health_regex_(R"(/(live|ready))"),
         infer_regex_(R"(/([^/]+)(?:/(\d+))?)"), status_regex_(R"(/(.*))"),
         modelcontrol_regex_(R"(/(load|unload)/([^/]+))"),
-        sharedmemorycontrol_regex_(
-            R"(/(register|unregister|unregisterall|status)/([^/]+)/(/[^/]+)/([0-9]+)/([0-9]+))")
+        sharedmemorycontrol_regex_(R"(/(register|unregister|unregisterall|status)(.*))")
   {
     TRTSERVER_Error* err = TRTSERVER_ServerId(server_.get(), &server_id_);
     if (err != nullptr) {
@@ -541,14 +540,34 @@ HTTPAPIServer::HandleSharedMemoryControl(
     return;
   }
 
+  re2::RE2 register_regex_(R"(/([^/]+)/(/[^/]+)/([0-9]+)/([0-9]+))");
+  re2::RE2 unregister_regex_(R"(/([^/]+))");
+
   std::string action_type_str, remaining, name, shm_key;
   std::string offset_str, byte_size_str;
   if ((sharedmemorycontrol_uri.empty()) ||
       (!RE2::FullMatch(
-          sharedmemorycontrol_uri, sharedmemorycontrol_regex_, &action_type_str,
-          &name, &shm_key, &offset_str, &byte_size_str))) {
+          sharedmemorycontrol_uri, sharedmemorycontrol_regex_, &action_type_str, &remaining))) {
     evhtp_send_reply(req, EVHTP_RES_BADREQ);
     return;
+  }
+  else {
+    if (remaining.empty()) {
+      if ((action_type_str != "unregisterall") && (action_type_str != "status")) {
+        evhtp_send_reply(req, EVHTP_RES_BADREQ);
+        return;
+      }
+    }
+    else {
+      if (action_type_str == "register" && (!RE2::FullMatch(remaining, register_regex_, &name, &shm_key, &offset_str, &byte_size_str))) {
+        evhtp_send_reply(req, EVHTP_RES_BADREQ);
+        return;
+      }
+      if (action_type_str == "unregister" && (!RE2::FullMatch(remaining, unregister_regex_, &name))) {
+        evhtp_send_reply(req, EVHTP_RES_BADREQ);
+        return;
+      }
+    }
   }
 
   size_t offset = std::atoll(offset_str.c_str());
