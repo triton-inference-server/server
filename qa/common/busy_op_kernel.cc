52ceb8b21689e972cf8c2f705ce083f21935891c
Tanmay Verma <tanmayv@nvidia.com>
2019-08-15 17:06:48 -0700

    Adding other unformatted files

--- qa/common/busy_op_kernel.cc
+++ qa/common/busy_op_kernel.cc
@@ -26,43 +26,47 @@
 
 #include <time.h>
 
+#include "tensorflow/core/framework/device_base.h"
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/tensor_shape.h"
-#include "tensorflow/core/framework/device_base.h"
 
 using namespace tensorflow;  // NOLINT(build/namespaces)
 
-REGISTER_OP("BusyLoop")
-    .Input("input: int32")
-    .Output("output: int32")
-    .Doc(R"doc(
+REGISTER_OP("BusyLoop").Input("input: int32").Output("output: int32").Doc(R"doc(
 Busy waits for input number of clock cycles
 )doc");
 
-void BusyLoopKernelLauncher(const Eigen::GpuDevice& device, const int* num_delay_cycles, int* out);
+void BusyLoopKernelLauncher(
+    const Eigen::GpuDevice& device, const int* num_delay_cycles, int* out);
 
 class BusyLoopOp : public OpKernel {
  public:
   explicit BusyLoopOp(OpKernelConstruction* context) : OpKernel(context) {}
 
-  void Compute(OpKernelContext* context) override {
+  void Compute(OpKernelContext* context) override
+  {
     // Grab the input
     const Tensor& input_tensor = context->input(0);
     auto num_delay_cycles = input_tensor.flat<int32>();
 
     // Create dummy output
     Tensor* output_tensor = nullptr;
-    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor.shape(),
-                                                     &output_tensor));
+    OP_REQUIRES_OK(
+        context,
+        context->allocate_output(0, input_tensor.shape(), &output_tensor));
     auto output = output_tensor->template flat<int32>();
 
     // Verify input dimension
-    OP_REQUIRES(context, TensorShapeUtils::IsVector(input_tensor.shape()),
-        errors::InvalidArgument("BusyLoop expects a single value as a 1-D Vector"));
+    OP_REQUIRES(
+        context, TensorShapeUtils::IsVector(input_tensor.shape()),
+        errors::InvalidArgument(
+            "BusyLoop expects a single value as a 1-D Vector"));
 
     // Call the cuda kernel launcher
-    BusyLoopKernelLauncher(context->eigen_device<Eigen::GpuDevice>(), num_delay_cycles.data(), output.data());
+    BusyLoopKernelLauncher(
+        context->eigen_device<Eigen::GpuDevice>(), num_delay_cycles.data(),
+        output.data());
   }
 };
 
