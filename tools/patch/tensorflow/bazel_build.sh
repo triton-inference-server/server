d815c322cbdcd13c27da412bdd76b62c9f8ae70f
Tanmay Verma <tanmayv@nvidia.com>
2019-08-26 12:10:07 -0700

    changes needed to use Tensorflow master container

--- tools/patch/tensorflow/bazel_build.sh
+++ tools/patch/tensorflow/bazel_build.sh
@@ -1,20 +1,18 @@
 diff --git a/bazel_build.sh b/bazel_build.sh
-index b3cb71f..2959753 100755
+index b3460a488f..42698ae440 100755
 --- a/bazel_build.sh
 +++ b/bazel_build.sh
-@@ -40,9 +40,14 @@ else
- fi
- 
- bazel build $BAZEL_OPTS \
--    tensorflow/tools/pip_package:build_pip_package
-+    tensorflow/tools/pip_package:build_pip_package \
-+    //tensorflow:libtensorflow_cc.so \
-+    //tensorflow:libtensorflow_framework.so
- BAZEL_BUILD_RETURN=$?
- 
-+mkdir -p /usr/local/lib/tensorflow
-+cp bazel-bin/tensorflow/libtensorflow_*.so* /usr/local/lib/tensorflow/
-+
- if [ ${BAZEL_BUILD_RETURN} -gt 0 ]
- then
-   exit ${BAZEL_BUILD_RETURN}
+@@ -42,10 +42,11 @@ fi
+ if [[ $IN_CONTAINER -eq 1 ]]; then
+   bazel build $BAZEL_OPTS \
+       tensorflow/tools/pip_package:build_pip_package \
+-      //tensorflow:libtensorflow_cc.so
++      //tensorflow:libtensorflow_cc.so \
++      //tensorflow:libtensorflow_framework.so
+   BAZEL_BUILD_RETURN=$?
+   mkdir -p /usr/local/lib/tensorflow
+-  cp bazel-bin/tensorflow/libtensorflow_cc.so.? /usr/local/lib/tensorflow
++  cp bazel-bin/tensorflow/libtensorflow_*.so.* /usr/local/lib/tensorflow
+ else
+   bazel build $BAZEL_OPTS \
+       tensorflow/tools/pip_package:build_pip_package
