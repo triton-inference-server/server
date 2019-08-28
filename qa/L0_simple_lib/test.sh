522c85ec1a61c892279b8b17518432b8b8a86d1d
GuanLuo <gluo@nvidia.com>
2019-08-28 14:27:25 -0700

    Add custom model into the test

--- qa/L0_simple_lib/test.sh
+++ qa/L0_simple_lib/test.sh
@@ -64,7 +64,7 @@ for trial in \
         plan_float32_float32_float32 ; do
     rm -rf $MODELSDIR/simple/1/*
     cp -r $DATADIR/${trial}/1/* $MODELSDIR/simple/1/.
-    cp  $DATADIR/${trial}/config.pbtxt $MODELSDIR/simple/.
+    cp $DATADIR/${trial}/config.pbtxt $MODELSDIR/simple/.
     (cd $MODELSDIR/simple && \
                 sed -i "s/^name:.*/name: \"simple\"/" config.pbtxt && \
                 sed -i "s/label_filename:.*//" config.pbtxt)
@@ -85,6 +85,29 @@ for trial in \
 
     set -e
 done
+
+rm -rf $MODELSDIR/simple/1/*
+cp -r ../custom_models/custom_float32_float32_float32/1/* $MODELSDIR/simple/1/.
+cp ../custom_models/custom_float32_float32_float32/config.pbtxt $MODELSDIR/simple/.
+(cd $MODELSDIR/simple && \
+            sed -i "s/^name:.*/name: \"simple\"/" config.pbtxt && \
+            sed -i "s/label_filename:.*//" config.pbtxt)
+
+set +e
+
+$SIMPLE_CLIENT -r $MODELSDIR >>$CLIENT_LOG 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "\n***\n*** Test Failed\n***"
+    RET=1
+fi
+
+$SIMPLE_CLIENT -r $MODELSDIR -g >>$CLIENT_LOG 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "\n***\n*** Test Failed\n***"
+    RET=1
+fi
+
+set -e
 
 if [ $RET -eq 0 ]; then
   echo -e "\n***\n*** Test Passed\n***"
