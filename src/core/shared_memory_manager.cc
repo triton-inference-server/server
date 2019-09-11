ec1ae9d365b5d1c34263fbc2f065a643bebe48a7
CoderHam <hemanham@gmail.com>
2019-09-10 19:01:36 -0700

    server side get shm status functionality

--- src/core/shared_memory_manager.cc
+++ src/core/shared_memory_manager.cc
@@ -231,13 +231,12 @@ SharedMemoryManager::GetSharedMemoryStatus(SharedMemoryStatus* shm_status)
 }
 
 Status
-SharedMemoryManager::GetSharedMemoryStatus(
-    std::vector<SharedMemoryInfo*>* active_shm_regions)
+SharedMemoryManager::GetSharedMemoryStatus(SharedMemoryControlResponse* shm_status)
 {
-  active_shm_regions->clear();
-  for (const auto& shm_info : shared_memory_map_) {
-    active_shm_regions->push_back(shm_info.second.get());
-  }
+  // active_shm_regions->clear();
+  // for (const auto& shm_info : shared_memory_map_) {
+  //   // active_shm_regions->push_back(shm_info.second.get());
+  // }
 
   return Status::Success;
 }
