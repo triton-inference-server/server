a8537cfbb7faa1831cc437f9e91a58e7b30b0135
GuanLuo <gluo@nvidia.com>
2019-09-16 17:48:02 -0700

    format

--- src/backends/onnx/onnx_backend.cc
+++ src/backends/onnx/onnx_backend.cc
@@ -38,9 +38,9 @@
 #include "src/core/server_status.h"
 
 #ifdef TRTIS_ENABLE_GPU
-#include <tensorrt_provider_factory.h>
 #include <cuda_provider_factory.h>
 #include <cuda_runtime_api.h>
+#include <tensorrt_provider_factory.h>
 #endif  // TRTIS_ENABLE_GPU
 
 namespace nvidia { namespace inferenceserver {
@@ -86,7 +86,8 @@ OnnxBackend::CreateExecutionContexts(
       session_options, &OrtReleaseSessionOptions);
   RETURN_IF_ORT_ERROR(OrtSetSessionThreadPoolSize(session_options, 1));
   // disable graph optimization
-  RETURN_IF_ORT_ERROR(OrtSetSessionGraphOptimizationLevel(session_options, ORT_DISABLE_ALL));
+  RETURN_IF_ORT_ERROR(
+      OrtSetSessionGraphOptimizationLevel(session_options, ORT_DISABLE_ALL));
 
   Status status = CreateExecutionContextsHelper(session_options, models);
 
@@ -211,32 +212,42 @@ OnnxBackend::CreateExecutionContext(
     if (Config().optimization().has_execution_accelerators()) {
       // Don't need to ensure uniqueness of the providers,
       // ONNX Runtime will check it.
-      for (const auto& execution_accelerator : Config().optimization().execution_accelerators().gpu_execution_accelerator()) {
+      for (const auto& execution_accelerator :
+           Config()
+               .optimization()
+               .execution_accelerators()
+               .gpu_execution_accelerator()) {
         if (execution_accelerator == kTensorRTExecutionAccelerator) {
-          RETURN_IF_ORT_ERROR(OrtSessionOptionsAppendExecutionProvider_Tensorrt(session_options, gpu_device));
+          RETURN_IF_ORT_ERROR(OrtSessionOptionsAppendExecutionProvider_Tensorrt(
+              session_options, gpu_device));
           LOG_VERBOSE(1) << "TensorRT Execution Accelerator is set for "
                          << instance_name << " on device " << gpu_device;
         } else {
-          LOG_ERROR << "Ignore unknown Execution Accelerator '" << execution_accelerator << "' for " << instance_name;
+          LOG_ERROR << "Ignore unknown Execution Accelerator '"
+                    << execution_accelerator << "' for " << instance_name;
         }
       }
     }
     RETURN_IF_ORT_ERROR(OrtSessionOptionsAppendExecutionProvider_CUDA(
         session_options, gpu_device));
-    LOG_VERBOSE(1) << "CUDA Execution Accelerator is set for "
-                   << instance_name << " on device " << gpu_device;
+    LOG_VERBOSE(1) << "CUDA Execution Accelerator is set for " << instance_name
+                   << " on device " << gpu_device;
 #else
     return Status(RequestStatusCode::INTERNAL, "GPU instances not supported");
 #endif  // TRTIS_ENABLE_GPU
   }
 
   if (Config().optimization().has_execution_accelerators()) {
-    for (const auto& execution_accelerator : Config().optimization().execution_accelerators().cpu_execution_accelerator()) {
+    for (const auto& execution_accelerator : Config()
+                                                 .optimization()
+                                                 .execution_accelerators()
+                                                 .cpu_execution_accelerator()) {
       if (execution_accelerator == kOpenVINOExecutionAccelerator) {
         LOG_ERROR << "OpenVINO Execution Accelerator is not supported for "
                   << instance_name;
       } else {
-        LOG_ERROR << "Ignore unknown Execution Accelerator '" << execution_accelerator << "' for " << instance_name;
+        LOG_ERROR << "Ignore unknown Execution Accelerator '"
+                  << execution_accelerator << "' for " << instance_name;
       }
     }
   }
