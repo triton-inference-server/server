79eb4e7a24d47e1b117f6daf07e2d9e213045787
GuanLuo <gluo@nvidia.com>
2019-11-12 18:55:59 -0800

    Add model control mode option to be more informative

--- qa/L0_lifecycle/test.sh
+++ qa/L0_lifecycle/test.sh
@@ -434,8 +434,9 @@ for i in graphdef ; do
     cp -r $DATADIR/qa_model_repository/${i}_int32_int32_int32 models/.
 done
 
-SERVER_ARGS="--model-repository=`pwd`/models --allow-poll-model-repository=false \
-             --exit-timeout-secs=5"
+# Show model control mode will override deprecated model control options
+SERVER_ARGS="--model-repository=`pwd`/models --model-control-mode=none \
+             --allow-model-control=true --exit-timeout-secs=5"
 SERVER_LOG="./inference_server_$LOG_IDX.log"
 run_server
 if [ "$SERVER_PID" == "0" ]; then
@@ -611,8 +612,9 @@ cp -r $DATADIR/qa_model_repository/savedmodel_float32_float32_float32 .
 cp -r $DATADIR/qa_model_repository/savedmodel_float32_float32_float32 models/. && \
     rm -rf models/savedmodel_float32_float32_float32/3
 
+# Show model control mode will override deprecated model control options
 SERVER_ARGS="--model-repository=`pwd`/models --model-repository=`pwd`/models_0 \
-             --allow-model-control=true  --allow-poll-model-repository=false \
+             --model-control-mode=explicit --allow-poll-model-repository=true\
              --exit-timeout-secs=5"
 SERVER_LOG="./inference_server_$LOG_IDX.log"
 run_server
