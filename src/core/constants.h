118f9abeb21103d93b735bb14d3c96e85b76328e
David Goodwin <davidg@nvidia.com>
2019-11-21 14:57:37 -0800

    In server status add a last-inference timestamp for each model/version

--- src/core/constants.h
+++ src/core/constants.h
@@ -93,6 +93,7 @@ constexpr uint64_t SEQUENCE_IDLE_DEFAULT_MICROSECONDS = 1000 * 1000;
 
 #define TIMESPEC_TO_NANOS(TS) \
   ((TS).tv_sec * nvidia::inferenceserver::NANOS_PER_SECOND + (TS).tv_nsec)
+#define TIMESPEC_TO_MILLIS(TS) (TIMESPEC_TO_NANOS(TS) * 1000 * 1000)
 
 #define DISALLOW_MOVE(TypeName) TypeName(Context&& o) = delete;
 #define DISALLOW_COPY(TypeName) TypeName(const TypeName&) = delete;
