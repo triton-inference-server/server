86f495b20b54d45f957d65b4ece378dc7888e006
Tanmay Verma <tanmay2592@gmail.com>
2019-12-17 12:59:46 -0800

    Use correct version of TRT enqueue API (#952)
    * Use correct version of TRT enqueue API
    
    * Validating whether bindings support batching

--- src/backends/tensorrt/loader.cc
+++ src/backends/tensorrt/loader.cc
@@ -55,8 +55,8 @@ LoadPlan(
         RequestStatusCode::INTERNAL, "unable to create TensorRT runtime");
   }
 
-  *engine = (*runtime)->deserializeCudaEngine(
-      &model_data[0], model_data.size());
+  *engine =
+      (*runtime)->deserializeCudaEngine(&model_data[0], model_data.size());
   if (*engine == nullptr) {
     return Status(
         RequestStatusCode::INTERNAL, "unable to create TensorRT engine");
