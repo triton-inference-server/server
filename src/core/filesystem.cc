1e93acd94ff060ff09921db3cef1c96d5745a954
CoderHam <hemanham@gmail.com>
2020-02-03 09:46:01 -0800

    reviw edits

--- src/core/filesystem.cc
+++ src/core/filesystem.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -39,7 +39,6 @@
 #include <aws/s3/model/HeadBucketRequest.h>
 #include <aws/s3/model/HeadObjectRequest.h>
 #include <aws/s3/model/ListObjectsRequest.h>
-#include <re2/re2.h>
 #endif  // TRTIS_ENABLE_S3
 
 #include <google/protobuf/text_format.h>
