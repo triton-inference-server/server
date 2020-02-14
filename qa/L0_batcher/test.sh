c5b77635248227436c51a1496f6b8e0c578fb7c6
CoderHam <hemanham@gmail.com>
2020-02-13 16:34:35 -0800

    Support for batcher test customization

--- qa/L0_batcher/test.sh
+++ qa/L0_batcher/test.sh
@@ -39,12 +39,16 @@ CLIENT_LOG="./client.log"
 BATCHER_TEST=batcher_test.py
 VERIFY_TIMESTAMPS=verify_timestamps.py
 
-DATADIR=/data/inferenceserver/${REPO_VERSION}
+DATADIR=${DATADIR:="/data/inferenceserver/${REPO_VERSION}"}
+OPTDIR=${OPTDIR:="/opt"}
+SERVER=${OPTDIR}/tensorrtserver/bin/trtserver
 
-SERVER=/opt/tensorrtserver/bin/trtserver
 source ../common/util.sh
 
 RET=0
+
+# If BACKENDS not specified, set to all
+BACKENDS=${BACKENDS:="graphdef savedmodel netdef onnx libtorch plan custom"}
 
 # Must run on a single device or else the TRTSERVER_DELAY_SCHEDULER
 # can fail when the requests are distributed to multiple devices.
@@ -52,37 +56,44 @@ export CUDA_VISIBLE_DEVICES=0
 
 # Setup non-variable-size model repository
 rm -fr *.log *.serverlog models && mkdir models
-for m in \
-        $DATADIR/qa_model_repository/savedmodel_float32_float32_float32 \
-        $DATADIR/qa_model_repository/graphdef_float32_float32_float32 \
-        $DATADIR/qa_model_repository/netdef_float32_float32_float32 \
-        $DATADIR/qa_model_repository/plan_float32_float32_float32 \
-        $DATADIR/qa_model_repository/libtorch_float32_float32_float32 \
-        $DATADIR/qa_model_repository/onnx_float32_float32_float32 \
-        ../custom_models/custom_float32_float32_float32 ; do
-    cp -r $m models/. &&
-        (cd models/$(basename $m) && \
-                sed -i "s/^max_batch_size:.*/max_batch_size: 8/" config.pbtxt && \
-                sed -i "s/^version_policy:.*/version_policy: { specific { versions: [1] }}/" config.pbtxt && \
-                echo "dynamic_batching { preferred_batch_size: [ 2, 6 ], max_queue_delay_microseconds: 10000000 }" >> config.pbtxt)
+for BACKEND in $BACKENDS; do
+    if [ "$BACKEND" != "custom" ]; then
+      cp -r $DATADIR/qa_model_repository/${BACKEND}_float32_float32_float32 models/. &&
+        (cd models/${BACKEND}_float32_float32_float32 && \
+            sed -i "s/^max_batch_size:.*/max_batch_size: 8/" config.pbtxt && \
+            sed -i "s/^version_policy:.*/version_policy: { specific { versions: [1] }}/" config.pbtxt && \
+            echo "dynamic_batching { preferred_batch_size: [ 2, 6 ], max_queue_delay_microseconds: 10000000 }" >> config.pbtxt)
+    else
+      cp -r ../custom_models/custom_float32_float32_float32 models/. &&
+        (cd models/custom_float32_float32_float32 && \
+            sed -i "s/^max_batch_size:.*/max_batch_size: 8/" config.pbtxt && \
+            sed -i "s/^version_policy:.*/version_policy: { specific { versions: [1] }}/" config.pbtxt && \
+            echo "dynamic_batching { preferred_batch_size: [ 2, 6 ], max_queue_delay_microseconds: 10000000 }" >> config.pbtxt)
+    fi
 done
 
 # Setup variable-size model repository
 rm -fr var_models && mkdir var_models
-for m in \
-        $DATADIR/qa_variable_model_repository/savedmodel_float32_float32_float32 \
-        $DATADIR/qa_variable_model_repository/graphdef_float32_float32_float32 \
-        $DATADIR/qa_variable_model_repository/netdef_float32_float32_float32 \
-        $DATADIR/qa_variable_model_repository/plan_float32_float32_float32 \
-        $DATADIR/qa_variable_model_repository/libtorch_float32_float32_float32 \
-        $DATADIR/qa_variable_model_repository/onnx_float32_float32_float32 \
-        ../custom_models/custom_float32_float32_float32 \
-        ../custom_models/custom_zero_1_float32; do
-    cp -r $m var_models/. && \
-        (cd var_models/$(basename $m) && \
-                sed -i "s/^max_batch_size:.*/max_batch_size: 8/" config.pbtxt && \
-                sed -i "s/^version_policy:.*/version_policy: { specific { versions: [1] }}/" config.pbtxt && \
-                echo "dynamic_batching { preferred_batch_size: [ 2, 6 ], max_queue_delay_microseconds: 10000000 }" >> config.pbtxt)
+for BACKEND in $BACKENDS; do
+    if [ "$BACKEND" != "custom" ]; then
+      cp -r $DATADIR/qa_variable_model_repository/${BACKEND}_float32_float32_float32 var_models/. &&
+        (cd var_models/${BACKEND}_float32_float32_float32 && \
+            sed -i "s/^max_batch_size:.*/max_batch_size: 8/" config.pbtxt && \
+            sed -i "s/^version_policy:.*/version_policy: { specific { versions: [1] }}/" config.pbtxt && \
+            echo "dynamic_batching { preferred_batch_size: [ 2, 6 ], max_queue_delay_microseconds: 10000000 }" >> config.pbtxt)
+    else
+      cp -r ../custom_models/custom_float32_float32_float32 var_models/. &&
+        (cd var_models/custom_float32_float32_float32 && \
+            sed -i "s/^max_batch_size:.*/max_batch_size: 8/" config.pbtxt && \
+            sed -i "s/^version_policy:.*/version_policy: { specific { versions: [1] }}/" config.pbtxt && \
+            echo "dynamic_batching { preferred_batch_size: [ 2, 6 ], max_queue_delay_microseconds: 10000000 }" >> config.pbtxt)
+
+      cp -r ../custom_models/custom_zero_1_float32 var_models/. &&
+        (cd var_models/custom_zero_1_float32 && \
+            sed -i "s/^max_batch_size:.*/max_batch_size: 8/" config.pbtxt && \
+            sed -i "s/^version_policy:.*/version_policy: { specific { versions: [1] }}/" config.pbtxt && \
+            echo "dynamic_batching { preferred_batch_size: [ 2, 6 ], max_queue_delay_microseconds: 10000000 }" >> config.pbtxt)
+    fi
 done
 
 for MC in `ls var_models/*/config.pbtxt`; do
@@ -90,13 +101,13 @@ for MC in `ls var_models/*/config.pbtxt`; do
 done
 
 # Create allow-ragged model to variable-size model repository
-for m in custom_zero_1_float32 ; do
-    (cd var_models/$m && \
-            mkdir -p 1 && cp ../../libidentity.so 1/libcustom.so && \
-            echo "instance_group [ { kind: KIND_CPU count: 1 }]" >> config.pbtxt && \
-            sed -i "s/dims:.*\[.*\]/dims: \[ -1 \]/g" config.pbtxt && \
-            sed -i "s/name:.*\"INPUT0\"/name: \"INPUT0\"\\nallow_ragged_batch: true/" config.pbtxt)
-done
+if [[ $BACKENDS == *"custom"* ]]; then
+    (cd var_models/custom_zero_1_float32 && \
+        mkdir -p 1 && cp ../../libidentity.so 1/libcustom.so && \
+        echo "instance_group [ { kind: KIND_CPU count: 1 }]" >> config.pbtxt && \
+        sed -i "s/dims:.*\[.*\]/dims: \[ -1 \]/g" config.pbtxt && \
+        sed -i "s/name:.*\"INPUT0\"/name: \"INPUT0\"\\nallow_ragged_batch: true/" config.pbtxt)
+fi
 
 # Need to launch the server for each test so that the model status is
 # reset (which is used to make sure the correctly batch size was used
@@ -240,98 +251,99 @@ done
 # by comparing the "response send" timestamps.
 TEST_CASE=test_multi_batch_preserve_ordering
 
-rm -fr ./custom_models && mkdir ./custom_models && \
-    cp -r ../custom_models/custom_zero_1_float32 ./custom_models/. && \
-    mkdir -p ./custom_models/custom_zero_1_float32/1 && \
-    cp ./libidentity.so ./custom_models/custom_zero_1_float32/1/libcustom.so
+if [[ $BACKENDS == *"custom"* ]]; then
+    rm -fr ./custom_models && mkdir ./custom_models && \
+        cp -r ../custom_models/custom_zero_1_float32 ./custom_models/. && \
+        mkdir -p ./custom_models/custom_zero_1_float32/1 && \
+        cp ./libidentity.so ./custom_models/custom_zero_1_float32/1/libcustom.so
 
-# Two instances will be created for the custom model, one delays 100 ms while
-# the other delays 400 ms
-(cd custom_models/custom_zero_1_float32 && \
-        sed -i "s/dims:.*\[.*\]/dims: \[ -1 \]/g" config.pbtxt && \
-        sed -i "s/max_batch_size:.*/max_batch_size: 4/g" config.pbtxt && \
-        echo "dynamic_batching { preferred_batch_size: [ 4 ] }" >> config.pbtxt && \
-        echo "instance_group [ { kind: KIND_CPU count: 2 }]" >> config.pbtxt && \
-        echo "parameters [" >> config.pbtxt && \
-        echo "{ key: \"execute_delay_ms\"; value: { string_value: \"100\" }}," >> config.pbtxt && \
-        echo "{ key: \"instance_wise_delay_multiplier\"; value: { string_value: \"4\" }}" >> config.pbtxt && \
-        echo "]" >> config.pbtxt)
+    # Two instances will be created for the custom model, one delays 100 ms while
+    # the other delays 400 ms
+    (cd custom_models/custom_zero_1_float32 && \
+            sed -i "s/dims:.*\[.*\]/dims: \[ -1 \]/g" config.pbtxt && \
+            sed -i "s/max_batch_size:.*/max_batch_size: 4/g" config.pbtxt && \
+            echo "dynamic_batching { preferred_batch_size: [ 4 ] }" >> config.pbtxt && \
+            echo "instance_group [ { kind: KIND_CPU count: 2 }]" >> config.pbtxt && \
+            echo "parameters [" >> config.pbtxt && \
+            echo "{ key: \"execute_delay_ms\"; value: { string_value: \"100\" }}," >> config.pbtxt && \
+            echo "{ key: \"instance_wise_delay_multiplier\"; value: { string_value: \"4\" }}" >> config.pbtxt && \
+            echo "]" >> config.pbtxt)
 
-# equeue 3 batches to guarantee that a large delay batch will be followed by
-# a small delay one regardless of the order issued to model instances.
-# i.e. the 3 batches will be queued: [1, 2, 3] and there are two delay instances
-# [small, large], then the distributions can be the following:
-# [1:small 2:large 3:small] or [1:large 2:small 3:*] (* depends on whether order
-# is preserved), and we only interested in the timestamps where the large delay
-# batch is followed by small delay batch
-export TRTSERVER_DELAY_SCHEDULER=12
+    # equeue 3 batches to guarantee that a large delay batch will be followed by
+    # a small delay one regardless of the order issued to model instances.
+    # i.e. the 3 batches will be queued: [1, 2, 3] and there are two delay instances
+    # [small, large], then the distributions can be the following:
+    # [1:small 2:large 3:small] or [1:large 2:small 3:*] (* depends on whether order
+    # is preserved), and we only interested in the timestamps where the large delay
+    # batch is followed by small delay batch
+    export TRTSERVER_DELAY_SCHEDULER=12
 
-# not preserve
-SERVER_ARGS="--trace-file=not_preserve.log --trace-level=MIN --trace-rate=1 --model-repository=`pwd`/custom_models"
-SERVER_LOG="./not_preserve.serverlog"
-run_server
-if [ "$SERVER_PID" == "0" ]; then
-    echo -e "\n***\n*** Failed to start $SERVER\n***"
-    cat $SERVER_LOG
-    exit 1
+    # not preserve
+    SERVER_ARGS="--trace-file=not_preserve.log --trace-level=MIN --trace-rate=1 --model-repository=`pwd`/custom_models"
+    SERVER_LOG="./not_preserve.serverlog"
+    run_server
+    if [ "$SERVER_PID" == "0" ]; then
+        echo -e "\n***\n*** Failed to start $SERVER\n***"
+        cat $SERVER_LOG
+        exit 1
+    fi
+
+    echo "Test: not_preserve" >>$CLIENT_LOG
+
+    set +e
+    python $BATCHER_TEST BatcherTest.$TEST_CASE >>$CLIENT_LOG 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "\n***\n*** Test Failed\n***"
+        RET=1
+    fi
+    set -e
+
+    kill $SERVER_PID
+    wait $SERVER_PID
+
+    set +e
+    python $VERIFY_TIMESTAMPS not_preserve.log
+    if [ $? -ne 0 ]; then
+        echo -e "\n***\n*** Test Failed\n***"
+        RET=1
+    fi
+    set -e
+
+    # preserve
+    (cd custom_models/custom_zero_1_float32 && \
+            sed -i "s/dynamic_batching.*/dynamic_batching { preferred_batch_size: [ 4 ] preserve_ordering: true }/g" config.pbtxt)
+
+    SERVER_ARGS="--trace-file=preserve.log --trace-level=MIN --trace-rate=1 --model-repository=`pwd`/custom_models"
+    SERVER_LOG="./preserve.serverlog"
+    run_server
+    if [ "$SERVER_PID" == "0" ]; then
+        echo -e "\n***\n*** Failed to start $SERVER\n***"
+        cat $SERVER_LOG
+        exit 1
+    fi
+
+    echo "Test: preserve" >>$CLIENT_LOG
+
+    set +e
+    python $BATCHER_TEST BatcherTest.$TEST_CASE >>$CLIENT_LOG 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "\n***\n*** Test Failed\n***"
+        RET=1
+    fi
+    set -e
+
+    kill $SERVER_PID
+    wait $SERVER_PID
+
+    set +e
+    python $VERIFY_TIMESTAMPS -p preserve.log
+    if [ $? -ne 0 ]; then
+        echo -e "\n***\n*** Test Failed\n***"
+        RET=1
+    fi
+    set -e
+    unset TRTSERVER_DELAY_SCHEDULER
 fi
-
-echo "Test: not_preserve" >>$CLIENT_LOG
-
-set +e
-python $BATCHER_TEST BatcherTest.$TEST_CASE >>$CLIENT_LOG 2>&1
-if [ $? -ne 0 ]; then
-    echo -e "\n***\n*** Test Failed\n***"
-    RET=1
-fi
-set -e
-
-kill $SERVER_PID
-wait $SERVER_PID
-
-set +e
-python $VERIFY_TIMESTAMPS not_preserve.log
-if [ $? -ne 0 ]; then
-    echo -e "\n***\n*** Test Failed\n***"
-    RET=1
-fi
-set -e
-
-# preserve
-(cd custom_models/custom_zero_1_float32 && \
-        sed -i "s/dynamic_batching.*/dynamic_batching { preferred_batch_size: [ 4 ] preserve_ordering: true }/g" config.pbtxt)
-
-SERVER_ARGS="--trace-file=preserve.log --trace-level=MIN --trace-rate=1 --model-repository=`pwd`/custom_models"
-SERVER_LOG="./preserve.serverlog"
-run_server
-if [ "$SERVER_PID" == "0" ]; then
-    echo -e "\n***\n*** Failed to start $SERVER\n***"
-    cat $SERVER_LOG
-    exit 1
-fi
-
-echo "Test: preserve" >>$CLIENT_LOG
-
-set +e
-python $BATCHER_TEST BatcherTest.$TEST_CASE >>$CLIENT_LOG 2>&1
-if [ $? -ne 0 ]; then
-    echo -e "\n***\n*** Test Failed\n***"
-    RET=1
-fi
-set -e
-
-kill $SERVER_PID
-wait $SERVER_PID
-
-set +e
-python $VERIFY_TIMESTAMPS -p preserve.log
-if [ $? -ne 0 ]; then
-    echo -e "\n***\n*** Test Failed\n***"
-    RET=1
-fi
-set -e
-
-unset TRTSERVER_DELAY_SCHEDULER
 
 # python unittest seems to swallow ImportError and still return 0 exit
 # code. So need to explicitly check CLIENT_LOG to make sure we see
