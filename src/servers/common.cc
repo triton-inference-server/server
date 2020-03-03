c7138a42a461ac8c99357cf93657aed41a930c70
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2020-03-03 12:10:09 -0800

    Add TRTServer options for setting priority and request timeout (#1145)
    * Add TRTServer options for setting priority and request timeout
    
    * Add FIXMEV2 comment for priority / timeout in GRPC v2
    
    * Address comment
    
    * Format

--- src/servers/common.cc
+++ src/servers/common.cc
@@ -115,6 +115,10 @@ SetTRTSERVER_InferenceRequestOptions(
       request_options, request_header.correlation_id()));
   RETURN_IF_ERR(TRTSERVER_InferenceRequestOptionsSetBatchSize(
       request_options, request_header.batch_size()));
+  RETURN_IF_ERR(TRTSERVER_InferenceRequestOptionsSetPriority(
+      request_options, request_header.priority()));
+  RETURN_IF_ERR(TRTSERVER_InferenceRequestOptionsSetTimeoutMicroseconds(
+      request_options, request_header.timeout_microseconds()));
 
   for (const auto& input : request_header.input()) {
     RETURN_IF_ERR(TRTSERVER_InferenceRequestOptionsAddInput(
@@ -146,6 +150,10 @@ SetInferenceRequestOptions(
   // FIXMEV2 parameters
   // RETURN_IF_ERR(TRTSERVER_InferenceRequestOptionsSetFlags(
   //    request_options, request_header.flags()));
+  // RETURN_IF_ERR(TRTSERVER_InferenceRequestOptionsSetPriority(
+  //   request_options, request_header.priority()));
+  // RETURN_IF_ERR(TRTSERVER_InferenceRequestOptionsSetTimeoutMicroseconds(
+  //   request_options, request_header.timeout_microseconds()));
   RETURN_IF_ERR(TRTSERVER_InferenceRequestOptionsSetCorrelationId(
       request_options, request.sequence_id()));
 
