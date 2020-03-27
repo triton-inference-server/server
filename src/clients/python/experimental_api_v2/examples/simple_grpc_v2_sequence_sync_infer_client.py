3fe042b8aade7ad34b9d81ae6914c8c2e22832de
Tanmay Verma <tanmay2592@gmail.com>
2020-03-26 20:50:39 -0700

    Removing unnecessary imports

--- src/clients/python/experimental_api_v2/examples/simple_grpc_v2_sequence_sync_infer_client.py
+++ src/clients/python/experimental_api_v2/examples/simple_grpc_v2_sequence_sync_infer_client.py
@@ -25,7 +25,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from functools import partial
 import argparse
 import numpy as np
 import sys
