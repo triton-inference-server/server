dabe0e673bd0e41046ad947206821d64768c5e1c
CoderHam <hemanham@gmail.com>
2020-03-30 14:05:58 -0700

    cleanup class client

--- src/clients/python/experimental_api_v2/examples/simple_http_v2_class_client.py
+++ src/clients/python/experimental_api_v2/examples/simple_http_v2_class_client.py
@@ -188,13 +188,12 @@ if __name__ == '__main__':
                         type=str,
                         required=True,
                         help='Name of model')
-    parser.add_argument(
-        '-x',
-        '--model-version',
-        type=str,
-        required=False,
-        default="",
-        help='Version of model. Default is to use latest version.')
+    parser.add_argument('-x',
+                        '--model-version',
+                        type=str,
+                        required=False,
+                        default="",
+                        help='Version of model. Default is to use latest version.')
     parser.add_argument('-b',
                         '--batch-size',
                         type=int,
@@ -207,14 +206,13 @@ if __name__ == '__main__':
                         required=False,
                         default=1,
                         help='Number of class results to report. Default is 1.')
-    parser.add_argument(
-        '-s',
-        '--scaling',
-        type=str,
-        choices=['NONE', 'INCEPTION', 'VGG'],
-        required=False,
-        default='NONE',
-        help='Type of scaling to apply to image pixels. Default is NONE.')
+    parser.add_argument('-s',
+                        '--scaling',
+                        type=str,
+                        choices=['NONE', 'INCEPTION', 'VGG'],
+                        required=False,
+                        default='NONE',
+                        help='Type of scaling to apply to image pixels. Default is NONE.')
     parser.add_argument('-u',
                         '--url',
                         type=str,
@@ -234,7 +232,7 @@ if __name__ == '__main__':
     # Make sure the model matches our requirements, and get some
     # properties of the model that we need for preprocessing
     try:
-        model_meta = triton_client.get_model_metadata(model_name=FLAGS.model_name)
+        model_metadata = triton_client.get_model_metadata(model_name=FLAGS.model_name)
     except InferenceServerException as e:
         print("failed to retrieve the metadata: " + str(e))
         sys.exit()
@@ -245,10 +243,8 @@ if __name__ == '__main__':
         print("failed to retrieve the config: " + str(e))
         sys.exit()
 
-    print("model_meta: ", model_meta)
-    print("model_config: ", model_config)
     input_name, output_name, c, h, w, format, dtype = parse_model(
-        model_meta, model_config)
+        model_metadata, model_config)
 
     # Send requests of FLAGS.batch_size images. If the number of
     # images isn't an exact multiple of FLAGS.batch_size then just
@@ -256,20 +252,18 @@ if __name__ == '__main__':
     requests = []
     results = []
 
-    # Send request
-    for inputs, outputs, FLAGS.model_name, FLAGS.model_version in requestGenerator(
-            input_name, output_name, c, h, w, format, dtype, FLAGS):
-        try:
-            results.append(
-                triton_client.infer(inputs,
-                                  FLAGS.model_name,
-                                  outputs=outputs,
-                                  model_version=FLAGS.model_version))
-        except InferenceServerException as e:
-            print("inference failed: " + str(e))
-            sys.exit()
+    try:
+        for inputs, outputs, model_name, model_version in requestGenerator(
+                input_name, output_name, c, h, w, format, dtype, FLAGS):
+            results.append(triton_client.infer(model_name,
+                                            inputs,
+                                            outputs=outputs,
+                                            model_version=model_version))
+    except InferenceServerException as e:
+        print("inference failed: " + str(e))
+        sys.exit()
 
     for result in results:
         postprocess(result, output_name, FLAGS.batch_size)
 
-    print("PASS : Classification ")
+    print("PASS : Classification")
