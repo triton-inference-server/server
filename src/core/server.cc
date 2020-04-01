90415919156687b55d3bef88c91e15ab0c3159af
Tanmay Verma <tanmay2592@gmail.com>
2020-04-01 09:26:17 -0700

    [HTTP V2] Bianry data extension support from client side (#1253)
    * Server side fixes
    
    * Enable Binary data on client side
    
    * Fixing the macro
    
    * Fix comments

--- src/core/server.cc
+++ src/core/server.cc
@@ -94,6 +94,9 @@ InferenceServer::InferenceServer()
   extensions_.push_back("model_configuration");
   extensions_.push_back("system_shared_memory");
   extensions_.push_back("cuda_shared_memory");
+#ifdef TRTIS_ENABLE_HTTP_V2
+  extensions_.push_back("binary_tensor_data");
+#endif
 #ifdef TRTIS_ENABLE_STATS
   extensions_.push_back("statistics");
 #endif
