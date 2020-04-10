487611b78cd5b9b44540b57a2efb78eab084e387
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2020-04-10 10:43:44 -0700

    Add sequence params handling in HTTP v2 (#1289)
    * Add sequence params handling in HTTP v2
    
    * Include simple test for sequence HTTP v2 extension
    
    * Add simple sequence model in HTTP v2 model directory
    
    * Fix QA build
    
    * Fix test. Add workaround
    
    * Update comment

--- src/clients/python/experimental_api_v2/examples/simple_http_v2_model_control.py
+++ src/clients/python/experimental_api_v2/examples/simple_http_v2_model_control.py
@@ -55,8 +55,8 @@ if __name__ == '__main__':
 
     model_name = 'simple'
 
-    # There are four models in the repository directory
-    if len(triton_client.get_model_repository_index()) != 4:
+    # There are five models in the repository directory
+    if len(triton_client.get_model_repository_index()) != 5:
         sys.exit(1)
 
     triton_client.load_model(model_name)
