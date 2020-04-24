8dfa9830e078140c47186608a4ea45db4e1b11e7
Hemant Jain <hemanham@gmail.com>
2020-04-24 14:27:28 -0700

    L0_infer and infer utilities for V2 testing (#1355)
    * x
    
    * add v2 infer utilities for testing
    
    * fix parsing of results to verify success
    
    * Add L0_infer_v2
    
    * Fix inference utilities
    
    * fix typo
    
    * More fixes for V2 api
    
    * Rectify test utility and infer test runner
    
    * In case model_version is passed as None
    
    * fix testing of class outputs
    -Force binary_data=False for http when class output
    
    * undo comments
    
    * Add support for request_id
    
    * Fix failure for test_ensemble_mix_batch_nobatch
    
    * Fix regex for versions in http_server_v2
    
    * remove v2 from naming
    - float16 cannot be passed as json
    
    * enable all tests in L0_infer
    
    * modify server run util for only V2
    
    * address review comments
    
    * add client log check
    
    * use_json -> use_http_json_tensors
    
    * pass entire shape to infer_exact

--- qa/L0_http_v2/test.sh
+++ qa/L0_http_v2/test.sh
@@ -76,7 +76,7 @@ SERVER=/opt/tritonserver/bin/tritonserver
 SERVER_ARGS="--model-repository=$DATADIR --api-version 2"
 source ../common/util.sh
 
-run_server_v2
+run_server
 if [ "$SERVER_PID" == "0" ]; then
     echo -e "\n***\n*** Failed to start $SERVER\n***"
     cat $SERVER_LOG
@@ -146,7 +146,7 @@ kill $SERVER_PID
 wait $SERVER_PID
 
 SERVER_ARGS="--model-repository=$DATADIR --model-control-mode=explicit --api-version 2"
-run_server_v2
+run_server
 if [ "$SERVER_PID" == "0" ]; then
     echo -e "\n***\n*** Failed to start $SERVER\n***"
     cat $SERVER_LOG
@@ -173,7 +173,7 @@ kill $SERVER_PID
 wait $SERVER_PID
 
 SERVER_ARGS="--model-repository=$DATADIR --model-control-mode=explicit --api-version 2"
-run_server_v2
+run_server
 if [ "$SERVER_PID" == "0" ]; then
     echo -e "\n***\n*** Failed to start $SERVER\n***"
     cat $SERVER_LOG
