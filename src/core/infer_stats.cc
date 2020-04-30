031cd08a512bace6b3c7e5ddc4e1f39abc59ebc9
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2020-04-30 16:34:27 -0700

    Fix model statistics. Clean up ServerStatus and related components (#1382)
    * Fix model statistics for V2
    
    * Remove ServerStatus
    
    * Address comment
    
    * Fix build with stats disable. Address comment

---
+++ src/core/infer_stats.cc
@@ -0,0 +1,126 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "src/core/infer_stats.h"
+
+#include <time.h>
+#include "src/core/constants.h"
+#include "src/core/logging.h"
+#include "src/core/metrics.h"
+
+namespace nvidia { namespace inferenceserver {
+
+void
+StatsAggregator::UpdateFailure(
+    const uint64_t request_start_ns, const uint64_t request_end_ns)
+{
+  std::lock_guard<std::mutex> lock(mu_);
+  infer_stats_.failure_count_++;
+  infer_stats_.failure_duration_ns_ += (request_end_ns - request_start_ns);
+
+#ifdef TRTIS_ENABLE_METRICS
+  if (metric_reporter_ != nullptr) {
+    metric_reporter_->MetricInferenceFailure(device).Increment();
+  }
+#endif  // TRTIS_ENABLE_METRICS
+}
+
+void
+StatsAggregator::UpdateSuccess(
+    const uint64_t request_start_ns, const uint64_t queue_start_ns,
+    const uint64_t compute_start_ns, const uint64_t compute_input_end_ns,
+    const uint64_t compute_output_start_ns, const uint64_t compute_end_ns,
+    const uint64_t request_end_ns)
+{
+  std::lock_guard<std::mutex> lock(mu_);
+
+  infer_stats_.success_count_++;
+  infer_stats_.request_duration_ns_ += (request_end_ns - request_start_ns);
+  infer_stats_.queue_duration_ns_ += (compute_start_ns - queue_start_ns);
+  infer_stats_.compute_input_duration_ns_ +=
+      (compute_input_end_ns - compute_start_ns);
+  infer_stats_.compute_infer_duration_ns_ +=
+      (compute_output_start_ns - compute_input_end_ns);
+  infer_stats_.compute_output_duration_ns_ +=
+      (compute_end_ns - compute_output_start_ns);
+
+#ifdef TRTIS_ENABLE_METRICS
+  if (metric_reporter_ != nullptr) {
+    auto compute_duration_ns = compute_input_duration_ns +
+                               compute_infer_duration_ns +
+                               compute_output_duration_ns;
+    metric_reporter_->MetricInferenceSuccess(device).Increment();
+    metric_reporter_->MetricInferenceCount(device).Increment(1);
+
+    metric_reporter_->MetricInferenceRequestDuration(device).Increment(
+        request_duration_ns / 1000);
+    metric_reporter_->MetricInferenceComputeDuration(device).Increment(
+        compute_duration_ns / 1000);
+    metric_reporter_->MetricInferenceQueueDuration(device).Increment(
+        queue_duration_ns / 1000);
+
+    metric_reporter_->MetricInferenceLoadRatio(device).Observe(
+        (double)request_duration_ns /
+        std::max(1.0, (double)compute_duration_ns));
+  }
+#endif  // TRTIS_ENABLE_METRICS
+}
+
+void
+StatsAggregator::UpdateInferBatchStats(
+    size_t batch_size, const uint64_t compute_start_ns,
+    const uint64_t compute_input_end_ns, const uint64_t compute_output_start_ns,
+    const uint64_t compute_end_ns)
+{
+  struct timespec last_ts;
+  clock_gettime(CLOCK_REALTIME, &last_ts);
+  auto inference_ms = TIMESPEC_TO_MILLIS(last_ts);
+
+  std::lock_guard<std::mutex> lock(mu_);
+  if (inference_ms > last_inference_ms_) {
+    last_inference_ms_ = inference_ms;
+  }
+
+  auto it = batch_stats_.find(batch_size);
+  if (it == batch_stats_.end()) {
+    it = batch_stats_.emplace(batch_size, InferBatchStats()).first;
+  }
+  it->second.count_++;
+  it->second.compute_input_duration_ns_ +=
+      (compute_input_end_ns - compute_start_ns);
+  it->second.compute_infer_duration_ns_ +=
+      (compute_output_start_ns - compute_input_end_ns);
+  it->second.compute_output_duration_ns_ +=
+      (compute_end_ns - compute_output_start_ns);
+
+#ifdef TRTIS_ENABLE_METRICS
+  if (metric_reporter_ != nullptr) {
+    metric_reporter_->MetricInferenceExecutionCount(device).Increment(1);
+  }
+#endif  // TRTIS_ENABLE_METRICS
+}
+
+}}  // namespace nvidia::inferenceserver
--- src/servers/grpc_server_v2.cc
+++ src/servers/grpc_server_v2.cc
@@ -716,12 +716,12 @@ CommonHandler::SetUpAllRequests()
     int64_t requested_model_version;
     auto err =
         GetModelVersionFromString(request.version(), &requested_model_version);
+    rapidjson::Document model_stats_json;
     if (err == nullptr) {
       TRITONSERVER_Message* model_stats_message = nullptr;
       err = TRITONSERVER_ServerModelStatistics(
           tritonserver_.get(), request.name().c_str(), requested_model_version,
           &model_stats_message);
-      rapidjson::Document model_stats_json;
       if (err == nullptr) {
         const char* buffer;
         size_t byte_size;
@@ -752,6 +752,8 @@ CommonHandler::SetUpAllRequests()
         const auto& infer_stats_json = model_stat["inference_stats"];
         statistics->set_name(model_stat["name"].GetString());
         statistics->set_version(model_stat["version"].GetString());
+        statistics->set_last_inference(
+            model_stat["last_inference"].GetUint64());
         statistics->mutable_inference_stats()->mutable_success()->set_count(
             infer_stats_json["success"]["count"].GetUint64());
         statistics->mutable_inference_stats()->mutable_success()->set_ns(
@@ -779,7 +781,24 @@ CommonHandler::SetUpAllRequests()
             ->set_count(
                 infer_stats_json["compute_output"]["count"].GetUint64());
         statistics->mutable_inference_stats()->mutable_compute_output()->set_ns(
-            infer_stats_json["compute_output"]["ns"].GetUint64());
+            infer_stats_json["compute_output"]["ns"].GetUInt64());
+
+        for (const auto& batch_stat : model_stat["batch_stats"].GetArray()) {
+          auto batch_statistics = statistics->add_batch_stats();
+          batch_statistics->set_batch_size(batch_stat["batch_size"].GetInt());
+          batch_statistics->mutable_compute_input()->set_count(
+              batch_stat["compute_input"]["count"].GetInt());
+          batch_statistics->mutable_compute_input()->set_ns(
+              batch_stat["compute_input"]["ns"].GetInt());
+          batch_statistics->mutable_compute_infer()->set_count(
+              batch_stat["compute_infer"]["count"].GetInt());
+          batch_statistics->mutable_compute_infer()->set_ns(
+              batch_stat["compute_infer"]["ns"].GetInt());
+          batch_statistics->mutable_compute_output()->set_count(
+              batch_stat["compute_output"]["count"].GetInt());
+          batch_statistics->mutable_compute_output()->set_ns(
+              batch_stat["compute_output"]["ns"].GetInt());
+        }
       }
     }
 #else
