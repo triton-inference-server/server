2aa5e9ac141a4f246eb62dd548f728ef16dbec74
CoderHam <hemanham@gmail.com>
2020-05-03 19:26:39 -0700

    Fix error json response

--- src/servers/http_server_v2.cc
+++ src/servers/http_server_v2.cc
@@ -1067,6 +1067,9 @@ HTTPAPIServerV2::HandleModelMetadata(
   }
 
   if (model_name.empty()) {
+    std::string message_json =
+        "{ \"error\" : \"missing model name in ModelMetadata request\" }";
+    evbuffer_add(req->buffer_out, message_json.c_str(), message_json.size());
     evhtp_send_reply(req, EVHTP_RES_BADREQ);
     return;
   }
@@ -1107,6 +1110,9 @@ HTTPAPIServerV2::HandleModelConfig(
   }
 
   if (model_name.empty()) {
+    std::string message_json =
+        "{ \"error\" : \"missing model name in ModelConfig request\" }";
+    evbuffer_add(req->buffer_out, message_json.c_str(), message_json.size());
     evhtp_send_reply(req, EVHTP_RES_BADREQ);
     return;
   }
@@ -1990,13 +1996,13 @@ HTTPAPIServerV2::HandleInfer(
   if (err != nullptr) {
     LOG_VERBOSE(1) << "Infer failed: " << TRITONSERVER_ErrorMessage(err);
     EVBufferAddErrorJson(req->buffer_out, err);
+    evhtp_send_reply(req, EVHTP_RES_BADREQ);
     TRITONSERVER_ErrorDelete(err);
 
     LOG_TRITONSERVER_ERROR(
         TRITONSERVER_InferenceRequestDelete(irequest),
         "deleting inference request");
 
-    evhtp_send_reply(req, EVHTP_RES_BADREQ);
   }
 }
 
