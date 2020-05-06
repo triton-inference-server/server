2be1de4cc7d27b7621d0463cc37027e7dd6f2e7f
Tanmay Verma <tanmay2592@gmail.com>
2020-05-06 16:42:49 -0700

    perf_client using V2 API (#1388)
    * Implementing model parser to get input details
    
    * Simple synchronous inference support
    
    * Enabling all DataLoader options
    
    * JSON data support
    
    * Enabling haredMemory support
    
    * Asynchronous inference support
    
    * streaming and sequence support
    
    * Fixing the documentation
    
    * Fixing copyrights
    
    * Adding the RequestRateMAnager and CustomLoadManager
    
    * Fixing the callback status
    
    * Fixing the shape validation in a batch
    
    * Fix custom shapes for shared memory
    
    * Fix free context id list
    
    * Fixing the dynamic shapes for HTTP protocol
    
    * Using constant ref as return type

---
+++ src/clients/c++/experimental_api_v2/perf_client/concurrency_manager.h
@@ -0,0 +1,127 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include "src/clients/c++/experimental_api_v2/perf_client/load_manager.h"
+
+//==============================================================================
+/// ConcurrencyManager is a helper class to send inference requests to inference
+/// server consistently, based on the specified setting, so that the perf_client
+/// can measure performance under different concurrency.
+///
+/// An instance of concurrency manager will be created at the beginning of the
+/// perf client and it will be used to simulate different load level in respect
+/// to number of concurrent infer requests and to collect per-request statistic.
+///
+/// Detail:
+/// Concurrency Manager will maintain the number of concurrent requests by
+/// spawning worker threads that keep sending randomly generated requests to the
+/// server. The worker threads will record the start time and end
+/// time of each request into a shared vector.
+///
+class ConcurrencyManager : public LoadManager {
+ public:
+  ~ConcurrencyManager();
+
+  /// Create a concurrency manager that is responsible to maintain specified
+  /// load on inference server.
+  /// \param async Whether to use asynchronous or synchronous API for infer
+  /// request.
+  /// \param streaming Whether to use gRPC streaming API for infer request
+  /// \param batch_size The batch size used for each request.
+  /// \param max_threads The maximum number of working threads to be spawned.
+  /// \param max_concurrency The maximum concurrency which will be requested.
+  /// \param sequence_length The base length of each sequence.
+  /// \param string_length The length of the string to create for input.
+  /// \param string_data The data to use for generating string input.
+  /// \param zero_input Whether to fill the input tensors with zero.
+  /// \param user_data The vector containing path/paths to user-provided data
+  /// that can be a directory or path to a json data file.
+  /// \param shared_memory_type The type of shared memory to use for inputs.
+  /// \param output_shm_size The size in bytes of the shared memory to
+  /// allocate for the output.
+  /// \param parser The ModelParser object to get the model details.
+  /// \param factory The TritonClientFactory object used to create
+  /// client to the server.
+  /// \param manager Returns a new ConcurrencyManager object.
+  /// \return Error object indicating success or failure.
+  static nic::Error Create(
+      const bool async, const bool streaming, const int32_t batch_size,
+      const size_t max_threads, const size_t max_concurrency,
+      const size_t sequence_length, const size_t string_length,
+      const std::string& string_data, const bool zero_input,
+      std::vector<std::string>& user_data,
+      const SharedMemoryType shared_memory_type, const size_t output_shm_size,
+      const std::shared_ptr<ModelParser>& parser,
+      const std::shared_ptr<TritonClientFactory>& factory,
+      std::unique_ptr<LoadManager>* manager);
+
+  /// Adjusts the number of concurrent requests to be the same as
+  /// 'concurrent_request_count' (by creating or pausing threads)
+  /// \param concurent_request_count The number of concurrent requests.
+  /// \return Error object indicating success or failure.
+  nic::Error ChangeConcurrencyLevel(const size_t concurrent_request_count);
+
+ private:
+  ConcurrencyManager(
+      const bool async, const bool streaming, const int32_t batch_size,
+      const size_t max_threads, const size_t max_concurrency,
+      const size_t sequence_length, const SharedMemoryType shared_memory_type,
+      const size_t output_shm_size, const std::shared_ptr<ModelParser>& parser,
+      const std::shared_ptr<TritonClientFactory>& factory);
+
+  struct ThreadConfig {
+    ThreadConfig(size_t thread_id)
+        : thread_id_(thread_id), concurrency_(0),
+          non_sequence_data_step_id_(thread_id), is_paused_(false)
+    {
+    }
+
+    // ID of corresponding worker thread
+    size_t thread_id_;
+    // The concurrency level that the worker should produce
+    size_t concurrency_;
+    // The current data step id in case of non-sequence model
+    size_t non_sequence_data_step_id_;
+    // Whether or not the thread is issuing new inference requests
+    bool is_paused_;
+  };
+
+  /// Function for worker that sends inference requests.
+  /// \param thread_stat Worker thread status specific data.
+  /// \param thread_config Worker thread configuration specific data.
+  void Infer(
+      std::shared_ptr<ThreadStat> thread_stat,
+      std::shared_ptr<ThreadConfig> thread_config);
+
+  // The number of worker threads with non-zero concurrencies
+  size_t active_threads_;
+
+  bool execute_;
+
+  size_t max_concurrency_;
+  std::vector<std::shared_ptr<ThreadConfig>> threads_config_;
+};
---
+++ src/clients/c++/experimental_api_v2/perf_client/perf_utils.h
@@ -0,0 +1,149 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <time.h>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <random>
+
+#include <rapidjson/document.h>
+#include <sys/stat.h>
+#include "rapidjson/rapidjson.h"
+#include "src/clients/c++/experimental_api_v2/library/grpc_client.h"
+#include "src/clients/c++/experimental_api_v2/library/http_client.h"
+#include "src/core/constants.h"
+
+namespace ni = nvidia::inferenceserver;
+namespace nic = nvidia::inferenceserver::client;
+
+using TimestampVector =
+    std::vector<std::tuple<struct timespec, struct timespec, uint32_t, bool>>;
+
+// Will use the characters specified here to construct random strings
+std::string const character_set =
+    "abcdefghijklmnaoqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 .?!";
+
+// A boolean flag to mark an interrupt and commencement of early exit
+extern volatile bool early_exit;
+
+
+#define RETURN_IF_ERROR(S)            \
+  do {                                \
+    const nic::Error& status__ = (S); \
+    if (!status__.IsOk()) {           \
+      return status__;                \
+    }                                 \
+  } while (false)
+
+#define FAIL_IF_ERR(X, MSG)                                        \
+  {                                                                \
+    nic::Error err = (X);                                          \
+    if (!err.IsOk()) {                                             \
+      std::cerr << "error: " << (MSG) << ": " << err << std::endl; \
+      exit(1);                                                     \
+    }                                                              \
+  }
+
+enum ProtocolType { HTTP = 0, GRPC = 1, UNKNOWN = 2 };
+
+enum Distribution { POISSON = 0, CONSTANT = 1, CUSTOM = 2 };
+enum SearchMode { LINEAR = 0, BINARY = 1, NONE = 2 };
+enum SharedMemoryType {
+  SYSTEM_SHARED_MEMORY = 0,
+  CUDA_SHARED_MEMORY = 1,
+  NO_SHARED_MEMORY = 2
+};
+
+constexpr uint64_t NO_LIMIT = 0;
+
+// Parse the communication protocol type
+ProtocolType ParseProtocol(const std::string& str);
+
+// Reads the data from file specified by path into vector of characters
+// \param path The complete path to the file to be read
+// \param contents The character vector that will contain the data read
+// \return error status. Returns Non-Ok if an error is encountered during
+//  read operation.
+nic::Error ReadFile(const std::string& path, std::vector<char>* contents);
+
+// Reads the string from file specified by path into vector of strings
+// \param path The complete path to the file to be read
+// \param contents The string vector that will contain the data read
+// \return error status. Returns Non-Ok if an error is encountered during
+//  read operation.
+nic::Error ReadTextFile(
+    const std::string& path, std::vector<std::string>* contents);
+
+// Reads the time intervals in microseconds from file specified by path into
+// vector of time intervals in nanoseconds.
+// \param path The complete path to the file to be read
+// \param contents The time interval vector that will contain the data read.
+// \return error status. Returns Non-Ok if an error is encountered during
+//  read operation.
+nic::Error ReadTimeIntervalsFile(
+    const std::string& path, std::vector<std::chrono::nanoseconds>* contents);
+
+// To check whether the path points to a valid system directory
+bool IsDirectory(const std::string& path);
+
+// To check whether the path points to a valid system file
+bool IsFile(const std::string& complete_path);
+
+// Calculates the byte size tensor for given shape and datatype.
+int64_t ByteSize(
+    const std::vector<int64_t>& shape, const std::string& datatype);
+
+// Get the number of elements in the tensor for given shape.
+int64_t ElementCount(const std::vector<int64_t>& shape);
+
+// Serializes the string tensor to length prepended bytes.
+void SerializeStringTensor(
+    std::vector<std::string> string_tensor, std::vector<char>* serialized_data);
+
+// Serializes an explicit tensor read from the data file to the
+// raw bytes.
+nic::Error SerializeExplicitTensor(
+    const rapidjson::Value& tensor, const std::string& dt,
+    std::vector<char>* decoded_data);
+
+// Generates a random string of specified length using characters specified in
+// character_set.
+std::string GetRandomString(const int string_length);
+
+// Returns the shape string containing the values provided in the vector
+std::string ShapeVecToString(
+    const std::vector<int64_t> shape_vec, bool skip_first = false);
+
+// Returns the string containing the shape tensor values
+std::string ShapeTensorValuesToString(const int* data_ptr, const int count);
+
+// Returns the request schedule distribution generator with the specified
+// request rate.
+template <Distribution distribution>
+std::function<std::chrono::nanoseconds(std::mt19937&)> ScheduleDistribution(
+    const double request_rate);
