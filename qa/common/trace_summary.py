7b6cd313937f08c5976cb9fb1240eafffcc9eb26
Tanmay Verma <tanmay2592@gmail.com>
2020-06-03 11:12:15 -0700

    Fixing the L0_cmdline_trace (#1599)

--- qa/common/trace_summary.py
+++ qa/common/trace_summary.py
@@ -32,15 +32,19 @@ import sys
 
 FLAGS = None
 
+
 def add_span(span_map, timestamps, span_name, ts_start, ts_end):
     for tag in (ts_start, ts_end):
         if tag not in timestamps:
-            raise ValueError('timestamps missing "{}": {}'.format(tag, timestamps))
+            raise ValueError('timestamps missing "{}": {}'.format(
+                tag, timestamps))
     if timestamps[ts_end] < timestamps[ts_start]:
-        raise ValueError('end timestamp "{}" < start timestamp "{}"'.format(ts_end, ts_start))
+        raise ValueError('end timestamp "{}" < start timestamp "{}"'.format(
+            ts_end, ts_start))
     if span_name not in span_map:
         span_map[span_name] = 0
     span_map[span_name] += timestamps[ts_end] - timestamps[ts_start]
+
 
 class AbstractFrontend():
     @property
@@ -53,67 +57,68 @@ class AbstractFrontend():
     def summarize_frontend_span(self, span_map, cnt):
         return None
 
+
 class HttpFrontend(AbstractFrontend):
     @property
     def filter_timestamp(self):
-        return "http recv start"
+        return "HTTP_RECV_START"
 
     def add_frontend_span(self, span_map, timestamps):
-        if ("http recv start" in timestamps) and ("http send end" in timestamps):
-            add_span(span_map, timestamps,
-                     "http infer", "http recv start", "http send end")
-            add_span(span_map, timestamps,
-                     "http recv", "http recv start", "http recv end")
-            add_span(span_map, timestamps,
-                     "http send", "http send start", "http send end")
+        if ("HTTP_RECV_START" in timestamps) and (
+                "HTTP_SEND_END" in timestamps):
+            add_span(span_map, timestamps, "HTTP_INFER", "HTTP_RECV_START",
+                     "HTTP_SEND_END")
+            add_span(span_map, timestamps, "HTTP_RECV", "HTTP_RECV_START",
+                     "HTTP_RECV_END")
+            add_span(span_map, timestamps, "HTTP_SEND", "HTTP_SEND_START",
+                     "HTTP_SEND_END")
 
     def summarize_frontend_span(self, span_map, cnt):
-        if "http infer" in span_map:
+        if "HTTP_INFER" in span_map:
             res = "HTTP infer request (avg): {}us\n".format(
-                    span_map["http infer"] / (cnt * 1000))
-            res += "\tReceive (avg): {}us\n".format(
-                    span_map["http recv"] / (cnt * 1000))
-            res += "\tSend (avg): {}us\n".format(
-                    span_map["http send"] / (cnt * 1000))
+                span_map["HTTP_INFER"] / (cnt * 1000))
+            res += "\tReceive (avg): {}us\n".format(span_map["HTTP_RECV"] /
+                                                    (cnt * 1000))
+            res += "\tSend (avg): {}us\n".format(span_map["HTTP_SEND"] /
+                                                 (cnt * 1000))
             res += "\tOverhead (avg): {}us\n".format(
-                    (span_map["http infer"] -
-                     span_map["request handler"] -
-                     span_map["http recv"] -
-                     span_map["http send"]) / (cnt * 1000))
+                (span_map["HTTP_INFER"] - span_map["HTTP_RECV"] -
+                 span_map["HTTP_SEND"]) / (cnt * 1000))
             return res
         else:
             return None
+
 
 class GrpcFrontend(AbstractFrontend):
     @property
     def filter_timestamp(self):
-        return "grpc wait/read start"
+        return "GRPC_WAITREAD_START"
 
     def add_frontend_span(self, span_map, timestamps):
-        if ("grpc wait/read start" in timestamps) and ("grpc send end" in timestamps):
-            add_span(span_map, timestamps,
-                     "grpc infer", "grpc wait/read start", "grpc send end")
-            add_span(span_map, timestamps,
-                     "grpc wait/read", "grpc wait/read start", "grpc wait/read end")
-            add_span(span_map, timestamps,
-                     "grpc send", "grpc send start", "grpc send end")
+        if ("GRPC_WAITREAD_START" in timestamps) and (
+                "GRPC_SEND_END" in timestamps):
+            add_span(span_map, timestamps, "GRPC_INFER", "GRPC_WAITREAD_START",
+                     "GRPC_SEND_END")
+            add_span(span_map, timestamps, "GRPC_WAITREAD",
+                     "GRPC_WAITREAD_START", "GRPC_WAITREAD_END")
+            add_span(span_map, timestamps, "GRPC_SEND", "GRPC_SEND_START",
+                     "GRPC_SEND_END")
 
     def summarize_frontend_span(self, span_map, cnt):
-        if "grpc infer" in span_map:
+        if "GRPC_INFER" in span_map:
             res = "GRPC infer request (avg): {}us\n".format(
-                    span_map["grpc infer"] / (cnt * 1000))
+                span_map["GRPC_INFER"] / (cnt * 1000))
             res += "\tWait/Read (avg): {}us\n".format(
-                    span_map["grpc wait/read"] / (cnt * 1000))
-            res += "\tSend (avg): {}us\n".format(
-                    span_map["grpc send"] / (cnt * 1000))
+                span_map["GRPC_WAITREAD"] / (cnt * 1000))
+            res += "\tSend (avg): {}us\n".format(span_map["GRPC_SEND"] /
+                                                 (cnt * 1000))
             res += "\tOverhead (avg): {}us\n".format(
-                    (span_map["grpc infer"] -
-                     span_map["request handler"] -
-                     span_map["grpc wait/read"] -
-                     span_map["grpc send"]) / (cnt * 1000))
+                (span_map["GRPC_INFER"] - span_map["GRPC_WAITREAD"] -
+                 span_map["GRPC_SEND"]) / (cnt * 1000))
             return res
         else:
             return None
+
 
 def summarize(frontend, traces):
     # map from (model_name, model_version) to # of traces
@@ -126,29 +131,52 @@ def summarize(frontend, traces):
 
     # Filter the trace that is not for the requested frontend
     match_frontend_id_set = set()
-    filtered_traces = []
+    # Filter the trace that is not meaningful and group them by 'id'
+    filtered_traces = dict()
     for trace in traces:
         if "id" not in trace:
             continue
+
         # Trace without a parent must contain frontend timestamps
+        add_trace = False
         if "parent_id" not in trace:
             if frontend.filter_timestamp is None:
                 continue
-            for ts in trace["timestamps"]:
-                if frontend.filter_timestamp in ts["name"]:
-                    match_frontend_id_set.add(trace["id"])
-                    filtered_traces.append(trace)
+            if "timestamps" in trace:
+                for ts in trace["timestamps"]:
+                    if frontend.filter_timestamp in ts["name"]:
+                        match_frontend_id_set.add(trace["id"])
+            if trace["id"] in match_frontend_id_set:
+                add_trace = True
         # Otherwise need to check whether parent is filtered
         elif trace["parent_id"] in match_frontend_id_set:
             match_frontend_id_set.add(trace["id"])
-            filtered_traces.append(trace)
+            add_trace = True
 
-    for trace in filtered_traces:
+        if add_trace:
+            if (trace['id'] in filtered_traces.keys()):
+                rep_trace = filtered_traces[trace['id']]
+                # Apend the timestamp to the trace representing this 'id'
+                if "model_name" in trace:
+                    rep_trace["model_name"] = trace["model_name"]
+                if "model_version" in trace:
+                    rep_trace["model_version"] = trace["model_version"]
+                if "timestamps" in trace:
+                    rep_trace["timestamps"] += trace["timestamps"]
+            else:
+                # Use this trace to represent this 'id'
+                if "timestamps" not in trace:
+                    trace["timestamps"] = []
+                filtered_traces[trace['id']] = trace
+
+    for trace_id, trace in filtered_traces.items():
+        if trace_id not in match_frontend_id_set:
+            filtered_traces.pop(trace_id, None)
+            continue
         timestamps = dict()
         for ts in trace["timestamps"]:
             timestamps[ts["name"]] = ts["ns"]
-
-        if ("request handler start" in timestamps) and ("request handler end" in timestamps):
+        if ("REQUEST_START" in timestamps) and ("REQUEST_END" in timestamps):
             key = (trace["model_name"], trace["model_version"])
             if key not in model_count_map:
                 model_count_map[key] = 0
@@ -158,26 +186,29 @@ def summarize(frontend, traces):
 
             frontend.add_frontend_span(model_span_map[key], timestamps)
 
-            add_span(model_span_map[key], timestamps,
-                     "request handler", "request handler start", "request handler end")
-            
-            # The tags below will be missing for ensemble model
-            if ("queue start" in timestamps) and ("compute start" in timestamps):
-                add_span(model_span_map[key], timestamps,
-                        "queue", "queue start", "compute start")
-            if ("compute start" in timestamps) and ("compute end" in timestamps):
-                add_span(model_span_map[key], timestamps,
-                        "compute", "compute start", "compute end")
-            if ("compute input end" in timestamps) and ("compute output start" in timestamps):
-                add_span(model_span_map[key], timestamps,
-                         "compute input", "compute start", "compute input end")
-                add_span(model_span_map[key], timestamps,
-                         "compute infer", "compute input end", "compute output start")
-                add_span(model_span_map[key], timestamps,
-                         "compute output", "compute output start", "compute end")
+            add_span(model_span_map[key], timestamps, "REQUEST",
+                     "REQUEST_START", "REQUEST_END")
 
+            # The tags below will be missing for ensemble model
+            if ("QUEUE_START" in timestamps) and (
+                    "COMPUTE_START" in timestamps):
+                add_span(model_span_map[key], timestamps, "QUEUE",
+                         "QUEUE_START", "COMPUTE_START")
+            if ("COMPUTE_START" in timestamps) and (
+                    "COMPUTE_END" in timestamps):
+                add_span(model_span_map[key], timestamps, "COMPUTE",
+                         "COMPUTE_START", "COMPUTE_END")
+            if ("COMPUTE_INPUT_END" in timestamps) and (
+                    "COMPUTE_OUTPUT_START" in timestamps):
+                add_span(model_span_map[key], timestamps, "COMPUTE_INPUT",
+                         "COMPUTE_START", "COMPUTE_INPUT_END")
+                add_span(model_span_map[key], timestamps, "COMPUTE_INFER",
+                         "COMPUTE_INPUT_END", "COMPUTE_OUTPUT_START")
+                add_span(model_span_map[key], timestamps, "COMPUTE_OUTPUT",
+                         "COMPUTE_OUTPUT_START", "COMPUTE_END")
             if FLAGS.show_trace:
-                print("{} ({}):".format(trace["model_name"], trace["model_version"]))
+                print("{} ({}):".format(trace["model_name"],
+                                        trace["model_version"]))
                 print("\tid: {}".format(trace["id"]))
                 if "parent_id" in trace:
                     print("\tparent id: {}".format(trace["parent_id"]))
@@ -195,37 +226,48 @@ def summarize(frontend, traces):
 
     for key, cnt in model_count_map.items():
         model_name, model_value = key
-        print("Summary for {} ({}): trace count = {}".format(model_name, model_value, cnt))
+        print("Summary for {} ({}): trace count = {}".format(
+            model_name, model_value, cnt))
 
-        frontend_summary = frontend.summarize_frontend_span(model_span_map[key], cnt)
+        frontend_summary = frontend.summarize_frontend_span(
+            model_span_map[key], cnt)
         if frontend_summary is not None:
             print(frontend_summary)
 
-        print("\tHandler (avg): {}us".format(
-            model_span_map[key]["request handler"] / (cnt * 1000)))
-        if ("queue" in model_span_map[key]) and "compute" in model_span_map[key]:
+        print("\tHandler (avg): {}us".format(model_span_map[key]["REQUEST"] /
+                                             (cnt * 1000)))
+        if ("QUEUE" in model_span_map[key]
+           ) and "COMPUTE" in model_span_map[key]:
             print("\t\tOverhead (avg): {}us".format(
-                (model_span_map[key]["request handler"] -
-                model_span_map[key]["queue"] -
-                model_span_map[key]["compute"]) / (cnt * 1000)))
-            print("\t\tQueue (avg): {}us".format(
-                model_span_map[key]["queue"] / (cnt * 1000)))
+                (model_span_map[key]["REQUEST"] - model_span_map[key]["QUEUE"] -
+                 model_span_map[key]["COMPUTE"]) / (cnt * 1000)))
+            print("\t\tQueue (avg): {}us".format(model_span_map[key]["QUEUE"] /
+                                                 (cnt * 1000)))
             print("\t\tCompute (avg): {}us".format(
-                model_span_map[key]["compute"] / (cnt * 1000)))
-        if ("compute input" in model_span_map[key]) and "compute output" in model_span_map[key]:
+                model_span_map[key]["COMPUTE"] / (cnt * 1000)))
+        if ("COMPUTE_INPUT" in model_span_map[key]
+           ) and "COMPUTE_OUTPUT" in model_span_map[key]:
             print("\t\t\tInput (avg): {}us".format(
-                model_span_map[key]["compute input"] / (cnt * 1000)))
+                model_span_map[key]["COMPUTE_INPUT"] / (cnt * 1000)))
             print("\t\t\tInfer (avg): {}us".format(
-                model_span_map[key]["compute infer"] / (cnt * 1000)))
+                model_span_map[key]["COMPUTE_INFER"] / (cnt * 1000)))
             print("\t\t\tOutput (avg): {}us".format(
-                model_span_map[key]["compute output"] / (cnt * 1000)))
+                model_span_map[key]["COMPUTE_OUTPUT"] / (cnt * 1000)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', action="store_true", required=False, default=False,
+    parser.add_argument('-v',
+                        '--verbose',
+                        action="store_true",
+                        required=False,
+                        default=False,
                         help='Enable verbose output')
-    parser.add_argument('-t', '--show-trace', action="store_true", required=False, default=False,
+    parser.add_argument('-t',
+                        '--show-trace',
+                        action="store_true",
+                        required=False,
+                        default=False,
                         help='Show timestamps for each individual trace')
     parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
     FLAGS = parser.parse_args()
