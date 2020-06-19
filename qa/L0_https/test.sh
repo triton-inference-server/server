288080d47210c8b634d4e006d4954988a5a48e17
Tanmay Verma <tanmay2592@gmail.com>
2020-06-18 20:33:45 -0700

    Enabling HTTPS in python HTTP client library (#1684)
    * Enabling HTTPS in python HTTP client library
    
    * Stop the proxy server when done

---
+++ qa/L0_https/test.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REPO_VERSION=${NVIDIA_TRITON_SERVER_VERSION}
+if [ "$#" -ge 1 ]; then
+    REPO_VERSION=$1
+fi
+if [ -z "$REPO_VERSION" ]; then
+    echo -e "Repository version must be specified"
+    echo -e "\n***\n*** Test Failed\n***"
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=0
+
+RET=0
+
+SIMPLE_INFER_CLIENT_PY=../clients/simple_http_infer_client.py
+
+NGINX_CONF=`pwd`/nginx.conf
+CLIENT_LOG=`pwd`/client.log
+DATADIR=`pwd`/models
+SERVER=/opt/tritonserver/bin/tritonserver
+SERVER_ARGS="--model-repository=$DATADIR"
+source ../common/util.sh
+
+
+# Generate a passphrase
+openssl rand -base64 48 > passphrase.txt
+
+# Generate a Private Key
+openssl genrsa -aes128 -passout file:passphrase.txt -out server.key 2048
+
+# Generate a CSR (Certificate Signing Request)
+openssl req -new -passin file:passphrase.txt -key server.key -out server.csr \
+    -subj "/C=FR/O=krkr/OU=Domain Control Validated/CN=*.krkr.io"
+
+# Remove Passphrase from Key
+cp server.key server.key.org
+openssl rsa -in server.key.org -passin file:passphrase.txt -out server.key
+
+# Generating a Self-Signed Certificate for 100 years
+openssl x509 -req -days 36500 -in server.csr -signkey server.key -out server.crt
+
+mv server.crt /etc/nginx/cert.crt
+mv server.key /etc/nginx/cert.key
+
+run_server
+if [ "$SERVER_PID" == "0" ]; then
+    echo -e "\n***\n*** Failed to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+# Setup the new configuration for the proxy. The HTTPS traffic will be
+# redirected to the running instance of server at localhost:8000
+cp ${NGINX_CONF} /etc/nginx/sites-available/default
+
+# Start the proxy server
+service nginx restart
+
+set +e
+
+# Test basic inference with https
+python $SIMPLE_INFER_CLIENT_PY -v -u localhost --ssl >> ${CLIENT_LOG}.ssl_infer 2>&1
+if [ $? -ne 0 ]; then
+    cat ${CLIENT_LOG}.ssl_infer
+    RET=1
+fi
+
+set -e
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+# Stop the proxy server
+service nginx stop
+
+if [ $RET -eq 0 ]; then
+    echo -e "\n***\n*** Test Passed\n***"
+else
+    echo -e "\n***\n*** Test FAILED\n***"
+fi
+
+exit $RET
