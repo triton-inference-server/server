a18b33c3d9ba37abe28ba3e34f4351bf4661a8da
CoderHam <hemanham@gmail.com>
2020-06-26 14:42:18 -0700

    improve fuzz test to include mutations of urls

--- qa/L0_http_fuzz/fuzztest.py
+++ qa/L0_http_fuzz/fuzztest.py
@@ -32,26 +32,17 @@ import os
 
 
 class FuzzTest(unittest.TestCase):
-    def _run_fuzz(self):
+    def _run_fuzz(self, url):
         session = Session(target=Target(
             connection=TCPSocketConnection("127.0.0.1", 8000)),
             keep_web_open=False)
 
-        s_initialize(name="Request")
+        s_initialize(name="Request"+url)
         with s_block("Request-Line"):
             s_group("Method", ["GET", "HEAD", "POST", "PUT",
                                "DELETE", "CONNECT", "OPTIONS", "TRACE"])
             s_delim(" ", name="space-1")
-            s_group("Request-URI", ["/v2/models/simple", "/v2/models/simple/infer",
-                                    "/v2/models/simple/versions/v1", "/v2/models/simple/versions/v1",
-                                    "/v2/models/simple/config", "/v2/models/simple/stats",
-                                    "/v2/health/ready", "/v2/health/live",
-                                    "/v2/repository/index", "/v2/repository/models/simple/unload",
-                                    "/v2/repository/models/simple/load",
-                                    "/v2/systemsharedmemory/status", "/v2/systemsharedmemory/register",
-                                    "/v2/systemsharedmemory/unregister", "/v2/systemsharedmemory/region/xx/status",
-                                    "/v2/cudasharedmemory/status", "/v2/cudasharedmemory/register",
-                                    "/v2/cudasharedmemory/unregister", "/v2/cudasharedmemory/region/xx/status"])
+            s_string(url, name="Request-URI")
             s_delim(" ", name="space-2")
             s_string("HTTP/1.1", name="HTTP-Version")
             s_static("\r\n", name="Request-Line-CRLF")
@@ -60,22 +51,34 @@ class FuzzTest(unittest.TestCase):
             s_static("\r\n", name="Host-Line-CRLF")
         s_static("\r\n", "Request-CRLF")
 
-        session.connect(s_get("Request"))
+        session.connect(s_get("Request"+url))
         session.fuzz()
 
     def test_failures_from_db(self):
-        self._run_fuzz()
+        url_list = ["/v2", "/v2/models/simple", "/v2/models/simple/infer",
+                    "/v2/models/simple/versions/v1", "/v2/models/simple/versions/v1",
+                    "/v2/models/simple/config", "/v2/models/simple/stats",
+                    "/v2/health/ready", "/v2/health/live",
+                    "/v2/repository/index", "/v2/repository/models/simple/unload",
+                    "/v2/repository/models/simple/load", 
+                    "/v2/systemsharedmemory/status", "/v2/systemsharedmemory/register",
+                    "/v2/systemsharedmemory/unregister", "/v2/systemsharedmemory/region/xx/status",
+                    "/v2/cudasharedmemory/status", "/v2/cudasharedmemory/register",
+                    "/v2/cudasharedmemory/unregister", "/v2/cudasharedmemory/region/xx/status"]
 
-        # Get latest db file
-        files = glob.glob('boofuzz-results/*')
-        dbfile = max(files, key=os.path.getctime)
+        for url in url_list:
+            self._run_fuzz(url)
 
-        conn = sqlite3.connect(dbfile)
-        c = conn.cursor()
+            # Get latest db file
+            files = glob.glob('boofuzz-results/*')
+            dbfile = max(files, key=os.path.getctime)
 
-        # Get number of failures, should be 0
-        self.assertEqual(len([x for x in c.execute(
-            "SELECT * FROM steps WHERE type=\"fail\"")]), 0)
+            conn = sqlite3.connect(dbfile)
+            c = conn.cursor()
+
+            # Get number of failures, should be 0
+            self.assertEqual(len([x for x in c.execute(
+                "SELECT * FROM steps WHERE type=\"fail\"")]), 0)
 
 
 if __name__ == "__main__":
