c3074e1e0554abaca38bae2e2e2cf32a8ddb7eca
CoderHam <hemanham@gmail.com>
2020-07-31 14:44:18 -0700

    Add Resnet50, Deeprecommender, Kaldi

--- qa/L0_kibana_summary/perf_summary.py
+++ qa/L0_kibana_summary/perf_summary.py
@@ -8,16 +8,44 @@ from email.mime.text import MIMEText
 import base64
 import glob
 
+
+def send_email(subject, html, suffix):
+    FROM = "hemantj@nvidia.com"
+    TO = 'sw-dl-triton@exchange.nvidia.com'
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = FROM
+    msg['To'] = TO
+    msg.attach(MIMEText(html, "html"))
+
+    for filename in glob.glob(suffix + "*.csv"):
+        attachment = open(filename, "rb")
+        p = MIMEBase('application', 'octet-stream')
+        p.set_payload((attachment).read())
+        encoders.encode_base64(p)
+        p.add_header('Content-Disposition',
+                     "attachment; filename= %s" % filename)
+        msg.attach(p)
+
+    mailServer = smtplib.SMTP("mailgw.nvidia.com")
+    mailServer.sendmail(FROM, TO, msg.as_string())
+    mailServer.quit()
+
+
+# NoModel
 html = '<html><body><a href=\"https://gpuwa.nvidia.com/kibana/app/kibana#/dashboard/ff9a1030-9a1c-11ea-8edb-c5a5e5f9de0d\">Nomodel Kibana Dashboard</a><br><center>'
 today = date.today().strftime("%Y-%m-%d")
-# pdf.set_font('Arial', size=12)
 for metric in ["latency", "throughput"]:
     for payload_size in ["1", "4194304"]:
         for protocol in ["grpc", "http"]:
-            instances = "2" if metric == "throughput" else "1"
+            if metric == "throughput":
+                instances = "2"
+                values = ["d_infer_per_sec", "s_framework", "\'@timestamp\'"]
+            else:
+                instances = "1"
+                values = ["d_latency_p95_ms", "s_framework", "\'@timestamp\'"]
             payload_label = "4B" if payload_size == "1" else "16MB"
 
-            value_list = ["d_infer_per_sec", "s_framework", "\'@timestamp\'"]
             where_dict = {
                 "s_shared_memory": "none",
                 "s_benchmark_name": "nomodel",
@@ -29,7 +57,7 @@ for metric in ["latency", "throughput"]:
             title = "Nomodel " + protocol.upper() + " " + metric + " with " + \
                 payload_label + " payload"
             ma_df = sk.current_moving_average_dataframe(metric,
-                                                        value_list,
+                                                        values,
                                                         where_dict,
                                                         today,
                                                         plot=True,
@@ -42,25 +70,117 @@ for metric in ["latency", "throughput"]:
             html += "<div style=\"font-weight:bold\">{0}</div>".format(title)
             html += "<br><br>"
 
-FROM = "hemantj@nvidia.com"
-TO = 'sw-dl-triton@exchange.nvidia.com'
-SUBJECT = "Triton Nomodel Performance Summary:" + today
-msg = MIMEMultipart('alternative')
-msg['Subject'] = SUBJECT
-msg['From'] = FROM
-msg['To'] = TO
+html += '</center></body></html>'
+send_email("Triton Nomodel Performance Summary: " + today, html, "Nomodel")
+
+# Resnet50
+html = '<html><body><a href=\"https://gpuwa.nvidia.com/kibana/app/kibana#/dashboard/072b6f60-b02f-11ea-9584-77098036527d\">Resnet50 Kibana Dashboard</a><br><center>'
+today = date.today().strftime("%Y-%m-%d")
+for batch_size in ["1", "128"]:
+    for metric in ["latency", "throughput"]:
+        for protocol in ["grpc", "http"]:
+            instances = "2" if batch_size == "128" else "1"
+            if metric == "throughput":
+                values = ["d_infer_per_sec", "s_framework", "\'@timestamp\'"]
+            else:
+                values = ["d_latency_p95_ms", "s_framework", "\'@timestamp\'"]
+
+            where_dict = {
+                "s_benchmark_name": "resnet50",
+                "l_batch_size": batch_size,
+                "s_protocol": protocol,
+                "l_instance_count": instances
+            }
+
+            title = "Resnet50 " + protocol.upper(
+            ) + " " + metric + " with Batch Size " + batch_size
+            ma_df = sk.current_moving_average_dataframe(metric,
+                                                        values,
+                                                        where_dict,
+                                                        today,
+                                                        plot=True,
+                                                        plot_file=title +
+                                                        ".png")
+            ma_df.to_csv(title + ".csv", index=False)
+            img = open(title + ".png", "rb")
+            data_uri = base64.b64encode(img.read()).decode('ascii')
+            html += "<img src=\"data:image/png;base64,{0}\">".format(data_uri)
+            html += "<div style=\"font-weight:bold\">{0}</div>".format(title)
+            html += "<br><br>"
+
+SUBJECT = "Triton Resnet50 Performance Summary: " + today
+html += '</center></body></html>'
+send_email("Triton Resnet50 Performance Summary: " + today, html, "Resnet50")
+
+# Deeprecommender
+html = '<html><body><a href=\"https://gpuwa.nvidia.com/kibana/app/kibana#/visualize/edit/b5bf0030-c53a-11ea-a3f8-d19d9c5c9954\">Deeprecommender Kibana Dashboard</a><br><center>'
+today = date.today().strftime("%Y-%m-%d")
+for batch_size in ["1", "256"]:
+    for metric in ["latency", "throughput"]:
+        for protocol in ["grpc", "http"]:
+            instances = "2" if batch_size == "256" else "1"
+            if metric == "throughput":
+                values = ["d_infer_per_sec", "s_framework", "\'@timestamp\'"]
+            else:
+                values = ["d_latency_p95_ms", "s_framework", "\'@timestamp\'"]
+
+            where_dict = {
+                "s_benchmark_name": "deeprecommender",
+                "l_batch_size": batch_size,
+                "s_protocol": protocol,
+                "l_instance_count": instances
+            }
+
+            title = "Deeprecommender " + protocol.upper(
+            ) + " " + metric + " with Batch Size " + batch_size
+            ma_df = sk.current_moving_average_dataframe(metric,
+                                                        values,
+                                                        where_dict,
+                                                        today,
+                                                        plot=True,
+                                                        plot_file=title +
+                                                        ".png")
+            ma_df.to_csv(title + ".csv", index=False)
+            img = open(title + ".png", "rb")
+            data_uri = base64.b64encode(img.read()).decode('ascii')
+            html += "<img src=\"data:image/png;base64,{0}\">".format(data_uri)
+            html += "<div style=\"font-weight:bold\">{0}</div>".format(title)
+            html += "<br><br>"
 
 html += '</center></body></html>'
-msg.attach(MIMEText(html, "html"))
+send_email("Triton Deeprecommender Performance Summary: " + today, html,
+           "Deeprecommender")
 
-for filename in glob.glob("*.csv"):
-    attachment = open(filename, "rb")
-    p = MIMEBase('application', 'octet-stream')
-    p.set_payload((attachment).read())
-    encoders.encode_base64(p)
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-    msg.attach(p)
+# Kaldi
+html = '<html><body><a href=\"https://gpuwa.nvidia.com/kibana/app/kibana#/dashboard/a2a27bb0-b4b1-11ea-9584-77098036527d\">Kaldi Kibana Dashboard</a><br><center>'
+today = date.today().strftime("%Y-%m-%d")
+for metric in ["latency", "throughput"]:
+    protocol = "grpc"
+    if metric == "throughput":
+        values = ["d_infer_per_sec", "s_framework", "\'@timestamp\'"]
+    else:
+        values = ["d_latency_p95_ms", "s_framework", "\'@timestamp\'"]
 
-mailServer = smtplib.SMTP("mailgw.nvidia.com")
-mailServer.sendmail(FROM, TO, msg.as_string())
-mailServer.quit()
+    where_dict = {
+        "s_benchmark_name": "kaldi",
+        "s_model": "asr_kaldi",
+        "l_instance_count": "1"
+    }
+
+    title = "ASR Kaldi " + protocol.upper(
+    ) + " " + metric + " with concurrency 2000"
+    ma_df = sk.current_moving_average_dataframe(metric,
+                                                values,
+                                                where_dict,
+                                                today,
+                                                plot=True,
+                                                plot_file=title + ".png")
+    ma_df.to_csv(title + ".csv", index=False)
+    img = open(title + ".png", "rb")
+    data_uri = base64.b64encode(img.read()).decode('ascii')
+    html += "<img src=\"data:image/png;base64,{0}\">".format(data_uri)
+    html += "<div style=\"font-weight:bold\">{0}</div>".format(title)
+    html += "<br><br>"
+
+html += '</center></body></html>'
+send_email("Triton Kaldi Performance Summary: " + today, html, "ASR Kaldi")
