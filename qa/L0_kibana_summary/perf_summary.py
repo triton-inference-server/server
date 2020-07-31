16085c4397b2c71ba5aa93bd5a42b8ff4bd96a27
CoderHam <hemanham@gmail.com>
2020-07-30 21:39:49 -0700

    Use HTML to render inline instead of pdf

--- qa/L0_kibana_summary/perf_summary.py
+++ qa/L0_kibana_summary/perf_summary.py
@@ -1,15 +1,16 @@
-from email import encoders
-from fpdf import FPDF
 import summarize_kibana as sk
+from email import encoders
 from datetime import date
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+import base64
 import glob
 
-pdf = FPDF()
+html = '<html><body><a href=\"https://gpuwa.nvidia.com/kibana/app/kibana#/dashboard/ff9a1030-9a1c-11ea-8edb-c5a5e5f9de0d\">Nomodel Kibana Dashboard</a><br><center>'
 today = date.today().strftime("%Y-%m-%d")
-pdf.set_font('Arial', size=12)
+# pdf.set_font('Arial', size=12)
 for metric in ["latency", "throughput"]:
     for payload_size in ["1", "4194304"]:
         for protocol in ["grpc", "http"]:
@@ -35,22 +36,24 @@ for metric in ["latency", "throughput"]:
                                                         plot_file=title +
                                                         ".png")
             ma_df.to_csv(title + ".csv", index=False)
-
-            pdf.add_page()
-            pdf.cell(200, 12, txt=title, align='C')
-            pdf.image(title + ".png", x=20, y=20, w=125, h=100)
-pdf.output("summary_" + today + ".pdf", "F")
+            img = open(title + ".png", "rb")
+            data_uri = base64.b64encode(img.read()).decode('ascii')
+            html += "<img src=\"data:image/png;base64,{0}\">".format(data_uri)
+            html += "<div style=\"font-weight:bold\">{0}</div>".format(title)
+            html += "<br><br>"
 
 FROM = "hemantj@nvidia.com"
 TO = 'sw-dl-triton@exchange.nvidia.com'
-SUBJECT = "Triton Performance Summary :"+ today
-msg = MIMEMultipart()
+SUBJECT = "Triton Nomodel Performance Summary:" + today
+msg = MIMEMultipart('alternative')
 msg['Subject'] = SUBJECT
 msg['From'] = FROM
 msg['To'] = TO
 
-files = glob.glob("*.csv") + ["summary_" + today + ".pdf"]
-for filename in files:
+html += '</center></body></html>'
+msg.attach(MIMEText(html, "html"))
+
+for filename in glob.glob("*.csv"):
     attachment = open(filename, "rb")
     p = MIMEBase('application', 'octet-stream')
     p.set_payload((attachment).read())
