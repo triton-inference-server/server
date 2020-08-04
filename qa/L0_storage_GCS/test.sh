b18e3a9c3fec58ba1e91bdbdaa5653e32e7a097b
CoderHam <hemanham@gmail.com>
2020-08-04 14:29:03 -0700

    fix gcs project key to avoid multiline breakage

--- qa/L0_storage_GCS/test.sh
+++ qa/L0_storage_GCS/test.sh
@@ -44,18 +44,16 @@ EXPECTED_NUM_TESTS="3"
 # GCS credentials are necessary for this test. Pass via ENV variables
 export GOOGLE_APPLICATION_CREDENTIALS="file.json"
 
-echo '{
-  "type": "service_account",
-  "project_id": "triton-285001",
-  "private_key_id": "'$PROJECT_KEY_ID'",
-  "private_key": "'$PROJECT_KEY'",
-  "client_email": "hemantj@triton-285001.iam.gserviceaccount.com",
-  "client_id": "106901301872481149333",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/hemantj%40triton-285001.iam.gserviceaccount.com"
-}' > $GOOGLE_APPLICATION_CREDENTIALS
+echo '{"type": "service_account",' >> $GOOGLE_APPLICATION_CREDENTIALS
+echo '"project_id": "triton-285001",' >> $GOOGLE_APPLICATION_CREDENTIALS
+echo -e "\"private_key_id\": \"$PROJECT_KEY_ID\"," >> $GOOGLE_APPLICATION_CREDENTIALS
+echo -e "\"private_key\": \"$PROJECT_KEY\"," >> $GOOGLE_APPLICATION_CREDENTIALS
+echo '"client_email": "hemantj@triton-285001.iam.gserviceaccount.com",' >> $GOOGLE_APPLICATION_CREDENTIALS
+echo '"client_id": "106901301872481149333",' >> $GOOGLE_APPLICATION_CREDENTIALS
+echo '"auth_uri": "https://accounts.google.com/o/oauth2/auth",' >> $GOOGLE_APPLICATION_CREDENTIALS
+echo '"token_uri": "https://oauth2.googleapis.com/token",' >> $GOOGLE_APPLICATION_CREDENTIALS
+echo '"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",' >> $GOOGLE_APPLICATION_CREDENTIALS
+echo '"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/hemantj%40triton-285001.iam.gserviceaccount.com"}' >> $GOOGLE_APPLICATION_CREDENTIALS 
 
 # Google cloud variables (Point to bucket when testing cloud storage)
 BUCKET_URL="gs://triton-bucket-${CI_PIPELINE_ID}"
