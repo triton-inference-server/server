4b5980d0159c7b6d6953736b375aa5380d0a52eb
Iman Tabrizian <itabrizian@nvidia.com>
2020-08-23 15:11:53 -0400

    Fix TestResultCollector error counting

--- qa/common/util.sh
+++ qa/common/util.sh
@@ -274,21 +274,27 @@ function check_test_results () {
     local log_file=$1
     local expected_num_tests=$2
 
-    if [ -z "$expected_num_tests" ]; then
+    if [[ -z "$expected_num_tests" ]]; then
         echo "=== expected number of tests must be defined"
         return 1
     fi
 
-    num_failures=`cat $log_file | grep -E ".*total.*errors.*failures.*skipped.*" | tail -n 1 | jq .failures`
-    num_tests=`cat $log_file | grep -E ".*total.*errors.*failures.*skipped.*" | tail -n 1 | jq .total`
-    if [ $? -ne 0 ]; then
+    num_failures=`cat $log_file | grep -E ".*total.*errors.*failures.*" | tail -n 1 | jq .failures`
+    num_tests=`cat $log_file | grep -E ".*total.*errors.*failures.*" | tail -n 1 | jq .total`
+    num_errors=`cat $log_file | grep -E ".*total.*errors.*failures.*" | tail -n 1 | jq .errors`
+
+    # Number regular expression
+    re='^[0-9]+$'
+
+    if [[ $? -ne 0 ]] || ! [[ $num_failures =~ $re ]] || ! [[ $num_tests =~ $re ]] || \
+     ! [[ $num_errors =~ $re ]]; then
         cat $log_file
         echo -e "\n***\n*** Test Failed: unable to parse test results\n***" >> $log_file
         return 1
     fi
-    if [ $num_failures != "0" ] || [ $num_tests -ne $expected_num_tests ]; then
+    if [[ $num_errors != "0" ]] || [[ $num_failures != "0" ]] || [[ $num_tests -ne $expected_num_tests ]]; then
         cat $log_file
-        echo -e "\n***\n*** Test Failed: Expected $expected_num_tests test(s), $num_tests test(s) executed, and $num_failures test(s) failed. \n***" >> $log_file
+        echo -e "\n***\n*** Test Failed: Expected $expected_num_tests test(s), $num_tests test(s) executed, $num_errors test(s) had error, and $num_failures test(s) failed. \n***" >> $log_file
         return 1
     fi
 
