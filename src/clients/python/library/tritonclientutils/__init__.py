eeece8a0ee8e72f5a2bcc04bd429ab11608ad7e8
DEKHTIARJonathan <jdekhtiar@nvidia.com>
2020-08-28 14:00:31 -0700

    Cleaning

--- src/clients/python/library/tritonclientutils/__init__.py
+++ src/clients/python/library/tritonclientutils/__init__.py
@@ -25,10 +25,13 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import warnings
+warnings.simplefilter('always', DeprecationWarning)
 
 warnings.warn(
     "The package `tritonclientutils` is deprecated and will be "
     "removed in a future version. Please use instead "
-    "`tritonclient.utils`", DeprecationWarning)
+    "`tritonclient.utils`",
+    DeprecationWarning
+)
 
 from tritonclient.utils import *
