dc550a7c6f02dc002923609efd8e5450bc7a4995
Iman Tabrizian <itabrizian@nvidia.com>
2020-08-29 23:02:22 -0400

    Add tests for initialization args and execution error

---
+++ qa/python_models/init_args/model.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import numpy as np
+import sys
+import json
+
+sys.path.append('../../')
+import triton_python_backend_utils as utils
+
+
+class TritonPythonModel:
+
+    def initialize(self, args):
+        self.args = args
+        print(args)
+
+    def execute(self, requests):
+        """ This function is called on inference request.
+        """
+        keys = [
+            'model_config', 'model_instance_kind', 'model_instance_name',
+            'model_instance_device_id', 'model_repository', 'model_version',
+            'model_name'
+        ]
+
+        correct_keys = 0
+        for key in keys:
+            if key in list(self.args):
+                correct_keys += 1
+
+        responses = []
+        for request in requests:
+            out_args = utils.Tensor("OUT", np.array([correct_keys], dtype=np.float32))
+            responses.append(utils.InferenceResponse([out_args]))
+        return responses
