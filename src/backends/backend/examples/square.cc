4204c51316001d073eded4278a1a31bc81bbc283
deadeyegoodwin <davidg@nvidia.com>
2020-09-04 10:48:59 -0700

    Move common backend files to triton-inference-server/backend repo (#1983)

--- src/backends/backend/examples/square.cc
+++ src/backends/backend/examples/square.cc
@@ -24,12 +24,11 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "src/backends/backend/examples/backend_utils.h"
-
 #include <atomic>
 #include <chrono>
 #include <memory>
 #include <thread>
+#include "triton/backend/backend_common.h"
 
 //
 // Backend that demonstrates the TRITONBACKEND API for a decoupled
