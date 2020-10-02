7cacaa909cda7ac8b6d9016586e4a2458953431e
Ashwin Ramesh <xprotobeast2@gmail.com>
2020-10-02 12:40:15 -0700

    Use valgrind massif to detect unbound memory growth

---
+++ qa/L0_memory_growth/check_massif_log.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+import re
+from collections import defaultdict
+
+# Thresholds
+MAX_ALLOWED_ALLOC_RATE = float(os.environ.get('MAX_ALLOWED_ALLOC_RATE', 1000.0))
+
+def parse_massif_out(filename):
+    """
+    Extract the allocation data from the massif output file, and compile
+    it into a dictionary.    
+    
+    """
+    # Read the file
+    with open(filename, 'r') as f:
+        contents = f.read()
+        snapshots = re.findall('snapshot=(.*?)heap_tree', contents, flags=re.DOTALL)
+
+    # Create snapshot dictionary    
+    summary = defaultdict(list)
+    
+    for snapshot in snapshots:
+        # Split the record and ignore first two columns
+        columns = snapshot.split()[2:]
+        
+        # Put columns and values into dictionary
+        for col in columns:
+            k, v = col.split('=')
+            summary[k].append(int(v))
+    
+    # Return dict
+    return summary
+
+def is_unbounded_growth(summary):
+    """
+    Check whether the rate of heap allocations is increasing     
+    
+    """
+    # Estimate allocation ratef from total bytes over allocations (~second derivative)
+    cumulatives = summary['mem_heap_B']
+
+    # Compute differences
+    allocs_mb = [float(cumulatives[i] - cumulatives[i-1])/1e6 for i in range(1, len(cumulatives))]
+    alloc_rate_mb = [allocs_mb[i] - allocs_mb[i-1] for i in range(1, len(allocs_mb))]
+    average_alloc_rate_mb = sum(alloc_rate_mb)/len(alloc_rate_mb)
+    
+    print("ESTIMATED ALLOC RATE: %f MB/snapshot"%average_alloc_rate_mb)
+
+    return (average_alloc_rate_mb > MAX_ALLOWED_ALLOC_RATE)
+
+if __name__ == '__main__':
+    summary = parse_massif_out(sys.argv[1])
+    if (is_unbounded_growth(summary)):
+        sys.exit(1)
+    else:
+        sys.exit(0)
---
+++ qa/L0_memory_growth/test.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REPO_VERSION=${NVIDIA_TRITON_SERVER_VERSION}
+if [ "$#" -ge 1 ]; then
+    REPO_VERSION=$1
+fi
+if [ -z "$REPO_VERSION" ]; then
+    echo -e "Repository version must be specified"
+    echo -e "\n***\n*** Test Failed\n***"
+    exit 1
+fi
+
+# Single GPU
+export CUDA_VISIBLE_DEVICES=0
+
+# Clients
+PERF_ANALYZER=../clients/perf_analyzer
+IMAGE=../images/vulture.jpeg
+
+# Models
+CAFFE2PLAN=../common/caffe2plan
+DATADIR=/data/inferenceserver/${REPO_VERSION}
+
+# Server
+SERVER=/opt/tritonserver/bin/tritonserver
+SERVER_TIMEOUT=1200
+
+# Valgrind massif
+LEAKCHECK=/usr/bin/valgrind
+LEAKCHECK_ARGS_BASE="--tool=massif --time-unit=B"
+MASSIF_TEST=check_massif_log.py
+
+source ../common/util.sh
+
+# Function that checks the massif logs
+function check_massif_log () {
+    local massif_out=$1
+}
+
+rm -rf *.log models/ *.massif
+
+# Test parameters
+STATIC_BATCH=128
+INSTANCE_CNT=2
+CONCURRENCY=32
+CLIENT_BS=8
+
+# Threshold memory growth in MB per snapshot
+MAX_ALLOWED_ALLOC_RATE="-0.1"
+
+# Create local model repository
+mkdir -p models/
+cp -r $DATADIR/perf_model_store/resnet50* models/
+
+# Copy and prepare trt model
+cp -r $DATADIR/caffe_models/trt_model_store/resnet50_plan models/resnet50_fp16_plan
+mkdir -p models/resnet50_fp16_plan/1
+sed -i "s/^name:.*/name: \"resnet50_fp16_plan\"/" models/resnet50_fp16_plan/config.pbtxt
+
+set +e
+
+# Create the PLAN
+$CAFFE2PLAN -h -b ${STATIC_BATCH} \
+    -n prob -o models/resnet50_fp16_plan/1/model.plan \
+    $DATADIR/caffe_models/resnet50.prototxt $DATADIR/caffe_models/resnet50.caffemodel
+if [ $? -ne 0 ]; then
+    echo -e "\n***\n*** Failed to generate resnet50 PLAN\n***"
+    exit 1
+fi
+
+set -e
+
+RET=0
+
+for MODEL in $(ls models); do
+    # Create temporary model repository and copy only the model being tested
+    rm -rf test_repo && mkdir test_repo
+    cp -r models/$MODEL test_repo/
+
+    # Set server, client and valgrind arguments
+    SERVER_ARGS="--model-repository=`pwd`/test_repo --log-verbose=1"
+    LEAKCHECK_LOG="test_${MODEL}.valgrind.log"
+    MASSIF_LOG="test_${MODEL}.massif"
+    LEAKCHECK_ARGS="$LEAKCHECK_ARGS_BASE --massif-out-file=$MASSIF_LOG --max-threads=3000 --log-file=$LEAKCHECK_LOG"
+    SERVER_LOG="test_$MODEL.server.log"
+    CLIENT_LOG="test_$MODEL.client.log"
+
+    # Enable dynamic batching, set max batch size and instance count
+    if [ "$MODEL" == "resnet50_fp32_libtorch" ]; then    
+        sed -i "s/^max_batch_size:.*/max_batch_size: 32/" test_repo/$MODEL/config.pbtxt
+    else
+        sed -i "s/^max_batch_size:.*/max_batch_size: ${STATIC_BATCH}/" test_repo/$MODEL/config.pbtxt
+    fi
+    echo "dynamic_batching {}" >> test_repo/$MODEL/config.pbtxt
+    echo "instance_group [{ count: ${INSTANCE_CNT} }]" >> test_repo/$MODEL/config.pbtxt
+
+    # Run the server
+    run_server_leakcheck
+    if [ "$SERVER_PID" == "0" ]; then
+        echo -e "\n***\n*** Failed to start $SERVER\n***"
+        cat $SERVER_LOG
+        exit 1
+    fi
+
+    set +e
+
+    # Run the perf analyzer 3 times
+    for i in {1..3}; do    
+        $PERF_ANALYZER -v -m $MODEL -i grpc --concurrency-range $CONCURRENCY -b $CLIENT_BS > $CLIENT_LOG 2>&1
+        if [ $? -ne 0 ]; then
+            cat $CLIENT_LOG
+            echo -e "\n***\n*** perf_analyzer for $MODEL failed on iteration $i\n***"
+            RET=1   
+        fi
+    done
+
+    set -e
+
+    # Stop Server
+    kill $SERVER_PID
+    wait $SERVER_PID
+
+    set +e
+
+    # Check the massif output
+    python $MASSIF_TEST $MASSIF_LOG >> $CLIENT_LOG 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "\n***\n*** Test for $MODEL Failed\n***"
+        RET=1
+    fi
+    set -e
+done
+
+if [ $RET -eq 0 ]; then
+    echo -e "\n***\n*** Test Passed\n***"
+else
+    echo -e "\n***\n*** Test FAILED\n***"
+fi
+
+exit $RET
