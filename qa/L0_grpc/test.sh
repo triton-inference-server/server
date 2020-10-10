c1cb5a51c33382e72a0e6b1a655b68bc1a1f8a4d
David Goodwin <davidg@nvidia.com>
2020-10-10 14:27:44 -0700

    L0_docs is removed so don't attempt to get models from there

--- qa/L0_grpc/test.sh
+++ qa/L0_grpc/test.sh
@@ -74,6 +74,8 @@ SIMPLE_REUSE_INFER_OBJECTS_CLIENT=../clients/reuse_infer_objects_client
 rm -f *.log
 rm -f *.log.*
 
+set -e
+
 # Get the TensorFlow inception model
 mkdir -p models/inception_graphdef/1
 wget -O /tmp/inception_v3_2016_08_28_frozen.pb.tar.gz \
@@ -84,10 +86,12 @@ cp -r /data/inferenceserver/${REPO_VERSION}/qa_model_repository/graphdef_int8_in
 cp -r /data/inferenceserver/${REPO_VERSION}/tf_model_store/resnet_v1_50_graphdef models/
 
 # Create model repository layout for ensemble image classification
-cp -r ../L0_docs/docs/examples/ensemble_model_repository/image_preprocess_nchw_3x224x224_inception models/ && \
-cp -r ../L0_docs/docs/examples/ensemble_model_repository/preprocess_resnet50_ensemble models/ && \
-mkdir -p models/image_preprocess_nchw_3x224x224_inception/1 && \
-mkdir -p models/preprocess_resnet50_ensemble/1 && \
+rm -fr models/image_preprocess_nchw_3x224x224_inception && \
+    rm -fr models/preprocess_resnet50_ensemble && \
+    cp -r ensemble_model_repository/image_preprocess_nchw_3x224x224_inception models/ && \
+    cp -r ensemble_model_repository/preprocess_resnet50_ensemble models/ && \
+    mkdir -p models/image_preprocess_nchw_3x224x224_inception/1 && \
+    mkdir -p models/preprocess_resnet50_ensemble/1
 
 # Obtain actual models
 cp -r /data/inferenceserver/${REPO_VERSION}/c2_model_store/resnet50_netdef models/ && \
