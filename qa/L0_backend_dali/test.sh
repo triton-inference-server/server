84e2de69b37f55f6be0f34e383c858886edbe4c5
Michał <7101064+szalpal@users.noreply.github.com>
2020-10-15 19:41:12 +0200

    DALI Backend tests for CI: identity and rn50_ensemble (#2116)
    * DALI Backend tests for CI: identity and rn50_ensemble
    
    Signed-off-by: szalpal <mszolucha@nvidia.com>
    
    * add sanity check for test.sh
    
    Signed-off-by: szalpal <mszolucha@nvidia.com>

---
+++ qa/L0_backend_dali/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+DALI_BACKEND_DIR=`pwd`/dali_backend_dir
+
+if [ -z ${DALI_BACKEND_REPO_TAG+unused} ]; then
+  echo -e "\n***\n*** Test FAILED: DALI_BACKEND_REPO_TAG is undefined\n***"
+  exit 1
+fi
+
+git clone --single-branch --depth=1 -b $DALI_BACKEND_REPO_TAG https://github.com/triton-inference-server/dali_backend.git $DALI_BACKEND_DIR
+
+sh ./test_identity.sh $DALI_BACKEND_DIR
+sh ./test_rn50_ensemble.sh $DALI_BACKEND_DIR
