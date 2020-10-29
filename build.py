3db07c2e35fa536306cd6ab0b8c4f48571b1d31b
David Goodwin <davidg@nvidia.com>
2020-10-29 10:21:07 -0700

    Add sm8.6 to pytorch build

--- build.py
+++ build.py
@@ -336,7 +336,7 @@ RUN (conda uninstall -y pytorch || true) && \
     pip uninstall -y torch
 RUN cd pytorch && \
     python setup.py clean && \
-    TORCH_CUDA_ARCH_LIST="5.2 6.0 6.1 7.0 7.5 8.0+PTX" \
+    TORCH_CUDA_ARCH_LIST="5.2 6.0 6.1 7.0 7.5 8.0 8.6+PTX" \
     CUDA_HOME="/usr/local/cuda" \
     CMAKE_PREFIX_PATH="$(dirname $(which conda))/../" \
     USE_DISTRIBUTED=OFF USE_OPENMP=OFF USE_NCCL=OFF USE_SYSTEM_NCCL=OFF \
