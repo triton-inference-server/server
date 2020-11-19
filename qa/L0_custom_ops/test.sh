e9fe3c38dfdca20117eae0438972ddc9b4fe66a0
Hemant Jain <hemanham@gmail.com>
2020-11-18 19:43:39 -0500

    Remove PyTorch from LD_LIBRARY_PATH; Fix PyTorch custom op docs (#2259)
    * Remove pytorch from LD_LIBRARY_PATH. User must add to load models with custom and torchvision ops
    
    * Rectify pytorch docs to for custom operations

--- qa/L0_custom_ops/test.sh
+++ qa/L0_custom_ops/test.sh
@@ -53,8 +53,8 @@ rm -f $SERVER_LOG $CLIENT_LOG
 RET=0
 
 # Must explicitly set LD_LIBRARY_PATH so that the custom operations
-# can find libtensorflow_framework.so and pytorch libraries.
-LD_LIBRARY_PATH=/opt/tritonserver/backends/tensorflow1:/opt/tritonserver/backends/pytorch:$LD_LIBRARY_PATH
+# can find libtensorflow_framework.so.
+LD_LIBRARY_PATH=/opt/tritonserver/backends/tensorflow1:$LD_LIBRARY_PATH
 
 # Tensorflow
 SERVER_ARGS="--model-repository=/data/inferenceserver/${REPO_VERSION}/qa_custom_ops/tf_custom_ops"
@@ -102,6 +102,10 @@ set -e
 kill $SERVER_PID
 wait $SERVER_PID
 
+# Must set LD_LIBRARY_PATH just for the server launch so that the 
+# custom operations can find libtorch.so and other pytorch dependencies.
+LD_LIBRARY_PATH=/opt/tritonserver/backends/pytorch:$LD_LIBRARY_PATH
+
 # Pytorch
 SERVER_ARGS="--model-repository=/data/inferenceserver/${REPO_VERSION}/qa_custom_ops/libtorch_custom_ops"
 SERVER_LD_PRELOAD="/data/inferenceserver/${REPO_VERSION}/qa_custom_ops/libtorch_custom_ops/libtorch_modulo/custom_modulo.so"
@@ -138,7 +142,8 @@ kill $SERVER_PID
 wait $SERVER_PID
 
 # ONNX
-mkdir -p onnx_custom_ops/custom_op/1 && \
+rm -rf onnx_custom_ops && \
+    mkdir -p onnx_custom_ops/custom_op/1 && \
     cp custom_op_test.onnx onnx_custom_ops/custom_op/1/model.onnx
 
 touch onnx_custom_ops/custom_op/config.pbtxt
