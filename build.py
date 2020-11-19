0da03fe01a7dfd9c430d24ce669b4fada97275f3
David Goodwin <davidg@nvidia.com>
2020-11-19 09:39:23 -0800

    Use cmake 3.18.4 explicitly

--- build.py
+++ build.py
@@ -541,7 +541,8 @@ RUN wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/nul
       tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null && \
     apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main' && \
     apt-get update && \
-    apt-get install -y --no-install-recommends cmake
+    apt-get install -y --no-install-recommends \
+      cmake-data=3.18.4-0kitware1ubuntu20.04.1 cmake=3.18.4-0kitware1ubuntu20.04.1
 '''
     if 'pytorch' in backends:
         df += '''
