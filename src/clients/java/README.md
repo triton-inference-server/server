adb538d044785a8f74c307b31546cfb2e305d9ba
sujitb-nvidia <64230457+sujitb-nvidia@users.noreply.github.com>
2020-12-10 11:30:35 -0800

    Add java client (#2320)
    * java client
    
    * add proto dir
    
    * update readme
    
    * update readme
    
    * delete generated stub
    
    * update readme, copy generated stub
    
    * add copyright
    
    * update readme
    
    * update readme, fix typo
    
    * update readme, fix typo
    
    Co-authored-by: Sujit Biswas <sujit.biswas@gmail.com>

---
+++ src/clients/java/README.md
@@ -0,0 +1,81 @@
+# Example Java and Scala client Using Generated GRPC API
+
+
+## Prerequsites
+Maven 3.3+ and JDK 1.8+
+
+## Generating java GRPC client stub
+Copy __*.proto__ files to Library src/main/proto
+```
+$ cd library
+$ cp ../../../core/*.proto src/main/proto/
+```
+
+__Note__ the *.proto files are copied from [triton-inference-server
+/server/src/core/](https://github.com/triton-inference-server/server/tree/master/src/core). After copying the __libray__ dir should look as below.
+
+
+<img src="images/proto-files.png" width="220" />
+
+```
+$ mvn compile
+```
+Once compiled, one should notice the generated *.java files under __target__ folder
+
+<img src="images/grpc-stubs.png" width="400" />
+
+## Use the generated files in any project
+
+To run the examples clients, copy the above generated stub into __examples__ folder
+
+```
+$ cd ..
+
+$ cp -R library/target/generated-sources/protobuf/java/inference  examples/src/main/java/inference
+
+$ cp -R library/target/generated-sources/protobuf/grpc-java/inference/*.java  examples/src/main/java/inference/
+
+```
+See the __examples__ project which has __scala__ and __java__ sample client. 
+
+## Running java example client 
+
+```
+$ cd examples
+
+$ mvn clean install 
+
+$ mvn exec:java -Dexec.mainClass=clients.SimpleJavaClient -Dexec.args="<host> <port>"
+```
+
+__host__  where triton inference server is running
+
+__port__ default grpc port is 8001
+
+## Running scala example client 
+
+```
+$ mvn exec:java -Dexec.mainClass=clients.SimpleClient -Dexec.args="<host> <port>"
+```
+
+Both the examples run inference with respect to __simple__ model. The __scala__ example is more comprehensive and checks APIs like server ready and model ready
+
+### Output of the scala client once run succesfully
+
+```
+name: "OUTPUT0"
+datatype: "INT32"
+shape: 2
+shape: 16
+
+2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32
+34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64
+
+name: "OUTPUT1"
+datatype: "INT32"
+shape: 2
+shape: 16
+
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+```
---
+++ src/clients/java/examples/src/main/scala/clients/SimpleClient.scala
@@ -0,0 +1,289 @@
+
+/**
+ * 
+// Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
+
+package clients
+
+import java.nio.ByteOrder
+import java.nio.IntBuffer
+import io.grpc.ManagedChannelBuilder
+import collection.JavaConverters._
+import java.util.Arrays
+
+import inference.GRPCInferenceServiceGrpc.{ GRPCInferenceServiceBlockingStub => GRPCStub }
+
+object SimpleClient {
+
+  def serverLive(grpc_stub: GRPCStub) = {
+
+    try {
+      val request = inference.GrpcService.ServerLiveRequest.getDefaultInstance();
+      val response = grpc_stub.serverLive(request);
+      println(response)
+      response.getLive
+    } catch {
+      case e: Exception =>
+        println("server: " + e)
+        false
+    }
+
+  }
+
+  def serverReady(grpc_stub: GRPCStub) = {
+
+    try {
+      val request = inference.GrpcService.ServerReadyRequest.getDefaultInstance();
+      val response = grpc_stub.serverReady(request);
+      println(response)
+      response.getReady
+    } catch {
+      case e: Exception =>
+        println("server: " + e)
+        false
+    }
+
+  }
+
+  def serverMetadata(grpc_stub: GRPCStub) = {
+
+    try {
+      val request = inference.GrpcService.ServerMetadataRequest.getDefaultInstance();
+      val response = grpc_stub.serverMetadata(request);
+
+      response.toString()
+    } catch {
+      case e: Exception =>
+        println("server: " + e)
+        null
+    }
+
+  }
+
+  def modelReady(grpc_stub: GRPCStub, model: String, version: String = "") = {
+
+    try {
+      val request = inference.GrpcService.ModelReadyRequest.newBuilder();
+      request.setName(model)
+      request.setVersion(version)
+      val response = grpc_stub.modelReady(request.build());
+      println(response)
+      response.getReady
+    } catch {
+      case e: Exception =>
+        println("server: " + e)
+        false
+    }
+
+  }
+
+  def modelMetadata(grpc_stub: GRPCStub, model: String, version: String = "") = {
+
+    try {
+      val request = inference.GrpcService.ModelMetadataRequest.newBuilder();
+      request.setName(model)
+      request.setVersion(version)
+      val response = grpc_stub.modelMetadata(request.build());
+      //println(response)
+      response.toString()
+    } catch {
+      case e: Exception =>
+        println("server: " + e)
+        null
+    }
+
+  }
+
+  def modelConfig(grpc_stub: GRPCStub, model: String, version: String = "") = {
+
+    try {
+      val request = inference.GrpcService.ModelConfigRequest.newBuilder();
+      request.setName(model)
+      request.setVersion(version)
+      val response = grpc_stub.modelConfig(request.build());
+      //println(response)
+      response.toString()
+    } catch {
+      case e: Exception =>
+        println("server: " + e)
+        null
+    }
+
+  }
+
+  def testInference(grpc_stub: GRPCStub,model: String, version: String = "") = {
+
+
+    val batch_size = 2
+    val dimension = 16
+
+    /**
+     * Generate the request
+     */
+    val request = inference.GrpcService.ModelInferRequest.newBuilder();
+    request.setModelName(model);
+    request.setModelVersion(version);
+
+    /**
+     * Input Data
+     * use input dimension [batch_size,dimension]
+     */
+    val lst_0 = (1 to batch_size * dimension).toList.map(x => new Integer(x)).asJava;
+    val lst_1 = (1 to batch_size * dimension).toList.map(x => new Integer(x)).asJava;
+
+    val input0_data = inference.GrpcService.InferTensorContents.newBuilder();
+    val input1_data = inference.GrpcService.InferTensorContents.newBuilder();
+
+    input0_data.addAllIntContents(lst_0);
+    input1_data.addAllIntContents(lst_1);
+
+    /**
+     * Populate the inputs in inference request
+     */
+    val input0 = inference.GrpcService.ModelInferRequest.InferInputTensor
+      .newBuilder();
+    input0.setName("INPUT0");
+    input0.setDatatype("INT32");
+    input0.addShape(batch_size);
+    input0.addShape(dimension);
+    input0.setContents(input0_data);
+
+    val input1 = inference.GrpcService.ModelInferRequest.InferInputTensor
+      .newBuilder();
+    input1.setName("INPUT1");
+    input1.setDatatype("INT32");
+    input1.addShape(batch_size);
+    input1.addShape(dimension);
+    input1.setContents(input1_data);
+
+    request.addInputs(0, input0);
+    request.addInputs(1, input1);
+
+    /**
+     *  Populate the outputs in the inference request
+     */
+    val output0 = inference.GrpcService.ModelInferRequest.InferRequestedOutputTensor
+      .newBuilder();
+    output0.setName("OUTPUT0");
+
+    val output1 = inference.GrpcService.ModelInferRequest.InferRequestedOutputTensor
+      .newBuilder();
+    output1.setName("OUTPUT1");
+
+    request.addOutputs(0, output0);
+    request.addOutputs(1, output1);
+
+    /**
+     * Infer
+     */
+    val response = grpc_stub.modelInfer(request.build());
+
+    //System.out.println(response);
+
+    /**
+     * check the two output tensors
+     */
+
+    for (i <- 0 until response.getOutputsCount) {
+
+      println(response.getOutputs(i))
+
+      /**
+       * print raw output
+       */
+      val outputArr = toArray(response.getRawOutputContents(i).asReadOnlyByteBuffer().order(ByteOrder.LITTLE_ENDIAN).asIntBuffer())
+      outputArr.grouped(dimension).foreach { rec =>
+        println(rec.mkString(","))
+      }
+
+      println
+
+    }
+
+  }
+
+  /**
+   * converts IntBuffer to Array[Int]
+   */
+  def toArray(b: IntBuffer) = {
+    // b.flip();
+    if (b.hasArray()) {
+      if (b.arrayOffset() == 0)
+        b.array();
+
+      Arrays.copyOfRange(b.array(), b.arrayOffset(), b.array().length);
+    }
+
+    b.rewind();
+    val temp = Array.ofDim[Int](b.remaining());
+    b.get(temp);
+
+    temp;
+  }
+
+  def main(args: Array[String]) {
+
+    /**
+     * define host port
+     */
+    val host = if (args.length > 0) args(0) else "localhost"
+    val port = if (args.length > 1) args(1).toInt else 8001
+
+    val model = "simple"
+    val version = ""
+
+    /**
+     * Create gRPC stub for communicating with the server
+     */
+    val channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+    val grpc_stub = inference.GRPCInferenceServiceGrpc.newBlockingStub(channel);
+
+    /**
+     * check server api, print as needed
+     */
+    serverLive(grpc_stub)
+    serverReady(grpc_stub)
+    serverMetadata(grpc_stub)
+
+    /**
+     * check model api
+     */
+    modelReady(grpc_stub, model, version)
+    modelConfig(grpc_stub, model, version)
+    modelMetadata(grpc_stub, model, version)
+
+    /**
+     * check simple model inference
+     */
+    testInference(grpc_stub, model, version)
+
+    
+    channel.shutdownNow()
+  }
+
+}
