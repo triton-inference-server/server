893b9235d4e3f45e589a5ab8bab011d7e1edbceb
GuanLuo <gluo@nvidia.com>
2020-12-17 14:55:56 -0800

    fix up

--- src/test/pinned_memory_manager_test.cc
+++ src/test/pinned_memory_manager_test.cc
@@ -26,12 +26,12 @@
 #include "gtest/gtest.h"
 
 #include <cuda_runtime_api.h>
-#include "src/core/tritonserver_apis.h"
-#include "src/core/pinned_memory_manager.h"
-#include <thread>
 #include <condition_variable>
 #include <mutex>
+#include <thread>
 #include <vector>
+#include "src/core/pinned_memory_manager.h"
+#include "src/core/tritonserver_apis.h"
 
 namespace ni = nvidia::inferenceserver;
 
@@ -63,8 +63,10 @@ namespace {
   } while (false)
 
 struct MemoryWorkMetadata {
-  MemoryWorkMetadata(size_t thread_count) : thread_count_(thread_count),
-  results_(thread_count, "") {}
+  MemoryWorkMetadata(size_t thread_count)
+      : thread_count_(thread_count), ready_count_(0), results_(thread_count, "")
+  {
+  }
   size_t thread_count_;
   size_t ready_count_;
   std::vector<std::string> results_;
@@ -72,7 +74,10 @@ struct MemoryWorkMetadata {
   std::condition_variable cv_;
 };
 
-void RunMemoryWork(size_t idx, size_t alloc_size, MemoryWorkMetadata* metadata)
+void
+RunMemoryWork(
+    size_t idx, size_t alloc_size, bool allow_nonpinned_fallback,
+    MemoryWorkMetadata* metadata)
 {
   // Prepare variable to hold input / output
   std::unique_ptr<char> input(new char[alloc_size]);
@@ -86,29 +91,36 @@ void RunMemoryWork(size_t idx, size_t alloc_size, MemoryWorkMetadata* metadata)
       while (metadata->ready_count_ != metadata->thread_count_) {
         metadata->cv_.wait(lk);
       }
-    } else {
-      metadata->cv_.notify_all();
     }
+    metadata->cv_.notify_one();
   }
 
   // Simulate receive input data -> alloc and write to input buffer
   // -> alloc and write to output buffer -> return output data
-  TRITONSERVER_MemoryType allocated_type;
+  TRITONSERVER_MemoryType allocated_type = TRITONSERVER_MEMORY_GPU;
   void* input_buffer = nullptr;
   STORE_RESULT_AND_RETURN_IF_ERROR(
-    metadata, idx,
-    ni::PinnedMemoryManager::Alloc(&input_buffer, alloc_size, &allocated_type, false /* allow_nonpinned_fallback */));
-  if (allocated_type != TRITONSERVER_MEMORY_CPU_PINNED) {
-    ni::Status status(ni::Status::Code::INVALID_ARG, "returned memory buffer is not pinned");
+      metadata, idx,
+      ni::PinnedMemoryManager::Alloc(
+          &input_buffer, alloc_size, &allocated_type,
+          allow_nonpinned_fallback));
+  if ((!allow_nonpinned_fallback) &&
+      (allocated_type != TRITONSERVER_MEMORY_CPU_PINNED)) {
+    ni::Status status(
+        ni::Status::Code::INVALID_ARG, "returned memory buffer is not pinned");
     STORE_RESULT_AND_RETURN_IF_ERROR(metadata, idx, status);
   }
   memcpy(input_buffer, input.get(), alloc_size);
   void* output_buffer = nullptr;
   STORE_RESULT_AND_RETURN_IF_ERROR(
-    metadata, idx,
-    ni::PinnedMemoryManager::Alloc(&output_buffer, alloc_size, &allocated_type, false /* allow_nonpinned_fallback */));
-  if (allocated_type != TRITONSERVER_MEMORY_CPU_PINNED) {
-    ni::Status status(ni::Status::Code::INVALID_ARG, "returned memory buffer is not pinned");
+      metadata, idx,
+      ni::PinnedMemoryManager::Alloc(
+          &output_buffer, alloc_size, &allocated_type,
+          allow_nonpinned_fallback));
+  if ((!allow_nonpinned_fallback) &&
+      (allocated_type != TRITONSERVER_MEMORY_CPU_PINNED)) {
+    ni::Status status(
+        ni::Status::Code::INVALID_ARG, "returned memory buffer is not pinned");
     STORE_RESULT_AND_RETURN_IF_ERROR(metadata, idx, status);
   }
   memcpy(output_buffer, input_buffer, alloc_size);
@@ -116,8 +128,10 @@ void RunMemoryWork(size_t idx, size_t alloc_size, MemoryWorkMetadata* metadata)
   for (size_t offset = 0; offset < alloc_size; offset++) {
     if (input.get()[offset] != output.get()[offset]) {
       std::lock_guard<std::mutex> lk(metadata->mtx_);
-      metadata->results_[idx] = std::string("mismatch between input and output for work idx ") + std::to_string(idx);
-      return;         
+      metadata->results_[idx] =
+          std::string("mismatch between input and output for work idx ") +
+          std::to_string(idx);
+      return;
     }
   }
 }
@@ -146,7 +160,9 @@ TEST_F(PinnedMemoryManagerTest, InitOOM)
   // Set to reserve too much memory
   options_.pinned_memory_pool_byte_size_ = uint64_t(1) << 40 /* 1024 GB */;
   auto status = ni::PinnedMemoryManager::Create(options_);
-  EXPECT_FALSE(status.IsOk()) << "Expect creation error";
+  // For pinned memory manager, it will still be created for "CPU fallback"
+  // allocation even if it fails to create pinned memory pool
+  EXPECT_TRUE(status.IsOk()) << status.Message();
 }
 
 TEST_F(PinnedMemoryManagerTest, InitSuccess)
@@ -162,7 +178,9 @@ TEST_F(PinnedMemoryManagerTest, InitZeroByte)
   EXPECT_TRUE(status.IsOk()) << status.Message();
 
   void* ptr = nullptr;
-  status = ni::PinnedMemoryManager::Alloc(&ptr, 1);
+  TRITONSERVER_MemoryType allocated_type = TRITONSERVER_MEMORY_GPU;
+  status = ni::PinnedMemoryManager::Alloc(
+      &ptr, 1, &allocated_type, false /* allow_nonpinned_fallback */);
   ASSERT_FALSE(status.IsOk()) << "Unexpected successful allocation";
 }
 
@@ -172,11 +190,32 @@ TEST_F(PinnedMemoryManagerTest, AllocSuccess)
   ASSERT_TRUE(status.IsOk()) << status.Message();
 
   void* ptr = nullptr;
-  status = ni::PinnedMemoryManager::Alloc(&ptr, 1024);
+  TRITONSERVER_MemoryType allocated_type = TRITONSERVER_MEMORY_GPU;
+  status = ni::PinnedMemoryManager::Alloc(
+      &ptr, 512, &allocated_type, false /* allow_nonpinned_fallback */);
   ASSERT_TRUE(status.IsOk()) << status.Message();
   ASSERT_TRUE(ptr) << "Expect pointer to allocated buffer";
+  ASSERT_TRUE(allocated_type == TRITONSERVER_MEMORY_CPU_PINNED)
+      << "Expect pointer to pinned memory";
   // check if returned pointer is pinned memory pointer
   CHECK_POINTER_ATTRIBUTES(ptr, cudaMemoryTypeHost, 0);
+}
+
+TEST_F(PinnedMemoryManagerTest, AllocFallbackSuccess)
+{
+  auto status = ni::PinnedMemoryManager::Create(options_);
+  ASSERT_TRUE(status.IsOk()) << status.Message();
+
+  void* ptr = nullptr;
+  TRITONSERVER_MemoryType allocated_type = TRITONSERVER_MEMORY_GPU;
+  status = ni::PinnedMemoryManager::Alloc(
+      &ptr, 2048, &allocated_type, true /* allow_nonpinned_fallback */);
+  ASSERT_TRUE(status.IsOk()) << status.Message();
+  ASSERT_TRUE(ptr) << "Expect pointer to allocated buffer";
+  ASSERT_TRUE(allocated_type == TRITONSERVER_MEMORY_CPU)
+      << "Expect pointer to non-pinned memory";
+  // check if returned pointer is non-pinned memory pointer
+  CHECK_POINTER_ATTRIBUTES(ptr, cudaMemoryTypeUnregistered, 0);
 }
 
 TEST_F(PinnedMemoryManagerTest, AllocFail)
@@ -185,7 +224,9 @@ TEST_F(PinnedMemoryManagerTest, AllocFail)
   ASSERT_TRUE(status.IsOk()) << status.Message();
 
   void* ptr = nullptr;
-  status = ni::PinnedMemoryManager::Alloc(&ptr, 2048);
+  TRITONSERVER_MemoryType allocated_type = TRITONSERVER_MEMORY_GPU;
+  status = ni::PinnedMemoryManager::Alloc(
+      &ptr, 2048, &allocated_type, false /* allow_nonpinned_fallback */);
   ASSERT_FALSE(status.IsOk()) << "Unexpected successful allocation";
 }
 
@@ -195,23 +236,31 @@ TEST_F(PinnedMemoryManagerTest, MultipleAlloc)
   ASSERT_TRUE(status.IsOk()) << status.Message();
 
   void* first_ptr = nullptr;
-  status = ni::PinnedMemoryManager::Alloc(&first_ptr, 600);
+  TRITONSERVER_MemoryType allocated_type = TRITONSERVER_MEMORY_GPU;
+  status = ni::PinnedMemoryManager::Alloc(
+      &first_ptr, 600, &allocated_type, false /* allow_nonpinned_fallback */);
   ASSERT_TRUE(status.IsOk()) << status.Message();
   ASSERT_TRUE(first_ptr) << "Expect pointer to allocated buffer";
+  ASSERT_TRUE(allocated_type == TRITONSERVER_MEMORY_CPU_PINNED)
+      << "Expect pointer to pinned memory";
   // check if returned pointer is pinned memory pointer
   CHECK_POINTER_ATTRIBUTES(first_ptr, cudaMemoryTypeHost, 0);
 
   // 512 + 600 > 1024
   void* second_ptr = nullptr;
-  status = ni::PinnedMemoryManager::Alloc(&second_ptr, 512);
+  status = ni::PinnedMemoryManager::Alloc(
+      &second_ptr, 512, &allocated_type, false /* allow_nonpinned_fallback */);
   ASSERT_FALSE(status.IsOk()) << "Unexpected successful allocation";
 
   // Free the first pointer and retry the second one
   status = ni::PinnedMemoryManager::Free(first_ptr);
   EXPECT_TRUE(status.IsOk()) << status.Message();
-  status = ni::PinnedMemoryManager::Alloc(&second_ptr, 512);
+  status = ni::PinnedMemoryManager::Alloc(
+      &second_ptr, 512, &allocated_type, false /* allow_nonpinned_fallback */);
   ASSERT_TRUE(status.IsOk()) << status.Message();
   ASSERT_TRUE(second_ptr) << "Expect pointer to allocated buffer";
+  ASSERT_TRUE(allocated_type == TRITONSERVER_MEMORY_CPU_PINNED)
+      << "Expect pointer to pinned memory";
   // check if returned pointer is pinned memory pointer
   CHECK_POINTER_ATTRIBUTES(second_ptr, cudaMemoryTypeHost, 0);
 }
@@ -223,12 +272,38 @@ TEST_F(PinnedMemoryManagerTest, ParallelAlloc)
   ASSERT_TRUE(status.IsOk()) << status.Message();
 
   // Create threads to perform operations on allocated memory in parallel
-  size_t thread_count = 16;
+  // Seems like for 1 MB alloc size, 100 threads is a good amount for
+  // pool manager not to use CPU fallback. But still low comparing to
+  // theoric limit (256 MB / 1 MB = 256)
+  size_t thread_count = 100;
   size_t allocated_size = 1 << 20 /* 1 MB */;
   MemoryWorkMetadata metadata(thread_count);
   std::vector<std::thread> threads;
   for (size_t idx = 0; idx < thread_count; idx++) {
-    threads.emplace_back(std::thread(RunMemoryWork, idx, allocated_size, &metadata));
+    threads.emplace_back(
+        std::thread(RunMemoryWork, idx, allocated_size, false, &metadata));
+  }
+  for (size_t idx = 0; idx < thread_count; idx++) {
+    threads[idx].join();
+    EXPECT_TRUE(metadata.results_[idx].empty()) << metadata.results_[idx];
+  }
+}
+
+
+TEST_F(PinnedMemoryManagerTest, ParallelAllocFallback)
+{
+  options_.pinned_memory_pool_byte_size_ = uint64_t(1) << 28 /* 256 MB */;
+  auto status = ni::PinnedMemoryManager::Create(options_);
+  ASSERT_TRUE(status.IsOk()) << status.Message();
+
+  // Create threads to perform operations on allocated memory in parallel
+  size_t thread_count = 128;
+  size_t allocated_size = 1 << 24 /* 4 MB */;
+  MemoryWorkMetadata metadata(thread_count);
+  std::vector<std::thread> threads;
+  for (size_t idx = 0; idx < thread_count; idx++) {
+    threads.emplace_back(
+        std::thread(RunMemoryWork, idx, allocated_size, true, &metadata));
   }
   for (size_t idx = 0; idx < thread_count; idx++) {
     threads[idx].join();
