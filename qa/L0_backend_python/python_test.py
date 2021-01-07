b7905f7aa2f5da5ad694c2cfb38ca7f246c4c2fd
Iman Tabrizian <itabrizian@nvidia.com>
2021-01-07 11:40:26 -0500

    Add test for Python backend when the input is in GPU

--- qa/L0_backend_python/python_test.py
+++ qa/L0_backend_python/python_test.py
@@ -203,7 +203,28 @@ class PythonTest(tu.TestResultCollector):
 
     def test_ensemble(self):
         model_name = "ensemble"
-        shape = [4]
+        shape = [16]
+        with httpclient.InferenceServerClient("localhost:8000") as client:
+            input_data_0 = np.random.random(shape).astype(np.float32)
+            input_data_1 = np.random.random(shape).astype(np.float32)
+            inputs = [
+                httpclient.InferInput("INPUT0", input_data_0.shape,
+                                      np_to_triton_dtype(input_data_0.dtype)),
+                httpclient.InferInput("INPUT1", input_data_1.shape,
+                                      np_to_triton_dtype(input_data_1.dtype))
+            ]
+            inputs[0].set_data_from_numpy(input_data_0)
+            inputs[1].set_data_from_numpy(input_data_1)
+            result = client.infer(model_name, inputs)
+            output0 = result.as_numpy('OUTPUT0')
+            output1 = result.as_numpy('OUTPUT1')
+            self.assertIsNotNone(output0)
+            self.assertIsNotNone(output1)
+
+            self.assertTrue(np.allclose(output0, 2 * input_data_0))
+            self.assertTrue(np.allclose(output1, 2 * input_data_1))
+
+        model_name = "ensemble_gpu"
         with httpclient.InferenceServerClient("localhost:8000") as client:
             input_data_0 = np.random.random(shape).astype(np.float32)
             input_data_1 = np.random.random(shape).astype(np.float32)
