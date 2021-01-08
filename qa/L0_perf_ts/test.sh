bdba74bc7fc44a1235b298b85864d3a795c52c6a
Tanmay Verma <tanmay2592@gmail.com>
2021-01-07 16:45:45 -0800

    Adding the test for benchmarking TorchServe using perf_analyzer (#2383)
    * Add benchmark for TorchServe
    
    * Suppressing the logging

--- qa/L0_perf_ts/test.sh
+++ qa/L0_perf_ts/test.sh
@@ -25,6 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+
 if [ "$#" -ge 1 ]; then
     REPO_VERSION=$1
 fi
@@ -35,95 +36,62 @@ if [ -z "$REPO_VERSION" ]; then
 fi
 
 apt update
- # needed by perf_analyzer
-apt install -y libb64-dev
+apt install -y libb64-dev curl
 # needed by reporter
 apt install -y python3 python3-pip python3-dev
 rm -f /usr/bin/python && ln -s /usr/bin/python3 /usr/bin/python
 pip3 install --upgrade requests
 
 REPODIR=/data/inferenceserver/${REPO_VERSION}
-rm -f *.log *.csv *.tjson *.json
-rm -rf model_store
-
-RET=0
-
-# Create model_store
-MODEL_NAME="resnet50v1.5_fp16_savedmodel"
-mkdir model_store
-mkdir -p model_store/${MODEL_NAME}
-cp -r ${REPODIR}/perf_model_store/${MODEL_NAME}/1/model.savedmodel model_store/${MODEL_NAME}/1
-
-# Run server
-tensorflow_model_server --port=8500 --model_name=${MODEL_NAME} --model_base_path=$PWD/model_store/${MODEL_NAME} > server.log 2>&1 &
-SERVER_PID=$!
-# Wait for the server to start
-sleep 10
-if [ "$SERVER_PID" == "0" ]; then
-    echo -e "\n***\n*** Failed to start server\n***"
-    cat server.log
-    exit 1
-fi
-
 PERF_ANALYZER=/perf_bin/perf_analyzer
 REPORTER=../common/reporter.py
 
-# To get the minimum latency
+rm -f *.log *.csv *.tjson *.json log4j.properties
+rm -rf model_store
+rm -rf serve
+
+RET=0
+
+# Create model archive. Using default handler for image classification
+MODEL_NAME="resnet50_fp32_libtorch"
+mkdir model_store
+torch-model-archiver --model-name resnet50 --version 1.0 --serialized-file ${REPODIR}/perf_model_store/${MODEL_NAME}/1/model.pt \
+--export-path model_store --handler image_classifier -f
+# Suppressing the logging for better performance
+echo "log4j.rootLogger = OFF" >> log4j.properties
+# Run server
+torchserve --start --ncs --model-store=model_store --models model_store/resnet50.mar --log-config log4j.properties
+
+sleep 5
+
+# Get the input image to be used for generating requests
 STATIC_BATCH=1
+curl -O https://raw.githubusercontent.com/pytorch/serve/master/docs/images/kitten_small.jpg
+echo "{\"data\":[{\"TORCHSERVE_INPUT\" : [\"kitten_small.jpg\"]}]}" >> data.json
 NAME=${MODEL_NAME}_sbatch${STATIC_BATCH}
+PERF_ANALYZER_ARGS="-m resnet50 --service-kind torchserve -i http -u localhost:8080 -b ${STATIC_BATCH} -p 5000 --input-data data.json"
 
 # Run client
 # To warmup the model
-$PERF_ANALYZER -m ${MODEL_NAME} --service-kind tfserving -i grpc -b 1 -p 5000
+$PERF_ANALYZER ${PERF_ANALYZER_ARGS}
 # Collect data
-$PERF_ANALYZER -m ${MODEL_NAME} --service-kind tfserving -i grpc -b ${STATIC_BATCH} -p 5000 -f ${NAME}.csv >> ${NAME}.log 2>&1
+$PERF_ANALYZER ${PERF_ANALYZER_ARGS} -f ${NAME}.csv >> ${NAME}.log 2>&1
 if (( $? != 0 )); then
     RET=1
 fi
 
+torchserve --stop
+
 echo -e "[{\"s_benchmark_kind\":\"benchmark_perf\"," >> ${NAME}.tjson
 echo -e "\"s_benchmark_name\":\"resnet50\"," >> ${NAME}.tjson
-echo -e "\"s_server\":\"tfserving\"," >> ${NAME}.tjson
-echo -e "\"s_protocol\":\"grpc\"," >> ${NAME}.tjson
-echo -e "\"s_framework\":\"savedmodel\"," >> ${NAME}.tjson
+echo -e "\"s_server\":\"torchserve\"," >> ${NAME}.tjson
+echo -e "\"s_protocol\":\"http\"," >> ${NAME}.tjson
+echo -e "\"s_framework\":\"libtorch\"," >> ${NAME}.tjson
 echo -e "\"s_model\":\"${MODEL_NAME}\"," >> ${NAME}.tjson
 echo -e "\"l_concurrency\":1," >> ${NAME}.tjson
 echo -e "\"l_batch_size\":1," >> ${NAME}.tjson
 echo -e "\"l_instance_count\":1}]" >> ${NAME}.tjson
 
-if [ -f $REPORTER ]; then
-    set +e
-
-    URL_FLAG=
-    if [ ! -z ${BENCHMARK_REPORTER_URL} ]; then
-        URL_FLAG="-u ${BENCHMARK_REPORTER_URL}"
-    fi
-
-    $REPORTER -v -o ${NAME}.json --csv ${NAME}.csv ${URL_FLAG} ${NAME}.tjson
-    if (( $? != 0 )); then
-        RET=1
-    fi
-
-    set -e
-fi
-
-# Large static batch size case.
-STATIC_BATCH=128
-NAME=${MODEL_NAME}_sbatch${STATIC_BATCH}
-$PERF_ANALYZER -m ${MODEL_NAME} --service-kind tfserving -i grpc -b ${STATIC_BATCH} -p 5000 -f ${NAME}.csv >> ${NAME}.log 2>&1
-if (( $? != 0 )); then
-    RET=1
-fi
-
-echo -e "[{\"s_benchmark_kind\":\"benchmark_perf\"," >> ${NAME}.tjson
-echo -e "\"s_benchmark_name\":\"resnet50\"," >> ${NAME}.tjson
-echo -e "\"s_server\":\"tfserving\"," >> ${NAME}.tjson
-echo -e "\"s_protocol\":\"grpc\"," >> ${NAME}.tjson
-echo -e "\"s_framework\":\"savedmodel\"," >> ${NAME}.tjson
-echo -e "\"s_model\":\"${MODEL_NAME}\"," >> ${NAME}.tjson
-echo -e "\"l_concurrency\":1," >> ${NAME}.tjson
-echo -e "\"l_batch_size\":128," >> ${NAME}.tjson
-echo -e "\"l_instance_count\":1}]" >> ${NAME}.tjson
 
 if [ -f $REPORTER ]; then
     set +e
