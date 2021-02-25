57000bcf732ff6864e36a6e92f4421e4defba8de
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2021-02-24 17:44:21 -0800

    Add option to inform TensorRT backend to prepare next batch early (#2527)
    * Add option to inform TensorRT backend to prepare next batch early
    
    * Fix plan backend context destructor on initialization error

--- src/core/backend_context.h
+++ src/core/backend_context.h
@@ -229,12 +229,14 @@ class BackendInputCollector {
       const std::vector<std::unique_ptr<InferenceRequest>>& requests,
       std::vector<std::unique_ptr<InferenceResponse>>* responses,
       const bool pinned_enabled, const size_t kernel_buffer_threshold,
-      cudaStream_t stream, cudaEvent_t event = nullptr)
+      cudaStream_t stream, cudaEvent_t event = nullptr,
+      cudaEvent_t buffer_ready_event = nullptr)
       : need_sync_(false), requests_(requests), responses_(responses),
         pinned_enabled_(pinned_enabled),
         kernel_buffer_threshold_(kernel_buffer_threshold),
         use_async_cpu_copy_(AsyncWorkQueue::WorkerCount() > 1), stream_(stream),
-        event_(event), pending_pinned_byte_size_(0), pending_pinned_offset_(0),
+        event_(event), buffer_ready_event_(buffer_ready_event),
+        pending_pinned_byte_size_(0), pending_pinned_offset_(0),
         pending_copy_kernel_buffer_byte_size_(0),
         pending_copy_kernel_buffer_offset_(0),
         pending_copy_kernel_input_buffer_counts_(0), async_task_count_(0)
@@ -287,7 +289,8 @@ class BackendInputCollector {
       const TRITONSERVER_MemoryType tensor_memory_type,
       const int64_t tensor_memory_type_id,
       const TRITONSERVER_MemoryType use_pinned_memory_type,
-      const bool use_kernel, std::unique_ptr<InferenceResponse>* response);
+      const bool use_kernel, const bool wait_buffer,
+      std::unique_ptr<InferenceResponse>* response);
   template <typename T>
   Status SetElementCount(
       const std::string& source_input, char* buffer,
@@ -305,6 +308,7 @@ class BackendInputCollector {
   const bool use_async_cpu_copy_;
   cudaStream_t stream_;
   cudaEvent_t event_;
+  cudaEvent_t buffer_ready_event_;
 
   using RequestsList = std::vector<std::pair<
       std::unique_ptr<InferenceResponse>*, const InferenceRequest::Input*>>;
