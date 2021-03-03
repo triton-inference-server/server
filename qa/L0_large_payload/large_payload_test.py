896af7a7da28d27d932aad7ea250420e7cb625df
Hemant Jain <hemanham@gmail.com>
2021-03-03 08:26:31 -0800

    Add python backend to nomodel perf benchmark (#2577)
    * Add python to nomodel perf benchmark
    
    * Replace legacy custom model with new custom identity model
    
    * change i/o names for python identity model to be consistent with Triton naming convention
    
    * Fix typos that caused test failure

--- qa/L0_large_payload/large_payload_test.py
+++ qa/L0_large_payload/large_payload_test.py
@@ -163,7 +163,7 @@ class LargePayLoadTest(tu.TestResultCollector):
         for client in self._clients:
             model_name = tu.get_zero_model_name("python_nobatch", 1,
                                                 self._data_type)
-            self._test_helper(client, model_name, "IN", "OUT")
+            self._test_helper(client, model_name)
 
     def test_plan(self):
         # plan_nobatch_zero_1_float32 is identity model with input shape [-1]
