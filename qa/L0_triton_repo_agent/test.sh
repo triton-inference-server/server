e7e6ce0646cf0a3d35caae455f858817a29ad601
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2021-03-03 13:57:52 -0800

    Invoke agent model load action at polling so the final location is treated as if it was directly presented by the user (#2550)
    * Invoke agent model load action at polling so the final location is treated as if it was directly presented by the user
    
    * Fix config returned by agent model
    
    * Add test for chained repo agent with location update
    
    * Return empty JSON string for empty config
    
    * Fix check on agent model config not to interfere the polling if unavailable

--- qa/L0_triton_repo_agent/test.sh
+++ qa/L0_triton_repo_agent/test.sh
@@ -25,15 +25,27 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+REPO_VERSION=${NVIDIA_TRITON_SERVER_VERSION}
+if [ "$#" -ge 1 ]; then
+    REPO_VERSION=$1
+fi
+if [ -z "$REPO_VERSION" ]; then
+    echo -e "Repository version must be specified"
+    echo -e "\n***\n*** Test Failed\n***"
+    exit 1
+fi
+
+source ../common/util.sh
+
+RET=0
+
 TEST_LOG="./triton_repo_agent_test.log"
 TRITON_REPO_AGENT_TEST=./triton_repo_agent_test
 
 
-RET=0
-
 export CUDA_VISIBLE_DEVICES=0
 
-rm -f TEST_LOG
+rm -fr *.log
 
 set +e
 $TRITON_REPO_AGENT_TEST >>$TEST_LOG 2>&1
@@ -43,10 +55,42 @@ if [ $? -ne 0 ]; then
 fi
 set -e
 
+rm -rf /opt/tritonserver/repoagents/relocation
+mkdir -p /opt/tritonserver/repoagents/relocation &&
+    cp libtritonrepoagent_relocation.so /opt/tritonserver/repoagents/relocation/.
+
+SERVER=/opt/tritonserver/bin/tritonserver
+
+SERVER_ARGS="--model-repository=`pwd`/models"
+SERVER_LOG="./inference_server.log"
+run_server
+if [ "$SERVER_PID" != "0" ]; then
+    kill $SERVER_PID
+    wait $SERVER_PID
+
+    echo -e "\n***\n*** Expect fail to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+set +e
+grep "Poll failed for model directory 'relocation_sanity_check': Relocation repoagent expects config does not contain 'model_repository_agents' field when 'empty_config' has value 'true' for relocation agent" $SERVER_LOG
+if [ $? -ne 0 ]; then
+    echo -e "\n***\n*** Failed. Expected repo agent of 'relocation_sanity_check' returns error on load\n***"
+    RET=1
+fi
+grep "Poll failed for model directory 'chain_relocation': Relocation repoagent" $SERVER_LOG
+if [ $? -eq 0 ]; then
+    echo -e "\n***\n*** Failed. Expected repo agent of 'chain_relocation' returns success on load\n***"
+    RET=1
+fi
+set -e
+
 if [ $RET -eq 0 ]; then
     echo -e "\n***\n*** Test Passed\n***"
 else
     cat $TEST_LOG
+    cat $SERVER_LOG
     echo -e "\n***\n*** Test FAILED\n***"
 fi
 
