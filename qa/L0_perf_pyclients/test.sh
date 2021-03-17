aabfd6d6343e3f27060ee84c56d056bf1f594213
Tanmay Verma <tanmay2592@gmail.com>
2021-03-17 15:04:15 -0700

    Adding simple perf test for python client library

---
+++ qa/L0_perf_pyclients/test.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+if [ "$#" -ge 1 ]; then
+    REPO_VERSION=$1
+fi
+if [ -z "$REPO_VERSION" ]; then
+    echo -e "Repository version must be specified"
+    echo -e "\n***\n*** Test Failed\n***"
+    exit 1
+fi
+
+REPORTER=../common/reporter.py
+CLIENT_LOG="./simple_perf_client.log"
+SIMPLE_PERF_CLIENT=simple_perf_client.py
+
+rm -f *.log *.csv *.tjson *.json
+
+RET=0
+
+MODEL_NAME="custom_zero_1_int32"
+
+run_server
+if [ "$SERVER_PID" == "0" ]; then
+    echo -e "\n***\n*** Failed to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+# Collect HTTP logs
+# To warmup the model
+python $SIMPLE_PERF_CLIENT -m $MODEL_NAME --shape 100000 
+# Collect data
+NAME=${MODEL_NAME}_http
+python $SIMPLE_PERF_CLIENT -m $MODEL_NAME --shape 100000  -csv ${NAME}.csv >> ${NAME}.log 2>&1
+if (( $? != 0 )); then
+    RET=1
+fi
+
+echo -e "[{\"s_benchmark_kind\":\"benchmark_perf\"," >> ${NAME}.tjson
+echo -e "\"s_benchmark_name\":\"python_client\"," >> ${NAME}.tjson
+echo -e "\"s_server\":\"triton\"," >> ${NAME}.tjson
+echo -e "\"s_protocol\":\"http\"," >> ${NAME}.tjson
+echo -e "\"s_framework\":\"custom\"," >> ${NAME}.tjson
+echo -e "\"s_model\":\"${MODEL_NAME}\"," >> ${NAME}.tjson
+echo -e "\"l_concurrency\":1," >> ${NAME}.tjson
+echo -e "\"l_batch_size\":1," >> ${NAME}.tjson
+echo -e "\"l_instance_count\":1}]" >> ${NAME}.tjson
+
+
+if [ -f $REPORTER ]; then
+    set +e
+
+    URL_FLAG=
+    if [ ! -z ${BENCHMARK_REPORTER_URL} ]; then
+        URL_FLAG="-u ${BENCHMARK_REPORTER_URL}"
+    fi
+
+    python $REPORTER -v -o ${NAME}.json --csv ${NAME}.csv ${URL_FLAG} ${NAME}.tjson
+    if (( $? != 0 )); then
+        RET=1
+    fi
+
+    set -e
+fi
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+run_server
+if [ "$SERVER_PID" == "0" ]; then
+    echo -e "\n***\n*** Failed to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+# Collect grpc logs
+# To warmup the model
+python $SIMPLE_PERF_CLIENT -m $MODEL_NAME --shape 100000 -i grpc -u localhost:8001
+# Collect data
+NAME=${MODEL_NAME}_grpc
+python $SIMPLE_PERF_CLIENT -m $MODEL_NAME --shape 100000 -i grpc -u localhost:8001 -csv ${NAME}.csv >> ${NAME}.log 2>&1
+if (( $? != 0 )); then
+    RET=1
+fi
+
+echo -e "[{\"s_benchmark_kind\":\"benchmark_perf\"," >> ${NAME}.tjson
+echo -e "\"s_benchmark_name\":\"python_client\"," >> ${NAME}.tjson
+echo -e "\"s_server\":\"triton\"," >> ${NAME}.tjson
+echo -e "\"s_protocol\":\"grpc\"," >> ${NAME}.tjson
+echo -e "\"s_framework\":\"custom\"," >> ${NAME}.tjson
+echo -e "\"s_model\":\"${MODEL_NAME}\"," >> ${NAME}.tjson
+echo -e "\"l_concurrency\":1," >> ${NAME}.tjson
+echo -e "\"l_batch_size\":1," >> ${NAME}.tjson
+echo -e "\"l_instance_count\":1}]" >> ${NAME}.tjson
+
+
+if [ -f $REPORTER ]; then
+    set +e
+
+    URL_FLAG=
+    if [ ! -z ${BENCHMARK_REPORTER_URL} ]; then
+        URL_FLAG="-u ${BENCHMARK_REPORTER_URL}"
+    fi
+
+    python $REPORTER -v -o ${NAME}.json --csv ${NAME}.csv ${URL_FLAG} ${NAME}.tjson
+    if (( $? != 0 )); then
+        RET=1
+    fi
+
+    set -e
+fi
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+
+
+if (( $RET == 0 )); then
+    echo -e "\n***\n*** Test Passed\n***"
+else
+    echo -e "\n***\n*** Test FAILED\n***"
+fi
+
+exit $RET
