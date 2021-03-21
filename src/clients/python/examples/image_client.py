46d3f3d7d725c343e142659b4c5c8e48556d8333
Dongwoo Kim <kimdwkimdw@gmail.com>
2021-03-21 16:26:46 +0900

    merge parse_model_http and parse_model_grpc

--- src/clients/python/examples/image_client.py
+++ src/clients/python/examples/image_client.py
@@ -32,6 +32,7 @@ import sys
 
 from PIL import Image
 import numpy as np
+from attrdict import AttrDict
 
 import tritonclient.grpc as grpcclient
 import tritonclient.grpc.model_config_pb2 as mc
@@ -60,7 +61,7 @@ def completion_callback(user_data, result, error):
 FLAGS = None
 
 
-def parse_model_grpc(model_metadata, model_config):
+def parse_model(model_metadata, model_config):
     """
     Check the configuration of a model to make sure it meets the
     requirements for an image classification network (as expected by
@@ -111,6 +112,10 @@ def parse_model_grpc(model_metadata, model_config):
                 format(expected_input_dims, model_metadata.name,
                        len(input_metadata.shape)))
 
+    if type(input_config.format) == str:
+        FORMAT_ENUM_TO_INT = dict(mc.ModelInput.Format.items())
+        input_config.format = FORMAT_ENUM_TO_INT[input_config.format]
+
     if ((input_config.format != mc.ModelInput.FORMAT_NCHW) and
             (input_config.format != mc.ModelInput.FORMAT_NHWC)):
         raise Exception("unexpected input format " +
@@ -132,79 +137,6 @@ def parse_model_grpc(model_metadata, model_config):
     return (model_config.max_batch_size, input_metadata.name,
             output_metadata.name, c, h, w, input_config.format,
             input_metadata.datatype)
-
-
-def parse_model_http(model_metadata, model_config):
-    """
-    Check the configuration of a model to make sure it meets the
-    requirements for an image classification network (as expected by
-    this client)
-    """
-    if len(model_metadata['inputs']) != 1:
-        raise Exception("expecting 1 input, got {}".format(
-            len(model_metadata['inputs'])))
-    if len(model_metadata['outputs']) != 1:
-        raise Exception("expecting 1 output, got {}".format(
-            len(model_metadata['outputs'])))
-
-    if len(model_config['input']) != 1:
-        raise Exception(
-            "expecting 1 input in model configuration, got {}".format(
-                len(model_config['input'])))
-
-    input_metadata = model_metadata['inputs'][0]
-    input_config = model_config['input'][0]
-    output_metadata = model_metadata['outputs'][0]
-
-    max_batch_size = 0
-    if 'max_batch_size' in model_config:
-        max_batch_size = model_config['max_batch_size']
-
-    if output_metadata['datatype'] != "FP32":
-        raise Exception("expecting output datatype to be FP32, model '" +
-                        model_metadata['name'] + "' output type is " +
-                        output_metadata['datatype'])
-
-    # Output is expected to be a vector. But allow any number of
-    # dimensions as long as all but 1 is size 1 (e.g. { 10 }, { 1, 10
-    # }, { 10, 1, 1 } are all ok). Ignore the batch dimension if there
-    # is one.
-    output_batch_dim = (max_batch_size > 0)
-    non_one_cnt = 0
-    for dim in output_metadata['shape']:
-        if output_batch_dim:
-            output_batch_dim = False
-        elif dim > 1:
-            non_one_cnt += 1
-            if non_one_cnt > 1:
-                raise Exception("expecting model output to be a vector")
-
-    # Model input must have 3 dims (not counting the batch dimension),
-    # either CHW or HWC
-    input_batch_dim = (max_batch_size > 0)
-    expected_input_dims = 3 + (1 if input_batch_dim else 0)
-    if len(input_metadata['shape']) != expected_input_dims:
-        raise Exception(
-            "expecting input to have {} dimensions, model '{}' input has {}".
-                format(expected_input_dims, model_metadata['name'],
-                       len(input_metadata['shape'])))
-
-    if ((input_config['format'] != "FORMAT_NCHW") and
-            (input_config['format'] != "FORMAT_NHWC")):
-        raise Exception("unexpected input format " + input_config['format'] +
-                        ", expecting FORMAT_NCHW or FORMAT_NHWC")
-
-    if input_config['format'] == "FORMAT_NHWC":
-        h = input_metadata['shape'][1 if input_batch_dim else 0]
-        w = input_metadata['shape'][2 if input_batch_dim else 1]
-        c = input_metadata['shape'][3 if input_batch_dim else 2]
-    else:
-        c = input_metadata['shape'][1 if input_batch_dim else 0]
-        h = input_metadata['shape'][2 if input_batch_dim else 1]
-        w = input_metadata['shape'][3 if input_batch_dim else 2]
-
-    return (max_batch_size, input_metadata['name'], output_metadata['name'], c,
-            h, w, input_config['format'], input_metadata['datatype'])
 
 
 def preprocess(img, format, dtype, c, h, w, scaling, protocol):
@@ -238,16 +170,10 @@ def preprocess(img, format, dtype, c, h, w, scaling, protocol):
         scaled = typed
 
     # Swap to CHW if necessary
-    if protocol == "grpc":
-        if format == mc.ModelInput.FORMAT_NCHW:
-            ordered = np.transpose(scaled, (2, 0, 1))
-        else:
-            ordered = scaled
+    if format == mc.ModelInput.FORMAT_NCHW:
+        ordered = np.transpose(scaled, (2, 0, 1))
     else:
-        if format == "FORMAT_NCHW":
-            ordered = np.transpose(scaled, (2, 0, 1))
-        else:
-            ordered = scaled
+        ordered = scaled
 
     # Channels are in RGB order. Currently model configuration data
     # doesn't provide any information as to other channel orderings
@@ -278,29 +204,27 @@ def postprocess(results, output_name, batch_size, batching):
 
 
 def requestGenerator(batched_image_data, input_name, output_name, dtype, FLAGS):
-    # Set the input data
-    inputs = []
-    if FLAGS.protocol.lower() == "grpc":
-        inputs.append(
-            grpcclient.InferInput(input_name, batched_image_data.shape, dtype))
-        inputs[0].set_data_from_numpy(batched_image_data)
-    else:
-        inputs.append(
-            httpclient.InferInput(input_name, batched_image_data.shape, dtype))
-        inputs[0].set_data_from_numpy(batched_image_data, binary_data=True)
+    protocol = FLAGS.protocol.lower()
 
-    outputs = []
-    if FLAGS.protocol.lower() == "grpc":
-        outputs.append(
-            grpcclient.InferRequestedOutput(output_name,
-                                            class_count=FLAGS.classes))
+    if protocol == "grpc":
+        client = grpcclient
     else:
-        outputs.append(
-            httpclient.InferRequestedOutput(output_name,
-                                            binary_data=True,
-                                            class_count=FLAGS.classes))
+        client = httpclient
+
+    # Set the input data
+    inputs = [client.InferInput(input_name, batched_image_data.shape, dtype)]
+    inputs[0].set_data_from_numpy(batched_image_data)
+
+    outputs = [client.InferRequestedOutput(output_name, class_count=FLAGS.classes)]
 
     yield inputs, outputs, FLAGS.model_name, FLAGS.model_version
+
+
+def convert_http_metadata_config(_metadata, _config):
+    _model_metadata = AttrDict(_metadata)
+    _model_config = AttrDict(_config)
+
+    return _model_metadata, _model_config
 
 
 if __name__ == '__main__':
@@ -411,11 +335,12 @@ if __name__ == '__main__':
         sys.exit(1)
 
     if FLAGS.protocol.lower() == "grpc":
-        max_batch_size, input_name, output_name, c, h, w, format, dtype = parse_model_grpc(
-            model_metadata, model_config.config)
+        model_config = model_config.config
     else:
-        max_batch_size, input_name, output_name, c, h, w, format, dtype = parse_model_http(
-            model_metadata, model_config)
+        model_metadata, model_config = convert_http_metadata_config(model_metadata, model_config)
+
+    max_batch_size, input_name, output_name, c, h, w, format, dtype = parse_model(
+        model_metadata, model_config)
 
     filenames = []
     if os.path.isdir(FLAGS.image_filename):
