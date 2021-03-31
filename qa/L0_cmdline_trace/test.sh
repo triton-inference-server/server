635ab6bcb7aff30a8623d97968c4a7ac3725a925
GuanLuo <gluo@nvidia.com>
2021-03-31 10:25:44 -0700

    Improve test. Update copyright

--- qa/L0_cmdline_trace/test.sh
+++ qa/L0_cmdline_trace/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2019-2021, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -295,11 +295,26 @@ set +e
 
 $TRACE_SUMMARY -t trace_ensemble.log > summary_ensemble.log
 
+# Check if the traces are captured with proper hierarchy
 if [ `grep -c "COMPUTE_INPUT_END" summary_ensemble.log` != "7" ]; then
-    cat summary_ensemble.log
-    echo -e "\n***\n*** Test Failed\n***"
+    echo -e "Ensemble trace log expects 7 compute"
     RET=1
 fi
+for trace_str in \
+        '{"id":3,"model_name":"simple","model_version":1}' \
+        '{"id":4,"model_name":"nop_TYPE_INT32_-1","model_version":1,"parent_id":3}' \
+        '{"id":5,"model_name":"fan_plan_int32_int32_int32","model_version":1,"parent_id":3}' \
+        '{"id":6,"model_name":"nop_TYPE_INT32_-1","model_version":1,"parent_id":5}' \
+        '{"id":7,"model_name":"plan_int32_int32_int32","model_version":1,"parent_id":5}' \
+        '{"id":8,"model_name":"nop_TYPE_INT32_-1","model_version":1,"parent_id":5}' \
+        '{"id":9,"model_name":"nop_TYPE_INT32_-1","model_version":1,"parent_id":5}' \
+        '{"id":10,"model_name":"nop_TYPE_INT32_-1","model_version":1,"parent_id":3}' \
+        '{"id":11,"model_name":"nop_TYPE_INT32_-1","model_version":1,"parent_id":3}' ; do
+    if [ `grep -c ${trace_str} trace_ensemble.log` != "1" ]; then
+        echo -e "Ensemble trace log expects trace: ${trace_str}"
+        RET=1
+    fi
+done
 
 if [ `grep -c ^simple summary_ensemble.log` != "1" ]; then
     cat summary_ensemble.log
