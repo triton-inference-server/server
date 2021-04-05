87a6cb356d1df32373c85349b8431860fe8c62b1
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2021-04-05 09:55:23 -0700

    Fix TRT log declaration (#2697)

--- src/backends/tensorrt/logging.cc
+++ src/backends/tensorrt/logging.cc
@@ -33,7 +33,7 @@ namespace nvidia { namespace inferenceserver {
 TensorRTLogger tensorrt_logger;
 
 void
-TensorRTLogger::log(Severity severity, const char* msg)
+TensorRTLogger::log(Severity severity, const char* msg) noexcept
 {
   switch (severity) {
     case Severity::kINTERNAL_ERROR:
