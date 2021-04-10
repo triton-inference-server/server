972ea9bcfbdf7709bda8ede58a3d87ed339ebb5c
DougM <mengdong0427@gmail.com>
2021-04-09 20:05:27 -0700

    21.04

--- deploy/gke-marketplace-app/server-deployer/build_and_push.sh
+++ deploy/gke-marketplace-app/server-deployer/build_and_push.sh
@@ -28,7 +28,7 @@ export REGISTRY=gcr.io/$(gcloud config get-value project | tr ':' '/')
 export APP_NAME=tritonserver
 export MAJOR_VERSION=2.8
 export MINOR_VERSION=2.8.7
-export NGC_VERSION=21.03-py3
+export NGC_VERSION=21.04-py3
 
 docker pull nvcr.io/nvidia/$APP_NAME:$NGC_VERSION
 
