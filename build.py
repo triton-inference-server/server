144024043e214cc9bce0ee23363edd0e77c2931c
CoderHam <hemanham@gmail.com>
2021-04-15 09:12:54 -0700

    change tmp container version flag

--- build.py
+++ build.py
@@ -64,7 +64,7 @@ from distutils.dir_util import copy_tree
 # incorrectly load the other version of the openvino libraries.
 #
 TRITON_VERSION_MAP = {
-    '2.10.0dev': ('21.05dev', '21.03', '1.7.1', '2021.2.200', '2021.2.200')
+    '2.10.0dev': ('21.04dev_hemantj', '21.03', '1.7.1', '2021.2.200', '2021.2.200')
 }
 
 EXAMPLE_BACKENDS = ['identity', 'square', 'repeat']
