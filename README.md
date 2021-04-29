9afb6f58054a426941a67d6a7d2bd3863f8df1b8
David Zier <42390249+dzier@users.noreply.github.com>
2021-04-29 09:01:46 -0700

    Update README and docs post-21.04 release (#2787)

--- README.md
+++ README.md
@@ -32,9 +32,9 @@
 
 **LATEST RELEASE: You are currently on the master branch which tracks
 under-development progress towards the next release. The latest
-release of the Triton Inference Server is 2.8.0 and is available on
+release of the Triton Inference Server is 2.9.0 and is available on
 branch
-[r21.03](https://github.com/triton-inference-server/server/tree/r21.03).**
+[r21.04](https://github.com/triton-inference-server/server/tree/r21.04).**
 
 Triton Inference Server provides a cloud and edge inferencing solution
 optimized for both CPUs and GPUs. Triton supports an HTTP/REST and
@@ -44,11 +44,11 @@ available as a shared library with a C API that allows the full
 functionality of Triton to be included directly in an
 application.
 
-The current release of the Triton Inference Server is 2.8.0 and
-corresponds to the 21.03 release of the tritonserver container on
+The current release of the Triton Inference Server is 2.9.0 and
+corresponds to the 21.04 release of the tritonserver container on
 [NVIDIA GPU Cloud (NGC)](https://ngc.nvidia.com). The branch for this
 release is
-[r21.03](https://github.com/triton-inference-server/server/tree/r21.03).
+[r21.04](https://github.com/triton-inference-server/server/tree/r21.04).
 
 ## Features
 
@@ -107,8 +107,8 @@ release is
 
 **The master branch documentation tracks the upcoming,
 under-development release and so may not be accurate for the current
-release of Triton. See the [r21.03
-documentation](https://github.com/triton-inference-server/server/tree/r21.03#documentation)
+release of Triton. See the [r21.04
+documentation](https://github.com/triton-inference-server/server/tree/r21.04#documentation)
 for the current release.**
 
 [Triton Architecture](docs/architecture.md) gives a high-level
--- build.py
+++ build.py
@@ -1070,21 +1070,21 @@ if __name__ == '__main__':
         action='append',
         required=False,
         help=
-        'Include specified backend in build as <backend-name>[:<repo-tag>]. If <repo-tag> starts with "pull/" then it refers to a pull-request reference, otherwise <repo-tag> indicates the git tag/branch to use for the build. If the version is non-development then the default <repo-tag> is the release branch matching the container version (e.g. version 21.03 -> branch r21.03); otherwise the default <repo-tag> is "main" (e.g. version 21.03dev -> branch main).'
+        'Include specified backend in build as <backend-name>[:<repo-tag>]. If <repo-tag> starts with "pull/" then it refers to a pull-request reference, otherwise <repo-tag> indicates the git tag/branch to use for the build. If the version is non-development then the default <repo-tag> is the release branch matching the container version (e.g. version 21.04 -> branch r21.04); otherwise the default <repo-tag> is "main" (e.g. version 21.04dev -> branch main).'
     )
     parser.add_argument(
         '--repo-tag',
         action='append',
         required=False,
         help=
-        'The version of a component to use in the build as <component-name>:<repo-tag>. <component-name> can be "common", "core", "backend" or "thirdparty". If <repo-tag> starts with "pull/" then it refers to a pull-request reference, otherwise <repo-tag> indicates the git tag/branch. If the version is non-development then the default <repo-tag> is the release branch matching the container version (e.g. version 21.03 -> branch r21.03); otherwise the default <repo-tag> is "main" (e.g. version 21.03dev -> branch main).'
+        'The version of a component to use in the build as <component-name>:<repo-tag>. <component-name> can be "common", "core", "backend" or "thirdparty". If <repo-tag> starts with "pull/" then it refers to a pull-request reference, otherwise <repo-tag> indicates the git tag/branch. If the version is non-development then the default <repo-tag> is the release branch matching the container version (e.g. version 21.04 -> branch r21.04); otherwise the default <repo-tag> is "main" (e.g. version 21.04dev -> branch main).'
     )
     parser.add_argument(
         '--repoagent',
         action='append',
         required=False,
         help=
-        'Include specified repo agent in build as <repoagent-name>[:<repo-tag>]. If <repo-tag> starts with "pull/" then it refers to a pull-request reference, otherwise <repo-tag> indicates the git tag/branch to use for the build. If the version is non-development then the default <repo-tag> is the release branch matching the container version (e.g. version 21.03 -> branch r21.03); otherwise the default <repo-tag> is "main" (e.g. version 21.03dev -> branch main).'
+        'Include specified repo agent in build as <repoagent-name>[:<repo-tag>]. If <repo-tag> starts with "pull/" then it refers to a pull-request reference, otherwise <repo-tag> indicates the git tag/branch to use for the build. If the version is non-development then the default <repo-tag> is the release branch matching the container version (e.g. version 21.04 -> branch r21.04); otherwise the default <repo-tag> is "main" (e.g. version 21.04dev -> branch main).'
     )
 
     FLAGS = parser.parse_args()
