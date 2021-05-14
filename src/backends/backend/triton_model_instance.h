8d02c9e8f1075baf525614120514ec2e12d3e842
Tanmay Verma <tanmay2592@gmail.com>
2021-05-15 02:29:48 +0530

    Add the Optimization Profile APIs for TRT backend (#2859)
    * Add the Optimization Profile APIs for TRT backend
    
    * Use vector instead of set

--- src/backends/backend/triton_model_instance.h
+++ src/backends/backend/triton_model_instance.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2020-2021, NVIDIA CORPORATION. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -50,6 +50,7 @@ class TritonModelInstance {
   TRITONSERVER_InstanceGroupKind Kind() const { return kind_; }
   int32_t DeviceId() const { return device_id_; }
   bool IsPassive() const { return passive_; }
+  const std::vector<std::string>& Profiles() const { return profile_names_; }
 
   TritonModel* Model() { return model_; }
   void* State() { return state_; }
@@ -63,11 +64,11 @@ class TritonModelInstance {
   TritonModelInstance(
       TritonModel* model, const std::string& name, const size_t index,
       const TRITONSERVER_InstanceGroupKind kind, const int32_t device_id,
-      const bool passive);
+      const std::vector<std::string>& profile_names, const bool passive);
   static Status CreateInstance(
       TritonModel* model, const std::string& name, const size_t index,
       const TRITONSERVER_InstanceGroupKind kind, const int32_t device_id,
-      const bool passive);
+      const std::vector<std::string>& profile_names, const bool passive);
 
   // The TritonModel object that owns this instance. The instance
   // holds this as a raw pointer because the lifetime of the model is
@@ -82,6 +83,7 @@ class TritonModelInstance {
   // GPU device to be used by the instance.
   TRITONSERVER_InstanceGroupKind kind_;
   int32_t device_id_;
+  std::vector<std::string> profile_names_;
   bool passive_;
 
   // Reporter for metrics, or nullptr if no metrics should be reported
