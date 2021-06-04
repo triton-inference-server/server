c6cf85932e31f67d0edf8bfa8c6118e4839156d9
David Goodwin <davidg@nvidia.com>
2021-06-04 13:57:38 -0700

    Serialize shared-library operations like dlopen and SetDllDirectory

--- src/core/shared_library.h
+++ src/core/shared_library.h
@@ -25,16 +25,48 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
+#include <memory>
 #include <string>
+#include "src/core/constants.h"
 #include "src/core/status.h"
 
 namespace nvidia { namespace inferenceserver {
 
-Status OpenLibraryHandle(const std::string& path, void** handle);
+// SharedLibrary
+//
+// Utility functions for shared libraries. Because some operations
+// require serialization, this object cannot be directly constructed
+// and must instead be accessed using Acquire().
+class SharedLibrary {
+ public:
+  // Acquire a SharedLibrary object exclusively. Any other attempts to
+  // concurrently acquire a SharedLibrary object will block.
+  // object. Ownership is released by destroying the SharedLibrary
+  // object.
+  static Status Acquire(std::unique_ptr<SharedLibrary>* slib);
 
-Status CloseLibraryHandle(void* handle);
+  ~SharedLibrary();
 
-Status GetEntrypoint(
-    void* handle, const std::string& name, const bool optional, void** befn);
+  // Configuration so that dependent libraries will be searched for in
+  // 'path' during OpenLibraryHandle.
+  Status SetLibraryDirectory(const std::string& path);
+
+  // Reset any configuration done by SetLibraryDirectory.
+  Status ResetLibraryDirectory();
+
+  // Open shared library and return generic handle.
+  Status OpenLibraryHandle(const std::string& path, void** handle);
+
+  // Close shared library.
+  Status CloseLibraryHandle(void* handle);
+
+  // Get a generic pointer for an entrypoint into a shared library.
+  Status GetEntrypoint(
+      void* handle, const std::string& name, const bool optional, void** befn);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SharedLibrary);
+  explicit SharedLibrary() = default;
+};
 
 }}  // namespace nvidia::inferenceserver
