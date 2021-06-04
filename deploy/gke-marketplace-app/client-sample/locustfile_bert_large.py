79bdd0cc01a070be524e7587d78eb3f0ea5cda3a
Dong <mengdong@users.noreply.github.com>
2021-06-04 10:45:02 -0700

    Add copyright (#2961)
    * fix copyright
    
    * NVIDIA CORPORATION & AFFILIATES

--- deploy/gke-marketplace-app/client-sample/locustfile_bert_large.py
+++ deploy/gke-marketplace-app/client-sample/locustfile_bert_large.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -8,7 +8,7 @@
 #  * Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#  * Neither the name of NVIDIA CORPORATION & AFFILIATES nor the names of its
 #    contributors may be used to endorse or promote products derived
 #    from this software without specific prior written permission.
 #
