dcb3d87f428f4ba30ca9427d21018dc61a5df376
Katherine Yang <katheriney@nvidia.com>
2021-06-07 11:16:10 -0700

    removed (c) and added comma for all copyrights

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2020-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2020-21, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
