ab5e6454df731b600a2301d343faefa7d691c7f3
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2021-06-10 08:53:14 -0700

    Fix mock shared library in Triton repo agent unit test (#2996)

--- qa/L0_triton_repo_agent/test.sh
+++ qa/L0_triton_repo_agent/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2020-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -50,7 +50,7 @@ rm -fr *.log
 set +e
 $TRITON_REPO_AGENT_TEST >>$TEST_LOG 2>&1
 if [ $? -ne 0 ]; then
-    echo -e "\n***\n*** Memory Test Failed\n***"
+    echo -e "\n***\n*** Repo Agent Unit Test Failed\n***"
     RET=1
 fi
 set -e
