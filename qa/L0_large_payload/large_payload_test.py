fc994644a40f67c1f14ae1ad5cf7cc3fd4347d04
Kris Hung <krish@krish-dt.nvidia.com>
2021-07-06 09:55:54 -0700

    Run formatter

--- qa/L0_large_payload/large_payload_test.py
+++ qa/L0_large_payload/large_payload_test.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright 2019-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -37,6 +37,7 @@ from tritonclientutils import np_to_triton_dtype, InferenceServerException
 
 
 class LargePayLoadTest(tu.TestResultCollector):
+
     def setUp(self):
         self._data_type = np.float32
 
@@ -45,17 +46,17 @@ class LargePayLoadTest(tu.TestResultCollector):
         # except the Python and plan backend should be able to handle payloads
         # larger than 2GBs using HTTP.
         very_large_tensor_shape = (math.trunc(
-            3 * (1024 * 1024 * 1024) / np.dtype(self._data_type).itemsize), )
-        self._very_large_in0 = np.random.random(
-            very_large_tensor_shape).astype(self._data_type)
+            3 * (1024 * 1024 * 1024) / np.dtype(self._data_type).itemsize),)
+        self._very_large_in0 = np.random.random(very_large_tensor_shape).astype(
+            self._data_type)
 
         # 1.9 GBs allows us to test gRPC with moderate sizes too.
         large_tensor_shape = (math.trunc(1.9 * (1024 * 1024 * 1024) //
-                                         np.dtype(self._data_type).itemsize), )
+                                         np.dtype(self._data_type).itemsize),)
         self._large_in0 = np.random.random(large_tensor_shape).astype(
             self._data_type)
 
-        small_tensor_shape = (1, )
+        small_tensor_shape = (1,)
         self._small_in0 = np.random.random(small_tensor_shape).astype(
             self._data_type)
 
@@ -95,7 +96,7 @@ class LargePayLoadTest(tu.TestResultCollector):
                 results = client[1].infer(model_name, inputs)
 
         # FIXME Test is terminated due to libprotobuf FATAL error when GRPC sends
-        # the second request with input tensors larger than 1.3GBs. In this test 
+        # the second request with input tensors larger than 1.3GBs. In this test
         # GRPC has been currently exempted from testing for Very Large tensor(3GBs)
         # until the problem is resolved. Should be uncommented once the GRPC issue is resolved.
         # See DLIS-2474.
