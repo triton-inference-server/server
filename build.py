7b5bda31d8d9b01d6b116cdf434df8c0a0998415
Ashwini Khade <askhade@microsoft.com>
2021-08-04 19:34:16 +0000

    update version for ort

--- build.py
+++ build.py
@@ -68,7 +68,7 @@ TRITON_VERSION_MAP = {
     '2.13.0dev': (
         '21.08dev',  # triton container
         '21.07',  # upstream container
-        '1.8.0',  # ORT
+        '1.8.1',  # ORT
         '2021.2.200',  # ORT OpenVINO
         '2021.2',  # Standalone OpenVINO
         '2.2.8')  # DCGM version
