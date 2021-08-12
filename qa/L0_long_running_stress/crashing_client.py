37b5c75b7f9ebd53efdf672b857d5b80d0422275
Kris Hung <krishung0521@gmail.com>
2021-08-12 13:43:46 -0700

    Create long-running stress test (#3125)
    * Add L0_long_running_stress
    
    * Fix the format
    
    * Use parallel to send multiple requests simultaneously
    
    * Use BACKENDS env var
    
    * Updat result check in nobatch stress
    
    * Add timeout and resnet cases using stress_thread
    
    * Update batched and non-batched request
    
    * Add bad client cases
    
    * Run crashing client inside a loop
    
    * Remove custom
    
    * Update timeout client
    
    * Add libtorch models
    
    * delete models dir
    
    * Run for 7 hours
    
    * Increase timeout
    
    Co-authored-by: Kris Hung <krish@krish-dt.nvidia.com>

---
+++ qa/L0_long_running_stress/crashing_client.py
@@ -0,0 +1,95 @@
+# Copyright 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+sys.path.append("../common")
+
+import numpy as np
+import multiprocessing
+import time
+import test_util as tu
+import argparse
+import tritongrpcclient as grpcclient
+from tritonclientutils import np_to_triton_dtype
+
+
+def crashing_client(model_name,
+                    dtype,
+                    triton_client,
+                    tensor_shape=(1,),
+                    input_name="INPUT0"):
+    in0 = np.random.random(tensor_shape).astype(dtype)
+    if "libtorch" in model_name:
+        input_name = "INPUT__0"
+    inputs = [
+        grpcclient.InferInput(input_name, tensor_shape,
+                              np_to_triton_dtype(dtype)),
+    ]
+    inputs[0].set_data_from_numpy(in0)
+
+    # Run in a loop so that it is guaranteed that
+    # the inference will not have completed when being terminated.
+    while True:
+        results = triton_client.infer(model_name, inputs)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t',
+                        '--trial',
+                        type=str,
+                        required=True,
+                        help='Set trial for the crashing client')
+    FLAGS = parser.parse_args()
+
+    trial = FLAGS.trial
+    dtype = np.float32
+    model_name = tu.get_zero_model_name(trial, 1, dtype)
+
+    triton_client = grpcclient.InferenceServerClient(url="localhost:8001",
+                                                     verbose=True)
+
+    p = multiprocessing.Process(target=crashing_client,
+                                name="crashing_client",
+                                args=(
+                                    model_name,
+                                    dtype,
+                                    triton_client,
+                                ))
+
+    p.start()
+
+    # Terminate the client after 5 seconds
+    time.sleep(5)
+    p.terminate()
+
+    # Cleanup
+    p.join()
+
+    if not triton_client.is_server_live():
+        sys.exit(1)
+
+    sys.exit(0)
---
+++ qa/L0_long_running_stress/test.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+# Copyright 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REPO_VERSION=${NVIDIA_TRITON_SERVER_VERSION}
+if [ "$#" -ge 1 ]; then
+    REPO_VERSION=$1
+fi
+if [ -z "$REPO_VERSION" ]; then
+    echo -e "Repository version must be specified"
+    echo -e "\n***\n*** Test Failed\n***"
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=0
+
+CLIENT_LOG="./client.log"
+STRESS_TEST=stress.py
+
+DATADIR=${DATADIR:="/data/inferenceserver/${REPO_VERSION}"}
+SERVER=/opt/tritonserver/bin/tritonserver
+source ../common/util.sh
+
+RET=0
+
+# If BACKENDS not specified, set to all
+BACKENDS=${BACKENDS:="graphdef savedmodel onnx plan libtorch"}
+export BACKENDS
+
+MODEL_DIR=models
+
+rm -fr *.log *.txt *.serverlog models && mkdir models
+
+# Get the datatype to use based on the backend
+function get_datatype () {
+  local dtype='int32'
+  if [[ $1 == "plan" ]] || [[ $1 == "savedmodel" ]]; then
+      dtype='float32'
+  elif [[ $1 == "graphdef" ]]; then
+      dtype='object'
+  fi
+  echo $dtype
+}
+
+# Setup model repository - two instances with batch-size 2
+MODELS=""
+for BACKEND in $BACKENDS; do
+  DTYPE=$(get_datatype $BACKEND)
+  MODELS="$MODELS $DATADIR/qa_sequence_model_repository/${BACKEND}_sequence_${DTYPE}"
+done
+
+for MODEL in $MODELS; do
+    cp -r $MODEL $MODEL_DIR/. && \
+      (cd $MODEL_DIR/$(basename $MODEL) && \
+        sed -i "s/^max_batch_size:.*/max_batch_size: 2/" config.pbtxt && \
+        sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 2/" config.pbtxt && \
+        sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 2/" config.pbtxt)
+done
+
+MODELS=""
+for BACKEND in $BACKENDS; do
+    DTYPE=$(get_datatype $BACKEND)
+    MODELS="$MODELS $DATADIR/qa_sequence_model_repository/${BACKEND}_nobatch_sequence_${DTYPE}"
+done
+
+for MODEL in $MODELS; do
+    cp -r $MODEL $MODEL_DIR/. && \
+      (cd $MODEL_DIR/$(basename $MODEL) && \
+        sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 2/" config.pbtxt && \
+        sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 2/" config.pbtxt)
+done
+
+for BACKEND in $BACKENDS; do
+    cp -r $DATADIR/qa_identity_model_repository/${BACKEND}_nobatch_zero_1_float32 \
+      $MODEL_DIR/.
+done
+
+cp -r $DATADIR/tf_model_store/resnet_v1_50_graphdef $MODEL_DIR/resnet_v1_50_graphdef_def && \
+  (cd $MODEL_DIR/resnet_v1_50_graphdef_def && \
+    sed -i 's/^name: "resnet_v1_50_graphdef"/name: "resnet_v1_50_graphdef_def"/' config.pbtxt && \
+    echo "optimization { }" >> config.pbtxt)
+
+SERVER_ARGS="--model-repository=`pwd`/$MODEL_DIR"
+SERVER_LOG="./serverlog"
+run_server
+if [ "$SERVER_PID" == "0" ]; then
+    echo -e "\n***\n*** Failed to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+set +e
+python $STRESS_TEST >>$CLIENT_LOG 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "\n***\n*** Test Failed\n***"
+    RET=1
+fi
+set -e
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+if [ $RET -eq 0 ]; then
+    echo -e "\n***\n*** Test Passed\n***"
+else
+    cat $CLIENT_LOG
+    echo -e "\n***\n*** Test FAILED\n***"
+fi
+
+exit $RET
