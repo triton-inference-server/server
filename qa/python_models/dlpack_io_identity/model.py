f24cac7277229205f9e57d9ad106cc3ed6bffb55
Iman Tabrizian <itabrizian@nvidia.com>
2021-09-01 11:06:14 -0400

    Add test for BLS GPU tensor

--- qa/python_models/dlpack_io_identity/model.py
+++ qa/python_models/dlpack_io_identity/model.py
@@ -27,7 +27,6 @@
 import triton_python_backend_utils as pb_utils
 from torch.utils.dlpack import to_dlpack, from_dlpack
 import numpy as np
-import json
 
 
 class TritonPythonModel:
@@ -35,6 +34,9 @@ class TritonPythonModel:
     This Python identity model passes the DLPack tensors as is. "OUTPUT_IS_GPU"
     input controls whether the model should put the output in GPU or in CPU.
     """
+    def initialize(self, args):
+        self._model_name = args['model_name']
+
     def execute(self, requests):
         responses = []
         for request in requests:
@@ -62,6 +64,42 @@ class TritonPythonModel:
 
             next_gpu_output = pb_utils.Tensor("NEXT_GPU_OUTPUT",
                                               gpu_output[1:])
+
+            # Do not perform BLS inference if it is the first
+            # model in the pipeline.
+            if self._model_name != 'dlpack_io_identity_1':
+                infer_request = pb_utils.InferenceRequest(
+                    model_name='dlpack_io_identity_1',
+                    inputs=[
+                        input0,
+                        pb_utils.get_input_tensor_by_name(
+                            request, "GPU_OUTPUT")
+                    ],
+                    requested_output_names=['OUTPUT0'])
+                infer_response = infer_request.exec()
+
+                if infer_response.has_error():
+                    raise pb_utils.TritonModelException(
+                        infer_response.error().message())
+
+                bls_output0 = pb_utils.get_output_tensor_by_name(
+                    infer_response, 'OUTPUT0')
+                if not output0.is_cpu():
+                    bls_output0 = from_dlpack(
+                        bls_output0.to_dlpack()).detach().cpu().numpy()
+                else:
+                    bls_output0 = bls_output0.as_numpy()
+
+                if not input0.is_cpu():
+                    input0 = from_dlpack(
+                        input0.to_dlpack()).detach().cpu().numpy()
+                else:
+                    input0 = input0.as_numpy()
+
+                if not np.allclose(bls_output0, input0):
+                    raise pb_utils.TritonModelException(
+                        'BLS input and output tensors are not equal')
+
             responses.append(
                 pb_utils.InferenceResponse([output0, next_gpu_output]))
 
