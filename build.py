84370fef1e0f0e97649bbde535e62378c6b247e0
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2021-09-30 11:18:29 -0700

    Update ca-certificates (#3423)
    * Update ca-certificates
    
    * Apply the same change in Dockerfile.sdk

--- build.py
+++ build.py
@@ -253,7 +253,7 @@ def core_cmake_args(components, backends, install_dir):
         cmake_enable(FLAGS.enable_gpu)))
     cargs.append('-DTRITON_MIN_COMPUTE_CAPABILITY={}'.format(
         FLAGS.min_compute_capability))
-    
+
     cargs.append('-DTRITON_ENABLE_MALI_GPU:BOOL={}'.format(
         cmake_enable(FLAGS.enable_mali_gpu)))
 
@@ -584,6 +584,7 @@ ENV DEBIAN_FRONTEND=noninteractive
 # scons is needed for armnn_tflite backend build dep
 RUN apt-get update && \
     apt-get install -y --no-install-recommends \
+            ca-certificates \
             autoconf \
             automake \
             build-essential \
@@ -640,7 +641,8 @@ COPY . .
 ENTRYPOINT []
 '''
         if FLAGS.enable_gpu:
-            df += install_dcgm_libraries(argmap['DCGM_VERSION'], target_machine())
+            df += install_dcgm_libraries(argmap['DCGM_VERSION'],
+                                         target_machine())
 
     df += '''
 ENV TRITON_SERVER_VERSION ${TRITON_VERSION}
@@ -710,7 +712,8 @@ FROM ${{BASE_IMAGE}}
 '''.format(argmap['TRITON_VERSION'], argmap['TRITON_CONTAINER_VERSION'],
            argmap['BASE_IMAGE'])
 
-    df += dockerfile_prepare_container_linux(argmap, backends, FLAGS.enable_gpu, target_machine())
+    df += dockerfile_prepare_container_linux(argmap, backends, FLAGS.enable_gpu,
+                                             target_machine())
 
     df += '''
 WORKDIR /opt/tritonserver
@@ -754,7 +757,8 @@ COPY --chown=1000:1000 --from=tritonserver_build /workspace/build/sagemaker/serv
         dfile.write(df)
 
 
-def dockerfile_prepare_container_linux(argmap, backends, enable_gpu, target_machine):
+def dockerfile_prepare_container_linux(argmap, backends, enable_gpu,
+                                       target_machine):
     gpu_enabled = 1 if enable_gpu else 0
     # Common steps to produce docker images shared by build.py and compose.py.
     # Sets enviroment variables, installs dependencies and adds entrypoint
@@ -964,9 +968,7 @@ def container_build(images, backends, repoagents, endpoints):
     # Windows docker runs in a VM and memory needs to be specified
     # explicitly.
     if target_platform() == 'windows':
-        commonargs += [
-            '--memory', FLAGS.container_memory
-        ]
+        commonargs += ['--memory', FLAGS.container_memory]
 
     log_verbose('buildbase container {}'.format(commonargs + cachefromargs))
     create_dockerfile_buildbase(FLAGS.build_dir, 'Dockerfile.buildbase',
@@ -1033,9 +1035,7 @@ def container_build(images, backends, repoagents, endpoints):
         if target_platform() == 'windows':
             # Windows docker runs in a VM and memory needs to be
             # specified explicitly.
-            dockerrunargs += [
-                '--memory', FLAGS.container_memory
-            ]
+            dockerrunargs += ['--memory', FLAGS.container_memory]
             dockerrunargs += [
                 '-v', '\\\\.\pipe\docker_engine:\\\\.\pipe\docker_engine'
             ]
@@ -1145,7 +1145,8 @@ if __name__ == '__main__':
         '--container-memory',
         default="8g",
         required=False,
-        help='Value for Docker --memory argument. Used only for windows builds.')
+        help='Value for Docker --memory argument. Used only for windows builds.'
+    )
     parser.add_argument(
         '--target-platform',
         required=False,
