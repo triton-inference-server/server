f399e56ac22f83f8061000a12c776b9bd8a11597
deadeyegoodwin <davidg@nvidia.com>
2021-10-29 09:27:53 -0700

    Add more error checking to L0_sdk (#3514)

--- qa/L0_sdk/grpc_test.cc
+++ qa/L0_sdk/grpc_test.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2021, NVIDIA CORPORATION. All rights reserved.
+// Copyright 2020-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -24,6 +24,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include <iostream>
 #include "grpc_client.h"
 
 namespace tc = triton::client;
@@ -34,12 +35,21 @@ main(int argc, char* argv[])
   std::unique_ptr<tc::InferenceServerGrpcClient> client;
   // Add a symbol from protobufs to verify correct linking
   inference::ModelConfigResponse model_config;
-  tc::InferenceServerGrpcClient::Create(&client, "localhost:8001");
-  bool live;
-  client->IsServerLive(&live);
+  tc::Error err = tc::InferenceServerGrpcClient::Create(&client, "localhost:8001");
+  if (!err.IsOk()) {
+    std::cerr << "InferenceServerGrpcClient::Create failed: "
+              << err.Message() << std::endl;
+    return 1;
+  }
 
-  if (live == 0) {
+  // No server is running so expect liveness call to fail
+  bool live;
+  err = client->IsServerLive(&live);
+  if (!err.IsOk()) {
+    std::cerr << "InferenceServerGrpcClient::IsServerLive expected fail: "
+              << err.Message() << std::endl;
     return 0;
   }
+
   return 1;
 }
