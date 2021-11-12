93471184fc78ab129f66c1d9b1c41b8d541c2b47
GuanLuo <gluo@nvidia.com>
2021-11-12 10:03:17 -0800

    Fix u

--- src/core/persistent_backend_manager.cc
+++ src/core/persistent_backend_manager.cc
@@ -61,8 +61,7 @@ Status
 PersistentBackendManager::InitPersistentBackends(
     const BackendCmdlineConfigMap& config_map)
 {
-  // [DO NOT MERGE] skipping TF backend for debugging
-  for (const auto& be : {"pytorch", "onnxruntime"}) {
+  for (const auto& be : {"pytorch", "tensorflow", "onnxruntime", "openvino"}) {
     RETURN_IF_ERROR(InitPersistentBackend(be, config_map));
   }
 
