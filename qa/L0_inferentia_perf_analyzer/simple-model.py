71b56956c80f7f6ecdad2845adeeae469b82100e
Katherine Yang <katheriney@nvidia.com>
2021-11-16 11:19:09 -0800

    adding test script and tests

---
+++ qa/L0_inferentia_perf_analyzer/simple-model.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import torch
+from torch import nn
+import torch_neuron
+
+class AddSubNet(nn.Module):
+    """
+    Simple AddSub network in PyTorch. This network outputs the sum and
+    subtraction of the inputs.
+    """
+    def __init__(self):
+        super(AddSubNet, self).__init__()
+        
+
+    def forward(self, input0, input1):
+        return torch.sub(input0, input1, alpha=-1), torch.sub(input0, input1, alpha=1)
+
+
+model = AddSubNet()
+model.eval()
+batch_size = 1
+example_inputs = torch.zeros([4], dtype=torch.int64), torch.zeros([4], dtype=torch.int64)
+model_neuron = torch.neuron.trace(model, example_inputs, dynamic_batch_size=True)
+model_neuron.save('add_sub_model.pt')
+
+'''
+python /home/ubuntu/python_backend/inferentia/scripts/gen_triton_model.py \
+    --triton_input INPUT__0,INT64,4 INPUT__1,INT64,4 \
+    --triton_output OUTPUT__0,INT64,4 OUTPUT__1,INT64,4 \
+    --compiled_model $PWD/add_sub_model.pt \
+    --triton_model_dir models/add-sub-1x4 --neuron_core_range 0:0
+
+
+docker buildx build --progress=plain --pull --push -t tritonsdk -f Dockerfile.sdk --build-arg "TRITON_CLIENT_REPO_SUBDIR=clientrepo"
+docker run -v /home/ubuntu:/home/ubuntu --net host -ti tritonsdk:latest /bin/bash
+perf_analyzer -v -m add-sub-1x4 --input-data=/home/ubuntu/python_backend/inferentia/test_data.json
+'''
