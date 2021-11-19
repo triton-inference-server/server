90bc9d7c1bacc8a7f21448a05576c2bdb743ea77
dzier <dzier@nvidia.com>
2021-11-19 15:22:51 -0800

    Update NGC versions post-21.11 release

--- build.py
+++ build.py
@@ -67,7 +67,7 @@ from distutils.dir_util import copy_tree
 TRITON_VERSION_MAP = {
     '2.17.0dev': (
         '21.12dev',  # triton container
-        '21.10',  # upstream container
+        '21.11',  # upstream container
         '1.9.0',  # ORT
         '2021.2.200',  # ORT OpenVINO
         '2021.2',  # Standalone OpenVINO
