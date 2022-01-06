8e817610768de0064da7d105cb73c2f6a12b9e6b
dyastremsky@nvidia.com <dyastremsky@nvidia.com>
2022-01-06 12:12:28 -0800

    Added float datatype test, update models location.

--- qa/L0_java_simple_example/test.sh
+++ qa/L0_java_simple_example/test.sh
@@ -25,20 +25,13 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-export CUDA_VISIBLE_DEVICES=0
-
-SIMPLE_CLIENT_JAVA="mvn compile -f /opt/tritonserver/javacpp-presets/tritonserver/samples exec:java -Djavacpp.platform=linux-x86_64 -Dexec.args=\"-r /workspace/models\""
-
-SERVER=/opt/tritonserver/bin/tritonserver
-SERVER_ARGS="--model-repository=/workspace/models"
-SERVER_LOG="./inference_server.log"
 source ../common/util.sh
 
 rm -f *.log
 RET=0
 
 # Run with default settings
-mvn compile -f /opt/tritonserver/javacpp-presets/tritonserver/samples exec:java -Djavacpp.platform=linux-x86_64 -Dexec.args="-r /workspace/models" >>client.log 2>&1
+mvn compile -f /opt/tritonserver/javacpp-presets/tritonserver/samples exec:java -Djavacpp.platform=linux-x86_64 -Dexec.args="-r `pwd`/models" >>client.log 2>&1
 if [ $? -ne 0 ]; then
     RET=1
 fi
@@ -49,7 +42,7 @@ if [ `grep -c "1 - 1 = 0" client.log` != "18" ]; then
 fi
 
 # Run with verbose logging
-mvn compile -f /opt/tritonserver/javacpp-presets/tritonserver/samples exec:java -Djavacpp.platform=linux-x86_64 -Dexec.args="-r /workspace/models -v" >>client.log 2>&1
+mvn compile -f /opt/tritonserver/javacpp-presets/tritonserver/samples exec:java -Djavacpp.platform=linux-x86_64 -Dexec.args="-r `pwd`/models -v" >>client.log 2>&1
 if [ $? -ne 0 ]; then
     RET=1
 fi
@@ -60,13 +53,26 @@ if [ `grep -c "Server side auto-completed config" client.log` != "1" ]; then
 fi
 
 # Run with memory set to system
-mvn compile -f /opt/tritonserver/javacpp-presets/tritonserver/samples exec:java -Djavacpp.platform=linux-x86_64 -Dexec.args="-r /workspace/models -m system" >>client.log 2>&1
+mvn compile -f /opt/tritonserver/javacpp-presets/tritonserver/samples exec:java -Djavacpp.platform=linux-x86_64 -Dexec.args="-r `pwd`/models -m system" >>client.log 2>&1
 if [ $? -ne 0 ]; then
     RET=1
 fi
 
 if [ `grep -c "OUTPUT0 is stored in system memory" client.log` != "9" ]; then
     echo -e "\n***\n*** Failed. Expected 9 'OUTPUT0 is stored in system memory'\n***"
+    RET=1
+fi
+
+# Run with FP32 datatype
+sed -i 's/TYPE_INT32/TYPE_FP32/g' `pwd`/models/simple/config.pbtxt
+mvn compile -f /opt/tritonserver/javacpp-presets/tritonserver/samples exec:java -Djavacpp.platform=linux-x86_64 -Dexec.args="-r `pwd`/models -v" >>client.log 2>&1
+sed -i 's/TYPE_FP32/TYPE_INT32/g' `pwd`/models/simple/config.pbtxt
+if [ $? -ne 0 ]; then
+    RET=1
+fi
+
+if [ `grep -c "data_type: TYPE_FP32" client.log` != "4" ]; then
+    echo -e "\n***\n*** Failed. Expected 4 'data_type: TYPE_FP32'\n***"
     RET=1
 fi
 
