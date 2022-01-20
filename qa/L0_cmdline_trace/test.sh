488edc4f580b9b90489111379b0df2dc088a419c
robertzhu <robertzhu@tencent.com>
2022-01-20 19:53:46 +0800

    Added trace tensor API

--- qa/L0_cmdline_trace/test.sh
+++ qa/L0_cmdline_trace/test.sh
@@ -104,6 +104,101 @@ fi
 
 set -e
 
+# trace-rate == 1, trace-level=MIN make sure every request is traced
+SERVER_ARGS="--trace-file=trace_min.log --trace-level=MIN --trace-rate=1 --model-repository=$MODELSDIR"
+SERVER_LOG="./inference_server_min.log"
+run_server
+if [ "$SERVER_PID" == "0" ]; then
+    echo -e "\n***\n*** Failed to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+set +e
+
+for p in {1..10}; do
+    $SIMPLE_HTTP_CLIENT >> client_min.log 2>&1
+    if [ $? -ne 0 ]; then
+        RET=1
+    fi
+
+    $SIMPLE_GRPC_CLIENT >> client_min.log 2>&1
+    if [ $? -ne 0 ]; then
+        RET=1
+    fi
+done
+
+set -e
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+set +e
+
+$TRACE_SUMMARY -t trace_min.log > summary_min.log
+
+if [ `grep -c "COMPUTE_INPUT_END" summary_min.log` != "20" ]; then
+    cat summary_min.log
+    echo -e "\n***\n*** Test Failed\n***"
+    RET=1
+fi
+
+if [ `grep -c ^simple summary_min.log` != "20" ]; then
+    cat summary_min.log
+    echo -e "\n***\n*** Test Failed\n***"
+    RET=1
+fi
+
+set -e
+
+# trace-rate == 9, trace-level=MAX
+SERVER_ARGS="--http-thread-count=1 --trace-file=trace_max.log \
+             --trace-level=MAX --trace-rate=9 --model-repository=$MODELSDIR"
+SERVER_LOG="./inference_server_max.log"
+run_server
+if [ "$SERVER_PID" == "0" ]; then
+    echo -e "\n***\n*** Failed to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+set +e
+
+for p in {1..10}; do
+    $SIMPLE_HTTP_CLIENT >> client_max.log 2>&1
+    if [ $? -ne 0 ]; then
+        RET=1
+    fi
+
+    $SIMPLE_GRPC_CLIENT >> client_max.log 2>&1
+    if [ $? -ne 0 ]; then
+        RET=1
+    fi
+done
+
+set -e
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+set +e
+
+$TRACE_SUMMARY -t trace_max.log > summary_max.log
+
+if [ `grep -c "COMPUTE_INPUT_END" summary_max.log` != "2" ]; then
+    cat summary_max.log
+    echo -e "\n***\n*** Test Failed\n***"
+    RET=1
+fi
+
+if [ `grep -c ^simple summary_max.log` != "2" ]; then
+    cat summary_max.log
+    echo -e "\n***\n*** Test Failed\n***"
+    RET=1
+fi
+
+set -e
+
 # trace-rate == 1, trace-level=TIMESTAMPS make sure every request is traced
 SERVER_ARGS="--trace-file=trace_1.log --trace-level=TIMESTAMPS --trace-rate=1 --model-repository=$MODELSDIR"
 SERVER_LOG="./inference_server_1.log"
@@ -395,6 +490,34 @@ fi
 
 if [ `grep -o TENSOR_BACKEND_OUTPUT trace_ensemble_tensor.log | wc -l` != "14" ]; then
     echo -e "Ensemble trace tensors log expects 14 TENSOR_BACKEND_OUTPUTs"
+    RET=1
+fi
+
+if [ `grep -c '{"id":3,"activity":"TENSOR_QUEUE_INPUT","tensor":{"name":"INPUT0",\
+"data":"0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15","shape":"1,16","dtype":"INT32"}}' \
+trace_ensemble_tensor.log` != "1" ]; then
+    echo -e "Ensemble trace tensors log checks ensemble input 'INPUT0' failed"
+    RET=1
+fi
+
+if [ `grep -c '{"id":3,"activity":"TENSOR_QUEUE_INPUT","tensor":{"name":"INPUT1",\
+"data":"1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1","shape":"1,16","dtype":"INT32"}}' \
+trace_ensemble_tensor.log` != "1" ]; then
+    echo -e "Ensemble trace tensors log checks ensemble input 'INPUT1' failed"
+    RET=1
+fi
+
+if [ `grep -c '{"id":3,"activity":"TENSOR_BACKEND_OUTPUT","tensor":{"name":"OUTPUT0",\
+"data":"1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16","shape":"1,16","dtype":"INT32"}}' \
+trace_ensemble_tensor.log` != "1" ]; then
+    echo -e "Ensemble trace tensors log checks ensemble output 'OUTPUT0' failed"
+    RET=1
+fi
+
+if [ `grep -c '{"id":3,"activity":"TENSOR_BACKEND_OUTPUT","tensor":{"name":"OUTPUT1",\
+"data":"-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14","shape":"1,16","dtype":"INT32"}}' \
+trace_ensemble_tensor.log` != "1" ]; then
+    echo -e "Ensemble trace tensors log checks ensemble output 'OUTPUT1' failed"
     RET=1
 fi
 
