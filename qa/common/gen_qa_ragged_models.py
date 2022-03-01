5d3e3e5e3790325827fbcb2cfc11bc19b4f0e762
GuanLuo <gluo@nvidia.com>
2022-02-28 17:41:09 -0800

    Add model generation for the new batch input type

--- qa/common/gen_qa_ragged_models.py
+++ qa/common/gen_qa_ragged_models.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright 2020-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -136,6 +136,14 @@ def create_savedmodel_modelfile(models_dir, model_version, dtype):
     # "BATCH_AND_SIZE_INPUT" will have shape [2]. Then the model output will
     # be [[2, 4, 1], [2, 4, 1]] and Triton will send responses that each has
     # value [[2, 4, 1]].
+    # For "BATCH_INPUT", the input tensor must only have one variable dimension
+    # to be broadcasted along the batch dimension properly, thus the currently
+    # allowed batch input types are:
+    # - BATCH_ACCUMULATED_ELEMENT_COUNT
+    # - BATCH_ACCUMULATED_ELEMENT_COUNT_WITH_ZERO
+    # - BATCH_MAX_ELEMENT_COUNT_AS_SHAPE
+    # - BATCH_ITEM_SHAPE_FLATTEN
+
     tf_dtype = np_to_tf_dtype(dtype)
 
     tf.reset_default_graph()
@@ -209,6 +217,14 @@ def create_plan_modelfile(models_dir, model_version, dtype):
     # "BATCH_AND_SIZE_INPUT" will have shape [2]. Then the model output will
     # be [[2, 4, 1], [2, 4, 1]] and Triton will send responses that each has
     # value [[2, 4, 1]].
+    # For "BATCH_INPUT", the input tensor must only have one variable dimension
+    # to be broadcasted along the batch dimension properly, thus the currently
+    # allowed batch input types are:
+    # - BATCH_ACCUMULATED_ELEMENT_COUNT
+    # - BATCH_ACCUMULATED_ELEMENT_COUNT_WITH_ZERO
+    # - BATCH_MAX_ELEMENT_COUNT_AS_SHAPE
+    # - BATCH_ITEM_SHAPE_FLATTEN
+
     TRT_LOGGER = trt.Logger(trt.Logger.INFO)
     builder = trt.Builder(TRT_LOGGER)
     network = builder.create_network(
@@ -296,6 +312,13 @@ def create_onnx_modelfile(models_dir, model_version, dtype):
     # "BATCH_AND_SIZE_INPUT" will have shape [2]. Then the model output will
     # be [[2, 4, 1], [2, 4, 1]] and Triton will send responses that each has
     # value [[2, 4, 1]].
+    # For "BATCH_INPUT", the input tensor must only have one variable dimension
+    # to be broadcasted along the batch dimension properly, thus the currently
+    # allowed batch input types are:
+    # - BATCH_ACCUMULATED_ELEMENT_COUNT
+    # - BATCH_ACCUMULATED_ELEMENT_COUNT_WITH_ZERO
+    # - BATCH_MAX_ELEMENT_COUNT_AS_SHAPE
+    # - BATCH_ITEM_SHAPE_FLATTEN
 
     onnx_dtype = np_to_onnx_dtype(dtype)
 
@@ -388,17 +411,11 @@ def create_modelconfig(models_dir, max_batch, model_version, dtype, backend,
                        platform):
     version_policy_str = "{ latest { num_versions: 1 }}"
 
-    # FIXME platform should be renamed and not required after all backends are ported
-    if backend != "tensorrt":
-        backend_spec = '''
+    backend_spec = '''
 backend: "{}"
 '''.format(backend)
-        if backend == "tensorflow":
-            backend_spec += '''
-platform: "{}_{}"
-'''.format(backend, platform)
-    else:
-        backend_spec = '''
+    if backend == "tensorflow":
+        backend_spec += '''
 platform: "{}_{}"
 '''.format(backend, platform)
 
@@ -471,20 +488,236 @@ dynamic_batching {{
         cfile.write(config)
 
 
+def create_savedmodel_itemshape_modelfile(models_dir, model_version, dtype):
+    # Create special identity model for batch input 'BATCH_ITEM_SHAPE' testing,
+    # such model has one ragged input and one batch input, and one output to
+    # return the batch input directly. Because 'BATCH_ITEM_SHAPE' should be
+    # generated to have matching batch dimension, the output can be produced
+    # via identity op and expect Triton will scatter the output properly.
+
+    tf_dtype = np_to_tf_dtype(dtype)
+
+    tf.reset_default_graph()
+    in_node = tf.placeholder(tf_dtype, tu.shape_to_tf_shape([-1]),
+                             "TENSOR_RAGGED_INPUT")
+    # Shape is predefined
+    batch_node = tf.placeholder(tf_dtype, tu.shape_to_tf_shape([-1, 2]),
+                                "TENSOR_BATCH_INPUT")
+    batch_output_node = tf.identity(batch_node, name="TENSOR_BATCH_OUTPUT")
+
+    model_name = "savedmodel_batch_item"
+    model_version_dir = models_dir + "/" + model_name + "/" + str(model_version)
+
+    try:
+        os.makedirs(model_version_dir)
+    except OSError as ex:
+        pass  # ignore existing dir
+
+    with tf.Session() as sess:
+        in_tensor = tf.get_default_graph().get_tensor_by_name(
+            "TENSOR_RAGGED_INPUT:0")
+        batch_tensor = tf.get_default_graph().get_tensor_by_name(
+            "TENSOR_BATCH_INPUT:0")
+        batch_out_tensor = tf.get_default_graph().get_tensor_by_name(
+            "TENSOR_BATCH_OUTPUT:0")
+        tf.saved_model.simple_save(sess,
+                                   model_version_dir + "/model.savedmodel",
+                                   inputs={
+                                       "RAGGED_INPUT": in_tensor,
+                                       "BATCH_INPUT": batch_tensor,
+                                   },
+                                   outputs={
+                                       "BATCH_OUTPUT": batch_out_tensor,
+                                   })
+
+
+def create_plan_itemshape_modelfile(models_dir, model_version, dtype):
+    # Create special identity model for batch input 'BATCH_ITEM_SHAPE' testing,
+    # such model has one ragged input and one batch input, and one output to
+    # return the batch input directly. Because 'BATCH_ITEM_SHAPE' should be
+    # generated to have matching batch dimension, the output can be produced
+    # via identity op and expect Triton will scatter the output properly.
+
+    TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+    builder = trt.Builder(TRT_LOGGER)
+    network = builder.create_network(
+        1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+    trt_dtype = np_to_trt_dtype(dtype)
+
+    in_node = network.add_input("RAGGED_INPUT", trt_dtype, [-1])
+    batch_node = network.add_input("BATCH_INPUT", trt_dtype, [-1, 2])
+
+    batch_out_node = network.add_identity(batch_node)
+    batch_out_node.get_output(0).name = "BATCH_OUTPUT"
+    network.mark_output(batch_out_node.get_output(0))
+
+    # Hard coded optimization profile
+    min_shape = [1]
+    opt_shape = [8]
+    max_shape = [32]
+
+    profile = builder.create_optimization_profile()
+    profile.set_shape("RAGGED_INPUT", min_shape, opt_shape, max_shape)
+    profile.set_shape("BATCH_INPUT", min_shape + [2], opt_shape + [2],
+                      max_shape + [2])
+    config = builder.create_builder_config()
+    config.add_optimization_profile(profile)
+    config.max_workspace_size = 1 << 20
+    try:
+        engine_bytes = builder.build_serialized_network(network, config)
+    except AttributeError:
+        engine = builder.build_engine(network, config)
+        engine_bytes = engine.serialize()
+        del engine
+
+    model_name = "plan_batch_item"
+    model_version_dir = models_dir + "/" + model_name + "/" + str(model_version)
+
+    try:
+        os.makedirs(model_version_dir)
+    except OSError as ex:
+        pass  # ignore existing dir
+
+    with open(model_version_dir + "/model.plan", "wb") as f:
+        f.write(engine_bytes)
+
+    del builder
+
+
+def create_onnx_itemshape_modelfile(models_dir, model_version, dtype):
+    # Create special identity model for batch input 'BATCH_ITEM_SHAPE' testing,
+    # such model has one ragged input and one batch input, and one output to
+    # return the batch input directly. Because 'BATCH_ITEM_SHAPE' should be
+    # generated to have matching batch dimension, the output can be produced
+    # via identity op and expect Triton will scatter the output properly.
+
+    onnx_dtype = np_to_onnx_dtype(dtype)
+
+    # Create the model
+    model_name = "onnx_batch_item"
+    model_version_dir = models_dir + "/" + model_name + "/" + str(model_version)
+
+    in0_shape, idx = tu.shape_to_onnx_shape([-1], 0)
+    batch_shape, idx = tu.shape_to_onnx_shape([-1, 2], 0)
+
+    in0 = onnx.helper.make_tensor_value_info("RAGGED_INPUT", onnx_dtype,
+                                             in0_shape)
+    batch_in = onnx.helper.make_tensor_value_info("BATCH_INPUT", onnx_dtype,
+                                                  batch_shape)
+
+    batch_out_shape, idx = tu.shape_to_onnx_shape([-1, -1], idx)
+    batch_out = onnx.helper.make_tensor_value_info("BATCH_OUTPUT", onnx_dtype,
+                                                   batch_out_shape)
+
+    onnx_nodes = [
+        onnx.helper.make_node("Identity", ["BATCH_INPUT"], ["BATCH_OUTPUT"])
+    ]
+    onnx_inputs = [in0, batch_in]
+    onnx_outputs = [batch_out]
+
+    graph_proto = onnx.helper.make_graph(onnx_nodes, model_name, onnx_inputs,
+                                         onnx_outputs)
+    if FLAGS.onnx_opset > 0:
+        model_opset = onnx.helper.make_operatorsetid("", FLAGS.onnx_opset)
+        model_def = onnx.helper.make_model(graph_proto,
+                                           producer_name="triton",
+                                           opset_imports=[model_opset])
+    else:
+        model_def = onnx.helper.make_model(graph_proto, producer_name="triton")
+
+    try:
+        os.makedirs(model_version_dir)
+    except OSError as ex:
+        pass  # ignore existing dir
+
+    onnx.save(model_def, model_version_dir + "/model.onnx")
+
+
+def create_itemshape_modelconfig(models_dir, max_batch, model_version, dtype,
+                                 backend, platform):
+    version_policy_str = "{ latest { num_versions: 1 }}"
+
+    backend_spec = '''
+backend: "{}"
+'''.format(backend)
+    if backend == "tensorflow":
+        backend_spec += '''
+platform: "{}_{}"
+'''.format(backend, platform)
+
+    # Use a different model name for the non-batching variant
+    model_name = "{}_batch_item".format(platform)
+    config_dir = models_dir + "/" + model_name
+    config = '''
+name: "{}"
+{}
+max_batch_size: {}
+version_policy: {}
+input [
+  {{
+    name: "RAGGED_INPUT"
+    data_type: {data_type}
+    dims: [ -1, -1 ]
+    allow_ragged_batch: true
+  }}
+]
+output [
+  {{
+    name: "BATCH_OUTPUT"
+    data_type: {data_type}
+    dims: [ 2 ]
+   }}
+]
+batch_input [
+  {{
+    kind: BATCH_ITEM_SHAPE
+    target_name: "BATCH_INPUT"
+    data_type: {data_type}
+    source_input: "RAGGED_INPUT"
+  }}
+]
+dynamic_batching {{
+  max_queue_delay_microseconds: 1000000
+}}
+'''.format(model_name,
+           backend_spec,
+           max_batch,
+           version_policy_str,
+           data_type=np_to_model_dtype(dtype))
+
+    try:
+        os.makedirs(config_dir)
+    except OSError as ex:
+        pass  # ignore existing dir
+
+    with open(config_dir + "/config.pbtxt", "w") as cfile:
+        cfile.write(config)
+
+
 def create_batch_input_models(models_dir):
     model_version = 1
     if FLAGS.tensorrt:
         create_modelconfig(models_dir, 4, model_version, np.float32, "tensorrt",
                            "plan")
         create_plan_modelfile(models_dir, model_version, np.float32)
+        create_itemshape_modelconfig(models_dir, 4, model_version, np.float32,
+                                     "tensorrt", "plan")
+        create_plan_itemshape_modelfile(models_dir, model_version, np.float32)
     if FLAGS.savedmodel:
         create_modelconfig(models_dir, 4, model_version, np.float32,
                            "tensorflow", "savedmodel")
         create_savedmodel_modelfile(models_dir, model_version, np.float32)
+        create_itemshape_modelconfig(models_dir, 4, model_version, np.float32,
+                                     "tensorflow", "savedmodel")
+        create_savedmodel_itemshape_modelfile(models_dir, model_version,
+                                              np.float32)
     if FLAGS.onnx:
         create_modelconfig(models_dir, 4, model_version, np.float32,
                            "onnxruntime", "onnx")
         create_onnx_modelfile(models_dir, model_version, np.float32)
+        create_itemshape_modelconfig(models_dir, 4, model_version, np.float32,
+                                     "onnxruntime", "onnx")
+        create_onnx_itemshape_modelfile(models_dir, model_version, np.float32)
 
 
 if __name__ == '__main__':
