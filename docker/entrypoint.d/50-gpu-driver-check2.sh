f5fa7955348da7165654fed75d8474a179b5f969
Katherine Yang <katheriney@nvidia.com>
2022-03-21 16:51:49 -0700

    fix l0 infer for cpu only containers

--- docker/entrypoint.d/50-gpu-driver-check2.sh
+++ docker/entrypoint.d/50-gpu-driver-check2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
-if [[ "${NVIDIA_CPU_ONLY:-0}" == "1" ]]; then
+if [[ "${NVIDIA_CPU_ONLY:-0}" == "1" || "${CUDA_VERSION:-0}" == "0" ]]; then
   export TRITON_SERVER_CPU_ONLY=1
 fi
