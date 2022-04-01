a40edf1b2e0701f3847cf81f06ac9763cb47f2de
Iman Tabrizian <itabrizian@nvidia.com>
2022-03-31 23:13:39 -0400

    Add shared memory leak detection to Python backend tests (#4122)
    * Add shared memory leak detection to Python backend tests
    
    * Add BLS tests to shared memory leak detection
    
    * Remove delays for the tests that involved growing the region
    
    * Update copyrights

--- qa/L0_backend_python/python_test.py
+++ qa/L0_backend_python/python_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright 2019-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2019-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -33,6 +33,7 @@ sys.path.append("../common")
 import unittest
 import numpy as np
 import test_util as tu
+import shm_util
 import requests as httpreq
 import os
 
@@ -44,6 +45,9 @@ TEST_JETSON = bool(int(os.environ.get('TEST_JETSON', 0)))
 
 class PythonTest(tu.TestResultCollector):
 
+    def setUp(self):
+        self._shm_leak_detector = shm_util.ShmLeakDetector()
+
     def _infer_help(self, model_name, shape, data_type):
         with httpclient.InferenceServerClient("localhost:8000") as client:
             input_data_0 = np.array(np.random.randn(*shape), dtype=data_type)
@@ -52,12 +56,14 @@ class PythonTest(tu.TestResultCollector):
                                       np_to_triton_dtype(input_data_0.dtype))
             ]
             inputs[0].set_data_from_numpy(input_data_0)
+
             result = client.infer(model_name, inputs)
             output0 = result.as_numpy('OUTPUT0')
             self.assertTrue(np.all(input_data_0 == output0))
 
-    # We do not use a docker on Jetson so it does not impose a shared memory allocation limit of 1GB.
-    # This means test will pass without the expected error on jetson and is hence unnecessary.
+    # We do not use a docker on Jetson so it does not impose a shared memory
+    # allocation limit of 1GB. This means test will pass without the expected
+    # error on jetson and is hence unnecessary.
     if not TEST_JETSON:
 
         def test_growth_error(self):
@@ -66,7 +72,8 @@ class PythonTest(tu.TestResultCollector):
             shape = [total_byte_size]
             model_name = 'identity_uint8_nobatch'
             dtype = np.uint8
-            self._infer_help(model_name, shape, dtype)
+            with self._shm_leak_detector.Probe() as shm_probe:
+                self._infer_help(model_name, shape, dtype)
 
             # 1 GiB payload leads to error in the main Python backned process.
             # Total shared memory available is 1GiB.
@@ -89,181 +96,197 @@ class PythonTest(tu.TestResultCollector):
             # Send a small paylaod to make sure it is still working properly
             total_byte_size = 2 * 1024 * 1024
             shape = [total_byte_size]
-            self._infer_help(model_name, shape, dtype)
+            with self._shm_leak_detector.Probe() as shm_probe:
+                self._infer_help(model_name, shape, dtype)
 
     def test_async_infer(self):
         model_name = "identity_uint8"
         request_parallelism = 4
         shape = [2, 2]
-        with httpclient.InferenceServerClient(
-                "localhost:8000", concurrency=request_parallelism) as client:
-            input_datas = []
-            requests = []
-            for i in range(request_parallelism):
-                input_data = (16384 * np.random.randn(*shape)).astype(np.uint8)
-                input_datas.append(input_data)
+
+        with self._shm_leak_detector.Probe() as shm_probe:
+            with httpclient.InferenceServerClient(
+                    "localhost:8000",
+                    concurrency=request_parallelism) as client:
+                input_datas = []
+                requests = []
+                for i in range(request_parallelism):
+                    input_data = (16384 * np.random.randn(*shape)).astype(
+                        np.uint8)
+                    input_datas.append(input_data)
+                    inputs = [
+                        httpclient.InferInput(
+                            "INPUT0", input_data.shape,
+                            np_to_triton_dtype(input_data.dtype))
+                    ]
+                    inputs[0].set_data_from_numpy(input_data)
+                    requests.append(client.async_infer(model_name, inputs))
+
+                for i in range(request_parallelism):
+                    # Get the result from the initiated asynchronous inference request.
+                    # Note the call will block till the server responds.
+                    results = requests[i].get_result()
+
+                    output_data = results.as_numpy("OUTPUT0")
+                    self.assertIsNotNone(output_data,
+                                         "error: expected 'OUTPUT0'")
+                    self.assertTrue(
+                        np.array_equal(output_data, input_datas[i]),
+                        "error: expected output {} to match input {}".format(
+                            output_data, input_datas[i]))
+
+                # Make sure the requests ran in parallel.
+                stats = client.get_inference_statistics(model_name)
+                test_cond = (len(stats['model_stats']) != 1) or (
+                    stats['model_stats'][0]['name'] != model_name)
+                self.assertFalse(
+                    test_cond,
+                    "error: expected statistics for {}".format(model_name))
+
+                stat = stats['model_stats'][0]
+                self.assertFalse((stat['inference_count'] != 8) or (
+                    stat['execution_count'] != 1
+                ), "error: expected execution_count == 1 and inference_count == 8, got {} and {}"
+                                 .format(stat['execution_count'],
+                                         stat['inference_count']))
+                batch_stat = stat['batch_stats'][0]
+                self.assertFalse(
+                    batch_stat['batch_size'] != 8,
+                    f"error: expected batch_size == 8, got {batch_stat['batch_size']}"
+                )
+                # Check metrics to make sure they are reported correctly
+                metrics = httpreq.get('http://localhost:8002/metrics')
+                print(metrics.text)
+
+                success_str = 'nv_inference_request_success{model="identity_uint8",version="1"}'
+                infer_count_str = 'nv_inference_count{model="identity_uint8",version="1"}'
+                infer_exec_str = 'nv_inference_exec_count{model="identity_uint8",version="1"}'
+
+                success_val = None
+                infer_count_val = None
+                infer_exec_val = None
+                for line in metrics.text.splitlines():
+                    if line.startswith(success_str):
+                        success_val = float(line[len(success_str):])
+                    if line.startswith(infer_count_str):
+                        infer_count_val = float(line[len(infer_count_str):])
+                    if line.startswith(infer_exec_str):
+                        infer_exec_val = float(line[len(infer_exec_str):])
+
+                self.assertFalse(
+                    success_val != 4,
+                    "error: expected metric {} == 4, got {}".format(
+                        success_str, success_val))
+                self.assertFalse(
+                    infer_count_val != 8,
+                    "error: expected metric {} == 8, got {}".format(
+                        infer_count_str, infer_count_val))
+                self.assertFalse(
+                    infer_exec_val != 1,
+                    "error: expected metric {} == 1, got {}".format(
+                        infer_exec_str, infer_exec_val))
+
+    def test_bool(self):
+        model_name = 'identity_bool'
+        with self._shm_leak_detector.Probe() as shm_probe:
+            with httpclient.InferenceServerClient("localhost:8000") as client:
+                input_data = np.array([[True, False, True]], dtype=bool)
                 inputs = [
                     httpclient.InferInput("INPUT0", input_data.shape,
                                           np_to_triton_dtype(input_data.dtype))
                 ]
                 inputs[0].set_data_from_numpy(input_data)
-                requests.append(client.async_infer(model_name, inputs))
-
-            for i in range(request_parallelism):
-                # Get the result from the initiated asynchronous inference request.
-                # Note the call will block till the server responds.
-                results = requests[i].get_result()
-                print(results)
-
-                output_data = results.as_numpy("OUTPUT0")
-                self.assertIsNotNone(output_data, "error: expected 'OUTPUT0'")
-                self.assertTrue(
-                    np.array_equal(output_data, input_datas[i]),
-                    "error: expected output {} to match input {}".format(
-                        output_data, input_datas[i]))
-
-            # Make sure the requests ran in parallel.
-            stats = client.get_inference_statistics(model_name)
-            test_cond = (len(stats['model_stats']) !=
-                         1) or (stats['model_stats'][0]['name'] != model_name)
-            self.assertFalse(
-                test_cond,
-                "error: expected statistics for {}".format(model_name))
-
-            stat = stats['model_stats'][0]
-            self.assertFalse((stat['inference_count'] != 8) or (
-                stat['execution_count'] != 1
-            ), "error: expected execution_count == 1 and inference_count == 8, got {} and {}"
-                             .format(stat['execution_count'],
-                                     stat['inference_count']))
-            batch_stat = stat['batch_stats'][0]
-            self.assertFalse(
-                batch_stat['batch_size'] != 8,
-                f"error: expected batch_size == 8, got {batch_stat['batch_size']}"
-            )
-            # Check metrics to make sure they are reported correctly
-            metrics = httpreq.get('http://localhost:8002/metrics')
-            print(metrics.text)
-
-            success_str = 'nv_inference_request_success{model="identity_uint8",version="1"}'
-            infer_count_str = 'nv_inference_count{model="identity_uint8",version="1"}'
-            infer_exec_str = 'nv_inference_exec_count{model="identity_uint8",version="1"}'
-
-            success_val = None
-            infer_count_val = None
-            infer_exec_val = None
-            for line in metrics.text.splitlines():
-                if line.startswith(success_str):
-                    success_val = float(line[len(success_str):])
-                if line.startswith(infer_count_str):
-                    infer_count_val = float(line[len(infer_count_str):])
-                if line.startswith(infer_exec_str):
-                    infer_exec_val = float(line[len(infer_exec_str):])
-
-            self.assertFalse(
-                success_val != 4,
-                "error: expected metric {} == 4, got {}".format(
-                    success_str, success_val))
-            self.assertFalse(
-                infer_count_val != 8,
-                "error: expected metric {} == 8, got {}".format(
-                    infer_count_str, infer_count_val))
-            self.assertFalse(
-                infer_exec_val != 1,
-                "error: expected metric {} == 1, got {}".format(
-                    infer_exec_str, infer_exec_val))
-
-    def test_bool(self):
-        model_name = 'identity_bool'
-        with httpclient.InferenceServerClient("localhost:8000") as client:
-            input_data = np.array([[True, False, True]], dtype=bool)
-            inputs = [
-                httpclient.InferInput("INPUT0", input_data.shape,
-                                      np_to_triton_dtype(input_data.dtype))
-            ]
-            inputs[0].set_data_from_numpy(input_data)
-            result = client.infer(model_name, inputs)
-            output0 = result.as_numpy('OUTPUT0')
-            self.assertIsNotNone(output0)
-            self.assertTrue(np.all(output0 == input_data))
+                result = client.infer(model_name, inputs)
+                output0 = result.as_numpy('OUTPUT0')
+                self.assertIsNotNone(output0)
+                self.assertTrue(np.all(output0 == input_data))
 
     def test_infer_pytorch(self):
         model_name = "pytorch_fp32_fp32"
         shape = [1, 1, 28, 28]
-        with httpclient.InferenceServerClient("localhost:8000") as client:
-            input_data = np.zeros(shape, dtype=np.float32)
-            inputs = [
-                httpclient.InferInput("IN", input_data.shape,
-                                      np_to_triton_dtype(input_data.dtype))
-            ]
-            inputs[0].set_data_from_numpy(input_data)
-            result = client.infer(model_name, inputs)
-            output_data = result.as_numpy('OUT')
-            self.assertIsNotNone(output_data, "error: expected 'OUT'")
+        with self._shm_leak_detector.Probe() as shm_probe:
+            with httpclient.InferenceServerClient("localhost:8000") as client:
+                input_data = np.zeros(shape, dtype=np.float32)
+                inputs = [
+                    httpclient.InferInput("IN", input_data.shape,
+                                          np_to_triton_dtype(input_data.dtype))
+                ]
+                inputs[0].set_data_from_numpy(input_data)
+                result = client.infer(model_name, inputs)
+                output_data = result.as_numpy('OUT')
+                self.assertIsNotNone(output_data, "error: expected 'OUT'")
 
-            # expected inference resposne from a zero tensor
-            expected_result = [
-                -2.2377274, -2.3976364, -2.2464046, -2.2790744, -2.3828976,
-                -2.2940576, -2.2928185, -2.340665, -2.275219, -2.292135
-            ]
-            self.assertTrue(np.allclose(output_data[0], expected_result),
-                            'Inference result is not correct')
+                # expected inference resposne from a zero tensor
+                expected_result = [
+                    -2.2377274, -2.3976364, -2.2464046, -2.2790744, -2.3828976,
+                    -2.2940576, -2.2928185, -2.340665, -2.275219, -2.292135
+                ]
+                self.assertTrue(np.allclose(output_data[0], expected_result),
+                                'Inference result is not correct')
 
     def test_init_args(self):
         model_name = "init_args"
         shape = [2, 2]
-        with httpclient.InferenceServerClient("localhost:8000") as client:
-            input_data = np.zeros(shape, dtype=np.float32)
-            inputs = [
-                httpclient.InferInput("IN", input_data.shape,
-                                      np_to_triton_dtype(input_data.dtype))
-            ]
-            inputs[0].set_data_from_numpy(input_data)
-            result = client.infer(model_name, inputs)
-            # output respone in this model is the number of keys in the args
-            self.assertTrue(
-                result.as_numpy("OUT") == 7,
-                "Number of keys in the init args is not correct")
+        with self._shm_leak_detector.Probe() as shm_probe:
+            with httpclient.InferenceServerClient("localhost:8000") as client:
+                input_data = np.zeros(shape, dtype=np.float32)
+                inputs = [
+                    httpclient.InferInput("IN", input_data.shape,
+                                          np_to_triton_dtype(input_data.dtype))
+                ]
+                inputs[0].set_data_from_numpy(input_data)
+                result = client.infer(model_name, inputs)
+                # output respone in this model is the number of keys in the args
+                self.assertTrue(
+                    result.as_numpy("OUT") == 7,
+                    "Number of keys in the init args is not correct")
 
     def test_unicode(self):
         model_name = "string"
         shape = [1]
 
         for i in range(3):
-            with httpclient.InferenceServerClient("localhost:8000") as client:
-                utf8 = '😀'
-                input_data = np.array([bytes(utf8, encoding='utf-8')],
-                                      dtype=np.bytes_)
-                inputs = [
-                    httpclient.InferInput("INPUT0", shape,
-                                          np_to_triton_dtype(input_data.dtype))
-                ]
-                inputs[0].set_data_from_numpy(input_data)
-                result = client.infer(model_name, inputs)
-                output0 = result.as_numpy('OUTPUT0')
-                self.assertIsNotNone(output0)
-                self.assertEqual(output0[0], input_data)
+            with self._shm_leak_detector.Probe() as shm_probe:
+                with httpclient.InferenceServerClient(
+                        "localhost:8000") as client:
+                    utf8 = '😀'
+                    input_data = np.array([bytes(utf8, encoding='utf-8')],
+                                          dtype=np.bytes_)
+                    inputs = [
+                        httpclient.InferInput(
+                            "INPUT0", shape,
+                            np_to_triton_dtype(input_data.dtype))
+                    ]
+                    inputs[0].set_data_from_numpy(input_data)
+                    result = client.infer(model_name, inputs)
+                    output0 = result.as_numpy('OUTPUT0')
+                    self.assertIsNotNone(output0)
+                    self.assertEqual(output0[0], input_data)
 
     def test_string(self):
         model_name = "string_fixed"
         shape = [1]
 
         for i in range(6):
-            with httpclient.InferenceServerClient("localhost:8000") as client:
-                input_data = np.array(['123456'], dtype=np.object_)
-                inputs = [
-                    httpclient.InferInput("INPUT0", shape,
-                                          np_to_triton_dtype(input_data.dtype))
-                ]
-                inputs[0].set_data_from_numpy(input_data)
-                result = client.infer(model_name, inputs)
-                output0 = result.as_numpy('OUTPUT0')
-                self.assertIsNotNone(output0)
+            with self._shm_leak_detector.Probe() as shm_probe:
+                with httpclient.InferenceServerClient(
+                        "localhost:8000") as client:
+                    input_data = np.array(['123456'], dtype=np.object_)
+                    inputs = [
+                        httpclient.InferInput(
+                            "INPUT0", shape,
+                            np_to_triton_dtype(input_data.dtype))
+                    ]
+                    inputs[0].set_data_from_numpy(input_data)
+                    result = client.infer(model_name, inputs)
+                    output0 = result.as_numpy('OUTPUT0')
+                    self.assertIsNotNone(output0)
 
-                if i % 2 == 0:
-                    self.assertEqual(output0[0], input_data.astype(np.bytes_))
-                else:
-                    self.assertEqual(output0.size, 0)
+                    if i % 2 == 0:
+                        self.assertEqual(output0[0],
+                                         input_data.astype(np.bytes_))
+                    else:
+                        self.assertEqual(output0.size, 0)
 
     def test_non_contiguous(self):
         model_name = 'non_contiguous'
