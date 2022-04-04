6d6ab7dc2884635d310bce4ab0a6a3ece514974e
Iman Tabrizian <itabrizian@nvidia.com>
2022-04-04 13:02:58 -0400

    Fix shared memory monitor on windows (#4148)
    * Fix shared memory monitor on windows
    
    * Review edit

--- qa/common/shm_util.py
+++ qa/common/shm_util.py
@@ -32,7 +32,6 @@ from ctypes import *
 
 import tritonclient.http as httpclient
 from tritonclient.utils import *
-import triton_shm_monitor
 
 # By default, find tritonserver on "localhost", but can be overridden
 # with TRITONSERVER_IPADDR envvar
@@ -381,6 +380,7 @@ class ShmLeakDetector:
             assert not shm_leak_detected, "Shared memory leak detected."
 
     def __init__(self, prefix='triton_python_backend_shm_region'):
+        import triton_shm_monitor
         self._shm_monitors = []
         shm_regions = listdir('/dev/shm')
         for shm_region in shm_regions:
