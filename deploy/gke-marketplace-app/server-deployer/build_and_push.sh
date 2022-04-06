0ac0c21e2959f5760de07e27264e922cb3d49519
Misha Chornyi <mchornyi@nvidia.com>
2022-04-06 08:48:25 -0700

    Update README and versions for 22.04 branch

--- deploy/gke-marketplace-app/server-deployer/build_and_push.sh
+++ deploy/gke-marketplace-app/server-deployer/build_and_push.sh
@@ -26,7 +26,7 @@
 
 export REGISTRY=gcr.io/$(gcloud config get-value project | tr ':' '/')
 export APP_NAME=tritonserver
-export MAJOR_VERSION=2.20
+export MAJOR_VERSION=2.21
 export MINOR_VERSION=2.21.0
 export NGC_VERSION=22.04-py3
 
