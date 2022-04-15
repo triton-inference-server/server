17f0d2490cb1858dfde6056fa920829ed5c54a6f
GuanLuo <gluo@nvidia.com>
2022-04-15 11:53:52 -0700

    Fix script exit mode

--- qa/L0_client_memory_growth/test.sh
+++ qa/L0_client_memory_growth/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -122,9 +122,11 @@ for PROTOCOL in http grpc; do
             EXTRA_ARGS="-r ${REPETITION_PY} -i ${PROTOCOL}"
         fi
 
+        set +e
         SECONDS=0
         $LEAKCHECK $LEAKCHECK_ARGS $MEMORY_GROWTH_TEST $EXTRA_ARGS >> ${CLIENT_LOG} 2>&1
         TEST_DURATION=$SECONDS
+        set -e
         if [ $? -ne 0 ]; then
             cat ${CLIENT_LOG}
             RET=1
