dae138eff98cb9d79e4d6f8b291f26176a5ca37a
Ryan McCormick <rmccormick@nvidia.com>
2022-04-19 14:23:26 -0700

    Cat SERVER_LOG when failing to grep it in L0_backend_tutorial (#4252)
    * Cat SERVER_LOG when failing to grep it
    
    * Include both logs on server log failure

--- qa/L0_backend_tutorial/test.sh
+++ qa/L0_backend_tutorial/test.sh
@@ -111,6 +111,7 @@ fi
 grep "model batching: requests in batch 2" $SERVER_LOG
 if [ $? -ne 0 ]; then
     echo -e "\n***\n*** Failed to verify minimal server log. \n***"
+    cat $SERVER_LOG
     cat $MINIMAL_LOG
     RET=1
 fi
@@ -118,6 +119,7 @@ fi
 grep "batched IN0 value: \[ 10, 11, 12, 13, 20, 21, 22, 23 \]" $SERVER_LOG
 if [ $? -ne 0 ]; then
     echo -e "\n***\n*** Failed to verify minimal server log. \n***"
+    cat $SERVER_LOG
     cat $MINIMAL_LOG
     RET=1
 fi
@@ -179,6 +181,7 @@ fi
 grep "model batching: requests in batch 2" $SERVER_LOG
 if [ $? -ne 0 ]; then
     echo -e "\n***\n*** Failed to verify recommended server log. \n***"
+    cat $SERVER_LOG
     cat $RECOMMENDED_LOG
     RET=1
 fi
@@ -186,6 +189,7 @@ fi
 grep "batched INPUT value: \[ 1.000000, 1.100000, 1.200000, 1.300000, 2.000000, 2.100000, 2.200000, 2.300000, 3.000000, 3.100000, 3.200000, 3.300000, 4.000000, 4.100000, 4.200000, 4.300000, 10.000000, 10.100000, 10.200000, 10.300000, 20.000000, 20.100000, 20.200001, 20.299999, 30.000000, 30.100000, 30.200001, 30.299999, 40.000000, 40.099998, 40.200001, 40.299999 \]" $SERVER_LOG
 if [ $? -ne 0 ]; then
     echo -e "\n***\n*** Failed to verify recommended server log. \n***"
+    cat $SERVER_LOG
     cat $RECOMMENDED_LOG
     RET=1
 fi
