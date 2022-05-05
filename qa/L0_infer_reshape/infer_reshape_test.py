18ce0cd7cae2c5a24301470d7876317a9371cf7c
hemantj <hemantj@nvidia.com>
2022-05-04 17:44:08 -0700

    Add identity model gen and test handling for libtorch string models

--- qa/L0_infer_reshape/infer_reshape_test.py
+++ qa/L0_infer_reshape/infer_reshape_test.py
@@ -140,23 +140,6 @@ class InferReshapeTest(tu.TestResultCollector):
             # skip variable size reshape on libtorch for now,
             # see "gen_qa_reshape_model.py" for detail
             if dtype != np.int32:
-                # model that supports batching
-                for bs in (1, 8):
-                    full_shapes = [[
-                        bs,
-                    ] + input_shape for input_shape in input_shapes]
-                    full_output_shapes = [[
-                        bs,
-                    ] + output_shape for output_shape in output_shapes]
-                    iu.infer_zero(
-                        self,
-                        'libtorch',
-                        bs,
-                        dtype,
-                        full_shapes,
-                        full_output_shapes,
-                        use_system_shared_memory=TEST_SYSTEM_SHARED_MEMORY,
-                        use_cuda_shared_memory=TEST_CUDA_SHARED_MEMORY)
                 # model that does not support batching
                 if no_batch:
                     iu.infer_zero(
@@ -168,6 +151,25 @@ class InferReshapeTest(tu.TestResultCollector):
                         output_shapes,
                         use_system_shared_memory=TEST_SYSTEM_SHARED_MEMORY,
                         use_cuda_shared_memory=TEST_CUDA_SHARED_MEMORY)
+
+                # model that supports batching. Skip for libtorch string I/O
+                if dtype == np_dtype_string:
+                    for bs in (1, 8):
+                        full_shapes = [[
+                            bs,
+                        ] + input_shape for input_shape in input_shapes]
+                        full_output_shapes = [[
+                            bs,
+                        ] + output_shape for output_shape in output_shapes]
+                        iu.infer_zero(
+                            self,
+                            'libtorch',
+                            bs,
+                            dtype,
+                            full_shapes,
+                            full_output_shapes,
+                            use_system_shared_memory=TEST_SYSTEM_SHARED_MEMORY,
+                            use_cuda_shared_memory=TEST_CUDA_SHARED_MEMORY)
 
         for name in ["simple_reshape", "sequence_reshape", "fan_reshape"]:
             # [TODO] Skip variable size reshape on ensemble for now.
