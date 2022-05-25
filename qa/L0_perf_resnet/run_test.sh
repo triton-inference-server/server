b5b1137de6e330f11e398370e2e00b3e65b1bf51
Ryan McCormick <rmccormick@nvidia.com>
2022-05-25 09:34:04 -0700

    Remove warmup, inputs different for each model

--- qa/L0_perf_resnet/run_test.sh
+++ qa/L0_perf_resnet/run_test.sh
@@ -53,19 +53,7 @@ rm -fr models && mkdir -p models && \
     cp -r $MODEL_PATH models/. && \
     (cd models/$MODEL_NAME && \
             sed -i "s/^max_batch_size:.*/max_batch_size: ${MAX_BATCH}/" config.pbtxt && \
-            echo "instance_group [ { count: ${INSTANCE_CNT} }]" >> config.pbtxt && \
-            echo "model_warmup [{" >> config.pbtxt && \
-            echo "    name : \"sample\"" >> config.pbtxt && \
-            echo "    batch_size: 1" >> config.pbtxt && \
-            echo "    inputs {" >> config.pbtxt && \
-            echo "        key: \"input\"" >> config.pbtxt && \
-            echo "        value: {" >> config.pbtxt && \
-            echo "            data_type: TYPE_FP32" >> config.pbtxt && \
-            echo "            dims: [ 224, 224, 3 ]" >> config.pbtxt && \
-            echo "            random_data: true" >> config.pbtxt && \
-            echo "        }" >> config.pbtxt && \
-            echo "    }" >> config.pbtxt && \
-            echo "}]" >> config.pbtxt)
+            echo "instance_group [ { count: ${INSTANCE_CNT} }]")
 
 # Onnx and onnx-trt models are very slow on Jetson.
 MEASUREMENT_WINDOW=5000
