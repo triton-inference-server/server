2874ad96a5227abbaaa1594a030ed38ebbd2ddde
GuanLuo <gluo@nvidia.com>
2022-06-03 17:50:02 -0700

    Add test for different variance of ONNX flavor

--- qa/L0_mlflow/plugin_test.py
+++ qa/L0_mlflow/plugin_test.py
@@ -42,6 +42,41 @@ class PluginTest(tu.TestResultCollector):
     def setUp(self):
         self.client_ = get_deploy_client('triton')
 
+    def _validate_deployment(self, model_name):
+        # create
+        self.client_.create_deployment(model_name,
+                                       "models:/{}/1".format(model_name),
+                                       flavor="onnx")
+
+        # list
+        deployment_list = self.client_.list_deployments()
+        self.assertEqual(len(deployment_list), 1)
+        self.assertEqual(deployment_list[0]['name'], model_name)
+
+        # get
+        deployment = self.client_.get_deployment(model_name)
+        self.assertEqual(deployment['name'], model_name)
+
+        # predict
+        inputs = {}
+        with open("./mlflow-triton-plugin/examples/input.json", "r") as f:
+            input_json = json.load(f)
+            for key, value in input_json['inputs'].items():
+                inputs[key] = np.array(value, dtype=np.float32)
+
+        output = self.client_.predict(model_name, inputs)
+        with open("./mlflow-triton-plugin/examples/expected_output.json",
+                  "r") as f:
+            output_json = json.load(f)
+            for key, value in output_json['outputs'].items():
+                np.testing.assert_allclose(
+                    output['outputs'][key],
+                    np.array(value, dtype=np.int32),
+                    err_msg='Inference result is not correct')
+
+        # delete
+        self.client_.delete_deployment(model_name)
+
     def test_onnx_flavor(self):
         # Log the ONNX model to MLFlow
         import mlflow.onnx
@@ -54,39 +89,27 @@ class PluginTest(tu.TestResultCollector):
                               "triton",
                               registered_model_name="onnx_model")
 
-        # create
-        self.client_.create_deployment("onnx_model",
-                                       "models:/onnx_model/1",
-                                       flavor="onnx")
+        self._validate_deployment("onnx_model")
 
-        # list
-        deployment_list = self.client_.list_deployments()
-        self.assertEqual(len(deployment_list), 1)
-        self.assertEqual(deployment_list[0]['name'], "onnx_model")
+    def test_onnx_flavor_with_files(self):
+        # Log the ONNX model to MLFlow
+        import mlflow.onnx
+        import onnx
+        model = onnx.load(
+            "./mlflow-triton-plugin/examples/onnx_float32_int32_int32/1/model.onnx"
+        )
+        config_path = "./mlflow-triton-plugin/examples/onnx_float32_int32_int32/config.pbtxt"
+        # Use a different name to ensure the plugin operates on correct model
+        mlflow.onnx.log_model(model,
+                              "triton",
+                              registered_model_name="onnx_model_with_files")
+        mlflow.log_artifact(config_path, "triton")
 
-        # get
-        deployment = self.client_.get_deployment("onnx_model")
-        self.assertEqual(deployment['name'], "onnx_model")
+        self._validate_deployment("onnx_model_with_files")
 
-        # predict
-        inputs = {}
-        with open("./mlflow-triton-plugin/examples/input.json", "r") as f:
-            input_json = json.load(f)
-            for key, value in input_json['inputs'].items():
-                inputs[key] = np.array(value, dtype=np.float32)
-
-        output = self.client_.predict("onnx_model", inputs)
-        with open("./mlflow-triton-plugin/examples/expected_output.json",
-                  "r") as f:
-            output_json = json.load(f)
-            for key, value in output_json['outputs'].items():
-                np.testing.assert_allclose(
-                    output['outputs'][key],
-                    np.array(value, dtype=np.int32),
-                    err_msg='Inference result is not correct')
-
-        # delete
-        self.client_.delete_deployment("onnx_model")
+        # Check if the additional files are properly copied
+        import filecmp
+        filecmp.cmp(config_path, "./models/onnx_model_with_files/config.pbtxt")
 
 
 if __name__ == '__main__':
