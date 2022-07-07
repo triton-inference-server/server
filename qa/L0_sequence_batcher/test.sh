1a7afd94cbaa726a5182811eb2a72d1e8190d7b6
fpetrini15 <francescogpetrini@gmail.com>
2022-07-07 10:02:19 -0700

    L0-sequence-batcher review comments

--- qa/L0_sequence_batcher/test.sh
+++ qa/L0_sequence_batcher/test.sh
@@ -254,60 +254,45 @@ for MODEL in $MODELS; do
   if [[ ! "$TEST_VALGRIND" -eq 1 ]]; then
     if [[ "$MODEL" =~ .*"python".* ]]; then
       generate_python_models "$MODEL" "models1"
-      (cd models1/$(basename $MODEL) && \
-            sed -i "s/^max_batch_size:.*/max_batch_size: 4/" config.pbtxt && \
-            sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 1/" config.pbtxt && \
-            sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 1/" config.pbtxt)
     else
-      cp -r $MODEL models1/. && \
+      cp -r $MODEL models1/.
+    fi
         (cd models1/$(basename $MODEL) && \
           sed -i "s/^max_batch_size:.*/max_batch_size: 4/" config.pbtxt && \
           sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 1/" config.pbtxt && \
           sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 1/" config.pbtxt)
-    fi
+
     if [[ "$MODEL" =~ .*"python".* ]]; then
       generate_python_models "$MODEL" "models2"
-      (cd models2/$(basename $MODEL) && \
-        sed -i "s/^max_batch_size:.*/max_batch_size: 2/" config.pbtxt && \
-        sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 2/" config.pbtxt && \
-        sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 2/" config.pbtxt)
     else
-      cp -r $MODEL models2/. && \
+      cp -r $MODEL models2/.
+    fi
         (cd models2/$(basename $MODEL) && \
           sed -i "s/^max_batch_size:.*/max_batch_size: 2/" config.pbtxt && \
           sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 2/" config.pbtxt && \
           sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 2/" config.pbtxt)
-    fi
+
     if [[ "$MODEL" =~ .*"python".* ]]; then
       generate_python_models "$MODEL" "models4"
-      (cd models4/$(basename $MODEL) && \
-        sed -i "s/^max_batch_size:.*/max_batch_size: 1/" config.pbtxt && \
-        sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 4/" config.pbtxt && \
-        sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 4/" config.pbtxt)
     else
-      cp -r $MODEL models4/. && \
+      cp -r $MODEL models4/.
+    fi
         (cd models4/$(basename $MODEL) && \
           sed -i "s/^max_batch_size:.*/max_batch_size: 1/" config.pbtxt && \
           sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 4/" config.pbtxt && \
           sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 4/" config.pbtxt)
-    fi
 
     # Duplicate the models for different delay settings
     if [[ "$MODEL" =~ .*"python".* ]]; then
       generate_python_models "$MODEL" "queue_delay_models"
-      (cd queue_delay_models/$(basename $MODEL) && \
-        sed -i "s/^max_batch_size:.*/max_batch_size: 4/" config.pbtxt && \
-        sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 1/" config.pbtxt && \
-        sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 1/" config.pbtxt && \
-        sed -i "s/sequence_batching {/sequence_batching {\\ndirect {\\nmax_queue_delay_microseconds: 3000000\\nminimum_slot_utilization: 0\\n}/" config.pbtxt)
     else
-      cp -r $MODEL queue_delay_models/. && \
+      cp -r $MODEL queue_delay_models/.
+    fi
         (cd queue_delay_models/$(basename $MODEL) && \
           sed -i "s/^max_batch_size:.*/max_batch_size: 4/" config.pbtxt && \
           sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 1/" config.pbtxt && \
           sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 1/" config.pbtxt && \
           sed -i "s/sequence_batching {/sequence_batching {\\ndirect {\\nmax_queue_delay_microseconds: 3000000\\nminimum_slot_utilization: 0\\n}/" config.pbtxt)
-    fi
 
     cp -r queue_delay_models/$(basename $MODEL) queue_delay_models/$(basename $MODEL)_half && \
       (cd queue_delay_models/$(basename $MODEL)_half && \
@@ -396,15 +381,12 @@ done
 for MODEL in $MODELS; do
   if [[ "$MODEL" =~ .*"python".* ]]; then
       generate_python_models "$MODEL" "models0"
-      (cd models0/$(basename $MODEL) && \
-          sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 4/" config.pbtxt && \
-          sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 4/" config.pbtxt)
   else
-    cp -r $MODEL models0/. && \
+    cp -r $MODEL models0/.
+  fi
       (cd models0/$(basename $MODEL) && \
           sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 4/" config.pbtxt && \
           sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 4/" config.pbtxt)
-  fi
 
   if [ "$INITIAL_STATE_FILE" == "1" ]; then
       mkdir -p models0/$(basename $MODEL)/initial_state/ && cp input_state_data models0/$(basename $MODEL)/initial_state/ && \
@@ -447,17 +429,13 @@ done
 for MODEL in $MODELS; do
   if [[ "$MODEL" =~ .*"python".* ]]; then
       generate_python_models "$MODEL" "modelsv"
-      (cd modelsv/$(basename $MODEL) && \
-            sed -i "s/^max_batch_size:.*/max_batch_size: 4/" config.pbtxt && \
-            sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 1/" config.pbtxt && \
-            sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 1/" config.pbtxt)
   else
-    cp -r $MODEL modelsv/. && \
+    cp -r $MODEL modelsv/.
+  fi
       (cd modelsv/$(basename $MODEL) && \
           sed -i "s/^max_batch_size:.*/max_batch_size: 4/" config.pbtxt && \
           sed -i "s/kind: KIND_GPU/kind: KIND_GPU\\ncount: 1/" config.pbtxt && \
           sed -i "s/kind: KIND_CPU/kind: KIND_CPU\\ncount: 1/" config.pbtxt)
-  fi
 
   if [ "$INITIAL_STATE_FILE" == "1" ]; then
       mkdir -p modelsv/$(basename $MODEL)/initial_state/ && cp input_state_data modelsv/$(basename $MODEL)/initial_state/ && \
--- qa/python_models/sequence_int32/model.py
+++ qa/python_models/sequence_int32/model.py
@@ -36,10 +36,10 @@ class TritonPythonModel:
 
         output_config = pb_utils.get_output_config_by_name(
             model_config, "OUTPUT")
-        
+
         self.output_dtype = pb_utils.triton_string_to_numpy(
             output_config['data_type'])
-        
+
         self.accumulator = np.zeros(1)
         self.max_batch_size = model_config["max_batch_size"]
 
@@ -52,21 +52,29 @@ class TritonPythonModel:
 
         """
         output_dtype = self.output_dtype
-        
+
         responses = []
         for request in requests:
-            input_tensor = pb_utils.get_input_tensor_by_name(request, "INPUT").as_numpy().astype(np.int32)
-            start_tensor = pb_utils.get_input_tensor_by_name(request, "START").as_numpy().astype(np.int32)
-            ready_tensor = pb_utils.get_input_tensor_by_name(request, "READY").as_numpy().astype(np.int32)
-            
-            if(self.max_batch_size == 0):
-                tmp = np.where(np.equal(start_tensor,1), input_tensor, np.add(self.accumulator,input_tensor))
-                newacc = np.where(np.equal(ready_tensor,1), tmp, self.accumulator)
+            input_tensor = pb_utils.get_input_tensor_by_name(
+                request, "INPUT").as_numpy().astype(np.int32)
+            start_tensor = pb_utils.get_input_tensor_by_name(
+                request, "START").as_numpy().astype(np.int32)
+            ready_tensor = pb_utils.get_input_tensor_by_name(
+                request, "READY").as_numpy().astype(np.int32)
+
+            if self.max_batch_size == 0:
+                tmp = np.where(np.equal(start_tensor, 1), input_tensor,
+                               np.add(self.accumulator, input_tensor))
+                newacc = np.where(np.equal(ready_tensor, 1), tmp,
+                                  self.accumulator)
                 self.accumulator = newacc
-                out_tensor = pb_utils.Tensor("OUTPUT", self.accumulator.astype(output_dtype))
+                out_tensor = pb_utils.Tensor(
+                    "OUTPUT", self.accumulator.astype(output_dtype))
             else:
-                tmp = np.where(np.equal(ready_tensor, 1), np.add(start_tensor, input_tensor),
-                       np.zeros(np.shape(input_tensor), dtype=output_dtype))
+                tmp = np.where(
+                    np.equal(ready_tensor, 1), np.add(start_tensor,
+                                                      input_tensor),
+                    np.zeros(np.shape(input_tensor), dtype=output_dtype))
                 out_tensor = pb_utils.Tensor("OUTPUT", tmp.astype(output_dtype))
 
             responses.append(pb_utils.InferenceResponse([out_tensor]))
