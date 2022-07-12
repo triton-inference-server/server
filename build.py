b498e31d27eab444fc7725ab201bc162ce4a404d
Misha Chornyi <mchornyi@nvidia.com>
2022-07-11 17:24:00 -0700

    Enable ORT 1.12.0 support

--- build.py
+++ build.py
@@ -92,7 +92,7 @@ TRITON_VERSION_MAP = {
     '2.25.0dev': (
         '22.08dev',  # triton container
         '22.07',  # upstream container
-        '1.11.1',  # ORT
+        '1.12.0',  # ORT
         '2021.4.582',  # ORT OpenVINO
         (('2021.4', None), ('2021.4', '2021.4.582'),
          ('SPECIFIC', 'f2f281e6')),  # Standalone OpenVINO
