218e0a682af19192a1a4073d552b5517c2422f65
krishung5 <krish@nvidia.com>
2022-07-27 15:30:20 -0700

    Change to KIND_CPU for numpy usage in L0_backend_python test

--- qa/L0_backend_python/bls/test.sh
+++ qa/L0_backend_python/bls/test.sh
@@ -79,7 +79,9 @@ mkdir -p models/dlpack_identity/1/
 cp ../../python_models/dlpack_identity/model.py models/dlpack_identity/1/
 cp ../../python_models/dlpack_identity/config.pbtxt models/dlpack_identity
 
-cp -r ${DATADIR}/qa_sequence_implicit_model_repository/onnx_nobatch_sequence_int32/ ./models
+cp -r ${DATADIR}/qa_sequence_implicit_model_repository/onnx_nobatch_sequence_int32/ ./models && \
+    (cd models/onnx_nobatch_sequence_int32 && \
+        sed -i "s/kind: KIND_GPU/kind: KIND_CPU/" config.pbtxt)
 
 run_server
 if [ "$SERVER_PID" == "0" ]; then
