6ca630d5bb96ef801aee68e7ce4841fe17f23053
krishung5 <krish@nvidia.com>
2022-10-17 17:25:17 -0700

    Update the limitation of multiple server binding to the same http/grpc port

--- src/main.cc
+++ src/main.cc
@@ -401,7 +401,9 @@ std::vector<Option> options_
        "The port for the server to listen on for HTTP requests."},
       {OPTION_REUSE_HTTP_PORT, "reuse-http-port", Option::ArgBool,
        "Allow multiple servers to listen on the same HTTP port when every "
-       "server has this option set."},
+       "server has this option set. The same set of models/same model "
+       "repository should be used for every server. Note that this feature "
+       "only supports stateless models."},
       {OPTION_HTTP_ADDRESS, "http-address", Option::ArgStr,
        "The address for the http server to binds to."},
       {OPTION_HTTP_THREAD_COUNT, "http-thread-count", Option::ArgInt,
@@ -414,7 +416,9 @@ std::vector<Option> options_
        "The port for the server to listen on for GRPC requests."},
       {OPTION_REUSE_GRPC_PORT, "reuse-grpc-port", Option::ArgBool,
        "Allow multiple servers to listen on the same GRPC port when every "
-       "server has this option set."},
+       "server has this option set. The same set of models/same model "
+       "repository should be used for every server. Note that this feature "
+       "only supports stateless models."},
       {OPTION_GRPC_ADDRESS, "grpc-address", Option::ArgStr,
        "The address for the grpc server to binds to."},
       {OPTION_GRPC_INFER_ALLOCATION_POOL_SIZE,
