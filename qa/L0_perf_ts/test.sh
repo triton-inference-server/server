55bddd4f41aa0acce474a3413522d4cb5f88db59
Kris Hung <krish@nvidia.com>
2022-12-05 09:21:47 -0800

    Revert tcmalloc changes in testing (#5135)
    * Remove setting LD_PRELOAD inside container. Only setting TCMALLOC_RELEASE_RATE
    
    * Revert tcmalloc changes in testing

--- qa/L0_perf_ts/test.sh
+++ qa/L0_perf_ts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2019-2022, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -41,8 +41,6 @@ fi
 apt update
 apt install -y libb64-dev curl
 apt install -y python3 python3-pip python3-dev
-# needed by tcmalloc
-apt install -y gperf libgoogle-perftools-dev
 pip3 install --upgrade requests
 
 REPODIR=/data/inferenceserver/${REPO_VERSION}
