5b3359be73220d905a24ae37c794a0a2d9b54fb7
Misha Chornyi <mchornyi@nvidia.com>
2022-12-21 10:38:43 -0800

    Update NGC versions post-22.12 release

--- build.py
+++ build.py
@@ -67,8 +67,8 @@ from inspect import getsourcefile
 # incorrectly load the other version of the openvino libraries.
 #
 TRITON_VERSION_MAP = {
-    '2.29.0': (
-        '22.12',  # triton container
+    '2.30.0dev': (
+        '23.01dev',  # triton container
         '22.12',  # upstream container
         '1.13.1',  # ORT
         '2022.1.0',  # ORT OpenVINO
