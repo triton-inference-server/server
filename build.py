20a866909dc59d12390c743750e64d8a0f68b89f
Misha Chornyi <mchornyi@nvidia.com>
2023-02-28 12:27:05 -0800

    Update development versions post-23.02 release

--- build.py
+++ build.py
@@ -67,8 +67,8 @@ from inspect import getsourcefile
 # incorrectly load the other version of the openvino libraries.
 #
 TRITON_VERSION_MAP = {
-    '2.31.0': (
-        '23.02',  # triton container
+    '2.32.0dev': (
+        '23.03dev',  # triton container
         '23.02',  # upstream container
         '1.13.1',  # ORT
         '2022.1.0',  # ORT OpenVINO
