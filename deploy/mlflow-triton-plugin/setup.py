2d6ad61b76e2811d34367024e7a82e16c68d4492
albertoperdomo2 <alberto.perdomo@crisalix.com>
2023-03-01 08:17:23 +0000

    Bump version to 0.2.0

--- deploy/mlflow-triton-plugin/setup.py
+++ deploy/mlflow-triton-plugin/setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlflow-triton",
-    version="0.1.0",
+    version="0.2.0",
     description="Triton Mlflow Deployment",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
