4dd3f1300763eed1176d96be0eacad7a0ecefc95
krishung5 <krish@nvidia.com>
2023-03-10 22:40:41 -0800

    Increase timeout for onnxruntime models

--- qa/L0_infer/test.sh
+++ qa/L0_infer/test.sh
@@ -61,9 +61,9 @@ if [ "$TEST_VALGRIND" -eq 1 ]; then
     LEAKCHECK_LOG_BASE="./valgrind_test"
     LEAKCHECK=/usr/bin/valgrind
     LEAKCHECK_ARGS_BASE="--leak-check=full --show-leak-kinds=definite --max-threads=3000 --num-callers=20"
-    SERVER_TIMEOUT=4000
+    SERVER_TIMEOUT=18000
     rm -f $LEAKCHECK_LOG_BASE*
-    BACKENDS="onnx"
+    BACKENDS="graphdef savedmodel onnx libtorch plan openvino"
 fi
 
 if [ "$TEST_SYSTEM_SHARED_MEMORY" -eq 1 ] || [ "$TEST_CUDA_SHARED_MEMORY" -eq 1 ]; then
