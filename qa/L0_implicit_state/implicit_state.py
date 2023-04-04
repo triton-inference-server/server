da60ae5922b2a5cc22822164843a1b030108b331
Ryan McCormick <rmccormick@nvidia.com>
2023-04-04 10:55:12 -0700

    Update copyright

--- qa/L0_implicit_state/implicit_state.py
+++ qa/L0_implicit_state/implicit_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
