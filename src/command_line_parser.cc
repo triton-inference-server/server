442cd7b61005741010353361a140c47652813a0a
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2023-04-10 16:41:49 -0700

    Fix metric address in cmdline (#5620)

--- src/command_line_parser.cc
+++ src/command_line_parser.cc
@@ -1070,13 +1070,13 @@ TritonParser::Parse(int argc, char** argv)
         break;
       case OPTION_HTTP_ADDRESS:
         lparams.http_address_ = optarg;
+#ifdef TRITON_ENABLE_METRICS
+        lparams.metrics_address_ = optarg;
+#endif  // TRITON_ENABLE_METRICS
         break;
       case OPTION_HTTP_HEADER_FORWARD_PATTERN:
         lparams.http_forward_header_pattern_ = optarg;
         break;
-#ifdef TRITON_ENABLE_METRICS
-        lparams.metrics_address_ = optarg;
-#endif  // TRITON_ENABLE_METRICS
         break;
       case OPTION_HTTP_THREAD_COUNT:
         lparams.http_thread_cnt_ = ParseOption<int>(optarg);
