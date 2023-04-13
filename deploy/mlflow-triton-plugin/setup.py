6a648fe4356ec37a67ac251395c11ca164079a70
albertoperdomo2 <alberto.perdomo@crisalix.com>
2023-04-13 09:13:37 +0100

    Add boto3 to plugin requirements.

--- deploy/mlflow-triton-plugin/setup.py
+++ deploy/mlflow-triton-plugin/setup.py
@@ -32,6 +32,6 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=["mlflow>=1.15.0,<2.0", "tritonclient[all]"],
+    install_requires=["mlflow>=1.15.0,<2.0", "tritonclient[all]", "boto3"],
     entry_points={"mlflow.deployments": "triton=mlflow_triton.deployments"},
 )
