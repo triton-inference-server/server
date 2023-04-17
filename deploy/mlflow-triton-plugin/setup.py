2723f70dd8dab1fbd10c521048438c19089352a9
Alberto Perdomo <alberto.perdomo@crisalix.com>
2023-04-17 17:31:04 +0100

    Implement s3 support for MLFLow plugin (#5435)
    * Implement s3 support
    
    * Bump version to 0.2.0
    
    * Review fixes
    
    * Add test.
    
    * Adapt to work with ONNX.
    
    * Add tests.
    
    Co-authored-by: GuanLuo <gluo@nvidia.com>
    
    * Evaluate flavor when uploading models.
    
    * Add boto3 to plugin requirements.
    
    * Restore plugin_test.py
    
    ---------
    
    Co-authored-by: GuanLuo <gluo@nvidia.com>
    Co-authored-by: GuanLuo <41310872+GuanLuo@users.noreply.github.com>

--- deploy/mlflow-triton-plugin/setup.py
+++ deploy/mlflow-triton-plugin/setup.py
@@ -27,11 +27,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlflow-triton",
-    version="0.1.0",
+    version="0.2.0",
     description="Triton Mlflow Deployment",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=["mlflow>=2.2.1,<3.0", "tritonclient[all]"],
+    install_requires=["mlflow>=2.2.1,<3.0", "tritonclient[all]", "boto3"],
     entry_points={"mlflow.deployments": "triton=mlflow_triton.deployments"},
 )
