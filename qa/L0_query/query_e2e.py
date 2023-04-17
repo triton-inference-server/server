52fe95c52a43bfaf1ab515b7fc1652483a8dbd84
David Yastremsky <dyastremsky@nvidia.com>
2023-04-17 13:05:28 -0700

    Fix copyrights

--- qa/L0_query/query_e2e.py
+++ qa/L0_query/query_e2e.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2019-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2021-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
