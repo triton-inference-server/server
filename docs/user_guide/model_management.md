a186b44f0b5f4c53141a1c6270ad539b4aab3e0e
Jacky <18255193+kthui@users.noreply.github.com>
2023-04-26 14:37:05 -0700

    Add model instance update test (#5635)
    * Add basic model instance update test
    
    * Add more tests
    
    * Add test for blocking behavior and update beyond instance
    
    * Add instance shuffle test
    
    * Add docs for model instance update
    
    * Add docs on sequence model update limitation
    
    * Refactor python model_init_del
    
    * Util function consolidation
    
    * Refactor instance update test
    
    * Add short test description
    
    * Add test for zero instance count
    
    * Add instance update with parallel infer
    
    * Improve test reliability on file access
    
    * Add test for rate limiter resource update

--- docs/user_guide/model_management.md
+++ docs/user_guide/model_management.md
@@ -193,6 +193,20 @@ existing shared-libraries to another location outside of the model
 repository, copy in the new shared libraries, and then reload the
 model.
 
+* If only the model instance configuration on the 'config.pbtxt' is modified
+(i.e. increasing/decreasing the instance count) for non-sequence models,
+then Triton will update the model rather then reloading it, when either a load
+request is received under
+[Model Control Mode EXPLICIT](#model-control-mode-explicit) or change to the
+'config.pbtxt' is detected under
+[Model Control Mode POLL](#model-control-mode-poll).
+
+* If a sequence model is updated with in-flight sequence(s), Triton does not
+guarentee any remaining request(s) from the in-flight sequence(s) will be routed
+to the same model instance for processing. It is currently the responsibility of
+the user to ensure any in-flight sequence(s) is complete before updating a
+sequence model.
+
 ## Concurrently Loading Models
 
 To reduce service downtime, Triton loads new models in the background while
---
+++ qa/L0_model_update/test.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# Copyright 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REPO_VERSION=${NVIDIA_TRITON_SERVER_VERSION}
+if [ "$#" -ge 1 ]; then
+    REPO_VERSION=$1
+fi
+if [ -z "$REPO_VERSION" ]; then
+    echo -e "Repository version must be specified"
+    echo -e "\n***\n*** Test Failed\n***"
+    exit 1
+fi
+if [ ! -z "$TEST_REPO_ARCH" ]; then
+    REPO_VERSION=${REPO_VERSION}_${TEST_REPO_ARCH}
+fi
+
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONDONTWRITEBYTECODE="True"
+export MODEL_LOG_DIR="`pwd`"
+
+SERVER=/opt/tritonserver/bin/tritonserver
+source ../common/util.sh
+
+function setup_models() {
+    rm -rf models && mkdir models
+    # Basic model that log instance creation and destruction
+    cp -r ../python_models/model_init_del models/model_init_del && \
+        mkdir models/model_init_del/1 && \
+        mv models/model_init_del/model.py models/model_init_del/1
+}
+
+SERVER_ARGS="--model-repository=models --model-control-mode=explicit --rate-limit=execution_count --log-verbose=2"
+SERVER_LOG="./server.log"
+setup_models
+run_server
+if [ "$SERVER_PID" == "0" ]; then
+    echo -e "\n***\n*** Failed to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+RET=0
+
+set +e
+python instance_update_test.py > instance_update_test.log 2>&1
+if [ $? -ne 0 ]; then
+    cat instance_update_test.log
+    RET=1
+fi
+set -e
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+if [ $RET -eq 0 ]; then
+    echo -e "\n***\n*** Test Passed\n***"
+else
+    echo -e "\n***\n*** Test FAILED\n***"
+fi
+
+exit $RET
