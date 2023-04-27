bd664a07702d256be88529e82336a862c1209eac
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2023-04-27 10:16:47 -0700

    Update README and docs post-23.04 release (#5707)
    * Update README and versions for 23.04 branch
    
    * Update README and docs post-23.04 release
    
    * Update 'main' to track development for 2.34.0 / r23.05
    
    * Update version references to 23.04
    
    * Fix URL address
    
    ---------
    
    Co-authored-by: Tanmay Verma <tanmay2592@gmail.com>

--- deploy/gke-marketplace-app/server-deployer/build_and_push.sh
+++ deploy/gke-marketplace-app/server-deployer/build_and_push.sh
@@ -26,9 +26,9 @@
 
 export REGISTRY=gcr.io/$(gcloud config get-value project | tr ':' '/')
 export APP_NAME=tritonserver
-export MAJOR_VERSION=2.32
-export MINOR_VERSION=2.32.0
-export NGC_VERSION=23.03-py3
+export MAJOR_VERSION=2.33
+export MINOR_VERSION=2.33.0
+export NGC_VERSION=23.04-py3
 
 docker pull nvcr.io/nvidia/$APP_NAME:$NGC_VERSION
 
