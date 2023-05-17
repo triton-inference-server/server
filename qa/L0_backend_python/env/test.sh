688528d343025768d1a00debf9cf7fe7fb80548f
Iman Tabrizian <itabrizian@nvidia.com>
2023-05-17 18:48:15 -0400

    Fix up

--- qa/L0_backend_python/env/test.sh
+++ qa/L0_backend_python/env/test.sh
@@ -150,7 +150,7 @@ wait $SERVER_PID
 
 set +e
 
-PY38_ENV_EXTRACTION="Extracting Python execution env" 
+PY310_ENV_EXTRACTION="Extracting Python execution env" 
 if [ `grep -c "${PY310_ENV_EXTRACTION}" ${SERVER_LOG}` != "2" ]; then
     cat $SERVER_LOG
     echo -e "\n***\n*** Python execution environment should be extracted exactly twice. \n***"
