17bac702248d4366913a5f2f73897bea42c2a767
krishung5 <krish@nvidia.com>
2023-06-26 12:47:36 -0700

    Improve the error message when the number of GPUs is insufficient

--- qa/L0_libtorch_instance_group_kind_model/test.sh
+++ qa/L0_libtorch_instance_group_kind_model/test.sh
@@ -69,7 +69,7 @@ python3 gen_models.py >> $CLIENT_LOG 2>&1
 if [ $? -ne 0 ]; then
     echo -e "\n***\n*** Error when generating models. \n***"
     cat $CLIENT_LOG
-    RET=1
+    exit 1
 fi
 set -e
 
