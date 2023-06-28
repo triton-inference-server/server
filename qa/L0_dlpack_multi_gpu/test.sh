647663b6ef6a8c46e511d64759aca3277141191e
Iman Tabrizian <itabrizian@nvidia.com>
2023-06-28 17:00:18 -0400

    Fix copyright

--- qa/L0_dlpack_multi_gpu/test.sh
+++ qa/L0_dlpack_multi_gpu/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
