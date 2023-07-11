62306b228e8f3648c53dc7cc418ec5722989d4be
David Yastremsky <dyastremsky@nvidia.com>
2023-07-10 19:47:08 -0700

    Run pre-commit hooks

--- qa/L0_implicit_state/implicit_state.py
+++ qa/L0_implicit_state/implicit_state.py
@@ -27,109 +27,110 @@
 
 import sys
 
-sys.path.append('../common')
+sys.path.append("../common")
+
+import os
+import unittest
+from builtins import range
 
 import numpy as np
-import os
-from builtins import range
+import test_util as tu
 import tritonclient.http as tritonhttpclient
 from tritonclient.utils import InferenceServerException
-import unittest
-import test_util as tu
 
-BACKENDS = os.environ.get('BACKENDS', "onnx plan libtorch")
+BACKENDS = os.environ.get("BACKENDS", "onnx plan libtorch")
 
 
 class ImplicitStateTest(tu.TestResultCollector):
-
     def test_no_implicit_state(self):
         triton_client = tritonhttpclient.InferenceServerClient("localhost:8000")
         inputs = []
-        inputs.append(tritonhttpclient.InferInput('INPUT', [1], 'INT32'))
-        inputs.append(tritonhttpclient.InferInput('TEST_CASE', [1], 'INT32'))
-        inputs[0].set_data_from_numpy(
-            np.random.randint(5, size=[1], dtype=np.int32))
+        inputs.append(tritonhttpclient.InferInput("INPUT", [1], "INT32"))
+        inputs.append(tritonhttpclient.InferInput("TEST_CASE", [1], "INT32"))
+        inputs[0].set_data_from_numpy(np.random.randint(5, size=[1], dtype=np.int32))
         inputs[1].set_data_from_numpy(np.asarray([0], dtype=np.int32))
 
         with self.assertRaises(InferenceServerException) as e:
-            triton_client.infer(model_name="no_implicit_state",
-                                inputs=inputs,
-                                sequence_id=1,
-                                sequence_start=True)
+            triton_client.infer(
+                model_name="no_implicit_state",
+                inputs=inputs,
+                sequence_id=1,
+                sequence_start=True,
+            )
 
         err_str = str(e.exception).lower()
         self.assertIn("unable to add state 'undefined_state'", err_str)
         self.assertIn(
-            "state configuration is missing for model 'no_implicit_state'",
-            err_str)
+            "state configuration is missing for model 'no_implicit_state'", err_str
+        )
 
     def test_wrong_implicit_state_name(self):
         triton_client = tritonhttpclient.InferenceServerClient("localhost:8000")
         inputs = []
-        inputs.append(tritonhttpclient.InferInput('INPUT', [1], 'INT32'))
-        inputs.append(tritonhttpclient.InferInput('TEST_CASE', [1], 'INT32'))
-        inputs[0].set_data_from_numpy(
-            np.random.randint(5, size=[1], dtype=np.int32))
+        inputs.append(tritonhttpclient.InferInput("INPUT", [1], "INT32"))
+        inputs.append(tritonhttpclient.InferInput("TEST_CASE", [1], "INT32"))
+        inputs[0].set_data_from_numpy(np.random.randint(5, size=[1], dtype=np.int32))
         inputs[1].set_data_from_numpy(np.asarray([0], dtype=np.int32))
 
         with self.assertRaises(InferenceServerException) as e:
-            triton_client.infer(model_name="wrong_internal_state",
-                                inputs=inputs,
-                                sequence_id=2,
-                                sequence_start=True)
+            triton_client.infer(
+                model_name="wrong_internal_state",
+                inputs=inputs,
+                sequence_id=2,
+                sequence_start=True,
+            )
 
         err_str = str(e.exception).lower()
-        self.assertIn("state 'undefined_state' is not a valid state name",
-                      err_str)
+        self.assertIn("state 'undefined_state' is not a valid state name", err_str)
 
     def test_no_update(self):
         # Test implicit state without updating any state
         triton_client = tritonhttpclient.InferenceServerClient("localhost:8000")
         inputs = []
-        inputs.append(tritonhttpclient.InferInput('INPUT', [1], 'INT32'))
-        inputs.append(tritonhttpclient.InferInput('TEST_CASE', [1], 'INT32'))
+        inputs.append(tritonhttpclient.InferInput("INPUT", [1], "INT32"))
+        inputs.append(tritonhttpclient.InferInput("TEST_CASE", [1], "INT32"))
         inputs[0].set_data_from_numpy(np.asarray([1], dtype=np.int32))
         inputs[1].set_data_from_numpy(np.asarray([1], dtype=np.int32))
         correlation_id = 3
 
         # Make sure the state is never updated.
-        result_start = triton_client.infer(model_name="no_state_update",
-                                           inputs=inputs,
-                                           sequence_id=correlation_id,
-                                           sequence_start=True)
-        self.assertEqual(result_start.as_numpy('OUTPUT')[0], 1)
+        result_start = triton_client.infer(
+            model_name="no_state_update",
+            inputs=inputs,
+            sequence_id=correlation_id,
+            sequence_start=True,
+        )
+        self.assertEqual(result_start.as_numpy("OUTPUT")[0], 1)
         for _ in range(10):
-            result = triton_client.infer(model_name="no_state_update",
-                                         inputs=inputs,
-                                         sequence_id=correlation_id)
-            self.assertEqual(result.as_numpy('OUTPUT')[0], 1)
+            result = triton_client.infer(
+                model_name="no_state_update", inputs=inputs, sequence_id=correlation_id
+            )
+            self.assertEqual(result.as_numpy("OUTPUT")[0], 1)
 
-        result_start = triton_client.infer(model_name="no_state_update",
-                                           inputs=inputs,
-                                           sequence_id=correlation_id,
-                                           sequence_end=True)
-        self.assertEqual(result.as_numpy('OUTPUT')[0], 1)
+        result_start = triton_client.infer(
+            model_name="no_state_update",
+            inputs=inputs,
+            sequence_id=correlation_id,
+            sequence_end=True,
+        )
+        self.assertEqual(result.as_numpy("OUTPUT")[0], 1)
 
     def test_request_output_not_allowed(self):
         triton_client = tritonhttpclient.InferenceServerClient("localhost:8000")
 
         for backend in BACKENDS.split(" "):
             inputs = []
-            if backend.strip() == 'libtorch':
-                inputs.append(
-                    tritonhttpclient.InferInput('INPUT__0', [1], 'INT32'))
+            if backend.strip() == "libtorch":
+                inputs.append(tritonhttpclient.InferInput("INPUT__0", [1], "INT32"))
             else:
-                inputs.append(tritonhttpclient.InferInput(
-                    'INPUT', [1], 'INT32'))
+                inputs.append(tritonhttpclient.InferInput("INPUT", [1], "INT32"))
             inputs[0].set_data_from_numpy(np.asarray([1], dtype=np.int32))
 
             outputs = []
-            if backend.strip() == 'libtorch':
-                outputs.append(
-                    tritonhttpclient.InferRequestedOutput('OUTPUT_STATE__1'))
+            if backend.strip() == "libtorch":
+                outputs.append(tritonhttpclient.InferRequestedOutput("OUTPUT_STATE__1"))
             else:
-                outputs.append(
-                    tritonhttpclient.InferRequestedOutput('OUTPUT_STATE'))
+                outputs.append(tritonhttpclient.InferRequestedOutput("OUTPUT_STATE"))
 
             with self.assertRaises(InferenceServerException) as e:
                 triton_client.infer(
@@ -138,38 +139,36 @@ class ImplicitStateTest(tu.TestResultCollector):
                     outputs=outputs,
                     sequence_id=1,
                     sequence_start=True,
-                    sequence_end=True)
-            if backend.strip() == 'libtorch':
+                    sequence_end=True,
+                )
+            if backend.strip() == "libtorch":
                 self.assertIn(
                     "unexpected inference output 'OUTPUT_STATE__1' for model",
-                    str(e.exception))
+                    str(e.exception),
+                )
             else:
                 self.assertIn(
                     "unexpected inference output 'OUTPUT_STATE' for model",
-                    str(e.exception))
+                    str(e.exception),
+                )
 
     def test_request_output(self):
         triton_client = tritonhttpclient.InferenceServerClient("localhost:8000")
         for backend in BACKENDS.split(" "):
             inputs = []
-            if backend.strip() == 'libtorch':
-                inputs.append(
-                    tritonhttpclient.InferInput('INPUT__0', [1], 'INT32'))
+            if backend.strip() == "libtorch":
+                inputs.append(tritonhttpclient.InferInput("INPUT__0", [1], "INT32"))
             else:
-                inputs.append(tritonhttpclient.InferInput(
-                    'INPUT', [1], 'INT32'))
+                inputs.append(tritonhttpclient.InferInput("INPUT", [1], "INT32"))
             inputs[0].set_data_from_numpy(np.asarray([1], dtype=np.int32))
 
             outputs = []
-            if backend.strip() == 'libtorch':
-                outputs.append(
-                    tritonhttpclient.InferRequestedOutput('OUTPUT_STATE__1'))
-                outputs.append(
-                    tritonhttpclient.InferRequestedOutput('OUTPUT__0'))
+            if backend.strip() == "libtorch":
+                outputs.append(tritonhttpclient.InferRequestedOutput("OUTPUT_STATE__1"))
+                outputs.append(tritonhttpclient.InferRequestedOutput("OUTPUT__0"))
             else:
-                outputs.append(
-                    tritonhttpclient.InferRequestedOutput('OUTPUT_STATE'))
-                outputs.append(tritonhttpclient.InferRequestedOutput('OUTPUT'))
+                outputs.append(tritonhttpclient.InferRequestedOutput("OUTPUT_STATE"))
+                outputs.append(tritonhttpclient.InferRequestedOutput("OUTPUT"))
 
             result = triton_client.infer(
                 model_name=f"{backend}_nobatch_sequence_int32_output",
@@ -177,14 +176,15 @@ class ImplicitStateTest(tu.TestResultCollector):
                 outputs=outputs,
                 sequence_id=1,
                 sequence_start=True,
-                sequence_end=True)
-            if backend.strip() == 'libtorch':
-                self.assertTrue(result.as_numpy('OUTPUT_STATE__1')[0], 1)
-                self.assertTrue(result.as_numpy('OUTPUT__0')[0], 1)
+                sequence_end=True,
+            )
+            if backend.strip() == "libtorch":
+                self.assertTrue(result.as_numpy("OUTPUT_STATE__1")[0], 1)
+                self.assertTrue(result.as_numpy("OUTPUT__0")[0], 1)
             else:
-                self.assertTrue(result.as_numpy('OUTPUT_STATE')[0], 1)
-                self.assertTrue(result.as_numpy('OUTPUT')[0], 1)
+                self.assertTrue(result.as_numpy("OUTPUT_STATE")[0], 1)
+                self.assertTrue(result.as_numpy("OUTPUT")[0], 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
