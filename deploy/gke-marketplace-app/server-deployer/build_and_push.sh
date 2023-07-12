47bb628dd088e7d89bd859f6db42fbe59629f30c
Dong Meng <mengdong0427@gmail.com>
2023-07-12 00:36:08 +0000

    cleannup

--- deploy/gke-marketplace-app/server-deployer/build_and_push.sh
+++ deploy/gke-marketplace-app/server-deployer/build_and_push.sh
@@ -26,7 +26,7 @@
 
 export REGISTRY=gcr.io/$(gcloud config get-value project | tr ':' '/')
 export APP_NAME=tritonserver
-export MAJOR_VERSION=2.33
+export MAJOR_VERSION=2.35
 export MINOR_VERSION=2.35.0
 export NGC_VERSION=23.06-py3
 
