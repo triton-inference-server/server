f86c56b501f4c5bcadfc63b8a93aff3ca5155169
David Yastremsky <dyastremsky@nvidia.com>
2023-07-12 19:14:39 -0700

    Numpy asserts, iterate over larger tensors

--- qa/L0_trt_data_dependent_shape/trt_data_dependent_shape_test.py
+++ qa/L0_trt_data_dependent_shape/trt_data_dependent_shape_test.py
@@ -43,7 +43,7 @@ class TrtDataDependentShapeTest(tu.TestResultCollector):
     def test_fixed(self):
         model_name = "plan_nobatch_nonzero_fixed"
         input_np = np.arange(16, dtype=np.int32).reshape((4, 4))
-        expected_output_np = np.nonzero(input_np)
+        expected_output_np = np.nonzero(np.nonzero(input_np))
 
         inputs = []
         inputs.append(client.InferInput('INPUT', [4, 4], "INT32"))
@@ -52,28 +52,32 @@ class TrtDataDependentShapeTest(tu.TestResultCollector):
         results = self.triton_client.infer(model_name=model_name, inputs=inputs)
         # Validate the results by comparing with precomputed values.
         output_np = results.as_numpy('OUTPUT')
-        self.assertTrue(
-            np.array_equal(output_np, expected_output_np),
+        np.testing.assert_equal(output_np, expected_output_np,
             "OUTPUT expected: {}, got {}".format(expected_output_np, output_np))
 
     def test_dynamic(self):
         model_name = "plan_nobatch_nonzero_dynamic"
-        input_data = []
-        for i in range(20 * 16):
-            input_data.append(i if (i % 2) == 0 else 0)
-        input_np = np.array(input_data, dtype=np.int32).reshape((20, 16))
-        expected_output_np = np.nonzero(input_np)
+        
+        input_sizes = [(20, 16), (32, 30)]
 
-        inputs = []
-        inputs.append(client.InferInput('INPUT', [20, 16], "INT32"))
-        inputs[-1].set_data_from_numpy(input_np)
+        for input_size in input_sizes:
+            x, y = input_size
+            input_data = []
+            
+            for i in range(x * y):
+                input_data.append(i if (i % 2) == 0 else 0)
+            input_np = np.array(input_data, dtype=np.int32).reshape((x, y))
+            expected_output_np = np.nonzero(np.nonzero(input_np))
 
-        results = self.triton_client.infer(model_name=model_name, inputs=inputs)
-        # Validate the results by comparing with precomputed values.
-        output_np = results.as_numpy('OUTPUT')
-        self.assertTrue(
-            np.array_equal(output_np, expected_output_np),
-            "OUTPUT expected: {}, got {}".format(expected_output_np, output_np))
+            inputs = []
+            inputs.append(client.InferInput('INPUT', [x, y], "INT32"))
+            inputs[-1].set_data_from_numpy(input_np)
+
+            results = self.triton_client.infer(model_name=model_name, inputs=inputs)
+            # Validate the results by comparing with precomputed values.
+            output_np = results.as_numpy('OUTPUT')
+            np.testing.assert_equal(output_np, expected_output_np,
+                "OUTPUT expected: {}, got {}".format(expected_output_np, output_np))
 
 
 if __name__ == '__main__':
