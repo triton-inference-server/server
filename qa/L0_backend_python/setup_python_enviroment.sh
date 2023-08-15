0c35c7ccba34ac2016d0d2ab34910387faec147e
David Yastremsky <dyastremsky@nvidia.com>
2023-08-15 11:19:18 -0700

    Remove deprecated distutils

--- qa/L0_backend_python/setup_python_enviroment.sh
+++ qa/L0_backend_python/setup_python_enviroment.sh
@@ -150,7 +150,6 @@ cp python_backend/builddir/triton_python_backend_stub /opt/tritonserver/backends
 add-apt-repository ppa:deadsnakes/ppa -y
 apt-get update && apt-get -y install \
                             "python3.${PYTHON_ENV_VERSION}-dev" \
-                            "python3.${PYTHON_ENV_VERSION}-distutils" \
                             libboost-dev
 rm -f /usr/bin/python3 && \
 ln -s "/usr/bin/python3.${PYTHON_ENV_VERSION}" /usr/bin/python3 && \
