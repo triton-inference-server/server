17b347cff0df53b97332615c475ceb2be78ba230
Tanmay Verma <tanmay2592@gmail.com>
2023-09-07 17:36:30 -0700

    Update qa/L0_client_cancellation/client_cancellation_test.py
    Co-authored-by: Ryan McCormick <rmccormick@nvidia.com>

--- qa/L0_client_cancellation/client_cancellation_test.py
+++ qa/L0_client_cancellation/client_cancellation_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2020-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
