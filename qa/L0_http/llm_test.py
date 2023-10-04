20380866ff20ad296b77e37fa8f837ffc243b317
GuanLuo <gluo@nvidia.com>
2023-10-04 06:40:08 -0700

    Complete test placeholder

--- qa/L0_http/llm_test.py
+++ qa/L0_http/llm_test.py
@@ -30,6 +30,7 @@ import sys
 sys.path.append("../common")
 
 import json
+import threading
 import unittest
 
 import requests
@@ -38,6 +39,9 @@ import test_util as tu
 
 
 class HttpTest(tu.TestResultCollector):
+    def setUp(self):
+        self._model_name = "vllm_proxy"
+
     def _get_infer_url(self, model_name, route):
         return f"http://localhost:8000/v2/models/{model_name}/{route}"
 
@@ -52,6 +56,10 @@ class HttpTest(tu.TestResultCollector):
             headers=headers,
             stream=False,
         )
+
+    def generate(self, model_name, inputs):
+        url = self._get_infer_url(model_name, "generate")
+        return requests.post(url, data=json.dumps(inputs))
 
     def generate_expect_failure(self, model_name, inputs, msg):
         url = self._get_infer_url(model_name, "generate")
@@ -107,14 +115,11 @@ class HttpTest(tu.TestResultCollector):
             )
 
     def test_generate(self):
-        model_name = "vllm_proxy"
         # Setup text-based input
         text = "hello world"
         inputs = {"PROMPT": text, "STREAM": False}
 
-        url = self._get_infer_url(model_name, "generate")
-        r = requests.post(url, data=json.dumps(inputs))
-
+        r = self.generate(self._model_name, inputs)
         r.raise_for_status()
 
         self.assertIn("Content-Type", r.headers)
@@ -125,15 +130,13 @@ class HttpTest(tu.TestResultCollector):
         self.assertEqual(text, data["TEXT"])
 
     def test_generate_stream(self):
-        model_name = "vllm_proxy"
         # Setup text-based input
         text = "hello world"
         rep_count = 3
         inputs = {"PROMPT": [text], "STREAM": True, "REPETITION": rep_count}
-        self.generate_stream_expect_success(model_name, inputs, text, rep_count)
+        self.generate_stream_expect_success(self._model_name, inputs, text, rep_count)
 
     def test_missing_inputs(self):
-        model_name = "vllm_proxy"
         missing_all_inputs = [
             # Missing all inputs
             {},
@@ -147,22 +150,21 @@ class HttpTest(tu.TestResultCollector):
         ]
         for inputs in missing_all_inputs:
             self.generate_expect_failure(
-                model_name, inputs, "expected 2 inputs but got 0"
+                self._model_name, inputs, "expected 2 inputs but got 0"
             )
             self.generate_stream_expect_failure(
-                model_name, inputs, "expected 2 inputs but got 0"
+                self._model_name, inputs, "expected 2 inputs but got 0"
             )
 
         for inputs in missing_one_input:
             self.generate_expect_failure(
-                model_name, inputs, "expected 2 inputs but got 1"
+                self._model_name, inputs, "expected 2 inputs but got 1"
             )
             self.generate_stream_expect_failure(
-                model_name, inputs, "expected 2 inputs but got 1"
+                self._model_name, inputs, "expected 2 inputs but got 1"
             )
 
     def test_invalid_input_types(self):
-        model_name = "vllm_proxy"
         invalid_bool = "attempt to access JSON non-boolean as boolean"
         invalid_string = "attempt to access JSON non-string as string"
         invalid_type_inputs = [
@@ -176,11 +178,10 @@ class HttpTest(tu.TestResultCollector):
         ]
 
         for inputs, error_msg in invalid_type_inputs:
-            self.generate_expect_failure(model_name, inputs, error_msg)
-            self.generate_stream_expect_failure(model_name, inputs, error_msg)
+            self.generate_expect_failure(self._model_name, inputs, error_msg)
+            self.generate_stream_expect_failure(self._model_name, inputs, error_msg)
 
     def test_duplicate_inputs(self):
-        model_name = "vllm_proxy"
         dupe_prompt = "input 'PROMPT' already exists in request"
         dupe_stream = "input 'STREAM' already exists in request"
         # Use JSON string directly as Python Dict doesn't support duplicate keys
@@ -202,15 +203,14 @@ class HttpTest(tu.TestResultCollector):
             ),
         ]
         for inputs, error_msg in invalid_type_inputs:
-            self.generate_expect_failure(model_name, inputs, error_msg)
-            self.generate_stream_expect_failure(model_name, inputs, error_msg)
+            self.generate_expect_failure(self._model_name, inputs, error_msg)
+            self.generate_stream_expect_failure(self._model_name, inputs, error_msg)
 
     def test_generate_stream_response_error(self):
-        model_name = "vllm_proxy"
         # Setup text-based input
         text = "hello world"
         inputs = {"PROMPT": [text], "STREAM": True, "REPETITION": 0, "FAIL_LAST": True}
-        r = self.generate_stream(model_name, inputs)
+        r = self.generate_stream(self._model_name, inputs)
 
         # With "REPETITION": 0, error will be first response and the HTTP code
         # will be set properly
@@ -222,81 +222,107 @@ class HttpTest(tu.TestResultCollector):
         # With "REPETITION" > 0, the first response is valid response and set
         # HTTP code to success, so user must validate each response
         inputs["REPETITION"] = 1
-        r = self.generate_stream(model_name, inputs)
+        r = self.generate_stream(self._model_name, inputs)
         r.raise_for_status()
 
         self.check_sse_responses(r, [{"TEXT": text}, {"error": "An Error Occurred"}])
 
-    # NOTE: The below is to reproduce a current segfault, not the real test.
-    """
-    # First call /generate_stream Stream=False
-    curl -s -w "\n%{http_code}\n" -X POST localhost:8000/v2/models/vllm_proxy/generate_stream -d '{"PROMPT": "hi there", "STREAM": false}'
-
-    # Then call /generate_stream Stream=True
-    curl -s -w "\n%{http_code}\n" -X POST localhost:8000/v2/models/vllm_proxy/generate_stream -d '{"PROMPT": "hi there", "STREAM": true}'
-
-    Thread 53 "tritonserver" received signal SIGSEGV, Segmentation fault.
-    [Switching to Thread 0x7fd11e7fc000 (LWP 3456520)]
-    0x000055882d952886 in evbuffer_get_length (buffer=0x0) at /mnt/triton/jira/llm_rest/server/build/_deps/repo-third-party-build/libevent/src/libevent/buffer.c:610
-    610             EVBUFFER_LOCK(buffer);
-    (gdb) bt
-    #0  0x000055882d952886 in evbuffer_get_length (buffer=0x0)
-        at /mnt/triton/jira/llm_rest/server/build/_deps/repo-third-party-build/libevent/src/libevent/buffer.c:610
-    #1  0x000055882d94935b in evhtp_send_reply_chunk (request=0x7fd10c005f50, buf=0x0)
-        at /mnt/triton/jira/llm_rest/server/build/_deps/repo-third-party-src/libevhtp/libevhtp/evhtp.c:3969
-    #2  0x000055882d902d27 in triton::server::HTTPAPIServer::GenerateRequestClass::ChunkResponseCallback (thr=0x55882ffc8de0,
-        arg=0x7fd10c003810, shared=0x55882ffc8cc0) at /mnt/triton/jira/llm_rest/server/src/http_server.cc:4003
-    """
-
-    def test_segfault(self):
-        model_name = "vllm_proxy"
-        # NOTE with curl on CLI, it seems to segfault consistently, but with
-        # requests.post() in this script, it seems intermittent and may require re-run
+    def test_race_condition(self):
         input1 = {"PROMPT": "hello", "STREAM": False, "param": "segfault"}
         input2 = {"PROMPT": "hello", "STREAM": True, "param": "segfault"}
-        r1 = self.generate_stream(model_name, input1)
-        r1.raise_for_status()
-        # FIXME: Causes segfault when executed in this order False -> True
-        r2 = self.generate_stream(model_name, input2)
-        r2.raise_for_status()
+        threads = []
 
-        self.assertTrue(
-            False,
-            "This segfaults consistently with curl, seeems intermittent with requests.post()",
-        )
+        def thread_func(model_name, input):
+            self.generate_stream(model_name, input1).raise_for_status()
 
-    # TODO
-    def test_non_decoupled(self):
-        # OK for /generate
-        # OK for /generate_stream
-        self.assertTrue(False, "Not implemented yet")
-
-    # TODO
-    def test_decoupled_one_response(self):
-        # OK for /generate
-        # OK for /generate_stream
-        self.assertTrue(False, "Not implemented yet")
+        for _ in range(50):
+            threads.append(
+                threading.Thread(target=thread_func, args=((self._model_name, input1)))
+            )
+            threads.append(
+                threading.Thread(target=thread_func, args=((self._model_name, input2)))
+            )
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
 
     # TODO
-    def test_decoupled_zero_response(self):
-        # FAIL for /generate
-        # OK for /generate_stream
-        self.assertTrue(False, "Not implemented yet")
+    def test_one_response(self):
+        # "STREAM" controls response behavior,
+        # True sends two responses, one with infer response and one with flag
+        # only, where generate endpoint will be able to handle as in
+        # non-decoupled mode.
+        # False sends one response with infer response and flag, which is the
+        # same behavior as how non-decoupled model return response.
+        inputs = {"PROMPT": "hello world", "STREAM": True}
+        r = self.generate_stream(self._model_name, inputs)
+        r.raise_for_status()
+        r = self.generate(self._model_name, inputs)
+        r.raise_for_status()
 
-    # TODO
-    def test_decoupled_many_response(self):
-        # FAIL for /generate
-        # OK for /generate_stream
-        self.assertTrue(False, "Not implemented yet")
+        inputs["STREAM"] = False
+        r = self.generate_stream(self._model_name, inputs)
+        r.raise_for_status()
+        r = self.generate(self._model_name, inputs)
+        r.raise_for_status()
 
-    # TODO: Use model from L0_parameters to verify some catch-all parameter
-    # behavior for JSON keys not found in model config.
-    def test_parameters(self):
-        self.assertTrue(False, "Not implemented yet")
+    def test_zero_response(self):
+        inputs = {"PROMPT": "hello world", "STREAM": True, "REPETITION": 0}
+        r = self.generate_stream(self._model_name, inputs)
+        r.raise_for_status()
+        # Expect generate fails the inference
+        r = self.generate(self._model_name, inputs)
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.assertIn(
+                "generate expects model to produce exactly 1 response",
+                r.json()["error"],
+            )
 
-    # TODO: Test unsupported parameters like floats, nested objects, etc.
-    def test_invalid_parameters(self):
-        self.assertTrue(False, "Not implemented yet")
+    def test_many_response(self):
+        inputs = {"PROMPT": "hello world", "STREAM": True, "REPETITION": 2}
+        r = self.generate_stream(self._model_name, inputs)
+        r.raise_for_status()
+        # Expect generate fails the inference
+        r = self.generate(self._model_name, inputs)
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.assertIn(
+                "generate expects model to produce exactly 1 response",
+                r.json()["error"],
+            )
+
+    def test_complex_schema(self):
+        # Currently only the fundamental conversion is supported, nested object
+        # in the request will results in parsing error
+
+        # complex object to parameters (specifying non model input)
+        inputs = {
+            "PROMPT": "hello world",
+            "STREAM": True,
+            "PARAMS": {"PARAM_0": 0, "PARAM_1": True},
+        }
+        r = self.generate(self._model_name, inputs)
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.assertIn("parameter 'PARAMS' has invalid type", r.json()["error"])
+
+        # complex object to model input
+        inputs = {
+            "PROMPT": {"USER": "hello world", "BOT": "world hello"},
+            "STREAM": True,
+        }
+        r = self.generate(self._model_name, inputs)
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            self.assertIn(
+                "attempt to access JSON non-string as string", r.json()["error"]
+            )
 
 
 if __name__ == "__main__":
