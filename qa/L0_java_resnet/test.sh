e27caf0a619de8ac57945cca35b960d455e39a70
Katherine Yang <80359429+jbkyang-nvi@users.noreply.github.com>
2023-10-16 20:07:20 -0700

    Add Javacpp-presets repo location as env variable in Java tests(#6385) (#6441)
    Simplify testing when upstream (javacpp-presets) build changes. Related to triton-inference-server/client#409

--- qa/L0_java_resnet/test.sh
+++ qa/L0_java_resnet/test.sh
@@ -41,6 +41,8 @@ fi
 # Models
 DATADIR=/data/inferenceserver/${REPO_VERSION}
 MODEL_REPO=`pwd`/models
+JAVACPP_BRANCH=${JAVACPP_BRANCH:="https://github.com/bytedeco/javacpp-presets.git"}
+JAVACPP_BRANCH_TAG=${JAVACPP_BRANCH_TAG:="master"}
 
 # Create local model repository
 mkdir -p ${MODEL_REPO}
@@ -55,7 +57,7 @@ done
 # https://github.com/bytedeco/javacpp-presets/blob/master/tritonserver/README.md
 set -e
 git clone --single-branch --depth=1 -b ${TRITON_CLIENT_REPO_TAG} https://github.com/triton-inference-server/client.git
-source client/src/java-api-bindings/scripts/install_dependencies_and_build.sh -b $PWD --keep-build-dependencies
+source client/src/java-api-bindings/scripts/install_dependencies_and_build.sh -b $PWD --javacpp-branch ${JAVACPP_BRANCH} --javacpp-tag ${JAVACPP_BRANCH_TAG} --keep-build-dependencies
 cd ..
 
 CLIENT_LOG="client.log"
