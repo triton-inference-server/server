6200f5b297083f985eaaeb1e381351ea47581ae7
Misha Chornyi <mchornyi@nvidia.com>
2023-10-26 17:54:45 -0700

    restore missed lines

--- build.py
+++ build.py
@@ -78,8 +78,6 @@ TRITON_VERSION_MAP = {
         "2023.0.0",  # Standalone OpenVINO
         "2.4.7",  # DCGM version
         "py310_23.1.0-1",  # Conda version
-        "9.1.0.1",  # TRT version for building TRT-LLM backend
-        "12.2",  # CUDA version for building TRT-LLM backend
         "0.2.1.post1",  # vLLM version
     )
 }
