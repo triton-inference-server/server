17221eaa034ea2f04b0e0fdd24c8f2562ea59f36
nnshah1 <neelays@nvidia.com>
2023-10-30 22:31:48 -0700

    updated

--- qa/L0_http/http_restricted_api_test.py
+++ qa/L0_http/http_restricted_api_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
