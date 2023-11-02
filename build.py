d5ea171adfbe15130c396120dc73288b4f5d93b2
Ryan McCormick <rmccormick@nvidia.com>
2023-11-02 14:50:12 -0700

    Build and instlal boost libraries for boost::filesystem

--- build.py
+++ build.py
@@ -1034,6 +1034,7 @@ RUN pip3 install --upgrade pip && \
 RUN wget -O /tmp/boost.tar.gz \
         https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz && \
     (cd /tmp && tar xzf boost.tar.gz) && \
+    cd /tmp/boost_1_80_0 && ./bootstrap.sh --prefix=/usr && ./b2 install && \
     mv /tmp/boost_1_80_0/boost /usr/include/boost
 
 # Server build requires recent version of CMake (FetchContent required)
