33c882bd0d8ba13b486a978226dfc5562be5f5cc
Ryan McCormick <rmccormick@nvidia.com>
2023-11-02 21:52:35 -0700

    Add built boost to final container too

--- build.py
+++ build.py
@@ -1258,6 +1258,14 @@ RUN apt-get update && \
             {backend_dependencies} && \
     rm -rf /var/lib/apt/lists/*
 
+# Install boost version >= 1.78 for boost::span
+# Current libboost-dev apt packages are < 1.78, so install from tar.gz
+RUN wget -O /tmp/boost.tar.gz \
+        https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz && \
+    (cd /tmp && tar xzf boost.tar.gz) && \
+    cd /tmp/boost_1_80_0 && ./bootstrap.sh --prefix=/usr && ./b2 install && \
+    rm -rf /tmp/boost*
+
 # Set TCMALLOC_RELEASE_RATE for users setting LD_PRELOAD with tcmalloc
 ENV TCMALLOC_RELEASE_RATE 200
 """.format(
