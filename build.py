e84356f29d402df2b89dd6a4102919828e37054c
Misha Chornyi <mchornyi@nvidia.com>
2023-11-09 18:45:21 -0800

    Improve visual experience. Add 'clang' package

--- build.py
+++ build.py
@@ -1245,17 +1245,18 @@ ENV DEBIAN_FRONTEND=noninteractive
 # example libcurl only needed for GCS?)
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
-            software-properties-common \
-            libb64-0d \
-            libcurl4-openssl-dev \
-            libre2-9 \
+            clang \
+            curl \
+            dirmngr \
             git \
             gperf \
-            dirmngr \
+            libb64-0d \
+            libcurl4-openssl-dev \
             libgoogle-perftools-dev \
-            libnuma-dev \
-            curl \
             libjemalloc-dev \
+            libnuma-dev \
+            libre2-9 \
+            software-properties-common \
             wget \
             {backend_dependencies} \
     && rm -rf /var/lib/apt/lists/*
@@ -1263,10 +1264,12 @@ RUN apt-get update \
 # Install boost version >= 1.78 for boost::span
 # Current libboost-dev apt packages are < 1.78, so install from tar.gz
 RUN wget -O /tmp/boost.tar.gz \
-        https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz && \
-    (cd /tmp && tar xzf boost.tar.gz) && \
-    cd /tmp/boost_1_80_0 && ./bootstrap.sh --prefix=/usr && ./b2 install && \
-    rm -rf /tmp/boost*
+        https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz \
+      && (cd /tmp && tar xzf boost.tar.gz) \
+      && cd /tmp/boost_1_80_0 \
+      && ./bootstrap.sh --prefix=/usr \
+      && ./b2 install \
+      && rm -rf /tmp/boost*
 
 # Set TCMALLOC_RELEASE_RATE for users setting LD_PRELOAD with tcmalloc
 ENV TCMALLOC_RELEASE_RATE 200
