d326cab4368f75e69a36da9411c9cdc0f780d805
Katherine Yang <katheriney@nvidia.com>
2023-11-27 16:10:32 -0800

    update protobuf after enviroment change

--- qa/L0_backend_python/setup_python_enviroment.sh
+++ qa/L0_backend_python/setup_python_enviroment.sh
@@ -155,7 +155,7 @@ apt-get update && apt-get -y install \
 rm -f /usr/bin/python3 && \
 ln -s "/usr/bin/python3.${PYTHON_ENV_VERSION}" /usr/bin/python3 && \
 rm -r /usr/bin/python3.10
-pip3 install --upgrade install requests numpy virtualenv
+pip3 install --upgrade install requests numpy virtualenv protobuf
 find /opt/tritonserver/qa/pkgs/ -maxdepth 1 -type f -name \
     "tritonclient-*linux*.whl" | xargs printf -- '%s[all]' | \
     xargs pip3 install --upgrade
