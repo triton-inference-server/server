44c373dc118ec7523fd98a529e609b51acbbd977
Misha Chornyi <mchornyi@nvidia.com>
2023-11-30 15:40:52 -0800

    REvert missing files creation

--- build.py
+++ build.py
@@ -1801,8 +1801,8 @@ def tensorrtllm_prebuild(cmake_script):
     # cmake_script.cmd("mv tensorrtllm/inflight_batcher_llm/cmake tensorrtllm")
     # cmake_script.cmd("mv tensorrtllm/inflight_batcher_llm/CMakeLists.txt tensorrtllm")
     # cmake_script.cmd("find /opt/conda -name ld -exec rm -f {} \;")
-    # cmake_script.cmd("cd tensorrtllm/tensorrt_llm")
-    # cmake_script.cmd("python3 scripts/build_wheel.py --trt_root /usr/local/tensorrt")
+    cmake_script.cmd("cd tensorrtllm/tensorrt_llm")
+    cmake_script.cmd("python3 scripts/build_wheel.py --trt_root /usr/local/tensorrt")
 
 
 def backend_build(
