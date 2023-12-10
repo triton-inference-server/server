afaf6b0fb5b07b5a3f541e89f7151a4489d0d56d
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2023-12-10 10:49:55 -0800

    Set CMake version to 3.27.7 (#6675)
    * Set CMake version to 3.27.7
    
    * Set CMake version to 3.27.7
    
    * Fix double slash typo

--- qa/L0_backend_python/common.sh
+++ qa/L0_backend_python/common.sh
@@ -45,15 +45,13 @@ install_conda() {
 install_build_deps() {
   apt update && apt install software-properties-common rapidjson-dev -y
   # Using CMAKE installation instruction from:: https://apt.kitware.com/
-  apt install -y gpg wget && \
-      wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | \
-            gpg --dearmor - |  \
-            tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null && \
-      . /etc/os-release && \
-      echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $UBUNTU_CODENAME main" | \
-      tee /etc/apt/sources.list.d/kitware.list >/dev/null && \
-      apt-get update && \
-      apt-get install -y --no-install-recommends cmake cmake-data
+  apt update -q=2 \
+    && apt install -y gpg wget \
+    && wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - |  tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null \
+    && . /etc/os-release \
+    && echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/kitware.list >/dev/null \
+    && apt-get update -q=2 \
+    && apt-get install -y --no-install-recommends cmake=3.27.7* cmake-data=3.27.7*
 }
 
 create_conda_env() {
