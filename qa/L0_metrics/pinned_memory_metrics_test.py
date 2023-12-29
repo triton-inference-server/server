1d08835a8776178c99c72a3b7b8acb75fd3c5e86
Sai Kiran Polisetty <spolisetty@nvidia.com>
2023-12-29 18:37:40 +0000

    Update test

--- qa/L0_metrics/pinned_memory_metrics_test.py
+++ qa/L0_metrics/pinned_memory_metrics_test.py
@@ -14,6 +14,30 @@ class TestPinnedMemoryMetrics(unittest.TestCase):
         self.total_bytes_pattern = re.compile(r"pool_total_bytes (\d+)")
         self.used_bytes_pattern = re.compile(r"pool_used_bytes (\d+)")
 
+        shape = [1, 16]
+        self.model_name = "libtorch_float32_float32_float32"
+        input0_data = np.random.rand(*shape).astype(np.float32)
+        input1_data = np.random.rand(*shape).astype(np.float32)
+
+        self.inputs = [
+            httpclient.InferInput(
+                "INPUT0", input0_data.shape, "FP32"
+            ).set_data_from_numpy(input0_data),
+            httpclient.InferInput(
+                "INPUT1", input1_data.shape, "FP32"
+            ).set_data_from_numpy(input1_data),
+        ]
+
+        self.outputs = [
+            httpclient.InferRequestedOutput("OUTPUT__0"),
+            httpclient.InferRequestedOutput("OUTPUT__1"),
+        ]
+
+        # Before loading the model
+        total_bytes_value, used_bytes_value = self._get_metrics()
+        self.assertEqual(int(total_bytes_value), 268435456)
+        self.assertEqual(int(used_bytes_value), 0)
+
     def _get_metrics(self):
         r = requests.get("http://localhost:8002/metrics")
         r.raise_for_status()
@@ -32,35 +56,12 @@ class TestPinnedMemoryMetrics(unittest.TestCase):
             self.assertEqual(int(total_bytes_value), 268435456)
             self.assertIn(int(used_bytes_value), [0, 64, 128, 192, 256])
 
-    def test_pinned_memory_metrics(self):
-        shape = [1, 16]
-        model_name = "libtorch_float32_float32_float32"
-        input0_data = np.random.rand(*shape).astype(np.float32)
-        input1_data = np.random.rand(*shape).astype(np.float32)
-
-        inputs = [
-            httpclient.InferInput(
-                "INPUT0", input0_data.shape, "FP32"
-            ).set_data_from_numpy(input0_data),
-            httpclient.InferInput(
-                "INPUT1", input1_data.shape, "FP32"
-            ).set_data_from_numpy(input1_data),
-        ]
-
-        outputs = [
-            httpclient.InferRequestedOutput("OUTPUT__0"),
-            httpclient.InferRequestedOutput("OUTPUT__1"),
-        ]
-
-        # Before loading the model
-        total_bytes_value, used_bytes_value = self._get_metrics()
-        self.assertEqual(int(total_bytes_value), 268435456)
-        self.assertEqual(int(used_bytes_value), 0)
-
+    def test_pinned_memory_metrics_asynchronous_requests(self):
         with httpclient.InferenceServerClient(
-            url="localhost:8000", concurrency=1
+            url="localhost:8000", concurrency=10
         ) as client:
-            client.load_model(model_name)
+            if not client.is_model_ready(self.model_name):
+                client.load_model(self.model_name)
 
             # Before starting the inference
             total_bytes_value, used_bytes_value = self._get_metrics()
@@ -73,10 +74,12 @@ class TestPinnedMemoryMetrics(unittest.TestCase):
 
             # Asynchronous inference requests
             async_requests = []
-            for _ in range(10):
+            for _ in range(100):
                 async_requests.append(
                     client.async_infer(
-                        model_name=model_name, inputs=inputs, outputs=outputs
+                        model_name=self.model_name,
+                        inputs=self.inputs,
+                        outputs=self.outputs,
                     )
                 )
 
@@ -88,6 +91,39 @@ class TestPinnedMemoryMetrics(unittest.TestCase):
             # Wait for all inference requests to complete
             for async_request in async_requests:
                 async_request.get_result()
+
+            # Wait for the metrics thread to complete
+            metrics_thread.join()
+
+        # After Completing inference, used_bytes_value should comedown to 0
+        total_bytes_value, used_bytes_value = self._get_metrics()
+        self.assertEqual(int(total_bytes_value), 268435456)
+        self.assertEqual(int(used_bytes_value), 0)
+
+    def test_pinned_memory_metrics_synchronous_requests(self):
+        with httpclient.InferenceServerClient(url="localhost:8000") as client:
+            if not client.is_model_ready(self.model_name):
+                client.load_model(self.model_name)
+
+            # Before starting the inference
+            total_bytes_value, used_bytes_value = self._get_metrics()
+            self.assertEqual(int(total_bytes_value), 268435456)
+            self.assertEqual(int(used_bytes_value), 0)
+
+            # Start a thread to collect metrics asynchronously
+            metrics_thread = threading.Thread(target=self._collect_metrics)
+            metrics_thread.start()
+
+            # Synchronous inference requests
+            for _ in range(100):
+                response = client.infer(
+                    model_name=self.model_name, inputs=self.inputs, outputs=self.outputs
+                )
+
+                result = response.get_response()
+
+            # Set the event to indicate that inference is completed
+            self.inference_completed.set()
 
             # Wait for the metrics thread to complete
             metrics_thread.join()
