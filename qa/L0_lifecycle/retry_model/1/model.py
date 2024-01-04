b5e46704c5bfdd19fb1f034513af8e59cf3b49f6
GuanLuo <gluo@nvidia.com>
2024-01-04 11:12:04 -0800

    Minor change on testing model

--- qa/L0_lifecycle/retry_model/1/model.py
+++ qa/L0_lifecycle/retry_model/1/model.py
@@ -75,30 +75,6 @@ class TritonPythonModel:
         os.remove(self.indicator_file)
 
     def execute(self, requests):
-        """This function is called on inference request."""
-
-        output0_dtype = self.output0_dtype
-        output1_dtype = self.output1_dtype
-
-        responses = []
-        for request in requests:
-            in_0 = pb_utils.get_input_tensor_by_name(request, "INPUT0")
-            in_1 = pb_utils.get_input_tensor_by_name(request, "INPUT1")
-            if (
-                in_0.as_numpy().dtype.type is np.bytes_
-                or in_0.as_numpy().dtype == np.object_
-            ):
-                out_0, out_1 = (
-                    in_0.as_numpy().astype(np.int32) + in_1.as_numpy().astype(np.int32),
-                    in_0.as_numpy().astype(np.int32) - in_1.as_numpy().astype(np.int32),
-                )
-            else:
-                out_0, out_1 = (
-                    in_0.as_numpy() + in_1.as_numpy(),
-                    in_0.as_numpy() - in_1.as_numpy(),
-                )
-
-            out_tensor_0 = pb_utils.Tensor("OUTPUT0", out_0.astype(output0_dtype))
-            out_tensor_1 = pb_utils.Tensor("OUTPUT1", out_1.astype(output1_dtype))
-            responses.append(pb_utils.InferenceResponse([out_tensor_0, out_tensor_1]))
-        return responses
+        # This model is for testing loading behavior only
+        # and is not intended to be executed
+        pass
