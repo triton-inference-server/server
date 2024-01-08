530a6f00f057472054ab22d79493cb8dd3babe94
kthui <18255193+kthui@users.noreply.github.com>
2024-01-08 15:24:25 -0800

    Update copyright

--- qa/L0_model_config/test.sh
+++ qa/L0_model_config/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2019-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2019-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
