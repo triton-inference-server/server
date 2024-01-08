6555abd85360a2c7c0df714a86b07bc6c01e0041
Ryan McCormick <rmccormick@nvidia.com>
2024-01-08 10:08:05 -0800

    Move from jfrog artifactory to archives.boost.io to fix boost download issues

--- build.py
+++ build.py
@@ -972,7 +972,7 @@ RUN pip3 install --upgrade pip && \
 # Install boost version >= 1.78 for boost::span
 # Current libboost-dev apt packages are < 1.78, so install from tar.gz
 RUN wget -O /tmp/boost.tar.gz \
-        https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz && \
+        https://archives.boost.io/release/1.80.0/source/boost_1_80_0.tar.gz && \
     (cd /tmp && tar xzf boost.tar.gz) && \
     cd /tmp/boost_1_80_0 && ./bootstrap.sh --prefix=/usr && ./b2 install && \
     mv /tmp/boost_1_80_0/boost /usr/include/boost
@@ -1225,7 +1225,7 @@ RUN apt-get update \
 # Install boost version >= 1.78 for boost::span
 # Current libboost-dev apt packages are < 1.78, so install from tar.gz
 RUN wget -O /tmp/boost.tar.gz \
-        https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz \
+        https://archives.boost.io/release/1.80.0/source/boost_1_80_0.tar.gz && \
       && (cd /tmp && tar xzf boost.tar.gz) \
       && cd /tmp/boost_1_80_0 \
       && ./bootstrap.sh --prefix=/usr \
