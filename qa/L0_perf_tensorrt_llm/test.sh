53d2de15c216480e9158a5da4d1d000e1ea32498
Sai Kiran Polisetty <spolisetty@nvidia.com>
2024-01-08 07:06:22 +0000

    Update report data type

--- qa/L0_perf_tensorrt_llm/test.sh
+++ qa/L0_perf_tensorrt_llm/test.sh
@@ -243,7 +243,7 @@ echo -e "\"s_server\":\"triton\"," >>${NAME}.tjson
 echo -e "\"s_protocol\":\"${PERF_CLIENT_PROTOCOL}\"," >>${NAME}.tjson
 echo -e "\"s_framework\":\"${MODEL_FRAMEWORK}\"," >>${NAME}.tjson
 echo -e "\"s_model\":\"${MODEL_NAME}\"," >>${NAME}.tjson
-echo -e "\"s_concurrency\":\"${CONCURRENCY}\"," >>${NAME}.tjson
+echo -e "\"l_concurrency\":${CONCURRENCY}," >>${NAME}.tjson
 echo -e "\"l_batch_size\":${STATIC_BATCH}," >>${NAME}.tjson
 echo -e "\"l_instance_count\":${INSTANCE_CNT}," >>${NAME}.tjson
 echo -e "\"s_architecture\":\"${ARCH}\"}]" >>${NAME}.tjson
