b09969218ab0b1ef9fbd2625c6fdd3283fa1769e
Ryan McCormick <rmccormick@nvidia.com>
2024-01-08 10:35:53 -0800

    Fix typo

--- build.py
+++ build.py
@@ -1225,7 +1225,7 @@ RUN apt-get update \
 # Install boost version >= 1.78 for boost::span
 # Current libboost-dev apt packages are < 1.78, so install from tar.gz
 RUN wget -O /tmp/boost.tar.gz \
-        https://archives.boost.io/release/1.80.0/source/boost_1_80_0.tar.gz && \
+        https://archives.boost.io/release/1.80.0/source/boost_1_80_0.tar.gz \
       && (cd /tmp && tar xzf boost.tar.gz) \
       && cd /tmp/boost_1_80_0 \
       && ./bootstrap.sh --prefix=/usr \
