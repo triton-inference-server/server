d1c8507f0f36b4b65b40183b11e0431d66fb815b
Ryan McCormick <rmccormick@nvidia.com>
2024-01-21 18:36:29 -0800

    Remove boost::filesystem (#6810)

--- build.py
+++ build.py
@@ -974,7 +974,6 @@ RUN pip3 install --upgrade pip && \
 RUN wget -O /tmp/boost.tar.gz \
         https://archives.boost.io/release/1.80.0/source/boost_1_80_0.tar.gz && \
     (cd /tmp && tar xzf boost.tar.gz) && \
-    cd /tmp/boost_1_80_0 && ./bootstrap.sh --prefix=/usr && ./b2 install && \
     mv /tmp/boost_1_80_0/boost /usr/include/boost
 
 # Server build requires recent version of CMake (FetchContent required)
@@ -1221,16 +1220,6 @@ RUN apt-get update \
             wget \
             {backend_dependencies} \
     && rm -rf /var/lib/apt/lists/*
-
-# Install boost version >= 1.78 for boost::span
-# Current libboost-dev apt packages are < 1.78, so install from tar.gz
-RUN wget -O /tmp/boost.tar.gz \
-        https://archives.boost.io/release/1.80.0/source/boost_1_80_0.tar.gz \
-      && (cd /tmp && tar xzf boost.tar.gz) \
-      && cd /tmp/boost_1_80_0 \
-      && ./bootstrap.sh --prefix=/usr \
-      && ./b2 install \
-      && rm -rf /tmp/boost*
 
 # Set TCMALLOC_RELEASE_RATE for users setting LD_PRELOAD with tcmalloc
 ENV TCMALLOC_RELEASE_RATE 200
