6f111ca4412a533dc6febf1fa2641a05c698ac85
oandreeva-nv <oandreeva@nvidia.com>
2024-01-30 17:38:49 -0800

    Refactored cmdline tests

--- qa/L0_cmdline_trace/test.sh
+++ qa/L0_cmdline_trace/test.sh
@@ -25,6 +25,19 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# ============================= Helpers =======================================
+function assert_server_startup_failed() {
+  if [ "$SERVER_PID" != "0" ]; then
+      echo -e "\n***\n***Fail: Server start should have failed $SERVER\n***"
+      cat $SERVER_LOG
+      set -e
+      kill $SERVER_PID
+      wait $SERVER_PID
+      set +e
+      exit 1
+  fi
+}
+
 TRACE_SUMMARY=../common/trace_summary.py
 CLIENT_SCRIPT=trace_client.py
 
@@ -630,15 +643,7 @@ export OTEL_BSP_MAX_QUEUE_SIZE="bad_value"
 SERVER_ARGS="--trace-config mode=opentelemetry --model-repository=$MODELSDIR"
 SERVER_LOG="./inference_server_trace_config_flag.log"
 run_server
-if [ "$SERVER_PID" != "0" ]; then
-    echo -e "\n***\n***Fail: Server start should have failed $SERVER\n***"
-    cat $SERVER_LOG
-    set -e
-    kill $SERVER_PID
-    wait $SERVER_PID
-    set +e
-    exit 1
-fi
+assert_server_startup_failed
 
 if [ `grep -c "Bad option: \"OTEL_BSP_MAX_QUEUE_SIZE\"" $SERVER_LOG` != "1" ]; then
     cat $SERVER_LOG
@@ -650,15 +655,7 @@ unset OTEL_BSP_MAX_QUEUE_SIZE
 
 export OTEL_BSP_SCHEDULE_DELAY="bad_value"
 run_server
-if [ "$SERVER_PID" != "0" ]; then
-    echo -e "\n***\n***Fail: Server start should have failed $SERVER\n***"
-    cat $SERVER_LOG
-    set -e
-    kill $SERVER_PID
-    wait $SERVER_PID
-    set +e
-    exit 1
-fi
+assert_server_startup_failed
 
 if [ `grep -c "Bad option: \"OTEL_BSP_SCHEDULE_DELAY\"" $SERVER_LOG` != "1" ]; then
     cat $SERVER_LOG
@@ -670,15 +667,7 @@ unset OTEL_BSP_SCHEDULE_DELAY
 
 export OTEL_BSP_MAX_EXPORT_BATCH_SIZE="bad_value"
 run_server
-if [ "$SERVER_PID" != "0" ]; then
-    echo -e "\n***\n***Fail: Server start should have failed $SERVER\n***"
-    cat $SERVER_LOG
-    set -e
-    kill $SERVER_PID
-    wait $SERVER_PID
-    set +e
-    exit 1
-fi
+assert_server_startup_failed
 
 if [ `grep -c "Bad option: \"OTEL_BSP_MAX_EXPORT_BATCH_SIZE\"" $SERVER_LOG` != "1" ]; then
     cat $SERVER_LOG
@@ -692,15 +681,7 @@ SERVER_ARGS="--model-repository=$MODELSDIR --trace-config mode=opentelemetry \
              --trace-config opentelemetry,bsp_max_queue_size=bad_value"
 SERVER_LOG="./inference_server_trace_config_flag.log"
 run_server
-if [ "$SERVER_PID" != "0" ]; then
-    echo -e "\n***\n***Fail: Server start should have failed $SERVER\n***"
-    cat $SERVER_LOG
-    set -e
-    kill $SERVER_PID
-    wait $SERVER_PID
-    set +e
-    exit 1
-fi
+assert_server_startup_failed
 
 if [ `grep -c "Bad option: \"--trace-config opentelemetry,bsp_max_queue_size\"" $SERVER_LOG` != "1" ]; then
     cat $SERVER_LOG
@@ -712,15 +693,7 @@ SERVER_ARGS="--model-repository=$MODELSDIR --trace-config mode=opentelemetry \
              --trace-config opentelemetry,bsp_schedule_delay=bad_value"
 SERVER_LOG="./inference_server_trace_config_flag.log"
 run_server
-if [ "$SERVER_PID" != "0" ]; then
-    echo -e "\n***\n***Fail: Server start should have failed $SERVER\n***"
-    cat $SERVER_LOG
-    set -e
-    kill $SERVER_PID
-    wait $SERVER_PID
-    set +e
-    exit 1
-fi
+assert_server_startup_failed
 
 if [ `grep -c "Bad option: \"--trace-config opentelemetry,bsp_schedule_delay\"" $SERVER_LOG` != "1" ]; then
     cat $SERVER_LOG
@@ -732,15 +705,7 @@ SERVER_ARGS="--model-repository=$MODELSDIR --trace-config mode=opentelemetry \
              --trace-config opentelemetry,bsp_max_export_batch_size=bad_value"
 SERVER_LOG="./inference_server_trace_config_flag.log"
 run_server
-if [ "$SERVER_PID" != "0" ]; then
-    echo -e "\n***\n***Fail: Server start should have failed $SERVER\n***"
-    cat $SERVER_LOG
-    set -e
-    kill $SERVER_PID
-    wait $SERVER_PID
-    set +e
-    exit 1
-fi
+assert_server_startup_failed
 
 if [ `grep -c "Bad option: \"--trace-config opentelemetry,bsp_max_export_batch_size\"" $SERVER_LOG` != "1" ]; then
     cat $SERVER_LOG
