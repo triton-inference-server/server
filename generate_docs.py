b276c424ba1974c06669dae129412a38580fd5b8
Indrajit Maloji Bhosale <ibhosale@nvidia.com>
2024-03-08 17:55:39 -0800

    Logging to /tmp/docs.log added

--- generate_docs.py
+++ generate_docs.py
@@ -2,21 +2,50 @@ import json
 import sys
 import subprocess
 import os
+import logging
+
+def setup_logger():
+    # Create a custom logger
+    logger = logging.getLogger(__name__)
+
+    # Set the log level
+    logger.setLevel(logging.INFO)
+
+    # Create handlers
+    file_handler = logging.FileHandler('/tmp/docs.log')
+
+    # Create formatters and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    # Add handlers to the logger
+    logger.addHandler(file_handler)
+
+    return logger
+
+def log_message(message):
+    # Setup the logger
+    logger = setup_logger()
+
+    # Log the message
+    logger.info(message)
 
 def run_command(command):
     try:
         result = subprocess.run(command, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(result.stdout)
+        log_message(result.stdout)
     except subprocess.CalledProcessError as e:
-        print(f"Error executing command: {e.cmd}")
-        print(e.output)
-        print(e.stderr)
+        log_message(f"Error executing command: {e.cmd}")
+        log_message(e.output)
+        log_message(e.stderr)
 
 def build_docker_image(tag):
+    log_message("Running Docker Build")
     command = f"docker build -t i_docs:1.0 ."
     run_command(command)
 
 def run_docker_image(tag, host_dir, container_dir):
+    log_message("Running Docker RUN")
     command = f"docker run -it -v {host_dir}:{container_dir} {tag}:1.0 /bin/bash -c 'cd {container_dir}/docs && make html'"
     run_command(command)
 
@@ -33,9 +62,9 @@ def clone_from_github(repo, tag, org):
     # Execute the git clone command
     try:
         subprocess.run(clone_command, check=True)
-        print(f"Successfully cloned {repo}")
+        log_message(f"Successfully cloned {repo}")
     except subprocess.CalledProcessError as e:
-        print(f"Failed to clone {repo}. Error: {e}")
+        log_message(f"Failed to clone {repo}. Error: {e}")
 
 def main():
     # Deserialize the JSON string back to Python data structure
@@ -56,7 +85,7 @@ def main():
 
     # Run the Docker image
     run_docker_image(tag, host_dir, container_dir)
-
+    log_message("**DONE**")
 
 if __name__ == "__main__":
     main()
