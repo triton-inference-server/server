9833f05ffa03eff0e4bde7acb595bc535d56c7ba
Misha Chornyi <mchornyi@nvidia.com>
2024-04-01 17:14:38 -0700

    Escape slash: align

--- build.py
+++ build.py
@@ -859,9 +859,11 @@ RUN curl -o /tmp/cuda-keyring.deb \\
 ENV DCGM_VERSION {}
 # Install DCGM. Steps from https://developer.nvidia.com/dcgm#Downloads
 RUN curl -o /tmp/cuda-keyring.deb \\
-    https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb \\
-    && apt install /tmp/cuda-keyring.deb && rm /tmp/cuda-keyring.deb && \\
-    apt-get update && apt-get install -y datacenter-gpu-manager=1:{}
+      https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb \\
+    && apt install /tmp/cuda-keyring.deb \\
+    && rm /tmp/cuda-keyring.deb \\
+    && apt-get update \\
+    && apt-get install -y datacenter-gpu-manager=1:{}
 """.format(
                 dcgm_version, dcgm_version
             )
