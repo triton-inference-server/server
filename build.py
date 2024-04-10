c30b5b441802d63746019781feb0b2d30b59d0b7
Misha Chornyi <mchornyi@nvidia.com>
2024-04-10 16:46:51 -0700

    Update README and versions for 2.45.0 / 24.04

--- build.py
+++ build.py
@@ -69,9 +69,9 @@ import requests
 # incorrectly load the other version of the openvino libraries.
 #
 TRITON_VERSION_MAP = {
-    "2.45.0dev": (
-        "24.04dev",  # triton container
-        "24.03",  # upstream container
+    "2.45.0": (
+        "24.04",  # triton container
+        "24.04",  # upstream container
         "1.17.2",  # ORT
         "2023.3.0",  # ORT OpenVINO
         "2023.3.0",  # Standalone OpenVINO
