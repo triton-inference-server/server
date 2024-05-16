9eb9b91f6c7cdbd8683b9676320bd801f44b2944
Jacky <18255193+kthui@users.noreply.github.com>
2024-05-16 13:02:28 -0700

    Fix L0_backend_python iGPU PyTorch installation (#7231)
    * Fix TEST_JETSON double square bracket issue
    
    * Use and instead of or

--- qa/L0_backend_python/examples/test.sh
+++ qa/L0_backend_python/examples/test.sh
@@ -38,7 +38,7 @@ rm -fr *.log python_backend/
 
 # Install torch
 pip3 uninstall -y torch
-if [[ "$TEST_JETSON" == "0" ]] || [[ ${TEST_WINDOWS} == 0 ]]; then
+if [ "$TEST_JETSON" == "0" ] && [[ ${TEST_WINDOWS} == 0 ]]; then
     pip3 install torch==2.0.0+cu117 -f https://download.pytorch.org/whl/torch_stable.html torchvision==0.15.0+cu117
 else
     pip3 install torch==2.0.0 -f https://download.pytorch.org/whl/torch_stable.html torchvision==0.15.0
