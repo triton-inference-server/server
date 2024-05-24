63927289b79b257dc7b999c60a163be908c8f06e
kthui <18255193+kthui@users.noreply.github.com>
2024-05-23 17:25:02 -0700

    Remove special handling for non-decoupled sending flag

--- qa/python_models/response_sender/model_async.py
+++ qa/python_models/response_sender/model_async.py
@@ -31,7 +31,7 @@ from model_common import ResponseSenderModelCommon
 
 class TritonPythonModel:
     def initialize(self, args):
-        self._common = ResponseSenderModelCommon(pb_utils, args)
+        self._common = ResponseSenderModelCommon(pb_utils)
 
     async def execute(self, requests):
         return self._common.execute(requests, use_async=True)
