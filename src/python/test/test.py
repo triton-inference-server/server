a76cc196a4cb124d7cb8e88426877687c9f0bbd4
Krishnan Prashanth <kprashanth@nvidia.com>
2024-08-12 15:03:58 -0700

    Removed unused import

--- src/python/test/test.py
+++ src/python/test/test.py
@@ -24,8 +24,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from subprocess import call
-
 import numpy as np
 import pytest
 import tritonclient.grpc as grpcclient
