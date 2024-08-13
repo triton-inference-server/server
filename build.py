6da6adb22e63aab33119cc27ad09e2e7b3c608c2
fpetrini15 <francescogpetrini@gmail.com>
2024-08-12 17:46:39 -0700

    Install jpeg lib

--- build.py
+++ build.py
@@ -955,7 +955,8 @@ RUN yum install -y \\
             libarchive-devel \\
             libxml2-devel \\
             numactl-devel \\
-            wget
+            wget \\
+            libjpeg-devel
 
 RUN pip3 install --upgrade pip \\
       && pip3 install --upgrade \\
