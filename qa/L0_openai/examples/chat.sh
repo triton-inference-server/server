363b40ea4faf259510ac642e69cc4a1812bf7884
Ryan McCormick <rmccormick@nvidia.com>
2024-08-19 16:06:32 -0700

    Move openai code to server/python folder

--- qa/L0_openai/examples/chat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# or "tensorrt_llm_bls" for TRT-LLM
-MODEL=${1:-"llama-3.1-8b-instruct"}
-curl -s http://localhost:8000/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -d '{
-     "model": "'${MODEL}'",
-     "messages": [{"role": "user", "content": "Say this is a test!"}]
-   }' | jq
