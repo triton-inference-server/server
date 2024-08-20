3c6980501dd66ee3a575050987e56372d7f5d71a
kthui <18255193+kthui@users.noreply.github.com>
2024-08-20 11:53:58 -0700

    Expand test to cover load action with only version addition to policy

--- qa/L0_lifecycle/lifecycle_test.py
+++ qa/L0_lifecycle/lifecycle_test.py
@@ -3593,6 +3593,33 @@ class LifeCycleTest(tu.TestResultCollector):
         self.assertEqual(server_log.count("[PB model] Loading version 4"), 2)
         self.assertEqual(server_log.count("successfully loaded 'identity_fp32'"), 4)
 
+        # update model config to load version 1 and 4
+        config_path = os.path.join("models", model_name, "config.pbtxt")
+        with open(config_path, mode="r+", encoding="utf-8", errors="strict") as f:
+            config = f.read()
+            config = config.replace(
+                "version_policy: { specific: { versions: [4] } }",
+                "version_policy: { specific: { versions: [1, 4] } }",
+            )
+            f.truncate(0)
+            f.seek(0)
+            f.write(config)
+        # reload the model
+        client.load_model(model_name)
+
+        # version 1 should be loaded and version 4 should not be reloaded
+        self.assertTrue(client.is_model_ready(model_name, "1"))
+        self.assertFalse(client.is_model_ready(model_name, "2"))
+        self.assertFalse(client.is_model_ready(model_name, "3"))
+        self.assertTrue(client.is_model_ready(model_name, "4"))
+        with open(os.environ["SERVER_LOG"]) as f:
+            server_log = f.read()
+        self.assertEqual(server_log.count("[PB model] Loading version 1"), 3)
+        self.assertEqual(server_log.count("[PB model] Loading version 2"), 3)
+        self.assertEqual(server_log.count("[PB model] Loading version 3"), 2)
+        self.assertEqual(server_log.count("[PB model] Loading version 4"), 2)
+        self.assertEqual(server_log.count("successfully loaded 'identity_fp32'"), 5)
+
 
 if __name__ == "__main__":
     unittest.main()
