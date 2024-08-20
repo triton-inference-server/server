f54a4faf197240e089f59277546bab217b8d106f
Ryan McCormick <rmccormick@nvidia.com>
2024-08-20 16:25:21 -0700

    Add placeholder test script

--- qa/L0_openai/test.sh
+++ qa/L0_openai/test.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
-echo "This is a placeholder"
+### Helpers ###
+
+
+# TODO: Detect vllm vs trtllm
+function install_deps() {
+    pushd openai/docker
+    pip install -r requirements.txt
+    pip install -r requirements_vllm.txt
+    popd
+}
+
+function pre_test() {
+
+    rm -rf openai/
+    rm -f *.xml *.log
+
+    # TODO: Use this instead when moving to devel container
+    # cp -r ../../python/openai .
+    cp -r /mnt/server/python/openai .
+
+    install_deps
+}
+
+function run_test() {
+    pushd openai/tests
+    pytest -s -v --junitxml=test_openai.xml 2>&1 | tee test_openai.log
+    cp *.xml *.log ../../
+    popd
+}
+
+function post_test() {
+    # no-op
+}
+
+
+### Test ###
+
+pre_test
+run_test
+post_test
