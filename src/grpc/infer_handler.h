44fa2078859ef2e0b116b26b7d27184b836c8c33
Indrajit Bhosale <iamindrajitb@gmail.com>
2024-08-20 12:49:24 -0700

    Intermittent `L0_decoupled_grpc_error` crash fixed. (#7552)

--- src/grpc/infer_handler.h
+++ src/grpc/infer_handler.h
@@ -667,6 +667,7 @@ class InferHandlerState {
 
     bool IsCancelled()
     {
+      std::lock_guard<std::recursive_mutex> lock(mu_);
       return received_notification_
                  ? (ctx_->IsCancelled() ||
                     gRPCErrorTracker_->CheckAndUpdateGRPCError())
