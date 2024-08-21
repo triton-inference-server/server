81ef479164f6d2f5e2fb1c131312fa991f499b95
Ryan McCormick <rmccormick@nvidia.com>
2024-08-21 14:46:20 -0700

    Remove use of NFS mount due to slow I/O for now

--- qa/L0_openai/test.sh
+++ qa/L0_openai/test.sh
@@ -2,16 +2,6 @@
 
 ### Helpers ###
 
-REPO_VERSION=${NVIDIA_TRITON_SERVER_VERSION}
-if [ "$#" -ge 1 ]; then
-    REPO_VERSION=$1
-fi
-if [ -z "$REPO_VERSION" ]; then
-    echo -e "Repository version must be specified"
-    echo -e "\n***\n*** Test Failed\n***"
-    exit 1
-fi
-
 function install_deps() {
     pushd openai/docker
     pip install /opt/tritonserver/python/triton*.whl
@@ -29,28 +19,15 @@ function prepare_vllm() {
 }
 
 function prepare_tensorrtllm() {
-    DATADIR=${DATADIR:="/data/inferenceserver/${REPO_VERSION}"}
-    # NOTE: This default path doesn't make sense if run on non-A100 GPU
-    NFS_ENGINE_DEST_PATH=${ENGINE_DEST_PATH:="${DATADIR}/trtllm_engines_A100"}
-    LOCAL_ENGINE_DEST_PATH="./trtllm_engines_A100"
-
     MODEL="llama-3-8b-instruct"
     MODEL_REPO="../openai/tests/tensorrtllm_models"
-    rm -rf ${MODEL_REPO} ${LOCAL_ENGINE_DEST_PATH}
+    rm -rf ${MODEL_REPO}
 
     # FIXME: This will require an upgrade each release to match the TRT-LLM version
     # Use Triton CLI to prepare model repository for testing
     pip install git+https://github.com/triton-inference-server/triton_cli.git@0.0.10
-    # Use ENGINE_DEST_PATH to re-use NFS mount when possible and skip engine build
-    ENGINE_DEST_PATH="${NFS_ENGINE_DEST_PATH}" triton import \
-        --model ${MODEL}  \
-        --backend tensorrtllm \
-        --model-repository "${MODEL_REPO}"
-    rm -rf "${MODEL_REPO}"
-
-    # To avoid too much I/O with NFS mount at test time, copy it out to a local dir first.
-    time rsync -ah ${NFS_ENGINE_DEST_PATH} ${LOCAL_ENGINE_DEST_PATH}
-    ENGINE_DEST_PATH="${LOCAL_ENGINE_DEST_PATH}" triton import \
+    # NOTE: Could use ENGINE_DEST_PATH set to NFS mount for pre-built engines in future
+    triton import \
         --model ${MODEL}  \
         --backend tensorrtllm \
         --model-repository "${MODEL_REPO}"
@@ -80,7 +57,6 @@ function post_test() {
     # Placeholder
     echo "post_test"
 }
-
 
 ### Test ###
 
