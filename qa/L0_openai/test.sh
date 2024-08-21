6361bd1cd0a87b8587c238a7f15d27f279bbec38
Ryan McCormick <rmccormick@nvidia.com>
2024-08-20 22:57:01 -0700

    Add TRT-LLM detection and model repo generation

--- qa/L0_openai/test.sh
+++ qa/L0_openai/test.sh
@@ -2,14 +2,47 @@
 
 ### Helpers ###
 
+REPO_VERSION=${NVIDIA_TRITON_SERVER_VERSION}
+if [ "$#" -ge 1 ]; then
+    REPO_VERSION=$1
+fi
+if [ -z "$REPO_VERSION" ]; then
+    echo -e "Repository version must be specified"
+    echo -e "\n***\n*** Test Failed\n***"
+    exit 1
+fi
+DATADIR=${DATADIR:="/data/inferenceserver/${REPO_VERSION}"}
+# NOTE: This default path doesn't make sense if run on non-A100 GPU
+ENGINE_DEST_PATH=${ENGINE_DEST_PATH:="${DATADIR}/trtllm_engines_A100"}
 
-# TODO: Detect vllm vs trtllm
 function install_deps() {
     pushd openai/docker
     pip install /opt/tritonserver/python/triton*.whl
     pip install -r requirements.txt
-    pip install -r requirements_vllm.txt
+    if [ "${IMAGE_KIND}" == "TRTLLM" ]; then
+        prepare_tensorrtllm
+    else
+        prepare_vllm
+    fi
     popd
+}
+
+function prepare_vllm() {
+    pip install -r requirements_vllm.txt
+}
+
+function prepare_tensorrtllm() {
+    # Use Triton CLI to prepare model repository for testing
+    pip install git+https://github.com/triton-inference-server/triton_cli.git@0.0.10
+    MODEL_REPO="../openai/tests/tensorrtllm_models"
+    rm -rf ${MODEL_REPO}
+    # Use ENGINE_DEST_PATH to re-use NFS mount when possible
+    ENGINE_DEST_PATH="${ENGINE_DEST_PATH}" triton import \
+        --model llama-3-8b-instruct \
+        --backend tensorrtllm \
+        --model-repository "${MODEL_REPO}"
+    # WAR for tests expecting default name of "tensorrt_llm_bls"
+    mv "${MODEL_REPO}/llama-3-8b-instruct" "${MODEL_REPO}/tensorrt_llm_bls"
 }
 
 function pre_test() {
