9a98b1f32985152c85d477b066e3a3c3dc3f1075
Suman Tatiraju <statiraju@nvidia.com>
2024-08-27 11:34:58 -0700

    Use default nvidia-sphinx-theme css

--- docs/conf.py
+++ docs/conf.py
@@ -154,7 +154,7 @@ html_theme = "nvidia_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["custom.css"]
+#html_css_files = ["custom.css"] # Not needed with new theme
 
 html_theme_options = {
     "collapse_navigation": False,
