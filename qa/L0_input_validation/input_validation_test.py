8492549290454509a335e2a9e0ddc1d6b91b2423
Yingge He <yinggeh@nvidia.com>
2024-08-31 02:23:51 -0700

    Update error msg checks

--- qa/L0_input_validation/input_validation_test.py
+++ qa/L0_input_validation/input_validation_test.py
@@ -195,7 +195,7 @@ class InputShapeTest(unittest.TestCase):
             triton_client.infer(model_name=model_name, inputs=inputs)
         err_str = str(e.exception)
         self.assertIn(
-            f"expected {input_size} string elements for inference input 'INPUT1', got {input_size-2}",
+            f"expected {input_size} string elements for inference input 'INPUT1' for model '{model_name}', got {input_size-2}",
             err_str,
         )
 
@@ -208,7 +208,7 @@ class InputShapeTest(unittest.TestCase):
             triton_client.infer(model_name=model_name, inputs=inputs)
         err_str = str(e.exception)
         self.assertIn(
-            f"expected {input_size} string elements for inference input 'INPUT1', got {input_size+2}",
+            f"expected {input_size} string elements for inference input 'INPUT1' for model '{model_name}', got {input_size+2}",
             err_str,
         )
 
