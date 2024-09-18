529b333d6a24180a76709ad6fd19b7739c0d1b2c
vd-nv <vdong@nvidia.com>
2024-09-18 14:05:43 +0800

    Update fetch_models.sh
    Fix onnx model download link.

--- docs/examples/fetch_models.sh
+++ docs/examples/fetch_models.sh
@@ -37,4 +37,4 @@ mv /tmp/inception_v3_2016_08_28_frozen.pb model_repository/inception_graphdef/1/
 # ONNX densenet
 mkdir -p model_repository/densenet_onnx/1
 wget -O model_repository/densenet_onnx/1/model.onnx \
-     https://contentmamluswest001.blob.core.windows.net/content/14b2744cf8d6418c87ffddc3f3127242/9502630827244d60a1214f250e3bbca7/08aed7327d694b8dbaee2c97b8d0fcba/densenet121-1.2.onnx
+     https://github.com/onnx/models/raw/main/validated/vision/classification/densenet-121/model/densenet-7.onnx
