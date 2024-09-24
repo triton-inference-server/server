8f7e994e856a8ee5cf63120517a4af901f2fe653
Ryan McCormick <rmccormick@nvidia.com>
2024-09-24 16:52:18 -0700

    Remove benchmarking scripts

--- python/openai/benchmarking/profile_triton.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-OUTPUT=${OUTPUT:-"cprofile_triton_chat_async.prof"}
-DIR="cprofile"
-mkdir -p ${DIR}
-python3 -m cProfile -o ${DIR}/${OUTPUT} ../openai_frontend/main.py --model-repository ../tests/vllm_models/ --tokenizer meta-llama/Meta-Llama-3.1-8B-Instruct
--- python/openai/benchmarking/profile_vllm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-OUTPUT=${OUTPUT:-"cprofile_vllm_serve.prof"}
-DIR="cprofile"
-mkdir -p ${DIR}
-python3 -m cProfile -o ${DIR}/${OUTPUT} -m vllm.entrypoints.openai.api_server --model meta-llama/Meta-Llama-3.1-8B-Instruct --served-model-name llama-3.1-8b-instruct --disable-log-requests
