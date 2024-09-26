8709680833c8a262c8ca2d533e76f334fbde952c
pvijayakrish <pvijayakrish@nvidia.com>
2024-09-26 00:09:35 -0700

    Update server versions post 24.09

--- build.py
+++ build.py
@@ -70,8 +70,8 @@ import requests
 # incorrectly load the other version of the openvino libraries.
 #
 TRITON_VERSION_MAP = {
-    "2.50.0": (
-        "24.09",  # triton container
+    "2.51.0dev": (
+        "24.10dev",  # triton container
         "24.09",  # upstream container
         "1.19.2",  # ORT
         "2024.0.0",  # ORT OpenVINO
