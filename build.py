1793bbe10bcb84b6bbe87bf7ae09f5f4e8ddd543
pvijayakrish <pvijayakrish@nvidia.com>
2024-10-08 13:37:26 -0700

    Updtae README and versions for 24.10

--- build.py
+++ build.py
@@ -70,9 +70,9 @@ import requests
 # incorrectly load the other version of the openvino libraries.
 #
 TRITON_VERSION_MAP = {
-    "2.51.0dev": (
-        "24.10dev",  # triton container
-        "24.09",  # upstream container
+    "2.51.0": (
+        "24.10",  # triton container
+        "24.10",  # upstream container
         "1.19.2",  # ORT
         "2024.0.0",  # ORT OpenVINO
         "2024.0.0",  # Standalone OpenVINO
