50bfc50d83cf98387c2142e8d85d0df217e0ab13
Krishnan Prashanth <kprashanth@nvidia.com>
2024-10-18 20:03:50 -0700

    reaching _populate_restricted_features helper function

--- src/python/tritonfrontend/_api/_kservehttp.py
+++ src/python/tritonfrontend/_api/_kservehttp.py
@@ -48,7 +48,7 @@ class KServeHttp:
         thread_count: int = Field(8, ge=0)
         header_forward_pattern: str = ""
         # DLIS-7215: Add restricted protocol support
-        # restricted_protocols: list
+        restricted_apis: str = "I am restricted HTTP"
 
     def __init__(self, server: tritonserver, options: "KServeHttp.Options" = None):
         try:
