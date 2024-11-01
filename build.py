3b4fabdeca03591026384e34d45f0fc53573f10c
Pavithra Vijayakrishnan <160681768+pvijayakrish@users.noreply.github.com>
2024-11-01 10:23:33 -0700

    Build: Update main branch post 24.10 release (#7754)

--- build.py
+++ build.py
@@ -70,14 +70,14 @@ import requests
 # incorrectly load the other version of the openvino libraries.
 #
 TRITON_VERSION_MAP = {
-    "2.51.0dev": (
-        "24.10dev",  # triton container
-        "24.09",  # upstream container
+    "2.52.0dev": (
+        "24.11dev",  # triton container
+        "24.10",  # upstream container
         "1.19.2",  # ORT
-        "2024.0.0",  # ORT OpenVINO
-        "2024.0.0",  # Standalone OpenVINO
+        "2024.4.0",  # ORT OpenVINO
+        "2024.4.0",  # Standalone OpenVINO
         "3.2.6",  # DCGM version
-        "0.5.3.post1",  # vLLM version
+        "0.5.5",  # vLLM version
         "3.12.3",  # RHEL Python version
     )
 }
