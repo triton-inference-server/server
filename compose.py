50cfd6eacf552b025111d6ab1965b94452169b66
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2024-12-30 12:35:43 -0800

    Add environment variable to `compose.py` (#7909)

--- compose.py
+++ compose.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2021-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2021-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -88,6 +88,8 @@ FROM {} AS min_container
 
     df += """
 FROM {}
+
+ENV PIP_BREAK_SYSTEM_PACKAGES=1
 """.format(
         images["min"]
     )
