3b99c7b7787c6d0bb20e23d5740ea2dfd25e47ac
Yingge He <157551214+yinggeh@users.noreply.github.com>
2025-01-14 14:05:38 -0800

    Update build.py

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2020-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2020-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
