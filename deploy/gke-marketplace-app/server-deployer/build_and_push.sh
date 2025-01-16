0687d71dc09ee533faba65a98ca0a503687d0679
Misha Chornyi <mchornyi@nvidia.com>
2025-01-15 21:16:48 -0800

    Update README and version

--- deploy/gke-marketplace-app/server-deployer/build_and_push.sh
+++ deploy/gke-marketplace-app/server-deployer/build_and_push.sh
@@ -28,8 +28,8 @@
 export REGISTRY=gcr.io/$(gcloud config get-value project | tr ':' '/')
 export APP_NAME=tritonserver
 export MAJOR_VERSION=2.53
-export MINOR_VERSION=2.53.0
-export NGC_VERSION=24.12-py3
+export MINOR_VERSION=2.54.0
+export NGC_VERSION=25.01-py3
 
 docker pull nvcr.io/nvidia/$APP_NAME:$NGC_VERSION
 
