7a253d4b4f227178016b4a3c189fb57a501ebfa6
Misha Chornyi <mchornyi@nvidia.com>
2025-01-31 15:35:39 -0800

    Update version to track next release

--- build.py
+++ build.py
@@ -71,8 +71,8 @@ import requests
 #
 
 DEFAULT_TRITON_VERSION_MAP = {
-    "release_version": "2.54.0",
-    "triton_container_version": "25.01",
+    "release_version": "2.55.0dev",
+    "triton_container_version": "25.02dev",
     "upstream_container_version": "25.01",
     "ort_version": "1.20.1",
     "ort_openvino_version": "2024.5.0",
