c9ee45112e0ab1e7396cd7c72ac30a1d5a69bd4f
Matt Wittwer <mwittwer@nvidia.com>
2025-02-10 18:46:51 +0000

    Add connection mutex to enforce shutdown order

--- src/grpc/stream_infer_handler.h
+++ src/grpc/stream_infer_handler.h
@@ -105,7 +105,14 @@ class ModelStreamInferHandler
   }
 
   std::atomic<uint32_t> GetConnectionCount() { return conn_cnt_.load(); }
-  void DisableConnections() { accepting_new_conn_ = false; }
+  void DisableConnections() {
+    std::lock_guard<std::recursive_mutex> lock(conn_mtx_);
+    accepting_new_conn_ = false;
+  }
+  void DisableResponses() {
+    std::lock_guard<std::recursive_mutex> lock(conn_mtx_);
+    cq_shutdown_ = true;
+  }
 
  protected:
   void StartNewRequest() override;
