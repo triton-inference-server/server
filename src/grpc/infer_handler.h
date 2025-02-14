d2295655a2302964feb67da4d3052379b85a0cd6
Matthew Wittwer <mwittwer@nvidia.com>
2025-02-14 12:37:35 -0800

    clean up pre-commit

--- src/grpc/infer_handler.h
+++ src/grpc/infer_handler.h
@@ -1660,11 +1660,13 @@ class ModelInferHandler
   }
 
   std::atomic<uint32_t> GetConnectionCount() { return conn_cnt_.load(); }
-  void DisableConnections() {
+  void DisableConnections()
+  {
     std::lock_guard<std::recursive_mutex> lock(conn_mtx_);
     accepting_new_conn_ = false;
   }
-  void DisableResponses() {
+  void DisableResponses()
+  {
     std::lock_guard<std::recursive_mutex> lock(conn_mtx_);
     cq_shutdown_ = true;
   }
