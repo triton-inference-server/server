9e47f196da6012adebee4a808fab89ff1131e4f6
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2025-02-22 01:13:38 +0100

    drop outdated

--- build.py
+++ build.py
@@ -1234,6 +1234,7 @@ COPY --chown=1000:1000 build/install tritonserver
 
 WORKDIR /opt/tritonserver
 COPY --chown=1000:1000 NVIDIA_Deep_Learning_Container_License.pdf .
+
 RUN find /opt/tritonserver/python -maxdepth 1 -type f -name \\
     "tritonserver-*.whl" | xargs -I {} pip install --upgrade {}[all] && \\
     find /opt/tritonserver/python -maxdepth 1 -type f -name \\
