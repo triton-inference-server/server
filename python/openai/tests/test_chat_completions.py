d2daa15091f88c87053304e49ba5165cde39dead
krishung5 <krish@nvidia.com>
2025-02-28 15:22:50 -0800

    Remove unused import

--- python/openai/tests/test_chat_completions.py
+++ python/openai/tests/test_chat_completions.py
@@ -25,7 +25,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import copy
-import os
 import subprocess
 from pathlib import Path
 from typing import List
--- python/openai/tests/test_completions.py
+++ python/openai/tests/test_completions.py
@@ -25,7 +25,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import copy
-import os
 
 import pytest
 
