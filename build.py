b859259e7b675fa1deff6f26a49952579dcab2c5
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2025-03-12 17:04:25 -0700

    Update README.md and versions (#8065)

--- build.py
+++ build.py
@@ -71,14 +71,14 @@ import requests
 #
 
 DEFAULT_TRITON_VERSION_MAP = {
-    "release_version": "2.56.0dev",
-    "triton_container_version": "25.03dev",
-    "upstream_container_version": "25.02",
-    "ort_version": "1.20.1",
+    "release_version": "2.56.0",
+    "triton_container_version": "25.03",
+    "upstream_container_version": "25.03",
+    "ort_version": "1.21.0",
     "ort_openvino_version": "2025.0.0",
     "standalone_openvino_version": "2025.0.0",
     "dcgm_version": "3.3.6",
-    "vllm_version": "0.7.0",
+    "vllm_version": "0.7.3",
     "rhel_py_version": "3.12.3",
 }
 
