5ae98177dc48e55443f1f2aca6960ec63ae6a79c
Kyle McGill <kmcgill@nvidia.com>
2025-03-23 09:22:28 -0700

    Fixed fetch_models.sh to convert the inception TF model to an ONNX model
    fetch_models.sh relied on a graphdef model being downloaded from Google
    to showcase all the cloud examples. Since we are not supporting TF in 25.03
    and later, we use the tf2onnx python module to convert this model to the
    ONNX format. This should work as a drop in replacement for all tests,
    documentation, and examples which rely on this model to function.

--- docs/examples/fetch_models.sh
+++ docs/examples/fetch_models.sh
@@ -27,12 +27,21 @@
 
 set -ex
 
+# Convert Tensorflow inception V3 modle to ONNX
+# Pre-requisite: Python3, venv, and Pip3 are installed on the system
+mkdir -p model_repository/inception_onnx/1
+wget -O /tmp/inception_v3_2016_08_28_frozen.pb.tar.gz \
+     https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
+(cd /tmp && tar xzf inception_v3_2016_08_28_frozen.pb.tar.gz)
+python3 -m venv tf2onnx
+source ./tf2onnx/bin/activate
+pip3 install "numpy<2" tensorflow tf2onnx
+python3 -m tf2onnx.convert --graphdef /tmp/inception_v3_2016_08_28_frozen.pb --output inception_v3_onnx.model.onnx --inputs input:0 --outputs InceptionV3/Predictions/Softmax:0
+deactivate
+mv inception_v3_onnx.model.onnx model_repository/inception_onnx/1/model.onnx
+
+
 # ONNX densenet
 mkdir -p model_repository/densenet_onnx/1
 wget -O model_repository/densenet_onnx/1/model.onnx \
      https://github.com/onnx/models/raw/main/validated/vision/classification/densenet-121/model/densenet-7.onnx
-
-# ONNX Inception-V3
-mkdir -p model_repository/inception_v3_onnx/1
-wget -O model_repository/inception_v3_onnx/1/model.onnx \
-     https://huggingface.co/qualcomm/Inception-v3/resolve/cefe3fc32e8dab58ae04cdce911bb5f2ba69799e/Inception-v3.onnx
