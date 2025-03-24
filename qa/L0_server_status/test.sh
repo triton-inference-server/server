8f988baf2389cc773478047796b8e1789ccd2b23
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2025-03-24 16:40:13 -0700

    rm:TensorFlow - fix to 'L0_server_status'  (#8100)

--- qa/L0_server_status/test.sh
+++ qa/L0_server_status/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2018-2024, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2018-2025, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -84,9 +84,9 @@ fi
 
 set -e
 
-rm -fr models/graphdef_int32_int32_int32/2 models/graphdef_int32_int32_int32/3
+rm -fr models/libtorch_int32_int32_int32/2 models/libtorch_int32_int32_int32/3
 rm -fr models/onnx_int32_int32_int32/2 models/onnx_int32_int32_int32/3
-cp -r models/graphdef_float16_float32_float32/1 models/graphdef_float16_float32_float32/7
+cp -r models/plan_float16_float32_float32/1 models/plan_float16_float32_float32/7
 sleep 3
 
 # Dumping the contents of the models that are currently loaded for debugging purposes
