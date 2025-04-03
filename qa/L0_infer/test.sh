a56824060d7e71f6e4fc26eb413087f2ff3fdb76
Yingge He <157551214+yinggeh@users.noreply.github.com>
2025-04-03 11:43:36 -0400

    ci: Fix L0_infer--cpu* tests (#8123)
    Co-authored-by: Misha Chornyi <99709299+mc-nv@users.noreply.github.com>

--- qa/L0_infer/test.sh
+++ qa/L0_infer/test.sh
@@ -238,7 +238,7 @@ function generate_model_repository() {
 
         ENSEMBLE_MODELS="${ENSEMBLE_MODELS} batch_to_nobatch_float32_float32_float32 batch_to_nobatch_nobatch_float32_float32_float32 nobatch_to_batch_float32_float32_float32 nobatch_to_batch_nobatch_float32_float32_float32 mix_nobatch_batch_float32_float32_float32"
 
-        if [[ $BACKENDS == *"plan"* ]] ; then
+        if [[ $BACKENDS == *"libtorch"* ]] ; then
           ENSEMBLE_MODELS="${ENSEMBLE_MODELS} mix_platform_float32_float32_float32 mix_ensemble_int32_float32_float32"
         fi
 
