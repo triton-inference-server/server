4fc0a5ed464f201afa67ff8d0803bf6eaa72982a
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2025-05-07 12:05:14 -0700

    Update default branch post-25.04 (#8188)
    Co-authored-by: Dmitry Tokarev <dtokarev@nvidia.com>
    Co-authored-by: richardhuo-nv <rihuo@nvidia.com>

--- build.py
+++ build.py
@@ -71,14 +71,14 @@ import requests
 #
 
 DEFAULT_TRITON_VERSION_MAP = {
-    "release_version": "2.57.0dev",
-    "triton_container_version": "25.04dev",
-    "upstream_container_version": "25.03",
+    "release_version": "2.58.0dev",
+    "triton_container_version": "25.05dev",
+    "upstream_container_version": "25.04",
     "ort_version": "1.21.0",
     "ort_openvino_version": "2025.0.0",
     "standalone_openvino_version": "2025.0.0",
     "dcgm_version": "3.3.6",
-    "vllm_version": "0.7.3",
+    "vllm_version": "0.8.1",
     "rhel_py_version": "3.12.3",
 }
 
