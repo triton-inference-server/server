3c52f8a74e52beeff2c679391bd84fd09f7c453f
Matthew Wittwer <mwittwer@nvidia.com>
2025-07-24 23:07:22 -0700

    update model control tests

--- qa/L0_backend_python/model_control/test.sh
+++ qa/L0_backend_python/model_control/test.sh
@@ -62,12 +62,12 @@ if [ $? -ne 0 ]; then
     RET=1
 fi
 
-echo -e "\n***\n*** Running simple input validation test\n***"
-SUBTEST="simple_validation"
-python3 -m pytest --junitxml=model_control.${SUBTEST}.report.xml model_control_test.py::InputValidationTest >> ${CLIENT_LOG} 2>&1
+echo -e "\n***\n*** Running model ID validation test\n***"
+SUBTEST="model_id_validation"
+python3 -m pytest --junitxml=model_control.${SUBTEST}.report.xml model_control_test.py::ModelIDValidationTest >> ${CLIENT_LOG} 2>&1
 
 if [ $? -ne 0 ]; then
-    echo -e "\n***\n*** simple_validation_test.py FAILED. \n***"
+    echo -e "\n***\n*** model_id_validation_test.py FAILED. \n***"
     echo -e "\n***\n*** Server logs:\n***"
     cat $SERVER_LOG
     echo -e "\n***\n*** Client logs:\n***"
@@ -80,9 +80,9 @@ set -e
 kill_server
 
 if [ $RET -eq 1 ]; then
-    echo -e "\n***\n*** Model control and input validation tests FAILED. \n***"
+    echo -e "\n***\n*** model_control_test FAILED. \n***"
 else
-    echo -e "\n***\n*** Model control and input validation tests PASSED. \n***"
+    echo -e "\n***\n*** model_control_test PASSED. \n***"
 fi
 
 exit $RET
