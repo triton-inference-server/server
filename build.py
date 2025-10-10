ebf8bffc50165b3e262fccb1ce631be93bfef74b
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2025-10-10 15:22:20 -0700

    Update default branch to track development of 2.63.0 / 25.11 (#8455)

--- build.py
+++ build.py
@@ -71,8 +71,8 @@ import requests
 #
 
 DEFAULT_TRITON_VERSION_MAP = {
-    "release_version": "2.62.0dev",
-    "triton_container_version": "25.10dev",
+    "release_version": "2.63.0dev",
+    "triton_container_version": "25.11dev",
     "upstream_container_version": "25.09",
     "ort_version": "1.23.1",
     "ort_openvino_version": "2025.3.0",
