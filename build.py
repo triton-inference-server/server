990ebaba108beb1846142e0297d741ee80c1e6d7
Misha Chornyi <mchornyi@nvidia.com>
2025-10-13 10:37:16 -0700

    update: Versions

--- build.py
+++ build.py
@@ -71,9 +71,9 @@ import requests
 #
 
 DEFAULT_TRITON_VERSION_MAP = {
-    "release_version": "2.62.0dev",
-    "triton_container_version": "25.10dev",
-    "upstream_container_version": "25.09",
+    "release_version": "2.62.0",
+    "triton_container_version": "25.10",
+    "upstream_container_version": "25.10",
     "ort_version": "1.23.1",
     "ort_openvino_version": "2025.3.0",
     "standalone_openvino_version": "2025.3.0",
