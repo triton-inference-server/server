366e0dbac552428b8caa339f0a65c2bd56d97727
Dmitry Tokarev <dtokarev@nvidia.com>
2025-11-09 15:03:43 -0500

    Upgrade vllm to 0.11.0
    Signed-off-by: Dmitry Tokarev <dtokarev@nvidia.com>

--- build.py
+++ build.py
@@ -78,7 +78,7 @@ DEFAULT_TRITON_VERSION_MAP = {
     "ort_openvino_version": "2025.3.0",
     "standalone_openvino_version": "2025.3.0",
     "dcgm_version": "4.4.0-1",
-    "vllm_version": "0.10.2",
+    "vllm_version": "0.11.0",
     "rhel_py_version": "3.12.3",
 }
 
