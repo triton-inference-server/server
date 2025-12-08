0b29cb433aa0124f146de2e67a5fe80d2f3e8fe2
Sai Kiran Polisetty <spolisetty@nvidia.com>
2025-12-08 14:00:59 +0530

    Update test name

--- qa/L0_backend_python/model_readiness/test.sh
+++ qa/L0_backend_python/model_readiness/test.sh
@@ -29,7 +29,7 @@ TEST_RESULT_FILE='test_results.txt'
 source ../common.sh
 source ../../common/util.sh
 
-SERVER_ARGS="--model-repository=${MODELDIR}/model_ready_check/models --backend-directory=${BACKEND_DIR} --log-verbose=1"
+SERVER_ARGS="--model-repository=${MODELDIR}/model_readiness/models --backend-directory=${BACKEND_DIR} --log-verbose=1"
 
 RET=0
 rm -fr *.log ./models
@@ -45,9 +45,9 @@ cp ../../python_models/$MODEL_NAME/config.pbtxt ./models/$MODEL_NAME/config.pbtx
 # 11 (SIGSEGV) - Segmentation fault / crash
 # 9  (SIGKILL) - Force kill
 for SIGNAL in 11 9; do
-    echo -e "\n***\n*** Testing model_ready_check with Signal $SIGNAL\n***"
-    SERVER_LOG="./model_ready_check_signal_${SIGNAL}_server.log"
-    CLIENT_LOG="./model_ready_check_${SIGNAL}_client.log"
+    echo -e "\n***\n*** Testing model_readiness with Signal $SIGNAL\n***"
+    SERVER_LOG="./model_readiness_signal_${SIGNAL}_server.log"
+    CLIENT_LOG="./model_readiness_${SIGNAL}_client.log"
 
     run_server
     if [ "$SERVER_PID" == "0" ]; then
@@ -62,7 +62,7 @@ for SIGNAL in 11 9; do
     echo "Checking Initial Readiness..."
     python3 -m unittest check_model_ready.ModelReadyTest.test_model_ready >> ${CLIENT_LOG} 2>&1
     if [ $? -ne 0 ]; then
-        echo -e "\n***\n*** Test model_ready_check Failed (Signal $SIGNAL): Initial readiness check failed \n***"
+        echo -e "\n***\n*** Test model_readiness Failed (Signal $SIGNAL): Initial readiness check failed \n***"
         RET=1
         kill_server
         exit 1
@@ -72,7 +72,7 @@ for SIGNAL in 11 9; do
     stub_pid=$(pgrep -f "triton_python_backend_stub")
 
     if [ -z "$stub_pid" ]; then
-        echo -e "\n***\n*** Test model_ready_check Failed (Signal $SIGNAL): Could not find stub process \n***"
+        echo -e "\n***\n*** Test model_readiness Failed (Signal $SIGNAL): Could not find stub process \n***"
         RET=1
         kill_server
     else
@@ -87,16 +87,16 @@ for SIGNAL in 11 9; do
         echo "Checking Not Ready Status..."
         python3 -m unittest check_model_ready.ModelReadyTest.test_model_not_ready >> ${CLIENT_LOG} 2>&1
         if [ $? -ne 0 ]; then
-            echo -e "\n***\n*** Test model_ready_check Failed (Signal $SIGNAL): Model reported ready after kill \n***"
+            echo -e "\n***\n*** Test model_readiness Failed (Signal $SIGNAL): Model reported ready after kill \n***"
             RET=1
         else
             # Verify correct error message in logs
             # Expect 2 occurrences: HTTP and gRPC checks
             error_count=$(grep -c "Model '${MODEL_NAME}' version 1 is not ready: Stub process '${MODEL_NAME}_0_0' is not healthy." $SERVER_LOG)
             if [ "$error_count" -eq 2 ]; then
-                 echo -e "\n***\n Test model_ready_check Passed for Signal $SIGNAL \n***"
+                 echo -e "\n***\n Test model_readiness Passed for Signal $SIGNAL \n***"
             else
-                 echo -e "\n***\n*** Test model_ready_check Failed (Signal $SIGNAL): Expected 2 error messages, found $error_count \n***"
+                 echo -e "\n***\n*** Test model_readiness Failed (Signal $SIGNAL): Expected 2 error messages, found $error_count \n***"
                  cat $SERVER_LOG
                  RET=1
             fi
