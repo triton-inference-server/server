5589dc8fe95c48e7c278b1a3cfe171e6ae9cfe16
J Wyman <jwyman@nvidia.com>
2025-12-08 18:12:27 -0500

    fix: Correctly Guard List Access
    This change adds missing mutex guards when accessing a shared list.

--- src/sagemaker_server.cc
+++ src/sagemaker_server.cc
@@ -182,10 +182,13 @@ SagemakerAPIServer::Handle(evhtp_request_t* req)
         if (action == "/invoke") {
           LOG_VERBOSE(1) << "SageMaker request: INVOKE MODEL";
 
-          if (sagemaker_models_list_.find(multi_model_name.c_str()) ==
-              sagemaker_models_list_.end()) {
-            evhtp_send_reply(req, EVHTP_RES_NOTFOUND); /* 404*/
-            return;
+          {
+            std::lock_guard<std::mutex> lock(models_list_mutex_);
+            if (sagemaker_models_list_.find(multi_model_name.c_str()) ==
+                sagemaker_models_list_.end()) {
+              evhtp_send_reply(req, EVHTP_RES_NOTFOUND); /* 404*/
+              return;
+            }
           }
           LOG_VERBOSE(1) << "SageMaker MME Custom Invoke Model Path";
 
@@ -646,12 +649,15 @@ SagemakerAPIServer::SageMakerMMEUnloadModel(
     target_model = model_name_hash;
   }
 
-  if (sagemaker_models_list_.find(model_name_hash) ==
-      sagemaker_models_list_.end()) {
-    LOG_VERBOSE(1) << "Model " << target_model << " with model hash "
-                   << model_name_hash << " is not loaded." << std::endl;
-    evhtp_send_reply(req, EVHTP_RES_NOTFOUND); /* 404*/
-    return;
+  {
+    std::lock_guard<std::mutex> lock(models_list_mutex_);
+    if (sagemaker_models_list_.find(model_name_hash) ==
+        sagemaker_models_list_.end()) {
+      LOG_VERBOSE(1) << "Model " << target_model << " with model hash "
+                    << model_name_hash << " is not loaded." << std::endl;
+      evhtp_send_reply(req, EVHTP_RES_NOTFOUND); /* 404*/
+      return;
+    }
   }
 
   LOG_INFO << "Unloading SageMaker TargetModel: " << target_model << std::endl;
@@ -722,7 +728,11 @@ SagemakerAPIServer::SageMakerMMEUnloadModel(
                  "result in SageMaker UNLOAD timeout.";
   }
 
-  std::string repo_parent_path = sagemaker_models_list_.at(model_name_hash);
+  std::string repo_parent_path;
+  {
+    std::lock_guard<std::mutex> lock(models_list_mutex_);
+    repo_parent_path = sagemaker_models_list_.at(model_name_hash);
+  }
 
   TRITONSERVER_Error* unregister_err = nullptr;
 
@@ -740,8 +750,10 @@ SagemakerAPIServer::SageMakerMMEUnloadModel(
 
   TRITONSERVER_ErrorDelete(unregister_err);
 
-  std::lock_guard<std::mutex> lock(models_list_mutex_);
-  sagemaker_models_list_.erase(model_name_hash);
+  {
+    std::lock_guard<std::mutex> lock(models_list_mutex_);
+    sagemaker_models_list_.erase(model_name_hash);
+  }
 }
 
 void
