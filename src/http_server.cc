cbcff942082dfd1181abe882a5a6f838c10fa734
J Wyman <jwyman@nvidia.com>
2026-01-20 15:48:02 -0500

    overflow check

--- src/http_server.cc
+++ src/http_server.cc
@@ -606,6 +606,11 @@ ReadDataFromJsonHelper(
         const char* cstr{nullptr};
         size_t len{0};
         RETURN_IF_ERR(tensor_data.AsString(&cstr, &len));
+        if (len > INT64_MAX) {
+          return TRITONSERVER_ErrorNew(
+              TRITONSERVER_ERROR_INTERNAL,
+              "Unable to parse 'data' field: string length exceeds size limitation");
+        }
         // Quick sanity check to ensure we don't write beyond `expected_cnt`.
         int64_t tmp_cnt = static_cast<int64_t>(*counter) +
                           static_cast<int64_t>(len) +
