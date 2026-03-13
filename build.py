ddec486953f63f582b7a8e0cd0e193b5e70b4258
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2026-03-13 10:38:20 -0700

    feat: Refactor vLLM build to use upstream container image 8697 (#8698)
    Co-authored-by: J Wyman <jwyman@nvidia.com>

--- build.py
+++ build.py
@@ -1207,12 +1207,19 @@ def create_dockerfile_linux(
     df = """
 ARG TRITON_VERSION={}
 ARG TRITON_CONTAINER_VERSION={}
-ARG BASE_IMAGE={}
-
 """.format(
         argmap["TRITON_VERSION"],
         argmap["TRITON_CONTAINER_VERSION"],
-        argmap["BASE_IMAGE"],
+    )
+    if "vllm" in backends and argmap["INFERENCE_IMAGE"] is None:
+        argmap[
+            "INFERENCE_IMAGE"
+        ] = f"nvcr.io/nvidia/vllm:{FLAGS.upstream_container_version}-py3"
+    df += """ARG BASE_IMAGE={}
+""".format(
+        argmap["INFERENCE_IMAGE"]
+        if argmap["INFERENCE_IMAGE"] is not None
+        else argmap["BASE_IMAGE"],
     )
 
     # PyTorch backends need extra CUDA and other
@@ -1465,44 +1472,6 @@ RUN apt-get update \\
             virtualenv \\
       && rm -rf /var/lib/apt/lists/*
 """
-
-    if "vllm" in backends:
-        df += f"""
-# Install required packages for vLLM models
-ARG BUILD_PUBLIC_VLLM="true"
-RUN --mount=type=secret,id=req,target=/run/secrets/requirements \\
-    --mount=type=secret,id=VLLM_INDEX_URL,env=VLLM_INDEX_URL \\
-    --mount=type=secret,id=PYTORCH_TRITON_URL,env=PYTORCH_TRITON_URL \\
-    --mount=type=secret,id=NVPL_SLIM_URL,env=NVPL_SLIM_URL \\
-    if [ "$BUILD_PUBLIC_VLLM" = "false" ]; then \\
-        if [ "$(uname -m)" = "x86_64" ]; then \\
-            pip3 install --no-cache-dir \\
-                mkl==2021.1.1 \\
-                mkl-include==2021.1.1 \\
-                mkl-devel==2021.1.1; \\
-        elif [ "$(uname -m)" = "aarch64" ]; then \\
-            echo "Downloading NVPL from: $NVPL_SLIM_URL" && \\
-            cd /tmp && \\
-            wget -O nvpl_slim_24.04.tar $NVPL_SLIM_URL && \\
-            tar -xf nvpl_slim_24.04.tar && \\
-            cp -r nvpl_slim_24.04/lib/* /usr/local/lib && \\
-            cp -r nvpl_slim_24.04/include/* /usr/local/include && \\
-            rm -rf nvpl_slim_24.04.tar nvpl_slim_24.04; \\
-        fi \\
-        && pip3 install --no-cache-dir --extra-index-url $VLLM_INDEX_URL -r /run/secrets/requirements \\
-        # Need to install in-house build of pytorch-triton to support triton_key definition used by torch 2.5.1
-        && cd /tmp \\
-        && wget $PYTORCH_TRITON_URL \\
-        && pip install --no-cache-dir /tmp/pytorch_triton-*.whl \\
-        && rm /tmp/pytorch_triton-*.whl; \\
-    else \\
-        # public vLLM needed for vLLM backend
-        pip3 install vllm=={DEFAULT_TRITON_VERSION_MAP["vllm_version"]}; \\
-    fi
-
-ARG PYVER=3.12
-ENV LD_LIBRARY_PATH /usr/local/lib:/usr/local/lib/python${{PYVER}}/dist-packages/torch/lib:${{LD_LIBRARY_PATH}}
-"""
     if "tensorrtllm" in backends or "vllm" in backends:
         df += """
 ENV TRITON_CUDACRT_PATH=/usr/local/cuda/include \\
@@ -1712,12 +1681,18 @@ def create_build_dockerfiles(
     else:
         base_image = "ubuntu:24.04"
 
+    if "inference" in images:
+        inference_image = images["inference"]
+    else:
+        inference_image = None
+
     dockerfileargmap = {
         "NVIDIA_BUILD_REF": "" if FLAGS.build_sha is None else FLAGS.build_sha,
         "NVIDIA_BUILD_ID": "<unknown>" if FLAGS.build_id is None else FLAGS.build_id,
         "TRITON_VERSION": FLAGS.version,
         "TRITON_CONTAINER_VERSION": FLAGS.container_version,
         "BASE_IMAGE": base_image,
+        "INFERENCE_IMAGE": inference_image,
         "DCGM_VERSION": FLAGS.dcgm_version,
     }
 
@@ -2947,7 +2922,7 @@ if __name__ == "__main__":
             len(parts) != 2, "--image must specify <image-name>,<full-image-registry>"
         )
         fail_if(
-            parts[0] not in ["base", "gpu-base", "pytorch"],
+            parts[0] not in ["base", "gpu-base", "pytorch", "inference"],
             "unsupported value for --image",
         )
         log('image "{}": "{}"'.format(parts[0], parts[1]))
