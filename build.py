688a5fa9a4ebc24f7e11851d79ae027821fafa01
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2026-04-09 17:47:37 -0700

    docs(server): update README and versions for r26.04 (#8729)

--- build.py
+++ build.py
@@ -72,8 +72,8 @@ import requests
 
 DEFAULT_TRITON_VERSION_MAP = {
     "release_version": "2.68.0",
-    "triton_container_version": "26.04dev",
-    "upstream_container_version": "26.03",
+    "triton_container_version": "26.04",
+    "upstream_container_version": "26.04",
     "ort_version": "1.24.4",
     "ort_openvino_version": "2026.0.0",
     "standalone_openvino_version": "2026.0.0",
