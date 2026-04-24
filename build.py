45a1c1214dfae9daeb8519efc47724ba66eeec48
Misha Chornyi <99709299+mc-nv@users.noreply.github.com>
2026-04-23 17:43:00 -0700

    ci: Enable Perf Analyzer + OpenVINO 2026.1.0 (#8757)

--- build.py
+++ build.py
@@ -75,10 +75,10 @@ DEFAULT_TRITON_VERSION_MAP = {
     "triton_container_version": "26.04",
     "upstream_container_version": "26.04",
     "ort_version": "1.24.4",
-    "ort_openvino_version": "2026.0.0",
-    "standalone_openvino_version": "2026.0.0",
+    "ort_openvino_version": "2026.1.0",
+    "standalone_openvino_version": "2026.1.0",
     "dcgm_version": "4.5.3-1",
-    "vllm_version": "0.16.0",
+    "vllm_version": "0.19.0",
     "rhel_py_version": "3.12.3",
 }
 
