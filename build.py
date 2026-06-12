a2f05b470047016f7a89a14f0e70b9ef564440ec
M. Chornyi <99709299+mc-nv@users.noreply.github.com>
2026-06-11 17:29:59 -0700

    Bump ONNX Runtime version to 1.26.0

--- build.py
+++ build.py
@@ -75,7 +75,7 @@ DEFAULT_TRITON_VERSION_MAP = {
     "release_version": "2.71.0dev",
     "triton_container_version": "26.07dev",
     "upstream_container_version": "26.05",
-    "ort_version": "1.24.4",
+    "ort_version": "1.26.0",
     "ort_openvino_version": "2026.2.0",
     "standalone_openvino_version": "2026.2.0",
     "dcgm_version": "4.5.3-1",
