2de58d9e43fe4a7fde7daf1d760fa4feb1dc027c
Shantanu Mirajgave <shantanu1058@gmail.com>
2026-06-16 18:05:05 +0530

    Removed unwanted files

--- docs/examples/model_repository/simple_identity/1/model.py
+++ docs/examples/model_repository/simple_identity/1/model.py
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:docs/examples/model_repository/simple_identity/1/model.py
 # Copyright 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-========
-# Copyright 2023-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
->>>>>>>> r25.03_shantanu:.github/workflows/pre-commit.yml
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -28,7 +24,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-<<<<<<<< HEAD:docs/examples/model_repository/simple_identity/1/model.py
 import json
 
 import triton_python_backend_utils as pb_utils
@@ -49,24 +44,3 @@ class TritonPythonModel:
             out_tensor_0 = pb_utils.Tensor("OUTPUT0", in_0.as_numpy())
             responses.append(pb_utils.InferenceResponse([out_tensor_0]))
         return responses
-========
-name: pre-commit
-
-on:
-  pull_request:
-
-jobs:
-  pre-commit:
-    runs-on: ubuntu-latest
-    steps:
-    - uses: actions/checkout@v5.0.0
-      with:
-        fetch-depth: 2
-    - name: Get modified files
-      id: modified-files
-      run: echo "modified_files=$(git diff --name-only -r HEAD^1 HEAD | xargs)" >> $GITHUB_OUTPUT
-    - uses: actions/setup-python@v6.0.0
-    - uses: pre-commit/action@v3.0.1
-      with:
-        extra_args: --files ${{ steps.modified-files.outputs.modified_files }}
->>>>>>>> r25.03_shantanu:.github/workflows/pre-commit.yml
