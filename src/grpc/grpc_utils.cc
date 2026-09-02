0e7d5abfbbd3892fb53b0fb44415ae1f47e70e19
Yingge He <yinggeh@nvidia.com>
2026-09-02 15:59:30 -0700

    Fix error message

--- src/grpc/grpc_utils.cc
+++ src/grpc/grpc_utils.cc
@@ -133,7 +133,7 @@ ParseClassificationParams(
     if (cnt <= 0) {
       return TRITONSERVER_ErrorNew(
           TRITONSERVER_ERROR_INVALID_ARG,
-          "invalid value for 'classification' parameter, expected >= 0");
+          "invalid value for 'classification' parameter, expected >= 1");
     }
 
     *classification_count = cnt;
