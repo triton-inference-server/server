9040d6a321873527a8700947f8150462172968bc
Sai Kiran Polisetty <spolisetty@nvidia.com>
2026-09-03 19:01:35 +0530

    Fix pre-commit
    Signed-off-by: Sai Kiran Polisetty <spolisetty@nvidia.com>

--- qa/L0_request_cancellation/trt_cancellation_test.py
+++ qa/L0_request_cancellation/trt_cancellation_test.py
@@ -57,7 +57,7 @@ class TestTrtRequestCancellation(unittest.TestCase):
             grpcclient.InferInput("INPUT0", TRT_SHAPE, "FP32"),
             grpcclient.InferInput("INPUT1", TRT_SHAPE, "FP32"),
         ]
-        for model_input in inputs: 
+        for model_input in inputs:
             model_input.set_data_from_numpy(np.full(TRT_SHAPE, value, dtype=np.float32))
          return inputs
 
