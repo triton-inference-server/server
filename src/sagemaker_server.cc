66572f05b867dcb1c9c6b34a457d1d52e4bd26ec
Sai Kiran Polisetty <spolisetty@nvidia.com>
2026-09-21 17:33:56 +0530

    Update
    Signed-off-by: Sai Kiran Polisetty <spolisetty@nvidia.com>

--- src/sagemaker_server.cc
+++ src/sagemaker_server.cc
@@ -338,6 +338,15 @@ SagemakerAPIServer::ParseSageMakerRequest(
     }
   }
 
+  // Reject an empty URL before calling std::filesystem::absolute(), because
+  // it can throw exception and crash the server.
+  if (url_string.empty()) {
+    HTTP_RESPOND_IF_ERR(
+        req, TRITONSERVER_ErrorNew(
+                 TRITONSERVER_ERROR_INVALID_ARG,
+                 "'url' property must not be empty"));
+  }
+
   std::filesystem::path url_path(url_string);
   url_path = std::filesystem::absolute(
       url_path.lexically_normal());  // Normalize the path to remove any
